d4bc48c8d3754d2321e5fccee92e0098fa374c6b
kcheng <kcheng>
2000-09-23 23:47:39 +0000

    Modified to use new 'action' add_update_product in shopping_cart.php to add a quickie.

--- catalog/catalog/includes/boxes/add_a_quickie.php
+++ catalog/catalog/includes/boxes/add_a_quickie.php
@@ -3,6 +3,6 @@
             <td bgcolor="<?=BOX_HEADING_BACKGROUND_COLOR;?>" class="boxborder" nowrap><font face="<?=BOX_HEADING_FONT_FACE;?>" color="<?=BOX_HEADING_FONT_COLOR;?>" size="<?=BOX_HEADING_FONT_SIZE;?>">&nbsp;<?=BOX_HEADING_ADD_PRODUCT_ID;?>&nbsp;</font></td>
           </tr>
           <tr>
-            <td bgcolor="<?=BOX_CONTENT_BACKGROUND_COLOR;?>"><font face="<?=BOX_CONTENT_FONT_FACE;?>" color="<?=BOX_CONTENT_FONT_COLOR;?>" size="<?=BOX_CONTENT_FONT_SIZE;?>"><div align="center"><input type="hidden" name="action" value="add_product"><input type="hidden" name="cart_quantity" value="1"><input type="text" name="products_id" size="10">&nbsp;<?=tep_image_submit(DIR_IMAGES . 'button_add_quick.gif', '16', '17', '0', BOX_HEADING_ADD_PRODUCT_ID);?></div><?=BOX_ADD_PRODUCT_ID_TEXT;?></font></td>
+            <td bgcolor="<?=BOX_CONTENT_BACKGROUND_COLOR;?>"><font face="<?=BOX_CONTENT_FONT_FACE;?>" color="<?=BOX_CONTENT_FONT_COLOR;?>" size="<?=BOX_CONTENT_FONT_SIZE;?>"><div align="center"><input type="hidden" name="action" value="add_update_product"><input type="text" name="products_id" size="10">&nbsp;<?=tep_image_submit(DIR_IMAGES . 'button_add_quick.gif', '16', '17', '0', BOX_HEADING_ADD_PRODUCT_ID);?></div><?=BOX_ADD_PRODUCT_ID_TEXT;?></font></td>
           </tr></form>
 <!-- add_a_quickie_eof //-->
