cefeb0fd1785c8cbe901ba1ac3de0dc4badf8514
hpdl <hpdl>
2000-10-22 17:34:41 +0000

    new front page statistics - will be soon enhanced to be configurable

--- admin/admin/default.php
+++ admin/admin/default.php
@@ -58,6 +58,113 @@
           </tr>
         </table></td>
       </tr>
+      <tr>
+        <td><br><table border="0" width="100%" cellspacing="0" cellpadding="2">
+          <tr>
+            <td width="50%"><table border="0" width="100%" cellspacing="0" cellpadding="2">
+              <tr>
+                <td colspan="2"><?=tep_black_line();?></td>
+              </tr>
+              <tr bgcolor="<?=SUB_BAR_BACKGROUND_COLOR;?>">
+                <td colspan="2" nowrap><font face="<?=SUB_BAR_FONT_FACE;?>" size="<?=SUB_BAR_FONT_SIZE;?>" color="<?=SUB_BAR_FONT_COLOR;?>">&nbsp;<?=TABLE_HEADING_NEW_CUSTOMERS;?>&nbsp;</font></td>
+              </tr>
+              <tr>
+                <td colspan="2"><?=tep_black_line();?></td>
+              </tr>
+<?
+  $customers_query = tep_db_query("select c.customers_id, c.customers_firstname, c.customers_lastname, i.customers_info_date_account_created from customers c, customers_info i where c.customers_id = i.customers_info_id order by i.customers_info_date_account_created DESC limit 5");
+  while ($customers = tep_db_fetch_array($customers_query)) {
+    echo '              <tr bgcolor="#d8e1eb" onmouseover="this.style.background=\'#cc9999\';this.style.cursor=\'hand\'" onmouseout="this.style.background=\'#d8e1eb\'"">' . "\n";
+    echo '                <td nowrap><font face="' . TEXT_FONT_FACE . '" size="' . TEXT_FONT_SIZE . '" color="' . TEXT_FONT_COLOR . '">&nbsp;' . $customers['customers_firstname'] . ' ' . $customers['customers_lastname'] . '&nbsp;</font></td>' . "\n";
+    echo '                <td align="right" nowrap><font face="' . TEXT_FONT_FACE . '" size="' . TEXT_FONT_SIZE . '" color="' . TEXT_FONT_COLOR . '">&nbsp;' . tep_date_short($customers['customers_info_date_account_created']) . '&nbsp;</font></td>' . "\n";
+    echo '              </tr>' . "\n";
+  }
+?>
+              <tr>
+                <td colspan="2"><?=tep_black_line();?></td>
+              </tr>
+            </table></td>
+            <td width="50%"><table border="0" width="100%" cellspacing="0" cellpadding="2">
+              <tr>
+                <td colspan="3"><?=tep_black_line();?></td>
+              </tr>
+              <tr bgcolor="<?=SUB_BAR_BACKGROUND_COLOR;?>">
+                <td colspan="3" nowrap><font face="<?=SUB_BAR_FONT_FACE;?>" size="<?=SUB_BAR_FONT_SIZE;?>" color="<?=SUB_BAR_FONT_COLOR;?>">&nbsp;<?=TABLE_HEADING_LAST_ORDERS;?>&nbsp;</font></td>
+              </tr>
+              <tr>
+                <td colspan="3"><?=tep_black_line();?></td>
+              </tr>
+<?
+  $orders_query = tep_db_query("select orders_id, customers_name, date_purchased, orders_status from orders order by date_purchased DESC limit 5");
+  while ($orders = tep_db_fetch_array($orders_query)) {
+    $total_cost = 0;
+    $orders_products_query = tep_db_query("select products_price, products_quantity from orders_products where orders_id = '" . $orders['orders_id'] . "'");
+    while ($orders_products = tep_db_fetch_array($orders_products_query)) {
+      $total_cost = $total_cost + ($orders_products['products_price'] * $orders_products['products_quantity']);
+    }
+
+    echo '              <tr bgcolor="#d8e1eb" onmouseover="this.style.background=\'#cc9999\';this.style.cursor=\'hand\'" onmouseout="this.style.background=\'#d8e1eb\'" onclick="document.location.href=\'' . tep_href_link(FILENAME_ORDERS, 'orders_id=' . $orders['orders_id'], 'NONSSL') . '\'">' . "\n";
+    echo '                <td nowrap><font face="' . TEXT_FONT_FACE . '" size="' . TEXT_FONT_SIZE . '" color="' . TEXT_FONT_COLOR . '">&nbsp;<a href="' . tep_href_link(FILENAME_ORDERS, 'orders_id=' . $orders['orders_id'], 'NONSSL') . '" class="blacklink">' . $orders['customers_name'] . '</a>&nbsp;</font></td>' . "\n";
+    echo '                <td nowrap><font face="' . TEXT_FONT_FACE . '" size="' . TEXT_FONT_SIZE . '" color="' . TEXT_FONT_COLOR . '">&nbsp;' . tep_currency_format($total_cost) . '&nbsp;</font></td>' . "\n";
+    echo '                <td align="right" nowrap><font face="' . TEXT_FONT_FACE . '" size="' . TEXT_FONT_SIZE . '" color="' . TEXT_FONT_COLOR . '">&nbsp;' . tep_date_short($orders['date_purchased']) . '&nbsp;</font></td>' . "\n";
+    echo '              </tr>' . "\n";
+  }
+?>
+              <tr>
+                <td colspan="3"><?=tep_black_line();?></td>
+              </tr>
+            </table></td>
+          </tr>
+          <tr>
+            <td width="50%"><table border="0" width="100%" cellspacing="0" cellpadding="2">
+              <tr>
+                <td colspan="2"><?=tep_black_line();?></td>
+              </tr>
+              <tr bgcolor="<?=SUB_BAR_BACKGROUND_COLOR;?>">
+                <td colspan="2" nowrap><font face="<?=SUB_BAR_FONT_FACE;?>" size="<?=SUB_BAR_FONT_SIZE;?>" color="<?=SUB_BAR_FONT_COLOR;?>">&nbsp;<?=TABLE_HEADING_NEW_PRODUCTS;?>&nbsp;</font></td>
+              </tr>
+              <tr>
+                <td colspan="2"><?=tep_black_line();?></td>
+              </tr>
+<?
+  $products_query = tep_db_query("select products_id, products_date_added from products order by products_date_added DESC limit 5");
+  while ($products = tep_db_fetch_array($products_query)) {
+    echo '              <tr bgcolor="#d8e1eb" onmouseover="this.style.background=\'#cc9999\';this.style.cursor=\'hand\'" onmouseout="this.style.background=\'#d8e1eb\'" onclick="document.location.href=\'' . tep_href_link(FILENAME_CATEGORIES, 'action=new_product_preview&read=only&pID=' . $products['products_id'], 'NONSSL') . '\'">' . "\n";
+    echo '                <td nowrap><font face="' . TEXT_FONT_FACE . '" size="' . TEXT_FONT_SIZE . '" color="' . TEXT_FONT_COLOR . '">&nbsp;<a href="' . tep_href_link(FILENAME_CATEGORIES, 'action=new_product_preview&read=only&pID=' . $products['products_id'], 'NONSSL') . '" class="blacklink">' . tep_products_name($products['products_id']) . '</a>&nbsp;</font></td>' . "\n";
+    echo '                <td align="right" nowrap><font face="' . TEXT_FONT_FACE . '" size="' . TEXT_FONT_SIZE . '" color="' . TEXT_FONT_COLOR . '">&nbsp;' . tep_date_short($products['products_date_added']) . '&nbsp;</font></td>' . "\n";
+    echo '              </tr>' . "\n";
+  }
+?>
+              <tr>
+                <td colspan="2"><?=tep_black_line();?></td>
+              </tr>
+            </table></td>
+            <td width="50%"><table border="0" width="100%" cellspacing="0" cellpadding="2">
+              <tr>
+                <td colspan="2"><?=tep_black_line();?></td>
+              </tr>
+              <tr bgcolor="<?=SUB_BAR_BACKGROUND_COLOR;?>">
+                <td colspan="2" nowrap><font face="<?=SUB_BAR_FONT_FACE;?>" size="<?=SUB_BAR_FONT_SIZE;?>" color="<?=SUB_BAR_FONT_COLOR;?>">&nbsp;<?=TABLE_HEADING_NEW_REVIEWS;?>&nbsp;</font></td>
+              </tr>
+              <tr>
+                <td colspan="2"><?=tep_black_line();?></td>
+              </tr>
+<?
+  $reviews_query = tep_db_query("select reviews_id, products_id, date_added from reviews_extra order by date_added DESC limit 5");
+  while ($reviews = tep_db_fetch_array($reviews_query)) {
+    echo '              <tr bgcolor="#d8e1eb" onmouseover="this.style.background=\'#cc9999\';this.style.cursor=\'hand\'" onmouseout="this.style.background=\'#d8e1eb\'">' . "\n";
+    echo '                <td nowrap><font face="' . TEXT_FONT_FACE . '" size="' . TEXT_FONT_SIZE . '" color="' . TEXT_FONT_COLOR . '">&nbsp;' . tep_products_name($reviews['products_id']) . '&nbsp;</font></td>' . "\n";
+    echo '                <td align="right" nowrap><font face="' . TEXT_FONT_FACE . '" size="' . TEXT_FONT_SIZE . '" color="' . TEXT_FONT_COLOR . '">&nbsp;' . tep_date_short($reviews['date_added']) . '&nbsp;</font></td>' . "\n";
+    echo '              </tr>' . "\n";
+  }
+?>
+              <tr>
+                <td colspan="2"><?=tep_black_line();?></td>
+              </tr>
+            </table></td>
+          </tr>
+        </table></td>
+      </tr>
     </table></td>
 <!-- body_text_eof //-->
   </tr>
