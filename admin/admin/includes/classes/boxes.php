c2011dbe6d0386c2da4dfeb17d80e25e3ef23ac9
hpdl <hpdl>
2000-10-28 17:04:07 +0000

    new class - in future all boxes will be parsed through this class.. currently part of the administration tool is already using this class so check it out!

---
+++ admin/admin/includes/classes/boxes.php
@@ -0,0 +1,59 @@
+<?
+  class tableBox {
+    var $table_border = '0';
+    var $table_width = '100%';
+    var $table_cellspacing = '0';
+    var $table_cellpadding = '2';
+    var $table_parameters = '';
+    var $table_row_parameters = '';
+    var $table_data_parameters = '';
+    var $font_style = FONT_STYLE_GENERAL;
+
+// class constructor
+    function tableBox($contents) {
+      echo "\n" . '<table border="' . $this->table_border . '" width="' . $this->table_width . '" cellspacing="' . $this->table_cellspacing . '" cellpadding="' . $this->table_cellpadding . '"';
+      if ($table_parameters != '') echo ' ' . $this->table_parameters;
+      echo '>' . "\n";
+
+      for ($i=0; $i<sizeof($contents); $i++) {
+        echo '  <tr';
+        if ($this->table_row_parameters != '') echo ' ' . $this->table_row_paramters;
+        echo '>' . "\n";
+
+        echo '    <td';
+        if ($contents[$i]['align'] != 'left') echo ' align="' . $contents[$i]['align'] . '"';
+        if ($this->table_data_parameters != '') echo ' ' . $this->table_data_parameters;
+        echo '>' . $this->font_style . $contents[$i]['text'] . '</font></td>' . "\n";
+
+        echo '  </tr>' . "\n";
+      }
+
+      echo '</table>' . "\n";
+    }
+
+  }
+
+  class infoBox extends tableBox {
+
+// class constructor
+    function infoBox($contents) {
+      $this->font_style = FONT_STYLE_INFO_BOX_BODY;
+
+      tableBox::tableBox($contents);
+    }
+
+  }
+
+  class infoBoxHeading extends infoBox {
+
+// class constructor
+    function infoBoxHeading($contents) {
+      $this->table_cellpadding = '0';
+      $this->table_data_parameters = 'nowrap';
+      $this->font_style = FONT_STYLE_INFO_BOX_HEADING;
+
+      tableBox::tableBox($contents);
+    }
+
+  }
+?>
