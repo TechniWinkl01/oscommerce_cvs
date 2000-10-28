2c16c99941de417d70e0b00eabaa950774805a34
hpdl <hpdl>
2000-10-28 17:17:38 +0000

    outputs parse time (i think its safe to do this for the administration tool)

--- admin/admin/includes/application_bottom.php
+++ admin/admin/includes/application_bottom.php
@@ -7,5 +7,6 @@
     $time_end = explode(' ', $parse_end_time);
     $parse_time = number_format(($time_end[1] + $time_end[0] - ($time_start[1] + $time_start[0])), 3);
     error_log(strftime(STORE_PARSE_DATE_TIME_FORMAT) . ' - ' . getenv(REQUEST_URI) . ' (' . $parse_time . 'ms)' . "\n", 3, STORE_PAGE_PARSE_TIME_LOG);
+    echo 'Parse Time: ' . $parse_time . 'ms';
   }
 ?>
