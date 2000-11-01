2a886c37e16a39d0b3ef578bb495c5a69df14ce1
tmoulton <tmoulton>
2000-11-01 10:46:37 +0000

    German ala bablefish...
    "No account? Click here to create one." might be close to
    "Kein Konto?  Klicken Sie hier, um ein zu erstellen."

--- catalog/catalog/includes/languages/german/login.php
+++ catalog/catalog/includes/languages/german/login.php
@@ -12,6 +12,7 @@ define('ENTRY_EMAIL_ADDRESS', 'E-Mail Adresse:');
 define('ENTRY_PASSWORD', 'Passwort:');
 define('TEXT_COOKIE', 'Speichern Sie die Anmeldeinformation in einem cookie?');
 define('TEXT_PASSWORD_FORGOTTEN', 'Passwort Vergessen? Klicken Sie hier.');
+define('TEXT_CREATE_ACCOUNT', 'Kein Konto?  Klicken Sie hier, um ein zu erstellen.');
 define('TEXT_VISITORS_CART', '<font color="#ff0000"><b>ACHTUNG:</b></font> Ihre Besuchereingaben werden automatisch mit ihrem Mitgliedschaft Konto verbunden. <a href="javascript:session_win();">[Mehr Information]</a>');
 define('TEXT_LOGIN_ERROR', '<font color="#ff0000"><b>FEHLER:</b></font> Keine &Uuml;bereinstimmung der \'E-Mail Adresse\' und/oder dem \'Passwort\'.');
 
