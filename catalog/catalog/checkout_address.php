e0d62cda3244bd653dde0ce26cf56976dd3038f4
tmoulton <tmoulton>
2000-11-12 12:40:20 +0000

    Another address label update...

--- catalog/catalog/checkout_address.php
+++ catalog/catalog/checkout_address.php
@@ -97,47 +97,12 @@
           <tr>
             <td><?=tep_black_line();?></td>
           </tr>
-<?
-  $address = tep_db_query("select customers_firstname, customers_lastname, customers_street_address, customers_suburb, customers_postcode, customers_city, customers_state, customers_country_id, customers_zone_id from customers where customers_id = '" . $customer_id . "'");
-  $address_values = tep_db_fetch_array($address);
-
-  $customers_country = tep_get_countries($address_values['customers_country_id']);
-?>
           <tr>
             <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
               <tr>
                 <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
                   <tr>
-                    <td nowrap><font face="<?=TEXT_FONT_FACE;?>" size="<?=TEXT_FONT_SIZE;?>" color="<?=TEXT_FONT_COLOR;?>">&nbsp;<?=$address_values['customers_firstname'] . ' ' . $address_values['customers_lastname'];?>&nbsp;</font></td>
-                  </tr>
-                  <tr>
-                    <td nowrap><font face="<?=TEXT_FONT_FACE;?>" size="<?=TEXT_FONT_SIZE;?>" color="<?=TEXT_FONT_COLOR;?>">&nbsp;<?=$address_values['customers_street_address'];?>&nbsp;</font></td>
-                  </tr>
-<?
-  if ($address_values['customers_suburb'] != '') {
-?>
-                  <tr>
-                    <td nowrap><font face="<?=TEXT_FONT_FACE;?>" size="<?=TEXT_FONT_SIZE;?>" color="<?=TEXT_FONT_COLOR;?>">&nbsp;<?=$address_values['customers_suburb'];?>&nbsp;</font></td>
-                  </tr>
-<?
-  }
-?>
-                  <tr>
-                    <td><font face="<?=TEXT_FONT_FACE;?>" size="<?=TEXT_FONT_SIZE;?>" color="<?=TEXT_FONT_COLOR;?>">&nbsp;<?=$address_values['customers_city'] . ', ' . $address_values['customers_postcode'];?>&nbsp;</font></td>
-                  </tr>
-                  <tr>
-<?
-  $state_country = '';
-  if ($address_values['customers_zone_id'] != 0) {
-     $state_country = tep_get_zone_name($address_values['customers_country_id'], $address_values['customers_zone_id'], $address_values['customers_state']) . ', ';
-  } else {
-    if ($address_values['customers_state'] != '') {
-     $state_country = $address_values['customers_state'] . ', ';
-    }
-  }
-  $state_country = $state_country . $customers_country['countries_name'];
-?>
-                    <td><font face="<?=TEXT_FONT_FACE;?>" size="<?=TEXT_FONT_SIZE;?>" color="<?=TEXT_FONT_COLOR;?>">&nbsp;<?=$state_country;?>&nbsp;</font></td>
+                    <td nowrap><font face="<?=TEXT_FONT_FACE;?>" size="<?=TEXT_FONT_SIZE;?>" color="<?=TEXT_FONT_COLOR;?>"><?=tep_format_address($customer_id, 0, 1, '&nbsp;', '<br>');?>&nbsp;</font></td>
                   </tr>
                 </table></td>
                 <td align="right" valign="middle"><font face="<?=TEXT_FONT_FACE;?>" size="<?=TEXT_FONT_SIZE;?>" color="<?=TEXT_FONT_COLOR;?>">&nbsp;<input type="radio" name="sendto" value="0" CHECKED>&nbsp;</font></td>
@@ -156,7 +121,7 @@
             <td><?=tep_black_line();?></td>
           </tr>
 <?
-  $address_book = tep_db_query("select address_book.address_book_id, address_book.entry_firstname, address_book.entry_lastname, address_book.entry_street_address, address_book.entry_suburb, address_book.entry_postcode, address_book.entry_city, address_book.entry_state, address_book.entry_zone_id, address_book.entry_country_id from address_book, address_book_to_customers where address_book_to_customers.customers_id = '" . $customer_id . "' and address_book_to_customers.address_book_id = address_book.address_book_id order by address_book.address_book_id");
+  $address_book = tep_db_query("select address_book_id from address_book_to_customers where customers_id = '" . $customer_id . "' order by address_book_id");
   if (!@tep_db_num_rows($address_book)) {
 ?>
           <tr>
@@ -169,20 +134,13 @@
             <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
 <?
     $row = 0;
+    $boln = '<td nowrap><font face="' . SMALL_TEXT_FONT_FACE . '" size="' . SMALL_TEXT_FONT_SIZE . '" color="' . SMALL_TEXT_FONT_COLOR . '">&nbsp;';
+    $eoln = '&nbsp;</font></td>' . "\n";
     while ($address_book_values = tep_db_fetch_array($address_book)) {
       $row++;
-      $entry_country = tep_get_countries($address_book_values['entry_country_id']);
       echo '              <tr>' . "\n";
       echo '                <td nowrap><font face="' . SMALL_TEXT_FONT_FACE . '" size="' . SMALL_TEXT_FONT_SIZE . '" color="' . SMALL_TEXT_FONT_COLOR . '">&nbsp;0' . $row . '.&nbsp;</font></td>' . "\n";
-      echo '                <td nowrap><font face="' . SMALL_TEXT_FONT_FACE . '" size="' . SMALL_TEXT_FONT_SIZE . '" color="' . SMALL_TEXT_FONT_COLOR . '">&nbsp;' . $address_book_values['entry_firstname'] . ' ' . $address_book_values['entry_lastname'] . '&nbsp;</font></td>' . "\n";
-      echo '                <td nowrap><font face="' . SMALL_TEXT_FONT_FACE . '" size="' . SMALL_TEXT_FONT_SIZE . '" color="' . SMALL_TEXT_FONT_COLOR . '">&nbsp;' . $address_book_values['entry_street_address'] . '&nbsp;</font></td>' . "\n";
-      echo '                <td nowrap><font face="' . SMALL_TEXT_FONT_FACE . '" size="' . SMALL_TEXT_FONT_SIZE . '" color="' . SMALL_TEXT_FONT_COLOR . '">&nbsp;' . $address_book_values['entry_suburb'] . '&nbsp;</font></td>' . "\n";
-      echo '                <td nowrap><font face="' . SMALL_TEXT_FONT_FACE . '" size="' . SMALL_TEXT_FONT_SIZE . '" color="' . SMALL_TEXT_FONT_COLOR . '">&nbsp;' . $address_book_values['entry_city'] . ', ' . $address_book_values['entry_postcode'] . '&nbsp;</font></td>' . "\n";
-      echo '                <td nowrap><font face="' . SMALL_TEXT_FONT_FACE . '" size="' . SMALL_TEXT_FONT_SIZE . '" color="' . SMALL_TEXT_FONT_COLOR . '">&nbsp;';
-      if ($address_book_values['entry_zone_id'] != 0 || $address_book_values['entry_state'] != '') {
-        echo tep_get_zone_name($address_book_values['entry_country_id'], $address_book_values['entry_zone_id'], $address_book_values['entry_state']) . ", ";
-      }
-      echo $entry_country['countries_name'] . '&nbsp;</font></td>' . "\n";
+      echo tep_format_address($customer_id, $address_book_values['address_book_id'], 1, $boln, $eoln);
       echo '                <td nowrap align="right"><font face="' . SMALL_TEXT_FONT_FACE . '" size="' . SMALL_TEXT_FONT_SIZE . '" color="' . SMALL_TEXT_FONT_COLOR . '">&nbsp;<input type="radio" name="sendto" value="' . $address_book_values['address_book_id'] . '">&nbsp;</font></td>' . "\n";
       echo '              </tr>' . "\n";
     }
