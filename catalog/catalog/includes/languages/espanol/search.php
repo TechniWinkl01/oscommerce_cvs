b807491a1594cc21817a30bd72ca5f8dcb65f22a
dwatkins <dwatkins>
2000-11-22 23:46:35 +0000

    Added missing defines in Spanish

--- catalog/catalog/includes/languages/espanol/search.php
+++ catalog/catalog/includes/languages/espanol/search.php
@@ -8,6 +8,8 @@ Author(s): David Garcia Watkins (dgw@q-logic.org)
 define('NAVBAR_TITLE', 'Resultados de la Busqueda');
 define('TOP_BAR_TITLE', 'Resultados de la Busqueda');
 define('HEADING_TITLE', 'No encuentro lo que buscas');
+define('TABLE_HEADING_MODEL', 'Modelo');
 define('TABLE_HEADING_PRODUCTS', 'Nombre del Producto');
 define('TABLE_HEADING_PRICE', 'Precio');
+define('TEXT_NO_PRODUCTS', 'No hay productos con ese criterio de busqueda.');
 ?>
