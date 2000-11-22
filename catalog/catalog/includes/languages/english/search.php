b72dc09315f7420350926b96ec053e72125fef0b
kcheng <kcheng>
2000-11-22 22:14:25 +0000

    Add couple of missing DEFINEs.

--- catalog/catalog/includes/languages/english/search.php
+++ catalog/catalog/includes/languages/english/search.php
@@ -8,6 +8,8 @@ Author(s): Harald Ponce de Leon (hpdl@theexchangeproject.org)
 define('NAVBAR_TITLE', 'Search Results');
 define('TOP_BAR_TITLE', 'Search Results');
 define('HEADING_TITLE', 'Can\'t Find What Your Looking For?');
+define('TABLE_HEADING_MODEL', 'Products Model');
 define('TABLE_HEADING_PRODUCTS', 'Products Name');
 define('TABLE_HEADING_PRICE', 'Products Price');
+define('TEXT_NO_PRODUCTS', 'There is no product that matches the search criteria.');
 ?>
