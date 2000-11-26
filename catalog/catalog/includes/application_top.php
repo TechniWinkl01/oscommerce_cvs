bdbff3ee1c5458c28701bc0b19bbd60f4cb0f89d
tmoulton <tmoulton>
2000-11-26 02:25:21 +0000

    Have application_top include ../configure.php which will have any define's that you want to change from what is already in the CVS version of application_top.php
    This works because the second time you define() a name it is not an error
    nor does it change the value of that define. This means you will no longer
    need to edit application_top.php each time...
    
    Note: *NEVER* commit 'configure.php'
    
    Note: This change *may* be revoked by Harald as I did not talk to him
          about this change before making it.

--- catalog/catalog/includes/application_top.php
+++ catalog/catalog/includes/application_top.php
@@ -1,4 +1,8 @@
 <?
+  @include('configure.php');
+  if (CONFIGURE_STATUS != 'COMPLETED') { // File not read properly
+     die('File configure.php was not found or was improperly formatted, contact webmaster of this domain.');
+  }
 // define our webserver variables
   define('HTTP_SERVER', 'http://exchange');
   define('HTTPS_SERVER', 'https://exchange');
