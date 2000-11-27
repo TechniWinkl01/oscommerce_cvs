aefa6d2da5dc21b23db877a52938e70e843b8f7f
tmoulton <tmoulton>
2000-11-27 11:41:26 +0000

    Update for working with empty tables.
    If there are no reviews defined it will not display the box.
    (Same for specials, etc)
    
    These only happen when the DB is empty, it should not normally be
    seen in production, but bothers new store owners...

--- catalog/catalog/includes/boxes/reviews.php
+++ catalog/catalog/includes/boxes/reviews.php
@@ -6,20 +6,22 @@
   if (@tep_db_num_rows($products_name) > 0) {
     tep_random_select("select r.reviews_id, r.reviews_text, r.reviews_rating, p.products_id, p.products_name, p.products_image from reviews r, reviews_extra re, products p where p.products_status = '1' and r.reviews_id = re.reviews_id and re.products_id = p.products_id order by r.reviews_id DESC limit " . MAX_RANDOM_SELECT_REVIEWS);
 
-    $info_box_contents = array();
-    $info_box_contents[] = array('align' => 'left',
-                                 'text'  => '<a href="' . tep_href_link(FILENAME_REVIEWS, '', 'NONSSL') . '" class="blacklink">' . BOX_HEADING_REVIEWS . '</a>'
-                                );
-    new infoBoxHeading($info_box_contents);
+    if ($random_product != '') {
+      $info_box_contents = array();
+      $info_box_contents[] = array('align' => 'left',
+                                   'text'  => '<a href="' . tep_href_link(FILENAME_REVIEWS, '', 'NONSSL') . '" class="blacklink">' . BOX_HEADING_REVIEWS . '</a>'
+                                  );
+      new infoBoxHeading($info_box_contents);
 
-    $review = htmlspecialchars(substr($random_product['reviews_text'], 0, 60));
-    $review = tep_break_string($review, 15);
+      $review = htmlspecialchars(substr($random_product['reviews_text'], 0, 60));
+      $review = tep_break_string($review, 15);
 
-    $info_box_contents = array();
-    $info_box_contents[] = array('align' => 'left',
-                                 'text'  => '<div align="center"><a href="' . tep_href_link(FILENAME_PRODUCT_REVIEWS_INFO, 'products_id=' . $random_product['products_id'] . '&reviews_id=' . $random_product['reviews_id'], 'NONSSL') . '">' . tep_image($random_product['products_image'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT, '0', $random_product['products_name']) . '</a></div><a href="' . tep_href_link(FILENAME_PRODUCT_REVIEWS_INFO, 'products_id=' . $random_product['products_id'] . '&reviews_id=' . $random_product['reviews_id'], 'NONSSL') . '">' . $review . ' ..</a><br><div align="center">' . tep_image(DIR_IMAGES . 'stars_' . $random_product['reviews_rating'] . '.gif' , '59', '12', '0', $random_product['reviews_rating'] . ' of 5 Stars!') . '<br><a href="' . tep_href_link(FILENAME_REVIEWS, '', 'NONSSL') . '">' . BOX_REVIEWS_MORE . '</a>'
-                                );
-    new infoBox($info_box_contents);
+      $info_box_contents = array();
+      $info_box_contents[] = array('align' => 'left',
+                                   'text'  => '<div align="center"><a href="' . tep_href_link(FILENAME_PRODUCT_REVIEWS_INFO, 'products_id=' . $random_product['products_id'] . '&reviews_id=' . $random_product['reviews_id'], 'NONSSL') . '">' . tep_image($random_product['products_image'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT, '0', $random_product['products_name']) . '</a></div><a href="' . tep_href_link(FILENAME_PRODUCT_REVIEWS_INFO, 'products_id=' . $random_product['products_id'] . '&reviews_id=' . $random_product['reviews_id'], 'NONSSL') . '">' . $review . ' ..</a><br><div align="center">' . tep_image(DIR_IMAGES . 'stars_' . $random_product['reviews_rating'] . '.gif' , '59', '12', '0', $random_product['reviews_rating'] . ' of 5 Stars!') . '<br><a href="' . tep_href_link(FILENAME_REVIEWS, '', 'NONSSL') . '">' . BOX_REVIEWS_MORE . '</a>'
+                                  );
+      new infoBox($info_box_contents);
+    }
   }
 ?>
             </td>
--- catalog/catalog/includes/boxes/whats_new.php
+++ catalog/catalog/includes/boxes/whats_new.php
@@ -4,17 +4,19 @@
 <?
   tep_random_select("select products.products_id, products.products_name, products.products_image, manufacturers.manufacturers_name, manufacturers.manufacturers_location from products, products_to_manufacturers, manufacturers where products.products_status='1' and products.products_id = products_to_manufacturers.products_id and products_to_manufacturers.manufacturers_id = manufacturers.manufacturers_id order by products.products_date_added desc limit " . MAX_RANDOM_SELECT_NEW);
 
-  $info_box_contents = array();
-  $info_box_contents[] = array('align' => 'left',
-                               'text'  => BOX_HEADING_WHATS_NEW
-                              );
-  new infoBoxHeading($info_box_contents);
-
-  $info_box_contents = array();
-  $info_box_contents[] = array('align' => 'center',
-                               'text'  => '<a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $random_product['products_id'], 'NONSSL') . '">' . tep_image($random_product['products_image'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT, '0', $random_product['products_name']) . '</a><br><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $random_product['products_id'], 'NONSSL') . '">' . $random_product['products_name'] . '</a>'
-                              );
-  new infoBox($info_box_contents);
+  if ($random_product != '') {
+    $info_box_contents = array();
+    $info_box_contents[] = array('align' => 'left',
+                                 'text'  => BOX_HEADING_WHATS_NEW
+                                );
+    new infoBoxHeading($info_box_contents);
+  
+    $info_box_contents = array();
+    $info_box_contents[] = array('align' => 'center',
+                                 'text'  => '<a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $random_product['products_id'], 'NONSSL') . '">' . tep_image($random_product['products_image'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT, '0', $random_product['products_name']) . '</a><br><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $random_product['products_id'], 'NONSSL') . '">' . $random_product['products_name'] . '</a>'
+                                );
+    new infoBox($info_box_contents);
+  }
 ?>
             </td>
           </tr>
