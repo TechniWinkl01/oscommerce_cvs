9930503313d245b826a3dd5e7b5052fc6c487b89
dwatkins <dwatkins>
2000-12-01 20:43:46 +0000

    text update

--- catalog/catalog/includes/languages/espanol/checkout.php
+++ catalog/catalog/includes/languages/espanol/checkout.php
@@ -18,7 +18,7 @@ define('SUB_TITLE_TAX', 'Impuestos:');
 define('SUB_TITLE_TOTAL', 'Total:');
 define('TEXT_CURRENT_CONNECT_STATUS_NONSSL', 'Estado Actual (haga click para modificar)');
 define('TEXT_CURRENT_CONNECT_STATUS_SSL', 'Estado Actual (haga click para modificar)');
-define('NO_SHIPPING_OR_TAX_TEXT', 'Nota: Puede haber gastos de envio adicionales que seran calculados antes de terminar el proceso.');
+define('NO_SHIPPING_OR_TAX_TEXT', '<font color="#ff0000"><small><b>NOTA:</b></small></font> Puede haber gastos de envio e impuestos adicionales de los que será informado antes de terminar el proceso.');
 
 define('IMAGE_SECURE_SERVER', 'Servidor Seguro');
 define('IMAGE_UNSECURE_SERVER', 'Servidor no Seguro');
