0b1b70adc0713059221f1b06522df83cb4af081b
dwatkins <dwatkins>
2000-12-03 17:39:23 +0000

    Update transalations

--- admin/admin/includes/languages/espanol/configuration.php
+++ admin/admin/includes/languages/espanol/configuration.php
@@ -1,27 +1,24 @@
 <?
 /*
-English Text for The Exchange Project Administration Tool Preview Release 2.0
+Spanish Text for The Exchange Project Administration Tool Preview Release 2.0
 Last Update: 02/12/2000
 Author(s): Harald Ponce de Leon (hpdl@theexchangeproject.org)
 */
 
-define('TOP_BAR_TITLE', 'Configuration Parameters');
-define('HEADING_TITLE', 'Configuration Parameters');
+define('TOP_BAR_TITLE', 'Configuracion');
+define('HEADING_TITLE', 'Configuracion');
 
-define('TABLE_HEADING_CONFIGURATION_TITLE', 'Title');
-define('TABLE_HEADING_CONFIGURATION_VALUE', 'Value');
-define('TABLE_HEADING_ACTION', 'Action');
+define('TABLE_HEADING_CONFIGURATION_TITLE', 'Titulo');
+define('TABLE_HEADING_CONFIGURATION_VALUE', 'Valor');
+define('TABLE_HEADING_ACTION', 'Accion');
 
-define('TEXT_INFO_EDIT_INTRO', 'Please make any necessary changes');
-define('TEXT_INFO_KEY', 'Key (define name):');
-define('TEXT_INFO_VALUE', 'Value (define value):');
-define('TEXT_INFO_FUNCTION', 'Function (admin side):');
-define('TEXT_INFO_DATE_ADDED', 'Date Added:');
-define('TEXT_INFO_LAST_MODIFIED', 'Last Modified:');
-define('TEXT_INFO_DESCRIPTION', 'Description:');
-define('TEXT_INFO_DEFINE_NOTE', '<small><b>NOTE:</b></small> The key is the name of the constant-variable (define), and the value is the value of the constant');
-define('TEXT_INFO_FUNCTION_NOTE', '<small><b>NOTE:</b></small> If the key is to be processed by a function (administration tool side), please enter it without the parentethis');
-define('TEXT_INFO_INSERT_INTRO', 'Please enter the new product name and price thats expected');
-define('TEXT_INFO_DELETE_INTRO', 'Are you sure you want to delete this expected product?');
-define('TEXT_INFO_HEADING_NEW_PARAMETER', 'New Parameter');
+define('TEXT_INFO_EDIT_INTRO', 'Haga los cambios necesarios');
+define('TEXT_INFO_KEY', 'Clave:');
+define('TEXT_INFO_VALUE', 'Valor:');
+define('TEXT_INFO_FUNCTION', 'Funcion:');
+define('TEXT_INFO_DATE_ADDED', 'Fecha de Alta:');
+define('TEXT_INFO_LAST_MODIFIED', 'Ultima Modificacion:');
+define('TEXT_INFO_DESCRIPTION', 'Descripcion:');
+define('TEXT_INFO_DEFINE_NOTE', '<small><b>NOTA:</b></small> La Clave es el nombre de la constante, y el Valor es el valor que tiene esa constante');
+define('TEXT_INFO_FUNCTION_NOTE', '<small><b>NOTA:</b></small> Si la clave debe usar una funcion para recuperar su valor, indiquela sin los parentesis');
 ?>
--- admin/admin/includes/languages/espanol/tax_rates.php
+++ admin/admin/includes/languages/espanol/tax_rates.php
@@ -1,28 +1,28 @@
 <?
 /*
-English Text for The Exchange Project Administration Tool Preview Release 2.0
+Spanish Text for The Exchange Project Administration Tool Preview Release 2.0
 Last Update: 02/12/2000
 Author(s): Harald Ponce de Leon (hpdl@theexchangeproject.org)
 */
 
-define('TOP_BAR_TITLE', 'Tax Rates');
-define('HEADING_TITLE', 'Tax Rates');
+define('TOP_BAR_TITLE', 'Tipos de Impuesto');
+define('HEADING_TITLE', 'Tipos de Impuesto');
 
-define('TABLE_HEADING_TAX_CLASS_TITLE', 'Tax Class');
-define('TABLE_HEADING_COUNTRIES_NAME', 'Country');
-define('TABLE_HEADING_ZONE', 'Zone');
-define('TABLE_HEADING_TAX_RATE', 'Tax Rate');
-define('TABLE_HEADING_ACTION', 'Action');
+define('TABLE_HEADING_TAX_CLASS_TITLE', 'Tipo de Impuesto');
+define('TABLE_HEADING_COUNTRIES_NAME', 'Pais');
+define('TABLE_HEADING_ZONE', 'Zona');
+define('TABLE_HEADING_TAX_RATE', 'Porcentaje');
+define('TABLE_HEADING_ACTION', 'Accion');
 
-define('TEXT_INFO_DATE_ADDED', 'Date Added:');
-define('TEXT_INFO_LAST_MODIFIED', 'Last Modified:');
-define('TEXT_INFO_EDIT_INTRO', 'Please make any necessary changes');
-define('TEXT_INFO_CLASS_TITLE', 'Tax Class Title:');
-define('TEXT_INFO_COUNTRY_NAME', 'Country:');
-define('TEXT_INFO_ZONE_NAME', 'Zone:');
-define('TEXT_INFO_TAX_RATE', 'Tax Rate (%):');
-define('TEXT_INFO_RATE_DESCRIPTION', 'Description:');
-define('TEXT_INFO_INSERT_INTRO', 'Please enter the new tax class with its related data');
-define('TEXT_INFO_DELETE_INTRO', 'Are you sure you want to delete this tax rate?');
-define('TEXT_INFO_HEADING_NEW_TAX_CLASS', 'New Tax Rate');
+define('TEXT_INFO_DATE_ADDED', 'Fecha de Alta:');
+define('TEXT_INFO_LAST_MODIFIED', 'Ultima Modificacion:');
+define('TEXT_INFO_EDIT_INTRO', 'Haga los cambios necesarios');
+define('TEXT_INFO_CLASS_TITLE', 'Nombre del Tipo de Impuesto:');
+define('TEXT_INFO_COUNTRY_NAME', 'Pais:');
+define('TEXT_INFO_ZONE_NAME', 'Zona:');
+define('TEXT_INFO_TAX_RATE', 'Porcentaje (%):');
+define('TEXT_INFO_RATE_DESCRIPTION', 'Descripcion:');
+define('TEXT_INFO_INSERT_INTRO', 'Introduzca un nombre y los datos del nuevo tipo de impuesto');
+define('TEXT_INFO_DELETE_INTRO', 'Seguro que desea eliminar este tipo de impuesto?');
+define('TEXT_INFO_HEADING_NEW_TAX_CLASS', 'Nuevo Tipo de Impuesto');
 ?>
