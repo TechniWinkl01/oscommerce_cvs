8c777c07153a3c28a5993e463da5b794db88906b
dwatkins <dwatkins>
2001-01-03 12:37:08 +0000

    Added missing constant.

--- catalog/extras/upgrade_to_2.0/misc_updates.sql
+++ catalog/extras/upgrade_to_2.0/misc_updates.sql
@@ -18,3 +18,4 @@ ALTER TABLE products CHANGE COLUMN products_model products_model varchar(12); # 
 ALTER TABLE products CHANGE COLUMN products_viewed products_viewed int(5); # was int(5) DEFAULT '0'
 ALTER TABLE products CHANGE COLUMN products_status products_status tinyint(1) DEFAULT '0' NOT NULL; # was tinyint(1) DEFAULT '1' NOT NULL
 
+INSERT INTO configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) VALUES ('State', 'ENTRY_STATE_MIN_LENGTH', '4', 'Minimum length of state', '2', '7', now());
