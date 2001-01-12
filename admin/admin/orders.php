149d108fb18cf0442e71268b58508cab3d18f6a0
tmoulton <tmoulton>
2001-01-12 12:26:39 +0000

    Fix tax display in admin too (TAX_DECIMAL_PLACES)

--- admin/admin/orders.php
+++ admin/admin/orders.php
@@ -208,7 +208,7 @@ function alertBox() {
       }
 //------display customer choosen option eof-----
 	  echo '</font></td>' . "\n";
-      echo '            <td align="center" valign="top" nowrap><font face="' . TEXT_FONT_FACE . '" size="' . TEXT_FONT_SIZE . '" color="' . TEXT_FONT_COLOR . '">&nbsp;' . round($products_values['products_tax']) . '%&nbsp;</font></td>' . "\n";
+      echo '            <td align="center" valign="top" nowrap><font face="' . TEXT_FONT_FACE . '" size="' . TEXT_FONT_SIZE . '" color="' . TEXT_FONT_COLOR . '">&nbsp;' . number_format($products_values['products_tax'], TAX_DECIMAL_PLACES) . '%&nbsp;</font></td>' . "\n";
       echo '            <td align="right" valign="top" nowrap><font face="' . TEXT_FONT_FACE . '" size="' . TEXT_FONT_SIZE . '" color="' . TEXT_FONT_COLOR . '">&nbsp;<b>' . tep_currency_format($products_values['products_quantity'] * $products_values['products_price']) . '</b>&nbsp;';
 //------display customer choosen option --------
     if ($attributes_exist == '1') {
