69b262bd36c82b69db745b8f401741361443fced
tmoulton <tmoulton>
2001-01-30 11:52:12 +0000

    Remove one more reference to SHIPPING_FREE

--- catalog/catalog/includes/languages/german/checkout_process.php
+++ catalog/catalog/includes/languages/german/checkout_process.php
@@ -22,7 +22,7 @@ define('EMAIL_TEXT_PAYPAL', 'PayPal');
 
 $email_order = STORE_NAME . "\n" . '------------------------------------------------------' . "\n" . EMAIL_TEXT_ORDER_NUMBER . ' ' . $insert_id . "\n" . EMAIL_TEXT_INVOICE_URL . " " . HTTP_SERVER . DIR_CATALOG . FILENAME_ACCOUNT_HISTORY_INFO . '?order_id=' . $insert_id . "\n";
 $email_order .= EMAIL_TEXT_DATE_ORDERED . ' ' . $date_formatted . "\n\n" . EMAIL_TEXT_PRODUCTS . "\n" . '------------------------------------------------------' . "\n" . $products_ordered . '------------------------------------------------------' . "\n" . EMAIL_TEXT_SUBTOTAL . ' ' . tep_currency_format($cart->show_total()) . "\n" . EMAIL_TEXT_TAX . tep_currency_format($total_tax) . "\n";
-if (!SHIPPING_FREE) {
+if ($shipping_cost > 0) {
   $email_order .= EMAIL_TEXT_SHIPPING . ' ' . tep_currency_format($shipping_cost) . "     via " . $shipping_method . "\n";
 }
 $email_order .= EMAIL_TEXT_TOTAL . ' ' . tep_currency_format($cart->show_total() + $total_tax + $shipping_cost) . "\n\n" . EMAIL_TEXT_DELIVERY_ADDRESS . "\n" . '------------------------------------------------------' . "\n";
