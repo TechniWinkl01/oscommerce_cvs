03d0bbaeff067e8dfd88ddcb70d15292db34f758
pkellum <pkellum>
2001-02-03 08:53:52 +0000

    Add FedEx (Federal Express) service to shipping options.
    Can only be used if the store is located in the USA or Canada and they only ship to the USA, Canada and Mexico.
    
    You *MUST* run cataog/extras/shipping_modules/shipping_3.sql
    
    Or, if you don't have shell access, load cataog/extras/shipping_modules/update.php in your browser and select shipping_3.
    
    Once this is done, go to the My Store section of your Admin page and set your country code.
    
    Error checking isn't finished yet, so be careful wth it :-)

---
+++ catalog/catalog/includes/modules/shipping/fedex.php
@@ -0,0 +1,84 @@
+<?php
+	/* $Id: fedex.php,v 1.1 2001/02/03 08:53:52 pkellum Exp $ */
+	define('SHIPPING_FEDEX_NAME', 'FedEx'); // It's a trademark, can't translate
+	switch($action) {
+		case 'select' :
+			print "<TR><TD><FONT FACE=\"" . TEXT_FONT_FACE . "\" SIZE=\"" . TEXT_FONT_SIZE . "\" COLOR=\"" . TEXT_FONT_COLOR . "\">&nbsp;";
+			print htmlentities(SHIPPING_FEDEX_NAME) . "</FONT></TD>";
+			print "<TD>&nbsp;</TD>";
+			print "<TD ALIGN=\"right\">&nbsp;<INPUT TYPE=\"checkbox\" NAME=\"shipping_quote_fedex\" VALUE=\"1\"";
+			if(!$shipping_count) {
+				print ' CHECKED';
+			}
+			print "></TD></TR>\n";
+			break;
+		case 'quote' :
+			if ($shipping_quote_fedex == "1") {
+				// convert TEP country id to ISO 3166 id
+				// only these three are needed since FedEx only ships to them
+				$fedex_countries = array(
+					38 => 'CA',		// Canada
+					138 => 'MX',	// Mexico
+					223 => 'US'		// USA
+				);
+				$shipping_quoted = 'fedex';
+				include('includes/fedex.php');
+				$rate = new FedEx(STORE_ORIGIN_ZIP, STORE_ORIGIN_COUNTRY);
+				$rate->SetDest($address_values['postcode'], $fedex_countries[$address_values['country']]);
+				$rate->SetWeight($total_weight);
+				$quote = $rate->GetQuote();
+				$shipping_fedex_cost = SHIPPING_HANDLING + $quote['TotalCharges'];
+				// clean up the service text a little
+				$shipping_fedex_method = str_replace(' Package', '', $quote['Service']);
+				$shipping_fedex_method = str_replace(' FedEx', '', $shipping_fedex_method);
+			}
+			break;
+		case 'cheapest' :
+			if($shipping_quote_fedex == "1") {
+				if($shipping_count == 0) {
+					$shipping_cheapest = 'fedex';
+					$shipping_cheapest_cost = $shipping_fedex_cost;
+				}
+				else {
+					if($shipping_fedex_cost < $shipping_cheapest_cost) {
+						$shipping_cheapest = 'fedex';
+						$shipping_cheapest_cost = $shipping_fedex_cost;
+					}
+				}
+			}
+			break;
+		case 'display' :
+			if($shipping_quote_fedex == "1") {
+				print "<TR>\n";
+				print '<TD><FONT FACE="' . TEXT_FONT_FACE . '" SIZE="' . TEXT_FONT_SIZE . '" COLOR="' . TEXT_FONT_COLOR . '">&nbsp;' . htmlentities(SHIPPING_FEDEX_NAME) . "</FONT></TD>\n";
+				print '<TD><FONT FACE="' . TEXT_FONT_FACE . '" SIZE="' . TEXT_FONT_SIZE . '" COLOR="' . TEXT_FONT_COLOR . '">' . $shipping_fedex_method . "</FONT></TD>\n";
+				print '<TD ALIGN="right"><FONT FACE="' . TEXT_FONT_FACE . '" SIZE="' . TEXT_FONT_SIZE . '" COLOR="' . TEXT_FONT_COLOR . '">' . tep_currency_format($shipping_fedex_cost) . "</FONT></TD>\n";
+				print '<TD ALIGN="right" NOWRAP>&nbsp;<INPUT TYPE="radio" NAME="shipping_selected" VALUE="fedex"';
+				if($shipping_cheapest == 'fedex') {
+					print ' CHECKED';
+				}
+				print ">&nbsp;</TD>\n";
+				print "</TR>\n";
+				print '<INPUT TYPE="hidden" NAME="shipping_fedex_cost" VALUE="' . $shipping_fedex_cost . "\">\n";
+				print '<INPUT TYPE="hidden" NAME="shipping_fedex_method" VALUE="' . $shipping_fedex_method . "\">\n";
+			}
+			break;
+		case 'confirm' :
+			if($HTTP_POST_VARS['shipping_selected'] == 'fedex') {
+				$shipping_cost = $HTTP_POST_VARS['shipping_fedex_cost'];
+				$shipping_method = $HTTP_POST_VARS['shipping_fedex_method'];
+			}
+			break;
+		case 'check' :
+			$check = tep_db_query("select configuration_value from configuration where configuration_key = 'SHIPPING_FEDEX_ENABLED'");
+			$check = tep_db_num_rows($check) + 1;
+			break;
+		case 'install' :
+			tep_db_query("INSERT INTO configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) VALUES ('Enable FedEx Shipping', 'SHIPPING_FEDEX_ENABLED', '1', 'Do you want to offer Federal Express (FedEx) shipping?', '7', '10', now())");
+			break;
+		case 'remove' :
+			tep_db_query("DELETE FROM configuration WHERE configuration_key = 'SHIPPING_FEDEX_ENABLED'");
+			break;
+	}
+	$shipping_count++;
+?>
