8f05b94611e16b33a84681dee372dd179929b21c
mkowalkowski <mkowalkowski>
2001-02-09 11:56:32 +0000

    typo / grammar fix.

--- catalog/catalog/includes/languages/german.php
+++ catalog/catalog/includes/languages/german.php
@@ -58,15 +58,15 @@ define('BOX_HEADING_CATEGORIES', 'Kategorien');
 
 // manufacturers box text in includes/boxes/manufacturers.php
 define('BOX_HEADING_MANUFACTURERS', 'Hersteller');
-define('BOX_MANUFACTURERS_SELECT_ONE', 'W‰hlen Sie ein aus:');
+define('BOX_MANUFACTURERS_SELECT_ONE', 'W‰hlen Sie aus:');
 
 // whats_new box text in includes/boxes/whats_new.php
-define('BOX_HEADING_WHATS_NEW', 'Neue Produkte');
+define('BOX_HEADING_WHATS_NEW', 'Neue Artikel');
 
 // quick_find box text in includes/boxes/quick_find.php
 define('BOX_HEADING_SEARCH', 'Schnellsuche');
 define('BOX_SEARCH_TEXT', 'Verwenden Sie Schl&uuml;sselw&ouml;rter, um ein Produkt zu finden.');
-define('BOX_SEARCH_ADVANCED_SEARCH', 'Hochentwickelte Suche');
+define('BOX_SEARCH_ADVANCED_SEARCH', 'erweiterte Suche');
 
 // add_a_quickie box text in includes/boxes/add_a_quickie.php
 define('BOX_HEADING_ADD_PRODUCT_ID', 'Schnelleinkauf');
@@ -88,11 +88,13 @@ define('BOX_SHOPPING_CART_VIEW_CONTENTS', 'Inhalt einsehen');
 
 // best_sellers box text in includes/boxes/best_sellers.php
 define('BOX_HEADING_BESTSELLERS', 'Bestseller');
-define('BOX_HEADING_BESTSELLERS_IN', 'Bestseller in<br>&nbsp;&nbsp;');
+define('BOX_HEADING_BESTSELLERS_IN', 'Bestseller<br>&nbsp;&nbsp;');
 
 // pull down default text
-define('PULL_DOWN_DEFAULT', 'Bitte W&auml;hlen');
-define('TYPE_BELOW', 'Type Below');
+define('PULL_DOWN_DEFAULT', 'Bitte w&auml;hlen');
+define('TYPE_BELOW', 'bitte unten eingeben');
+
+
 
 // checkout procedure text
 define('CHECKOUT_BAR_CART_CONTENTS', 'Warenkorbinhalt');
@@ -105,93 +107,93 @@ define('CHECKOUT_BAR_FINISHED', 'fertig');
 define('JS_ERROR', 'Notwendige Angaben fehlen!\nBitte richtig ausf¸llen.\n\n');
 
 define('JS_CC_OWNER', '* Der \'Name des Eigent¸mers\' muﬂ mindestens aus ' . CC_OWNER_MIN_LENGTH . ' Buchstaben bestehen.\n');
-define('JS_CC_NUMBER', '* Die \'Kredit Karten Nr.\' muﬂ mindestens aus ' . CC_NUMBER_MIN_LENGTH . ' Zahlen bestehen.\n');
+define('JS_CC_NUMBER', '* Die \'Kreditkartenr.\' muﬂ mindestens aus ' . CC_NUMBER_MIN_LENGTH . ' Zahlen bestehen.\n');
 define('JS_CC_EXPIRES', '* Das G¸ltigkeitsdatum muﬂ mindestens aus ' . CC_EXPIRY_MIN_LENGTH . ' Zahlen bestehen.\n');
 
 define('JS_REVIEW_TEXT', '* Der Text muﬂ mindestens aus ' . REVIEW_TEXT_MIN_LENGTH . ' Buchstaben bestehen.\n');
-define('JS_REVIEW_RATING', '* Geben Sie ihre Bewertung.\n');
+define('JS_REVIEW_RATING', '* Geben Sie ihre Bewertung ein.\n');
 
 define('JS_GENDER', '* Anredeform festlegen.\n');
 define('JS_FIRST_NAME', '* Der \'Vornname\' muﬂ mindestens aus ' . ENTRY_FIRST_NAME_MIN_LENGTH . ' Buchstaben bestehen.\n');
 define('JS_LAST_NAME', '* Der \'Nachname\' muﬂ mindestens aus ' . ENTRY_LAST_NAME_MIN_LENGTH . ' Buchstaben bestehen.\n');
-define('JS_DOB', '* Die \'Geburtsdaten\' mit xx/xx/xxxx (datum/monat/jahr) eingeben.\n');
-define('JS_EMAIL_ADDRESS', '* Die Eingabe der \'E-Mail Adresse\' muﬂ mindestens aus ' . ENTRY_EMAIL_ADDRESS_MIN_LENGTH . ' Buchstaben bestehen.\n');
+define('JS_DOB', '* Die \'Geburtsdaten\' im Fornmat xx/xx/xxxx (Tag/Monat/Jahr) eingeben.\n');
+define('JS_EMAIL_ADDRESS', '* Die \'E-Mail Adresse\' muﬂ mindestens aus ' . ENTRY_EMAIL_ADDRESS_MIN_LENGTH . ' Buchstaben bestehen.\n');
 define('JS_ADDRESS', '* Die \'Strasse/Nr.\' muﬂ mindestens aus ' . ENTRY_STREET_ADDRESS_MIN_LENGTH . ' Buchstaben bestehen.\n');
 define('JS_POST_CODE', '* Die \'Postleitzahl\' muﬂ mindestens aus ' . ENTRY_POSTCODE_MIN_LENGTH . ' Buchstaben bestehen.\n');
-define('JS_CITY', '* Der \'Stadtname\' muﬂ mindestens aus ' . ENTRY_CITY_MIN_LENGTH . ' Buchstaben bestehen.\n');
-define('JS_STATE', '* Der \'Bundesland\' muﬂ mindestens aus ' . ENTRY_STATE_LENGTH . ' Buchstaben bestehen.\n');
-define('JS_STATE_SELECT', '-- W‰hlen Sie ‹ber Aus --');
-define('JS_ZONE', ' * Der Eintrag Bundesland muﬂ von der Liste f¸r dieses Land ausgew‰hlt werden.');
-define('JS_COUNTRY', '* Der \'Land\' Eintrag muﬂ ausgew‰hlt werden.');
-define('JS_TELEPHONE', '* Die \'Telefonnummer\' muﬂ mindestens ' . ENTRY_TELEPHONE_MIN_LENGTH . ' Zahlen enthalten.\n');
-define('JS_PASSWORD', '* Das \'Passwort\' und die \'Best‰tigung\' muﬂ ¸bereinstimmen und mindestens ' . ENTRY_PASSWORD_MIN_LENGTH . ' Buchstaben enthalten.\n');
+define('JS_CITY', '* Die \'Stadt\' muﬂ mindestens aus ' . ENTRY_CITY_MIN_LENGTH . ' Buchstaben bestehen.\n');
+define('JS_STATE', '* Das \'Bundesland\' muﬂ mindestens aus ' . ENTRY_STATE_LENGTH . ' Buchstaben bestehen.\n');
+define('JS_STATE_SELECT', '-- w‰hlen sie oberhalb aus --');
+define('JS_ZONE', ' * Das Bundesland muss aus der Liste f¸r dieses Land ausgew‰hlt werden.');
+define('JS_COUNTRY', '* Das \'Land\' muss ausgew‰hlt werden.');
+define('JS_TELEPHONE', '* Die \'Telefonnummer\' muss mindestens aus ' . ENTRY_TELEPHONE_MIN_LENGTH . ' Zahlen bestehen.\n');
+define('JS_PASSWORD', '* Das \'Passwort\' und die \'Best‰tigung\' m¸ssen ¸bereinstimmen und mindestens ' . ENTRY_PASSWORD_MIN_LENGTH . ' Buchstaben enthalten.\n');
 
 define('CATEGORY_PERSONAL', '<b>[ Pers&ouml;nliche Daten ]</b>');
 define('CATEGORY_ADDRESS', '<b>[ Adresse ]</b>');
 define('CATEGORY_CONTACT', '<b>[ Kontakt ]</b>');
 define('CATEGORY_PASSWORD', '<b>[ Passwort ]</b>');
 define('ENTRY_GENDER', 'Geschlecht:');
-define('ENTRY_GENDER_ERROR', '&nbsp;<small><font color="#AABBDD">ben&ouml;tigt</font></small>');
-define('ENTRY_GENDER_TEXT', '&nbsp;<small><font color="#AABBDD">ben&ouml;tigt</font></small>');
+define('ENTRY_GENDER_ERROR', '&nbsp;<small><font color="#AABBDD">notwendige Eingabe</font></small>');
+define('ENTRY_GENDER_TEXT', '&nbsp;<small><font color="#AABBDD">notwendige Eingabe</font></small>');
 define('ENTRY_FIRST_NAME', 'Vorname:');
 define('ENTRY_FIRST_NAME_ERROR', '&nbsp;<small><font color="#FF0000">mindestens ' . ENTRY_FIRST_NAME_MIN_LENGTH . ' Buchstaben</font></small>');
-define('ENTRY_FIRST_NAME_TEXT', '&nbsp;<small><font color="#AABBDD">ben&ouml;tigt</font></small>');
+define('ENTRY_FIRST_NAME_TEXT', '&nbsp;<small><font color="#AABBDD">notwendige Eingabe</font></small>');
 define('ENTRY_LAST_NAME', 'Nachname:');
 define('ENTRY_LAST_NAME_ERROR', '&nbsp;<small><font color="#FF0000">mindestens ' . ENTRY_LAST_NAME_MIN_LENGTH . ' Buchstaben</font></small>');
-define('ENTRY_LAST_NAME_TEXT', '&nbsp;<small><font color="#AABBDD">ben&ouml;tigt</font></small>');
+define('ENTRY_LAST_NAME_TEXT', '&nbsp;<small><font color="#AABBDD">notwendige Eingabe</font></small>');
 define('ENTRY_DATE_OF_BIRTH', 'Geburtsdatum:');
 define('ENTRY_DATE_OF_BIRTH_ERROR', '&nbsp;<small><font color="#FF0000">(z.B. 21/05/1970)</font></small>');
-define('ENTRY_DATE_OF_BIRTH_TEXT', '&nbsp;<small>(z.B. 21/05/1970) <font color="#AABBDD">ben&ouml;tigt</font></small>');
+define('ENTRY_DATE_OF_BIRTH_TEXT', '&nbsp;<small>(z.B. 21/05/1970) <font color="#AABBDD">notwendige Eingabe</font></small>');
 define('ENTRY_EMAIL_ADDRESS', 'E-Mail Adresse:');
 define('ENTRY_EMAIL_ADDRESS_ERROR', '&nbsp;<small><font color="#FF0000">mindestens ' . ENTRY_EMAIL_ADDRESS_MIN_LENGTH . ' Buchstaben</font></small>');
 define('ENTRY_EMAIL_ADDRESS_CHECK_ERROR', '&nbsp;<small><font color="#FF0000">Ung&uuml;ltige E-Mail Adresse!</font></small>');
 define('ENTRY_EMAIL_ADDRESS_ERROR_EXISTS', '&nbsp;<small><font color="#FF0000">Diese E-Mail Adresse existiert schon!</font></small>');
-define('ENTRY_EMAIL_ADDRESS_TEXT', '&nbsp;<small><font color="#AABBDD">ben&ouml;tigt</font></small>');
+define('ENTRY_EMAIL_ADDRESS_TEXT', '&nbsp;<small><font color="#AABBDD">notwendige Eingabe</font></small>');
 define('ENTRY_STREET_ADDRESS', 'Strasse/Nr.:');
 define('ENTRY_STREET_ADDRESS_ERROR', '&nbsp;<small><font color="#FF0000">mindestens ' . ENTRY_STREET_ADDRESS_MIN_LENGTH . ' Buchstaben</font></small>');
-define('ENTRY_STREET_ADDRESS_TEXT', '&nbsp;<small><font color="#AABBDD">ben&ouml;tigt</font></small>');
+define('ENTRY_STREET_ADDRESS_TEXT', '&nbsp;<small><font color="#AABBDD">notwendige Eingabe</font></small>');
 define('ENTRY_SUBURB', 'Wohnort:');
 define('ENTRY_SUBURB_ERROR', '');
 define('ENTRY_SUBURB_TEXT', '');
 define('ENTRY_POST_CODE', 'Postleitzahl:');
 define('ENTRY_POST_CODE_ERROR', '&nbsp;<small><font color="#FF0000">mindestens ' . ENTRY_POSTCODE_MIN_LENGTH . ' Buchstaben</font></small>');
-define('ENTRY_POST_CODE_TEXT', '&nbsp;<small><font color="#AABBDD">ben&ouml;tigt</font></small>');
+define('ENTRY_POST_CODE_TEXT', '&nbsp;<small><font color="#AABBDD">notwendige Eingabe</font></small>');
 define('ENTRY_CITY', 'Stadt:');
 define('ENTRY_CITY_ERROR', '&nbsp;<small><font color="#FF0000">mindestens ' . ENTRY_CITY_MIN_LENGTH . ' Buchstaben</font></small>');
-define('ENTRY_CITY_TEXT', '&nbsp;<small><font color="#AABBDD">ben&ouml;tigt</font></small>');
+define('ENTRY_CITY_TEXT', '&nbsp;<small><font color="#AABBDD">notwendige Eingabe</font></small>');
 define('ENTRY_STATE', 'Bundesland:');
 define('ENTRY_STATE_ERROR', '');
 define('ENTRY_STATE_TEXT', '');
 define('ENTRY_COUNTRY', 'Land:');
 define('ENTRY_COUNTRY_ERROR', '');
-define('ENTRY_COUNTRY_TEXT', '&nbsp;<small><font color="#AABBDD">ben&ouml;tigt</font></small>');
+define('ENTRY_COUNTRY_TEXT', '&nbsp;<small><font color="#AABBDD">notwendige Eingabe</font></small>');
 define('ENTRY_TELEPHONE_NUMBER', 'Telefonnummer:');
 define('ENTRY_TELEPHONE_NUMBER_ERROR', '&nbsp;<small><font color="#FF0000">mindestens ' . ENTRY_TELEPHONE_MIN_LENGTH . ' Zahlen</font></small>');
-define('ENTRY_TELEPHONE_NUMBER_TEXT', '&nbsp;<small><font color="#AABBDD">ben&ouml;tigt</font></small>');
+define('ENTRY_TELEPHONE_NUMBER_TEXT', '&nbsp;<small><font color="#AABBDD">notwendige Eingabe</font></small>');
 define('ENTRY_FAX_NUMBER', 'Faxnummer:');
 define('ENTRY_FAX_NUMBER_ERROR', '');
 define('ENTRY_FAX_NUMBER_TEXT', '');
 define('ENTRY_PASSWORD', 'Passwort:');
 define('ENTRY_PASSWORD_CONFIRMATION', 'Best&auml;tigung:');
-define('ENTRY_PASSWORD_CONFIRMATION_TEXT', '&nbsp;<small><font color="#AABBDD">ben&ouml;tigt</font></small>');
+define('ENTRY_PASSWORD_CONFIRMATION_TEXT', '&nbsp;<small><font color="#AABBDD">notwendige Eingabe</font></small>');
 define('ENTRY_PASSWORD_ERROR', '&nbsp;<small><font color="#FF0000">mindestens ' . ENTRY_PASSWORD_MIN_LENGTH . ' Buchstaben</font></small>');
-define('ENTRY_PASSWORD_TEXT', '&nbsp;<small><font color="#AABBDD">ben&ouml;tigt</font></small>');
+define('ENTRY_PASSWORD_TEXT', '&nbsp;<small><font color="#AABBDD">notwendige Eingabe</font></small>');
 define('PASSWORD_HIDDEN', '--VERSTECKT--');
 
 // constants for use in tep_prev_next_display function
-define('TEXT_RESULT_PAGE', 'Ergebniss Seiten:');
-define('TEXT_DISPLAY_NUMBER_OF_PRODUCTS', 'Ergebnisse <b>%d</b> bis <b>%d</b> (von <b>%d</b> Produkte)');
-define('TEXT_DISPLAY_NUMBER_OF_ORDERS', 'Ergebnisse <b>%d</b> bis <b>%d</b> (von <b>%d</b> Bestellungen)');
+define('TEXT_RESULT_PAGE', 'Seiten:');
+define('TEXT_DISPLAY_NUMBER_OF_PRODUCTS', 'angezeigte Artikel: <b>%d</b> bis <b>%d</b> (von <b>%d</b> insgesamt)');
+define('TEXT_DISPLAY_NUMBER_OF_ORDERS', 'angezeigte Bestellungen: <b>%d</b> bis <b>%d</b> (von <b>%d</b> insgesamt)');
 
-define('PREVNEXT_TITLE_FIRST_PAGE', 'Erste Seite');
-define('PREVNEXT_TITLE_PREVIOUS_PAGE', 'Vorherige Seite');
-define('PREVNEXT_TITLE_NEXT_PAGE', 'Folgende Seite');
-define('PREVNEXT_TITLE_LAST_PAGE', 'Letzte Seite');
+define('PREVNEXT_TITLE_FIRST_PAGE', 'erste Seite');
+define('PREVNEXT_TITLE_PREVIOUS_PAGE', 'vorherige Seite');
+define('PREVNEXT_TITLE_NEXT_PAGE', 'n&auml;chste Seite');
+define('PREVNEXT_TITLE_LAST_PAGE', 'letzte Seite');
 define('PREVNEXT_TITLE_PAGE_NO', 'Seite %d');
-define('PREVNEXT_TITLE_PREV_SET_OF_NO_PAGE', 'Vorhergehendes Set von %d Seiten');
-define('PREVNEXT_TITLE_NEXT_SET_OF_NO_PAGE', 'Folgendes Set von %d Seiten');
-define('PREVNEXT_BUTTON_FIRST', '&lt;&lt;ZUERST');
-define('PREVNEXT_BUTTON_PREV', '[&lt;&lt; Vorhergehend]');
-define('PREVNEXT_BUTTON_NEXT', '[Zun&auml;chst &gt;&gt;]');
-define('PREVNEXT_BUTTON_LAST', 'LETZTES&gt;&gt;');
+define('PREVNEXT_TITLE_PREV_SET_OF_NO_PAGE', 'Vorhergehende %d Seiten');
+define('PREVNEXT_TITLE_NEXT_SET_OF_NO_PAGE', 'N‰chste %d Seiten');
+define('PREVNEXT_BUTTON_FIRST', '&lt;&lt;ERSTE');
+define('PREVNEXT_BUTTON_PREV', '[&lt;&lt; vorherige]');
+define('PREVNEXT_BUTTON_NEXT', '[n&auml;chste &gt;&gt;]');
+define('PREVNEXT_BUTTON_LAST', 'LETZTE&gt;&gt;');
 ?>
