af48c308a676a7198eadc4309226bf003d6b3b0b
hpdl <hpdl>
2001-02-24 15:24:32 +0000

    addded language files for currencies
    DAVID, can you do the spanish translation please..

---
+++ admin/admin/includes/languages/german/currencies.php
@@ -0,0 +1,28 @@
+<?
+/*
+German Text for The Exchange Project Administration Tool Preview Release 2.1
+Last Update: 24/02/2001
+Author(s): Harald Ponce de Leon (hpdl@theexchangeproject.org)
+*/
+
+define('TOP_BAR_TITLE', 'W&auml;hrungen');
+define('HEADING_TITLE', 'W&auml;hrungen');
+
+define('TABLE_HEADING_COUNTRY_ID', 'ID');
+define('TABLE_HEADING_COUNTRY_NAME', 'W&auml;hrung');
+define('TABLE_HEADING_COUNTRY_CODES', 'Codierung');
+define('TABLE_HEADING_ACTION', 'Aktion');
+
+define('TEXT_INFO_EDIT_INTRO', 'Bitte f&uuml;hren Sie die notwendigen &Auml;nderungen durch');
+define('TEXT_INFO_CURRENCY_TITLE', 'Name:');
+define('TEXT_INFO_CURRENCY_CODE', 'Codierung:');
+define('TEXT_INFO_CURRENCY_SYMBOL_LEFT', 'Symbol Links:');
+define('TEXT_INFO_CURRENCY_SYMBOL_RIGHT', 'Symbol Rechts:');
+define('TEXT_INFO_CURRENCY_DECIMAL_POINT', 'Dezimalkomma:');
+define('TEXT_INFO_CURRENCY_THOUSANDS_POINT', 'Tausendepunkt:');
+define('TEXT_INFO_CURRENCY_DECIMAL_PLACES', 'Dezimalstellen:');
+define('TEXT_INFO_CURRENCY_EXAMPLE', 'Beispiel:');
+define('TEXT_INFO_INSERT_INTRO', 'Bitte f&uuml;llen Sie die folgenden Daten f&uuml;r die neue Wahrung:');
+define('TEXT_INFO_DELETE_INTRO', 'Sind Sie sicher, dass sie diese Wahrung l&ouml;schen m&ouml;chten?');
+define('TEXT_INFO_HEADING_NEW_CURRENCY', 'Neue W&auml;hrung');
+?>
