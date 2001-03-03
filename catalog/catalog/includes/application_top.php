1df9f3578feea48c077bc8d436a1bdeef439a4bd
dwatkins <dwatkins>
2001-03-03 19:00:28 +0000

    Better default value for DIR_IMAGES

--- catalog/catalog/includes/application_top.php
+++ catalog/catalog/includes/application_top.php
@@ -16,7 +16,7 @@
   define('DIR_SERVER_ROOT', '/usr/local/apache/');
   define('DIR_LOGS', DIR_SERVER_ROOT . 'logs/');
   define('DIR_CATALOG', '/catalog/');
-  define('DIR_IMAGES', '/catalog/images/'); // from webserver
+  define('DIR_IMAGES', 'images/'); // from webserver
   define('DIR_IMAGES_PHYSICAL', DIR_SERVER_ROOT); // 'images/' is hardcoded in the database .. all these paths will later fixed for a better structure..!!
   define('DIR_INCLUDES', 'includes/');
   define('DIR_BOXES', DIR_INCLUDES . 'boxes/');
