4407f54a6fa2b97ca75e10d7affe894091104353
hpdl <hpdl>
2001-03-04 12:57:54 +0000

    updated shopping cart to display products name, and not products model
    its also (now) the first box on the right hand side

--- catalog/catalog/includes/boxes/shopping_cart.php
+++ catalog/catalog/includes/boxes/shopping_cart.php
@@ -10,17 +10,32 @@
 
   $cart_contents_string = '';
   if ($cart->count_contents() > 0) {
-    $models = $cart->get_products();
-    for ($i=0; $i<sizeof($models); $i++) { // $models[$i]['id'] .. $models[$i]['model'] .. $models[$i]['quantity']
-      $cart_contents_string .= '<a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $models[$i]['id'], 'NONSSL') . '">';
-      if ((tep_session_is_registered('new_products_id_in_cart')) && ($new_products_id_in_cart == $models[$i]['id'])) $cart_contents_string .=  '<font color="' . NEW_CART_ITEM_COLOR . '">';
-      $cart_contents_string .= $models[$i]['quantity'] . 'x ' . $models[$i]['model'];
-      if ((tep_session_is_registered('new_products_id_in_cart')) && ($new_products_id_in_cart == $models[$i]['id'])) {
-        $cart_contents_string .= '</font>';
+    $cart_contents_string = '<table border="0" width="100%" cellspacing="0" cellpadding="0">';
+    $products = $cart->get_products(); // $products[$i]['id'] .. $products[$i]['name'] .. $products[$i]['quantity']
+    for ($i=0; $i<sizeof($products); $i++) {
+      $cart_contents_string .= '<tr><td align="right" valign="top">';
+
+      if ((tep_session_is_registered('new_products_id_in_cart')) && ($new_products_id_in_cart == $products[$i]['id'])) {
+        $cart_contents_string .= FONT_STYLE_NEW_CART_ITEM; // highlight product quantity
+      } else {
+        $cart_contents_string .= FONT_STYLE_INFO_BOX_BODY;
+      }
+
+      $cart_contents_string .= $products[$i]['quantity'] . 'x</font></td><td valign="top"><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $products[$i]['id'], 'NONSSL') . '">';
+
+      if ((tep_session_is_registered('new_products_id_in_cart')) && ($new_products_id_in_cart == $products[$i]['id'])) {
+        $cart_contents_string .= FONT_STYLE_NEW_CART_ITEM; // highlight product name
+      } else {
+        $cart_contents_string .= FONT_STYLE_INFO_BOX_BODY;
+      }
+
+      $cart_contents_string .= $products[$i]['name'] . '</font></a></td></tr>';
+
+      if ((tep_session_is_registered('new_products_id_in_cart')) && ($new_products_id_in_cart == $products[$i]['id'])) {
         tep_session_unregister('new_products_id_in_cart');
       }
-      $cart_contents_string .= '</a><br>';
     }
+    $cart_contents_string .= '</table>';
   } else {
     $cart_empty = 1;
     $cart_contents_string .= BOX_SHOPPING_CART_EMPTY;
@@ -30,16 +45,13 @@
   $info_box_contents[] = array('align' => 'left',
                                'text'  => $cart_contents_string
                               );
-  $info_box_contents[] = array('align' => 'left',
-                               'text' => tep_black_line()
-                         );
 
-  if ($cart_empty != 1) {
+  if (!$cart_empty) {
+    $info_box_contents[] = array('align' => 'left',
+                                 'text' => tep_black_line()
+                           );
     $info_box_contents[] = array('align' => 'right',
                                  'text'  => BOX_SHOPPING_CART_SUBTOTAL . ' ' . tep_currency_format($cart->show_total())
-                                );
-    $info_box_contents[] = array('align' => 'right',
-                                 'text'  => '<a href="' . tep_href_link(FILENAME_SHOPPING_CART, '', 'NONSSL') . '">' . BOX_SHOPPING_CART_VIEW_CONTENTS . '</a>'
                                 );
   }
 
