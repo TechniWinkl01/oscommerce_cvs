257fedc15d09d13b1405a07c31f3a736b933d464
hpdl <hpdl>
2001-03-15 21:57:19 +0000

    made a bit of order in the filenames

--- admin/admin/includes/application_top.php
+++ admin/admin/includes/application_top.php
@@ -50,25 +50,26 @@
   define('FILENAME_BACKUP', 'backup.php');
   define('FILENAME_CATEGORIES', 'categories.php');
   define('FILENAME_CONFIGURATION', 'configuration.php');
-  define('FILENAME_PAYMENT_MODULES', 'payment_modules.php');
-  define('FILENAME_SHIPPING_MODULES', 'shipping_modules.php');
   define('FILENAME_COUNTRIES', 'countries.php');
+  define('FILENAME_CURRENCIES', 'currencies.php');
   define('FILENAME_CUSTOMERS', 'customers.php');
   define('FILENAME_DEFAULT', 'default.php');
+  define('FILENAME_LANGUAGES', 'languages.php');
   define('FILENAME_MANUFACTURERS', 'manufacturers.php');
   define('FILENAME_ORDERS', 'orders.php');
+  define('FILENAME_PAYMENT_MODULES', 'payment_modules.php');
   define('FILENAME_PRODUCTS_ATTRIBUTES', 'products_attributes.php');
   define('FILENAME_PRODUCTS_EXPECTED', 'products_expected.php');
   define('FILENAME_REVIEWS', 'reviews.php');
+  define('FILENAME_SHIPPING_MODULES', 'shipping_modules.php');
   define('FILENAME_SPECIALS', 'specials.php');
   define('FILENAME_STATS_CUSTOMERS', 'stats_customers.php');
   define('FILENAME_STATS_PRODUCTS_PURCHASED', 'stats_products_purchased.php');
   define('FILENAME_STATS_PRODUCTS_VIEWED', 'stats_products_viewed.php');
   define('FILENAME_TAX_CLASSES', 'tax_classes.php');
   define('FILENAME_TAX_RATES', 'tax_rates.php');
+  define('FILENAME_WHOS_ONLINE', 'whos_online.php');
   define('FILENAME_ZONES', 'zones.php');
-  define('FILENAME_CURRENCIES', 'currencies.php');
-  define('FILENAME_LANGUAGES', 'languages.php');
 
 // define our database connection
   define('DB_SERVER', $HTTP_ENV_VARS['HOSTNAME']);
