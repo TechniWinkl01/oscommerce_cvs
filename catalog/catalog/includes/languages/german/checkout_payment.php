80ef6524dece61656b8f35f2dfb3ae9baec24b23
dwatkins <dwatkins>
2001-04-08 15:36:55 +0000

    Sorry about my german, it is equal NULL ... here are the corrections ive been told to commit.
    Let me know if there are any more errors

--- catalog/catalog/includes/languages/german/checkout_payment.php
+++ catalog/catalog/includes/languages/german/checkout_payment.php
@@ -2,21 +2,21 @@
 /*
 German Text for The Exchange Project Preview Release 2.0
 Last Update: 01/12/2000
-Author(s): Mathias Kowalkowski (mathias@zoomed.de)
-*/
+Author(s): Marcel Bossert-Schwab (webmaster@wernich.de) 
+*/ 
 
-define('NAVBAR_TITLE_1', 'Kasse');
-define('NAVBAR_TITLE_2', 'Zahlungsweise');
-define('TOP_BAR_TITLE', 'Bestellassistent');
-define('HEADING_TITLE', 'Zahlungsweise :');
-define('TABLE_HEADING_COMMENTS', 'Fügen Sie Kommentar Über Ihre Ordnung Hinzu');
-define('TABLE_HEADING_METHODS', 'Zahlungsmethoden');
-define('TABLE_HEADING_SELECTION', 'Zahlungsweise');
-define('TABLE_HEADING_DELIVERY_ADDRESS', 'Lieferanschrift');
-define('TABLE_HEADING_SHIPPING_INFO', 'VerschiffenFördermaschine');
-define('TABLE_HEADING_SHIPPING_QUOTE', 'Wählen Sie Bevorzugte Kosten Aus');
-define('CHANGE_DELIVERY_ADDRESS', 'Ändern Sie Adresse');
-define('CHANGE_SHIPPING_OPTIONS', 'Verschiffenoptionen');
+define('NAVBAR_TITLE_1', 'Kasse'); 
+define('NAVBAR_TITLE_2', 'Zahlungsweise'); 
+define('TOP_BAR_TITLE', 'Bestellassistent'); 
+define('HEADING_TITLE', 'Zahlungsweise :'); 
+define('TABLE_HEADING_COMMENTS', 'Fügen Sie Ihrer Bestellung ein Kommentar hinzu'); 
+define('TABLE_HEADING_METHODS', 'Zahlungsmethoden'); 
+define('TABLE_HEADING_SELECTION', 'Zahlungsweise'); 
+define('TABLE_HEADING_DELIVERY_ADDRESS', 'Lieferanschrift'); 
+define('TABLE_HEADING_SHIPPING_INFO','Versandart'); 
+define('TABLE_HEADING_SHIPPING_QUOTE','Versandkosten'); 
+define('CHANGE_DELIVERY_ADDRESS', 'Versandadresse ändern'); 
+define('CHANGE_SHIPPING_OPTIONS', 'Versandoptionen'); 
 
-define('IMAGE_NEXT', 'Weiter');
+define('IMAGE_NEXT', 'Weiter'); 
 ?>
