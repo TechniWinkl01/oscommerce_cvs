f128a9945ffb2b70fe0e48913ca8c7318c51120d
dwatkins <dwatkins>
2001-04-13 15:22:55 +0000

    Got rid a couple of warnings.

--- catalog/catalog/includes/classes/shopping_cart.php
+++ catalog/catalog/includes/classes/shopping_cart.php
@@ -270,9 +270,9 @@
 
     function unserialize($broken) { 
       for(reset($broken);$kv=each($broken);) { 
-        $key=$kv[key]; 
+        $key=$kv['key']; 
         if (gettype($this->$key)!="user function") 
-        $this->$key=$kv[value]; 
+        $this->$key=$kv['value']; 
       }
     }
 
