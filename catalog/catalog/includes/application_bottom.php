81ab9fff7ec0d5fed66a08ee2b9b0c28f23499dd
dwatkins <dwatkins>
2001-04-14 22:33:25 +0000

    get rid of the warning

--- catalog/catalog/includes/application_bottom.php
+++ catalog/catalog/includes/application_bottom.php
@@ -7,7 +7,7 @@
     $time_start = explode(' ', PAGE_PARSE_START_TIME);
     $time_end = explode(' ', $parse_end_time);
     $parse_time = number_format(($time_end[1] + $time_end[0] - ($time_start[1] + $time_start[0])), 3);
-    error_log(strftime(STORE_PARSE_DATE_TIME_FORMAT) . ' - ' . getenv(REQUEST_URI) . ' (' . $parse_time . 'ms)' . "\n", 3, STORE_PAGE_PARSE_TIME_LOG);
+    error_log(strftime(STORE_PARSE_DATE_TIME_FORMAT) . ' - ' . getenv('REQUEST_URI') . ' (' . $parse_time . 'ms)' . "\n", 3, STORE_PAGE_PARSE_TIME_LOG);
 
     if (DISPLAY_PAGE_PARSE_TIME == true) {
       echo FONT_STYLE_SMALL_TEXT . 'Parse Time: ' . $parse_time . 'ms</font>';
