b9a15c5dec9d680a529acd81cd02f94f6adf051d
hpdl <hpdl>
2001-04-15 17:50:04 +0000

    little optimizations

--- catalog/catalog/includes/header.php
+++ catalog/catalog/includes/header.php
@@ -1,25 +1,31 @@
 <table border="0" width="100%" cellspacing="0" cellpadding="0">
   <tr class="header">
-    <td align="left" valign="middle" nowrap><? echo tep_image(DIR_WS_IMAGES . 'header_exchange_logo.gif', STORE_NAME) . tep_image(DIR_WS_IMAGES . 'pixel_trans.gif', '', '6', '1') . tep_image(DIR_WS_IMAGES . 'header_exchange.gif', STORE_NAME); ?></td>
+    <td valign="middle" nowrap><?php echo tep_image(DIR_WS_IMAGES . 'header_exchange_logo.gif', STORE_NAME) . tep_image(DIR_WS_IMAGES . 'pixel_trans.gif', '', '6', '1') . tep_image(DIR_WS_IMAGES . 'header_exchange.gif', STORE_NAME); ?></td>
     <td align="right" nowrap>
-<?
+<?php
   if ($customer_id) {
     echo '<a href="' . tep_href_link(FILENAME_ACCOUNT, '', 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'header_account.gif', HEADER_TITLE_MY_ACCOUNT) . '</a>';
   } else {
     echo '<a href="' . tep_href_link(FILENAME_CREATE_ACCOUNT, '', 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'header_account.gif', HEADER_TITLE_CREATE_ACCOUNT) . '</a>';
   }
+
+  echo '&nbsp;&nbsp;<a href="' . tep_href_link(FILENAME_SHOPPING_CART, '', 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'header_cart.gif', HEADER_TITLE_CART_CONTENTS) . '</a>&nbsp;&nbsp;<a href="' . tep_href_link(FILENAME_CHECKOUT_PAYMENT, '', 'SSL') . '">' . tep_image(DIR_WS_IMAGES . 'header_checkout.gif', HEADER_TITLE_CHECKOUT) . '</a>&nbsp;&nbsp;<a href="' . tep_href_link(FILENAME_CONTACT_US, '', 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'header_contact_us.gif', HEADER_TITLE_CONTACT_US) . '</a>&nbsp;&nbsp;';
 ?>
-&nbsp;&nbsp;<a href="<? echo tep_href_link(FILENAME_SHOPPING_CART, '', 'NONSSL'); ?>"><? echo tep_image(DIR_WS_IMAGES . 'header_cart.gif', HEADER_TITLE_CART_CONTENTS); ?></a>&nbsp;&nbsp;<a href="<? echo tep_href_link(FILENAME_CHECKOUT_PAYMENT, '', 'SSL'); ?>"><? echo tep_image(DIR_WS_IMAGES . 'header_checkout.gif', HEADER_TITLE_CHECKOUT); ?></a>&nbsp;&nbsp;<a href="<? echo tep_href_link(FILENAME_CONTACT_US, '', 'NONSSL'); ?>"><? echo tep_image(DIR_WS_IMAGES . 'header_contact_us.gif', HEADER_TITLE_CONTACT_US); ?></a>&nbsp;&nbsp;</td>
+    </td>
   </tr>
 </table>
 <table border="0" width="100%" cellspacing="0" cellpadding="0">
   <tr class="headerNavigation" height="19">
-    <td nowrap class="headerNavigation">&nbsp;&nbsp;<a href="<? echo HTTP_SERVER; ?>" class="whitelink"><? echo HEADER_TITLE_TOP; ?></a> : <a href="<? echo tep_href_link(FILENAME_DEFAULT, '', 'NONSSL'); ?>" class="whitelink"><? echo HEADER_TITLE_CATALOG; ?></a>
-<?
+    <td nowrap class="headerNavigation">
+<?php
+  echo '&nbsp;&nbsp;<a href="' . HTTP_SERVER . '" class="whitelink">' . HEADER_TITLE_TOP . '</a> : <a href="' . tep_href_link(FILENAME_DEFAULT, '', 'NONSSL') . '" class="whitelink">' . HEADER_TITLE_CATALOG . '</a>';
+
   if ($cPath) {
-    if (!ereg('_', $cPath)) $cPath_array = array($cPath);
+    if (!ereg('_', $cPath)) {
+      $cPath_array = array($cPath);
+    }
     $cPath_new = '';
-    for($i=0;$i<sizeof($cPath_array);$i++) {
+    for($i=0; $i<sizeof($cPath_array); $i++) {
       if ($cPath_new == '') {
         $cPath_new .= $cPath_array[$i];
       } else {
@@ -42,32 +48,35 @@
   if ($location) {
     echo $location;
   }
-  echo '</td>' . "\n"; ?>
-    <td align="right" nowrap class="headerNavigation"><?
+?>
+    </td>
+    <td align="right" nowrap class="headerNavigation">
+<?php
   if (tep_session_is_registered('customer_id')) {
     echo '<a href="' . tep_href_link(FILENAME_LOGOFF, '', 'NONSSL') . '" class="whitelink">' . HEADER_TITLE_LOGOFF . '</a> &nbsp;|&nbsp; <a href="' . tep_href_link(FILENAME_ACCOUNT, '', 'NONSSL') . '" class="whitelink">' . HEADER_TITLE_MY_ACCOUNT . '</a> &nbsp;|&nbsp; <a href="' . tep_href_link(FILENAME_SHOPPING_CART, '', 'NONSSL') . '" class="whitelink">' . HEADER_TITLE_CART_CONTENTS . '</a> &nbsp;|&nbsp; <a href="' . tep_href_link(FILENAME_CHECKOUT_PAYMENT, '', 'SSL') . '" class="whitelink">' . HEADER_TITLE_CHECKOUT . '</a> &nbsp;|&nbsp; <a href="' . tep_href_link(FILENAME_CONTACT_US, '', 'NONSSL') . '" class="whitelink">' . HEADER_TITLE_CONTACT_US . '</a>&nbsp;&nbsp;';
   } else {
     echo '<a href="' . tep_href_link(FILENAME_LOGIN, '', 'NONSSL') . '" class="whitelink">' . HEADER_TITLE_LOGIN . '</a> &nbsp;|&nbsp; <a href="' . tep_href_link(FILENAME_CREATE_ACCOUNT, '', 'NONSSL') . '" class="whitelink">' . HEADER_TITLE_CREATE_ACCOUNT . '</a> &nbsp;|&nbsp; <a href="' . tep_href_link(FILENAME_SHOPPING_CART, '', 'NONSSL') . '" class="whitelink">' . HEADER_TITLE_CART_CONTENTS . '</a> &nbsp;|&nbsp; <a href="' . tep_href_link(FILENAME_CHECKOUT_PAYMENT, '', 'SSL') . '" class="whitelink">' . HEADER_TITLE_CHECKOUT . '</a> &nbsp;|&nbsp; <a href="' . tep_href_link(FILENAME_CONTACT_US, '', 'NONSSL') . '" class="whitelink">' . HEADER_TITLE_CONTACT_US . '</a>&nbsp;&nbsp;';
-  } ?></td>
+  }
+?>
+    </td>
   </tr>
 </table>
-<?
+<?php
   if ($HTTP_GET_VARS['error_message'] != '') {
 ?>
 <table border="0" width="100%" cellspacing="0" cellpadding="2">
   <tr class="headerError">
-    <td nowrap class="headerError"><? echo $HTTP_GET_VARS['error_message']; ?></td>
+    <td nowrap class="headerError"><?php echo $HTTP_GET_VARS['error_message']; ?></td>
   </tr>
 </table>
 <?
   }
-?>
-<?
+
   if ($HTTP_GET_VARS['info_message'] != '') {
 ?>
 <table border="0" width="100%" cellspacing="0" cellpadding="2">
   <tr class="headerInfo">
-    <td nowrap class="headerInfo"><? echo $HTTP_GET_VARS['info_message']; ?></td>
+    <td nowrap class="headerInfo"><?php echo $HTTP_GET_VARS['info_message']; ?></td>
   </tr>
 </table>
 <?
