f5df8f6f927c0ff41ea6a95888230e5cdf99a4da
dwatkins <dwatkins>
2001-04-15 19:44:49 +0000

    Text updates

--- catalog/catalog/includes/languages/espanol/product_info.php
+++ catalog/catalog/includes/languages/espanol/product_info.php
@@ -10,6 +10,7 @@ define('TEXT_PRODUCT_NOT_FOUND', 'No encontrado!');
 define('TEXT_CURRENT_REVIEWS', 'Comentarios:');
 define('TEXT_MORE_INFORMATION', 'Para obtener mas informacion, visite la <a href="http://%s" target="blank"><u>pagina</u></a> del producto.');
 define('TEXT_DATE_ADDED', 'Este producto esta en nuestro catalogo desde %s.');
+define('TEXT_DATE_AVAILABLE', '<font color="#ff0000">Este producto estara disponible el %s.</font>');
 define('TEXT_ALSO_PURCHASED_PRODUCTS', 'Clientes que compraron este producto, tambien han comprado');
 define('TEXT_PRODUCT_OPTIONS', 'Opciones:');
 define('PLEASE_SELECT', '-- Seleccione --');
