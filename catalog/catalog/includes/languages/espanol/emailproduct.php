d5fec82fe04af399d268e43276a1cf1c1281eea8
dwatkins <dwatkins>
2001-05-07 21:14:38 +0000

    Text updates

--- catalog/catalog/includes/languages/espanol/emailproduct.php
+++ catalog/catalog/includes/languages/espanol/emailproduct.php
@@ -5,17 +5,17 @@ Last Update: 04/24/2001
 Author(s): Marcel Bossert-Schwab (webmaster@wernich.de)
 */
 
-define('NAVBAR_TITLE', 'Email Product');
-define('TOP_BAR_TITLE', 'Email Product');
-define('HEADING_TITLE', 'Tell a friend about this product');
+define('NAVBAR_TITLE', 'Enviar a un Amigo');
+define('TOP_BAR_TITLE', 'Enviar a un Amigo');
+define('HEADING_TITLE', 'Enviar informacion sobre un producto a un amigo');
 
-define('TEXT_EMAILPRODUCT_EMAIL', 'Your Email Address:');
-define('TEXT_EMAILPRODUCT_NAME', 'Your Name:');
-define('TEXT_EMAILPRODUCT_FRIEND_EMAIL', 'Your Friends Email Address:');
-define('TEXT_EMAILPRODUCT_MESSAGE', 'Your Message:');
-define('TEXT_EMAILPRODUCT_TELLAFRIEND', 'Tell your friend about:');
-define('TEXT_EMAILPRODUCT_YOUR_MAIL_ABOUT', 'Your email about');
-define('TEXT_EMAILPRODUCT_HAS_BEEN_SENT', 'has been sent to -');
+define('TEXT_EMAILPRODUCT_EMAIL', 'Tu Direccion eMail:');
+define('TEXT_EMAILPRODUCT_NAME', 'Tu Nombre:');
+define('TEXT_EMAILPRODUCT_FRIEND_EMAIL', 'La Direccion eMail de tu Amigo:');
+define('TEXT_EMAILPRODUCT_MESSAGE', 'Tu Mensaje:');
+define('TEXT_EMAILPRODUCT_TELLAFRIEND', 'Informacion sobre:');
+define('TEXT_EMAILPRODUCT_YOUR_MAIL_ABOUT', 'Tu email sobre ');
+define('TEXT_EMAILPRODUCT_HAS_BEEN_SENT', 'ha sido enviado a  -');
 
 $email_subject = 'Your friend ' . $yourname . ' has recommended this great product from ' . STORE_NAME;
 $email_taf = 'Your friend, ' . $HTTP_POST_VARS['yourname'] . ', thought that you would be interested in the ' . $products_name . ' from ' . STORE_NAME . '.' . "\n\n";
@@ -26,6 +26,6 @@ if ($yourmessage != '') {
 $email_taf .= 'To view the product click on the link below or copy and paste the link into your web browser:' . "\n\n" . HTTP_SERVER . DIR_WS_CATALOG . FILENAME_PRODUCT_INFO . '?products_id=' . $products_id . "\n\n";
 $email_taf .= 'Regards' . "\n" . STORE_NAME . "\n" . HTTP_SERVER . DIR_WS_CATALOG . "\n";
 
-define('IMAGE_PROCESS', 'Process');
-define('IMAGE_BACK', 'Back');
+define('IMAGE_PROCESS', 'Procesar');
+define('IMAGE_BACK', 'Volver');
 ?>
--- catalog/catalog/includes/languages/espanol/login.php
+++ catalog/catalog/includes/languages/espanol/login.php
@@ -6,10 +6,10 @@ Author(s): David Garcia Watkins (dgw@q-logic.org)
 */
 
 if ($HTTP_GET_VARS['origin'] == 'checkout_payment.php') {
-  define('NAVBAR_TITLE', 'Order');
-  define('TOP_BAR_TITLE', 'Order');
-  define('HEADING_TITLE', 'Ordering online is easy.');
-  define('TEXT_STEP_BY_STEP', 'We\'ll walk you through the process, step by step.');
+  define('NAVBAR_TITLE', 'Realizar Pedido');
+  define('TOP_BAR_TITLE', 'Realizar Pedido');
+  define('HEADING_TITLE', 'Comprar aqui es facil.');
+  define('TEXT_STEP_BY_STEP', 'Te ayudaremos a conseguirlo paso a paso.');
 } else {
   define('NAVBAR_TITLE', 'Entrar');
   define('TOP_BAR_TITLE', 'Entrar en \'' . STORE_NAME . '\'');
@@ -17,13 +17,13 @@ if ($HTTP_GET_VARS['origin'] == 'checkout_payment.php') {
   define('TEXT_STEP_BY_STEP', ''); // should be empty
 }
 
-define('ENTRY_EMAIL_ADDRESS2', 'Enter your e-mail address:');
-define('TEXT_NEW_CUSTOMER', 'I am a new customer.');
-define('TEXT_RETURNING_CUSTOMER', 'I am a returning customer,<br>&nbsp;and my password is:');
+define('ENTRY_EMAIL_ADDRESS2', 'Escriba su direccion eMail');
+define('TEXT_NEW_CUSTOMER', 'Soy un nuevo cliente.');
+define('TEXT_RETURNING_CUSTOMER', 'Ya he comprado otras veces,<br>&nbsp; y my contraseña es:');
 define('TEXT_COOKIE', '¿Guardar informacion en un \'cookie\'?');
 define('TEXT_PASSWORD_FORGOTTEN', '¿Ha olvidado su contraseña? Siga este enlace y se la enviamos.');
 define('TEXT_LOGIN_ERROR', '<font color="#ff0000"><b>ERROR:</b></font> Ese \'E-Mail\' y/o \'Contraseña\' no figuran en nuestros datos.');
-define('TEXT_LOGIN_ERROR_EMAIL', '<font color="#ff0000"><b>ERROR:</b></font> You\'r \'E-Mail Address\' was not found on our database, please use your \'Password\' for login.');
+define('TEXT_LOGIN_ERROR_EMAIL', '<font color="#ff0000"><b>ERROR:</b></font> Ese \'E-Mail\' no figura en nuestros datos, use su \'contraseña\' para entrar.');
 
-define('IMAGE_NEXT', 'Next');
+define('IMAGE_NEXT', 'Continuar');
 ?>
