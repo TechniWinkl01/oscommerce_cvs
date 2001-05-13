191b15bd7b80be088607f8993bb4d0342f9d0569
dwatkins <dwatkins>
2001-05-13 21:02:44 +0000

    UPS configuration moved to module configuration

--- catalog/catalog/includes/modules/shipping/ups.php
+++ catalog/catalog/includes/modules/shipping/ups.php
@@ -43,10 +43,10 @@
         $country_post = str_replace(' ', '', $address_values['postcode']);
         // $rate->dest($address_values['postcode'], STORE_ORIGIN_COUNTRY);      // Use ISO country codes!
         $rate->dest($country_post, $country_name['countries_iso_code_2']);      // Use ISO country codes!
-        $rate->rate(SHIPPING_UPS_PICKUP);        // See the rate() function for codes
-        $rate->container(SHIPPING_UPS_PACKAGE);    // See the container() function for codes
+        $rate->rate(MODULE_SHIPPING_UPS_PICKUP);        // See the rate() function for codes
+        $rate->container(MODULE_SHIPPING_UPS_PACKAGE);    // See the container() function for codes
         $rate->weight($shipping_weight);
-        $rate->rescom(SHIPPING_UPS_RES);    // See the rescom() function for codes
+        $rate->rescom(MODULE_SHIPPING_UPS_RES);    // See the rescom() function for codes
         $quote = $rate->getQuote();
         $shipping_ups_cost = SHIPPING_HANDLING + $quote;
         $shipping_ups_method = 'UPS ' . $prod . ' ' . $shipping_num_boxes . ' X ' . $shipping_weight;
@@ -113,14 +113,20 @@
 
     function install() {
       tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Enable UPS Shipping', 'MODULE_SHIPPING_UPS_STATUS', '1', 'Do you want to offer UPS shipping?', '6', '9', now())");
+	  tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('UPS Pickup Method', 'MODULE_SHIPPING_UPS_PICKUP', 'CC', 'How do you give packages to UPS? CC - Customer Counter, RDP - Daily Pickup, OTP - One Time Pickup, LC - Letter Center, OCA - On Call Air', '6', '10', now())");
+	  tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('UPS Packaging?', 'MODULE_SHIPPING_UPS_PACKAGE', 'CP', 'CP - Your Packaging, ULE - UPS Letter, UT - UPS Tube, UBE - UPS Express Box', '6', '11', now())");
+	  tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Residential Delivery?', 'MODULE_SHIPPING_UPS_RES', 'RES', 'Quote for Residential (RES) or Commercial Delivery (COM)', '6', '12', now())");
     }
 
     function remove() {
       tep_db_query("delete from configuration where configuration_key = 'MODULE_SHIPPING_UPS_STATUS'");
+      tep_db_query("delete from configuration where configuration_key = 'MODULE_SHIPPING_UPS_PICKUP'");
+      tep_db_query("delete from configuration where configuration_key = 'MODULE_SHIPPING_UPS_PACKAGE'");
+      tep_db_query("delete from configuration where configuration_key = 'MODULE_SHIPPING_UPS_RES'");
     }
 
     function keys() {
-      $keys = array('MODULE_SHIPPING_UPS_STATUS');
+      $keys = array('MODULE_SHIPPING_UPS_STATUS', 'MODULE_SHIPPING_UPS_PICKUP', 'MODULE_SHIPPING_UPS_PACKAGE', 'MODULE_SHIPPING_UPS_RES');
 
       return $keys;
     }
