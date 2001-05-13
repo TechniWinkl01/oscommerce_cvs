18fc63c7604ab45a719949708b96033188681bc2
hpdl <hpdl>
2001-05-13 19:02:12 +0000

    added new files for the new module administrative interface
    remove redundant files

---
+++ admin/admin/includes/classes/module_info.php
@@ -0,0 +1,19 @@
+<?
+  class moduleInfo {
+    var $code, $description, $keys;
+
+// class constructor
+    function moduleInfo($mInfo_array) {
+      $this->code = $mInfo_array['code'];
+
+      for ($i=0; $i<sizeof($mInfo_array)-1; $i++) { // minus 1 due to 'code'
+        $key_value_query = tep_db_query("select configuration_title, configuration_value, configuration_description from configuration where configuration_key = '" . $mInfo_array[$i] . "'");
+        $key_value = tep_db_fetch_array($key_value_query);
+
+        $this->keys[$mInfo_array[$i]]['title'] = $key_value['configuration_title'];
+        $this->keys[$mInfo_array[$i]]['value'] = $key_value['configuration_value'];
+        $this->keys[$mInfo_array[$i]]['description'] = $key_value['configuration_description'];
+      }
+    }
+  }
+?>
--- admin/admin/modules.php
+++ admin/admin/modules.php
@@ -1,23 +1,35 @@
-<? include('includes/application_top.php'); ?>
 <?
+  require('includes/application_top.php');
+
+  switch ($HTTP_GET_VARS['set']) {
+    case 'payment'  : $module_directory = DIR_FS_PAYMENT_MODULES;
+                      $module_key = 'MODULE_PAYMENT_INSTALLED';
+                      $heading_title = HEADING_TITLE_PAYMENT_MODULES;
+                      break;
+    case 'shipping' : $module_directory = DIR_FS_SHIPPING_MODULES;
+                      $module_key = 'MODULE_SHIPPING_INSTALLED';
+                      $heading_title = HEADING_TITLE_SHIPPING_MODULES;
+                      break;
+  }
+
   if ($HTTP_GET_VARS['action']) {
     switch ($HTTP_GET_VARS['action']) {
       case 'save' :    while (list($key, $value) = each($HTTP_POST_VARS['configuration'])) {
                          tep_db_query("update configuration set configuration_value = '" . $value . "' where configuration_key = '" . $key . "'");
                        }
-                       header('Location: ' . tep_href_link(FILENAME_PAYMENT_MODULES, 'info=' . $HTTP_GET_VARS['info'], 'NONSSL')); tep_exit();
+                       header('Location: ' . tep_href_link(FILENAME_MODULES, 'set=' . $HTTP_GET_VARS['set'] . '&info=' . $HTTP_GET_VARS['info'], 'NONSSL')); tep_exit();
                        break;
-      case 'install' : include(DIR_FS_PAYMENT_MODULES . $HTTP_GET_VARS['module']);
+      case 'install' : include($module_directory . $HTTP_GET_VARS['module']);
                        $class = substr($HTTP_GET_VARS['module'], 0, strrpos($HTTP_GET_VARS['module'], '.'));
-                       $payment_module = new $class;
-                       $payment_module->install();
-                       header('Location: ' . tep_href_link(FILENAME_PAYMENT_MODULES, 'info=' . substr($HTTP_GET_VARS['module'], 0, strrpos($HTTP_GET_VARS['module'], '.')), 'NONSSL')); tep_exit();
+                       $module = new $class;
+                       $module->install();
+                       header('Location: ' . tep_href_link(FILENAME_MODULES, 'set=' . $HTTP_GET_VARS['set'] . '&info=' . substr($HTTP_GET_VARS['module'], 0, strrpos($HTTP_GET_VARS['module'], '.')), 'NONSSL')); tep_exit();
                        break;
-      case 'remove' :  include(DIR_FS_PAYMENT_MODULES . $HTTP_GET_VARS['module']);
+      case 'remove' :  include($module_directory . $HTTP_GET_VARS['module']);
                        $class = substr($HTTP_GET_VARS['module'], 0, strrpos($HTTP_GET_VARS['module'], '.'));
-                       $payment_module = new $class;
-                       $payment_module->remove();
-                       header('Location: ' . tep_href_link(FILENAME_PAYMENT_MODULES, 'info=' . substr($HTTP_GET_VARS['module'], 0, strrpos($HTTP_GET_VARS['module'], '.')), 'NONSSL')); tep_exit();
+                       $module = new $class;
+                       $module->remove();
+                       header('Location: ' . tep_href_link(FILENAME_MODULES, 'set=' . $HTTP_GET_VARS['set'] . '&info=' . substr($HTTP_GET_VARS['module'], 0, strrpos($HTTP_GET_VARS['module'], '.')), 'NONSSL')); tep_exit();
                        break;
     }
   }
@@ -56,7 +68,7 @@
       <tr>
         <td width="100%"><table border="0" width="100%" cellspacing="0" cellpadding="0">
           <tr>
-            <td nowrap><font face="<? echo HEADING_FONT_FACE; ?>" size="<? echo HEADING_FONT_SIZE; ?>" color="<? echo HEADING_FONT_COLOR; ?>">&nbsp;<? echo HEADING_TITLE; ?>&nbsp;</font></td>
+            <td nowrap><font face="<? echo HEADING_FONT_FACE; ?>" size="<? echo HEADING_FONT_SIZE; ?>" color="<? echo HEADING_FONT_COLOR; ?>">&nbsp;<? echo $heading_title; ?>&nbsp;</font></td>
             <td align="right" nowrap>&nbsp;<? echo tep_image(DIR_WS_CATALOG . 'images/pixel_trans.gif', HEADING_IMAGE_WIDTH, HEADING_IMAGE_HEIGHT, '0', ''); ?>&nbsp;</td>
           </tr>
         </table></td>
@@ -78,7 +90,7 @@
               </tr>
 <?
   $installed_modules = '';
-  $dir = opendir(DIR_FS_PAYMENT_MODULES);
+  $dir = opendir($module_directory);
 
   if ($dir) {
     while ($file = readdir($dir)) {
@@ -86,82 +98,81 @@
         $directory_array[] = $file;
       }
     }
-    sort($directory_array); 
+    sort($directory_array);
 
     for ($files=0; $files<sizeof($directory_array); $files++) {
       $entry = $directory_array[$files];
 
       $check = 0;
-      include(DIR_FS_PAYMENT_MODULES . $entry);
+      include($module_directory . $entry);
       $class = substr($entry, 0, strrpos($entry, '.'));
-      $payment_module = new $class;
-      $check = $payment_module->check();
-      if ($check > 1) {
+      $module = new $class;
+      $check = $module->check();
+      if ($check == '1') {
         $installed_modules .= ($installed_modules) ? ';' . $entry : $entry;
       }
-      if ($check) {
-        if (((!$HTTP_GET_VARS['info']) || (@$HTTP_GET_VARS['info'] == $class)) && (!$pmInfo)) {
-          $payment_module_info = array('payment_code' => $payment_module->payment_code);
-          $pmInfo_array = tep_array_merge($payment_module_info, $payment_module->keys());
-          $pmInfo = new paymentModuleInfo($pmInfo_array);
-        }
 
-        if ($class == $pmInfo->payment_code) {
-          echo '              <tr bgcolor="#b0c8df">' . "\n";
-        } else {
-          echo '              <tr bgcolor="#d8e1eb" onmouseover="this.style.background=\'#cc9999\';this.style.cursor=\'hand\'" onmouseout="this.style.background=\'#d8e1eb\'" onclick="document.location.href=\'' . tep_href_link(FILENAME_PAYMENT_MODULES, tep_get_all_get_params(array('info', 'action')) . 'info=' . $class, 'NONSSL') . '\'">' . "\n";
-        }
+      if (((!$HTTP_GET_VARS['info']) || (@$HTTP_GET_VARS['info'] == $class)) && (!$mInfo)) {
+        $module_info = array('code' => $module->code);
+        $mInfo_array = tep_array_merge($module_info, $module->keys());
+        $mInfo = new moduleInfo($mInfo_array);
+      }
+
+      if ($class == $mInfo->code) {
+        echo '              <tr bgcolor="#b0c8df">' . "\n";
+      } else {
+        echo '              <tr bgcolor="#d8e1eb" onmouseover="this.style.background=\'#cc9999\';this.style.cursor=\'hand\'" onmouseout="this.style.background=\'#d8e1eb\'" onclick="document.location.href=\'' . tep_href_link(FILENAME_MODULES, tep_get_all_get_params(array('info', 'action')) . 'info=' . $class, 'NONSSL') . '\'">' . "\n";
+      }
 ?>
                 <td nowrap><font face="<? echo SMALL_TEXT_FONT_FACE; ?>" size="<? echo SMALL_TEXT_FONT_SIZE; ?>" color="<? echo SMALL_TEXT_FONT_COLOR; ?>">&nbsp;<? echo $entry; ?>&nbsp;</font></td>
                 <td align="right" nowrap><font face="<? echo SMALL_TEXT_FONT_FACE; ?>" size="<? echo SMALL_TEXT_FONT_SIZE; ?>" color="<? echo SMALL_TEXT_FONT_COLOR; ?>">&nbsp;
 <?
-        if ($check != '1') {
-          echo tep_image(DIR_WS_IMAGES . 'icon_status_green.gif', '10', '10', '0', 'Active') . '&nbsp;&nbsp;<a href="' . tep_href_link(FILENAME_PAYMENT_MODULES, tep_get_all_get_params(array('action', 'module')) . 'action=remove&module=' . $entry, 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'icon_status_red_light.gif', '10', '10', '0', 'Set Inactive') . '</a>';
-        } else {
-          echo '<a href="' . tep_href_link(FILENAME_PAYMENT_MODULES, tep_get_all_get_params(array('action', 'module')) . 'action=install&module=' . $entry, 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'icon_status_green_light.gif', '10', '10', '0', 'Set Active') . '</a>&nbsp;&nbsp;' . tep_image(DIR_WS_IMAGES . 'icon_status_red.gif', '10', '10', '0', 'Inactive');
-        }
+      if ($module->check() == '1') {
+        echo tep_image(DIR_WS_IMAGES . 'icon_status_green.gif', '10', '10', '0', 'Active') . '&nbsp;&nbsp;<a href="' . tep_href_link(FILENAME_MODULES, tep_get_all_get_params(array('action', 'module')) . 'action=remove&module=' . $entry, 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'icon_status_red_light.gif', '10', '10', '0', 'Set Inactive') . '</a>';
+      } else {
+        echo '<a href="' . tep_href_link(FILENAME_MODULES, tep_get_all_get_params(array('action', 'module')) . 'action=install&module=' . $entry, 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'icon_status_green_light.gif', '10', '10', '0', 'Set Active') . '</a>&nbsp;&nbsp;' . tep_image(DIR_WS_IMAGES . 'icon_status_red.gif', '10', '10', '0', 'Inactive');
+      }
 ?>&nbsp;</font></td>
 <?
-        if ($class == $pmInfo->payment_code) {
+      if ($class == $mInfo->code) {
 ?>
                     <td align="right" nowrap><font face="<? echo SMALL_TEXT_FONT_FACE; ?>" size="<? echo SMALL_TEXT_FONT_SIZE; ?>" color="<? echo SMALL_TEXT_FONT_COLOR; ?>">&nbsp;<? echo tep_image(DIR_WS_IMAGES . 'icon_arrow_right.gif', 13, 13, 0, ''); ?>&nbsp;</font></td>
 <?
-        } else {
+      } else {
 ?>
-                    <td align="right" nowrap><font face="<? echo SMALL_TEXT_FONT_FACE; ?>" size="<? echo SMALL_TEXT_FONT_SIZE; ?>" color="<? echo SMALL_TEXT_FONT_COLOR; ?>">&nbsp;<? echo '<a href="' . tep_href_link(FILENAME_PAYMENT_MODULES, tep_get_all_get_params(array('info', 'action')) . 'info=' . $class, 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'icon_info.gif', '13', '13', '0', IMAGE_ICON_INFO) . '</a>'; ?>&nbsp;</font></td>
+                    <td align="right" nowrap><font face="<? echo SMALL_TEXT_FONT_FACE; ?>" size="<? echo SMALL_TEXT_FONT_SIZE; ?>" color="<? echo SMALL_TEXT_FONT_COLOR; ?>">&nbsp;<? echo '<a href="' . tep_href_link(FILENAME_MODULES, tep_get_all_get_params(array('info', 'action')) . 'info=' . $class, 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'icon_info.gif', '13', '13', '0', IMAGE_ICON_INFO) . '</a>'; ?>&nbsp;</font></td>
 <?
-        }
+      }
 ?>
 
                 <td></td>
               </tr>
 <?
-      }
     }
   }
   closedir($dir);
 
-  $check = tep_db_query("select configuration_value from configuration where configuration_key = 'PAYMENT_MODULES'");
-  if (tep_db_num_rows($check) > 0) {
+  $check = tep_db_query("select configuration_value from configuration where configuration_key = '" . $module_key . "'");
+  if (tep_db_num_rows($check)) {
     list($check) = tep_db_fetch_array($check);
     if ($check <> $installed_modules) {
-      tep_db_query("update configuration set configuration_value = '" . $installed_modules . "' where configuration_key = 'PAYMENT_MODULES'");
+      tep_db_query("update configuration set configuration_value = '" . $installed_modules . "' where configuration_key = '" . $module_key . "'");
     }
   } else {
-    tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Installed Payment Modules', 'PAYMENT_MODULES', '" . $installed_modules . "', 'This is automatically updated. No need to edit.', '6', '0', now())");
+    tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Installed Modules', '" . $module_key . "', '" . $installed_modules . "', 'This is automatically updated. No need to edit.', '6', '0', now())");
   }
 ?>
               <tr>
                 <td colspan="3"><? echo tep_black_line(); ?></td>
               </tr>
               <tr>
-                <td colspan="3"><font face="<? echo SMALL_TEXT_FONT_FACE; ?>" size="<? echo SMALL_TEXT_FONT_SIZE; ?>" color="<? echo SMALL_TEXT_FONT_COLOR; ?>">Module Directory: <? echo DIR_FS_PAYMENT_MODULES; ?></font></td>
+                <td colspan="3"><font face="<? echo SMALL_TEXT_FONT_FACE; ?>" size="<? echo SMALL_TEXT_FONT_SIZE; ?>" color="<? echo SMALL_TEXT_FONT_COLOR; ?>">Module Directory: <? echo $module_directory; ?></font></td>
               </tr>
             </table></td>
             <td width="25%" valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="2">
 <?
   $info_box_contents = array();
-  if ($pmInfo) $info_box_contents[] = array('align' => 'left', 'text' => '&nbsp;<b>' . $pmInfo->payment_code . '</b>&nbsp;');
+  if ($mInfo) $info_box_contents[] = array('align' => 'left', 'text' => '&nbsp;<b>' . $mInfo->code . '</b>&nbsp;');
 ?>
               <tr bgcolor="#81a2b6">
                 <td>
@@ -174,28 +185,28 @@
 <?
   if ($HTTP_GET_VARS['action'] == 'edit') {
     $keys = '';
-    reset($pmInfo->keys);
-    while (list($key, $value) = each($pmInfo->keys)) {
+    reset($mInfo->keys);
+    while (list($key, $value) = each($mInfo->keys)) {
       $keys .= '<b>' . $value['title'] . '</b><br>' . $value['description'] . '<br><input type="text" name="configuration[' . $key . ']" value="' . $value['value'] . '"><br><br>';
     }
 
-    $form = '<form name="payment_module" action="' . tep_href_link(FILENAME_PAYMENT_MODULES, tep_get_all_get_params(array('action')) . 'action=save&info=' . $HTTP_GET_VARS['info'], 'NONSSL') . '" method="post">' . "\n";
+    $form = '<form name="module" action="' . tep_href_link(FILENAME_MODULES, tep_get_all_get_params(array('action')) . 'action=save&info=' . $HTTP_GET_VARS['info'], 'NONSSL') . '" method="post">' . "\n";
 
     $info_box_contents = array();
     $info_box_contents[] = array('align' => 'left', 'text' => $keys);
-    $info_box_contents[] = array('align' => 'center', 'text' => tep_image_submit(DIR_WS_IMAGES . 'button_update.gif', '66', '20', '0', IMAGE_UPDATE) . '&nbsp;<a href="' . tep_href_link(FILENAME_PAYMENT_MODULES, tep_get_all_get_params(array('action')), 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'button_cancel.gif', '66', '20', '0', IMAGE_CANCEL) . '</a>');
+    $info_box_contents[] = array('align' => 'center', 'text' => tep_image_submit(DIR_WS_IMAGES . 'button_update.gif', '66', '20', '0', IMAGE_UPDATE) . '&nbsp;<a href="' . tep_href_link(FILENAME_MODULES, tep_get_all_get_params(array('action')), 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'button_cancel.gif', '66', '20', '0', IMAGE_CANCEL) . '</a>');
   } else {
     $field_set = 0;
     $keys = '';
-    reset($pmInfo->keys);
-    while (list(, $value) = each($pmInfo->keys)) {
+    reset($mInfo->keys);
+    while (list(, $value) = each($mInfo->keys)) {
       $keys .= '<b>' . $value['title'] . '</b><br>' . $value['value'] . '<br><br>';
       if ( ($value['title'] != '') && ($value['value'] != '')) $field_set = 1;
     }
     $keys = substr($keys, 0, strrpos($keys, '<br><br>'));
     $info_box_contents = array();
     if ($field_set == '1') {
-      $info_box_contents[] = array('align' => 'center', 'text' => '<a href="' . tep_href_link(FILENAME_PAYMENT_MODULES, tep_get_all_get_params(array('action')) . 'action=edit', 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'button_edit.gif', '66', '20', '0', IMAGE_EDIT) . '</a>');
+      $info_box_contents[] = array('align' => 'center', 'text' => '<a href="' . tep_href_link(FILENAME_MODULES, tep_get_all_get_params(array('action')) . 'action=edit', 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'button_edit.gif', '66', '20', '0', IMAGE_EDIT) . '</a>');
       $info_box_contents[] = array('align' => 'left', 'text' => '<br>' . $keys);
     }
   }
