44943b74e8c8d0d5bb14fcb63f0c57d7905aa54e
mbs <mbs>
2001-05-18 09:12:24 +0000

    update for stock check

--- catalog/catalog/includes/languages/german/shopping_cart.php
+++ catalog/catalog/includes/languages/german/shopping_cart.php
@@ -1,20 +1,23 @@
 <?
 /*
-German Text for The Exchange Project Preview Release 2.0
-Last Update: 04/05/2001
-Author(s): Mathias Kowalkowski (m.kowalkowski@comnet-gmbh.net)
+German Text for The Exchange Project Preview Release 2.2
+Last Update: 18/05/2001
+Author(s): Marcel Bossert-Schwab (webmaster@wernich.de)
 */
 
 define('NAVBAR_TITLE', 'Warenkorb');
 define('TOP_BAR_TITLE', 'Warenkorb');
 define('HEADING_TITLE', 'Ihr Warenkorb enth&auml;t :');
 define('TABLE_HEADING_QUANTITY', 'Anzahl');
-define('TABLE_HEADING_MODEL', 'Modell');
+define('TABLE_HEADING_MODEL', 'Artikelnr.');
 define('TABLE_HEADING_PRODUCTS', 'Artikel');
 define('TABLE_HEADING_TOTAL', 'Summe');
 define('TEXT_CART_EMPTY', 'Sie haben noch nichts in Ihrem Warenkorb.');
 define('SUB_TITLE_SUB_TOTAL', 'Zwischensumme:');
 define('SUB_TITLE_TOTAL', 'Summe:');
+
+define('OUT_OF_STOCK_CANT_CHECKOUT', 'Die markierten Produkte mit \'***\', sind leider nicht in Ihrer gew&uuml;nschten Menge auf Lager.<br>Bitte reduzieren Sie Ihre Bestellmenge für die gekennzeichneten Produkten, vielen Dank');
+define('OUT_OF_STOCK_CAN_CHECKOUT', 'Die markierten Produkte mit \'***\', sind leider nicht in Ihrer gew&uuml;nschten Menge auf Lager.<br>Die bestellte Menge wird kurzfristig von uns geliefert, wenn Sie w&uuml;nschen nehmen wir auch eine Teillieferung vor.');
 
 define('IMAGE_CHECKOUT', 'Kasse');
 define('IMAGE_REMOVE_ALL', 'Inhalt l&ouml;schen');
