e4a35fe43a10ba58c7ab4248d23c37fddb746927
mbs <mbs>
2001-05-26 16:01:19 +0000

    fixing image description.

--- catalog/catalog/includes/boxes/tell_a_friend.php
+++ catalog/catalog/includes/boxes/tell_a_friend.php
@@ -11,7 +11,7 @@
   $info_box_contents = array();
   $info_box_contents[] = array('form' => '<form name="tell_a_friend" method="post" action="' . tep_href_link(FILENAME_EMAILPRODUCT, 'products_id=' . $HTTP_GET_VARS['products_id'], 'NONSSL') . '">',
                                'align' => 'left',
-                               'text'  => '<div align="center"><input type="text" name="send_to" size="10">&nbsp;' . tep_image_submit('button_tell_a_friend.gif', BOX_HEADING_SEARCH) . '</div>' . BOX_TELL_A_FRIEND_TEXT
+                               'text'  => '<div align="center"><input type="text" name="send_to" size="10">&nbsp;' . tep_image_submit('button_tell_a_friend.gif', BOX_HEADING_TELL_A_FRIEND) . '</div>' . BOX_TELL_A_FRIEND_TEXT
                               );
   new infoBox($info_box_contents);
 ?>
