895a98aaef44a6276d87b0ca1f305c3bd39e6057
mbs <mbs>
2001-05-26 17:37:11 +0000

    language update.

--- catalog/catalog/includes/languages/german/product_info.php
+++ catalog/catalog/includes/languages/german/product_info.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: product_info.php,v 1.9 2001/05/26 16:49:39 hpdl Exp $
+  $Id: product_info.php,v 1.10 2001/05/26 17:37:11 mbs Exp $
 
   The Exchange Project - Community Made Shopping!
   http://www.theexchangeproject.org
@@ -15,7 +15,7 @@ define('TEXT_PRODUCT_NOT_FOUND', 'Produkt nicht gefunden!');
 define('TEXT_CURRENT_REVIEWS', 'Aktuelle Meinungen:');
 define('TEXT_MORE_INFORMATION', 'F&uuml;r weitere Informationen, besuchen Sie bitte die <a href="http://%s" target="blank"><u>webpage</u></a> zu diesem Produkt.');
 define('TEXT_DATE_ADDED', 'Dieses Produkt wurde am %s in unserem Katalog aufgenommen.');
-define('TEXT_DATE_AVAILABLE', '<font color="#ff0000">This product will be in stock on %s.</font>');
+define('TEXT_DATE_AVAILABLE', '<font color="#ff0000">Dieser Artikel wird voraussichtlich ab dem %s wieder vorrätig sein.</font>');
 define('TEXT_ALSO_PURCHASED_PRODUCTS', 'Kunden, die diesen Artikel kauften, haben auch die folgende(n) Artikel gekauft:');
 define('TEXT_PRODUCT_OPTIONS', 'Optionen:');
 define('PLEASE_SELECT', 'Bitte w&auml;hlen');
