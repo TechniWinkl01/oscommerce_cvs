29ebdfdeb9fb5994793aa99c34c95cdc78c037a0
dwatkins <dwatkins>
2001-06-11 09:57:44 +0000

    I commented these changes in the previous log (sorry).
    Here I have removed some unused constants.
    
    Changed Haralds address for EMAIL_FROM (will be defined in application_top.php)

--- admin/admin/includes/languages/english/mail.php
+++ admin/admin/includes/languages/english/mail.php
@@ -1,18 +1,7 @@
 <?
 define('TOP_BAR_TITLE', 'Mail');
-if ($HTTP_GET_VARS['action'] == 'sendNewsletter') {
-  define('HEADING_TITLE', 'Send newsletter');
-  define('SUB_BAR_TITLE', 'Send newsletter with your latest products');
-} elseif ($HTTP_GET_VARS['action'] == 'email_user') {
-  define('HEADING_TITLE', 'Send eMail to customer(s)');
-  define('SUB_BAR_TITLE', 'Select a customer, newsletter subscribers or all customers to send a eMail.');
-} elseif ($HTTP_GET_VARS['action'] == 'send_email_to_user') {
-  define('HEADING_TITLE', 'eMail was sent');
-  define('SUB_BAR_TITLE', 'Well, your eMail was sent!');
-} else {
-  define('HEADING_TITLE', 'Mail');
-  define('SUB_BAR_TITLE', 'eMail functions');
-}
+define('HEADING_TITLE', 'Send eMail to customer(s)');
+define('SUB_BAR_TITLE', 'Select a customer, newsletter subscribers or all customers to send a eMail.');
 
 define('TEXT_CUSTOMER_NAME', 'Customer name:');
 define('TEXT_SUBJECT', 'Subject:');
@@ -21,14 +10,10 @@ define('TEXT_SELECTCUSTOMER', 'Select customer');
 define('TEXT_ALLCUSTOMERS', 'All customers');
 define('TEXT_NEWSLETTERCUSTOMERS', 'To all newsletter subscribers');
 define('TEXT_MSGTO', 'Message to');
-define('TEXT_EMAILSSENDED', 'eMail(s) sent');
-define('TEXT_EMAILFROM', 'info@theexchangeproject.org'); // Your default sendername
+define('TEXT_EMAILSENT', 'eMail(s) sent');
+define('TEXT_EMAILFROM', EMAIL_FROM);
 define('TEXT_NO_EMAILS_TO_SEND', 'No eMails to sent.');
 define('TEXT_EMAIL_FROM', 'From eMail:');
 define('TEXT_SEND_EMAIL', 'send eMail');
 define('TEXT_NO_CUSTOMER_SELECTED', 'No customer(s) selected to send mails!');
-
-define('MAIL_FOOTER','================================================================================\nIf you don\'t want to continue receiving our newsletter,\nyou can unsubscribe at ' . HTTP_SERVER . DIR_WS_CATALOG . 'account_edit.php\n================================================================================\n');
-
-define('HEADING_TITLE_SENDMAIL', 'send eMail');
 ?>
--- admin/admin/includes/languages/espanol/mail.php
+++ admin/admin/includes/languages/espanol/mail.php
@@ -1,18 +1,7 @@
 <?
 define('TOP_BAR_TITLE', 'Mail');
-if ($HTTP_GET_VARS['action'] == 'sendNewsletter') {
-  define('HEADING_TITLE', 'Enviar Boletin');
-  define('SUB_BAR_TITLE', 'Enviar Boletin con los ultimos productos');
-} elseif ($HTTP_GET_VARS['action'] == 'email_user') {
-  define('HEADING_TITLE', 'Enviar eMail a cliente(s)');
-  define('SUB_BAR_TITLE', 'Seleccione un cliente, clientes suscritos o todos los clientes para enviarles un eMail.');
-} elseif ($HTTP_GET_VARS['action'] == 'send_email_to_user') {
-  define('HEADING_TITLE', 'eMail enviado');
-  define('SUB_BAR_TITLE', 'El eMail ha sido enviado!');
-} else {
-  define('HEADING_TITLE', 'eMail');
-  define('SUB_BAR_TITLE', 'Funciones de eMail');
-}
+define('HEADING_TITLE', 'Enviar eMail a cliente(s)');
+define('SUB_BAR_TITLE', 'Seleccione un cliente, clientes suscritos o todos los clientes para enviarles un eMail.');
 
 define('TEXT_CUSTOMER_NAME', 'Nombre del Cliente:');
 define('TEXT_SUBJECT', 'Asunto:');
@@ -21,14 +10,10 @@ define('TEXT_SELECTCUSTOMER', 'Seleccionar Cliente');
 define('TEXT_ALLCUSTOMERS', 'Todos los Clientes');
 define('TEXT_NEWSLETTERCUSTOMERS', 'Todos los Suscritos');
 define('TEXT_MSGTO', 'Mensaje para');
-define('TEXT_EMAILSSENDED', 'eMail(s) enviados');
-define('TEXT_EMAILFROM', 'info@theexchangeproject.org'); // Your default sendername
+define('TEXT_EMAILSENT', 'eMail(s) enviados');
+define('TEXT_EMAILFROM', EMAIL_FROM);
 define('TEXT_NO_EMAILS_TO_SEND', 'No hay eMails que enviar.');
 define('TEXT_EMAIL_FROM', 'Desde eMail:');
 define('TEXT_SEND_EMAIL', 'A eMail');
 define('TEXT_NO_CUSTOMER_SELECTED', 'No hay cliente(s) seleccionado(s) para enviar eMail(s)!');
-
-define('MAIL_FOOTER','================================================================================\nSi no desea recibir mas este boletin vaya a ' . HTTP_SERVER . DIR_WS_CATALOG . 'account_edit.php.\n================================================================================\n');
-
-define('HEADING_TITLE_SENDMAIL', 'Enviar eMail');
 ?>
