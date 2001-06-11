32874a8dfd20a13c3d078200f6b9631806a002a5
dwatkins <dwatkins>
2001-06-11 18:58:34 +0000

    I got a bit mixed up with currencies. The currency used here should be the default currency used in the database and in the admin tool.

--- catalog/catalog/includes/modules/shipping/table.php
+++ catalog/catalog/includes/modules/shipping/table.php
@@ -19,12 +19,12 @@
     }
 
     function quote() {
-      global $cart, $currency_rates, $shipping_quote_table, $shipping_quote_all, $shipping_quoted, $shipping_table_cost, $shipping_table_method;
+      global $cart, $shipping_quote_table, $shipping_quote_all, $shipping_quoted, $shipping_table_cost, $shipping_table_method;
 
       if ( ($shipping_quote_table) || ($shipping_quote_all == "1") ) {
         $shipping_quoted = 'table';
 
-        $order_total = $cart->show_total() * $currency_rates[MODULE_SHIPPING_TABLE_CURRENCY];
+        $order_total = $cart->show_total();
         $table_cost = MODULE_SHIPPING_TABLE_COST;
         $high = split("[-,]" , $table_cost);
         $n=1;
@@ -39,7 +39,7 @@
           $n = $n + 3;
           $y = $y + 3;
         }
-        $shipping_table_cost = ($shipping + MODULE_SHIPPING_TABLE_HANDLING) * $currency_rates[MODULE_SHIPPING_TABLE_CURRENCY];
+        $shipping_table_cost = ($shipping + MODULE_SHIPPING_TABLE_HANDLING);
       }
     }
 
@@ -99,18 +99,16 @@
       tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) VALUES ('Enable Table Method', 'MODULE_SHIPPING_TABLE_STATUS', '1', 'Do you want to offer table rate shipping?', '6', '0', now())");
       tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Shipping Table', 'MODULE_SHIPPING_TABLE_COST', '1-25,8.50,25-50,5.50,50-10000,0.00', 'Shipping based on the total cost of items. Example: 1-25,8.50,25-50,5.50,etc.. From 1 to 25 charge 8.50, from 25 to 50 charge 5.50, etc', '6', '0', now())");
       tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Handling Fee', 'MODULE_SHIPPING_TABLE_HANDLING', '5', 'Handling Fee for this shipping method', '6', '0', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Currency Used', 'MODULE_SHIPPING_TABLE_CURRENCY', 'USD', 'Currency used in the above numbers', '6', '0', now())");
     }
 
     function remove() {
       tep_db_query("delete from configuration where configuration_key = 'MODULE_SHIPPING_TABLE_STATUS'");
       tep_db_query("delete from configuration where configuration_key = 'MODULE_SHIPPING_TABLE_COST'");
       tep_db_query("delete from configuration where configuration_key = 'MODULE_SHIPPING_TABLE_HANDLING'");
-      tep_db_query("delete from configuration where configuration_key = 'MODULE_SHIPPING_TABLE_CURRENCY'");
     }
 
     function keys() {
-      $keys = array('MODULE_SHIPPING_TABLE_STATUS', 'MODULE_SHIPPING_TABLE_COST', 'MODULE_SHIPPING_TABLE_HANDLING', 'MODULE_SHIPPING_TABLE_CURRENCY');
+      $keys = array('MODULE_SHIPPING_TABLE_STATUS', 'MODULE_SHIPPING_TABLE_COST', 'MODULE_SHIPPING_TABLE_HANDLING');
       return $keys;
     }
   }
