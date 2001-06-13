c7775628f9cb085bb1a26a9aeea4a252651be1ae
dwatkins <dwatkins>
2001-06-13 21:06:34 +0000

    text updates

--- catalog/catalog/includes/languages/espanol/tell_a_friend.php
+++ catalog/catalog/includes/languages/espanol/tell_a_friend.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: tell_a_friend.php,v 1.3 2001/06/13 15:23:23 hpdl Exp $
+  $Id: tell_a_friend.php,v 1.4 2001/06/13 21:06:34 dwatkins Exp $
 
   The Exchange Project - Community Made Shopping!
   http://www.theexchangeproject.org
@@ -14,19 +14,19 @@ define('NAVBAR_TITLE', 'Enviar a un Amigo');
 define('TOP_BAR_TITLE', 'Enviar a un Amigo');
 define('HEADING_TITLE', 'Enviar informacion sobre \'%s\' un amigo');
 
-define('FORM_TITLE_CUSTOMER_DETAILS', 'Your Details');
-define('FORM_TITLE_FRIEND_DETAILS', 'Your Friend\'s Details');
-define('FORM_TITLE_FRIEND_MESSAGE', 'Your Message');
+define('FORM_TITLE_CUSTOMER_DETAILS', 'Tus Datos');
+define('FORM_TITLE_FRIEND_DETAILS', 'Los Datos De Tu Amigo');
+define('FORM_TITLE_FRIEND_MESSAGE', 'Tu Mensaje');
 
-define('FORM_FIELD_CUSTOMER_NAME', 'Your Name:');
-define('FORM_FIELD_CUSTOMER_EMAIL', 'Your Email Address:');
-define('FORM_FIELD_FRIEND_NAME', 'Your Friend\'s Name:');
-define('FORM_FIELD_FRIEND_EMAIL', 'Your Friend\'s Email Address:');
+define('FORM_FIELD_CUSTOMER_NAME', 'Tu Nombre:');
+define('FORM_FIELD_CUSTOMER_EMAIL', 'Tu Email:');
+define('FORM_FIELD_FRIEND_NAME', 'El Nombre De Tu Amigo:');
+define('FORM_FIELD_FRIEND_EMAIL', 'El Email De Tu Amigo:');
 
-define('TEXT_EMAIL_SUCCESSFUL_SENT', 'Your email about <b>%s</b> has been successfully sent to <b>%s</b>.');
+define('TEXT_EMAIL_SUCCESSFUL_SENT', 'Tu email sobre <b>%s</b> ha sido enviado con exito a <b>%s</b>.');
 
-define('TEXT_EMAIL_SUBJECT', 'Su amigo %s le quiere recomendar este product de %s');
-define('TEXT_EMAIL_INTRO', 'Hola %s!' . "\n\n" . 'Su amigo %s, pensó que estaria interesado en %s de %s.');
-define('TEXT_EMAIL_LINK', 'Para ver el producto use el siguiente enlace:' . "\n\n" . '%s');
+define('TEXT_EMAIL_SUBJECT', 'Tu amigo %s te quiere recomendar "%s"');
+define('TEXT_EMAIL_INTRO', 'Hola %s!' . "\n\n" . 'Tu amigo %s, pensó que estarias interesado en %s de %s.');
+define('TEXT_EMAIL_LINK', 'Para ver el producto usa el siguiente enlace:' . "\n\n" . '%s');
 define('TEXT_EMAIL_SIGNATURE', 'Atentamente,' . "\n\n" . '%s');
 ?>
