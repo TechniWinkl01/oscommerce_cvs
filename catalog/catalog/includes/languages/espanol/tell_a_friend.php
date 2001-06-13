5113dd54c27eeb6ee4b1fbf82bb55532189e9620
hpdl <hpdl>
2001-06-13 15:23:25 +0000

    text updates to tell a friend

--- catalog/catalog/includes/languages/espanol/tell_a_friend.php
+++ catalog/catalog/includes/languages/espanol/tell_a_friend.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: tell_a_friend.php,v 1.2 2001/06/12 22:54:24 hpdl Exp $
+  $Id: tell_a_friend.php,v 1.3 2001/06/13 15:23:23 hpdl Exp $
 
   The Exchange Project - Community Made Shopping!
   http://www.theexchangeproject.org
@@ -18,14 +18,15 @@ define('FORM_TITLE_CUSTOMER_DETAILS', 'Your Details');
 define('FORM_TITLE_FRIEND_DETAILS', 'Your Friend\'s Details');
 define('FORM_TITLE_FRIEND_MESSAGE', 'Your Message');
 
-define('FORM_FIELD_CUSTOMER_EMAIL', 'Your Email Address:');
 define('FORM_FIELD_CUSTOMER_NAME', 'Your Name:');
+define('FORM_FIELD_CUSTOMER_EMAIL', 'Your Email Address:');
+define('FORM_FIELD_FRIEND_NAME', 'Your Friend\'s Name:');
 define('FORM_FIELD_FRIEND_EMAIL', 'Your Friend\'s Email Address:');
 
 define('TEXT_EMAIL_SUCCESSFUL_SENT', 'Your email about <b>%s</b> has been successfully sent to <b>%s</b>.');
 
 define('TEXT_EMAIL_SUBJECT', 'Su amigo %s le quiere recomendar este product de %s');
-define('TEXT_EMAIL_INTRO', 'Su amigo %s, pensó que estaria interesado en %s de %s.');
-define('TEXT_EMAIL_LINK', "Para ver el producto use el siguiente enlace:\n%s");
-define('TEXT_EMAIL_SIGNATURE', "Atentamente, \n%s");
+define('TEXT_EMAIL_INTRO', 'Hola %s!' . "\n\n" . 'Su amigo %s, pensó que estaria interesado en %s de %s.');
+define('TEXT_EMAIL_LINK', 'Para ver el producto use el siguiente enlace:' . "\n\n" . '%s');
+define('TEXT_EMAIL_SIGNATURE', 'Atentamente,' . "\n\n" . '%s');
 ?>
--- catalog/catalog/includes/languages/german/tell_a_friend.php
+++ catalog/catalog/includes/languages/german/tell_a_friend.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: tell_a_friend.php,v 1.3 2001/06/13 07:33:47 mbs Exp $
+  $Id: tell_a_friend.php,v 1.4 2001/06/13 15:23:25 hpdl Exp $
 
   The Exchange Project - Community Made Shopping!
   http://www.theexchangeproject.org
@@ -18,14 +18,15 @@ define('FORM_TITLE_CUSTOMER_DETAILS', 'Ihre Angaben');
 define('FORM_TITLE_FRIEND_DETAILS', 'Angaben Ihres Freundes');
 define('FORM_TITLE_FRIEND_MESSAGE', 'Ihre Nachricht (wird mit der Empfehlung versendet)');
 
+define('FORM_FIELD_CUSTOMER_NAME', 'Ihr Name:');
 define('FORM_FIELD_CUSTOMER_EMAIL', 'Ihre EMail-Adresse:');
-define('FORM_FIELD_CUSTOMER_NAME', 'Ihr Vor- und Nachname:');
+define('FORM_FIELD_FRIEND_NAME', 'Name Ihres Freundes:');
 define('FORM_FIELD_FRIEND_EMAIL', 'EMail-Adresse Ihres Freundes:');
 
-define('TEXT_EMAIL_SUCCESSFUL_SENT', 'Ihre EMail über <b>%s</b> wurde gesendet an <b>%s</b>.');
+define('TEXT_EMAIL_SUCCESSFUL_SENT', 'Ihre Email über <b>%s</b> wurde gesendet an <b>%s</b>.');
 
 define('TEXT_EMAIL_SUBJECT', 'Ihr Freund %s, hat diesen tollen Artikel gefunden, und zwar hier: %s');
-define('TEXT_EMAIL_INTRO', 'Ihr Freund, %s, hat diesen tollen Artikel %s bei %s gefunden.');
-define('TEXT_EMAIL_LINK', "Um das Produkt anzusehen, klicken Sie bitte auf den Link oder kopieren diesen und fügen Sie ihn in die URL-Zeile Ihres Browsers ein:\n%s");
-define('TEXT_EMAIL_SIGNATURE', "Mit freundlichen Grüßen, \n%s");
+define('TEXT_EMAIL_INTRO', 'Hi %s!' . "\n\n" . 'Ihr Freund, %s, hat diesen tollen Artikel %s bei %s gefunden.');
+define('TEXT_EMAIL_LINK', 'Um das Produkt anzusehen, klicken Sie bitte auf den Link oder kopieren diesen und fügen Sie ihn in die URL-Zeile Ihres Browsers ein:' . "\n\n" . '%s');
+define('TEXT_EMAIL_SIGNATURE', 'Mit freundlichen Grüßen,' . "\n\n" . '%s');
 ?>
