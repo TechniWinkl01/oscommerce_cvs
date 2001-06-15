1dbb49e4b370d5d28ecf692afae2831abff47357
dwatkins <dwatkins>
2001-06-15 13:31:57 +0000

    switched to stylesheets.
    I also simplified the path for images.

--- admin/admin/banner_manager.php
+++ admin/admin/banner_manager.php
@@ -20,13 +20,13 @@
 
       $image_location = '';
       if ($banners_image != 'none') {
-        $image_location = (substr(DIR_WS_CATALOG_IMAGES, 0, 1) == '/') ? DIR_FS_DOCUMENT_ROOT . DIR_WS_CATALOG_IMAGES : DIR_FS_DOCUMENT_ROOT . '/' . DIR_WS_CATALOG_IMAGES;
+        $image_location = DIR_FS_DOCUMENT_ROOT . DIR_WS_CATALOG_IMAGES;
         if ($HTTP_POST_VARS['banners_image_target'] != '') {
           $image_location .= $HTTP_POST_VARS['banners_image_target'];
         }
         copy($banners_image, $image_location . $banners_image_name);
       } elseif ($banners_image_local != '') {
-        $image_location = (substr(DIR_WS_CATALOG_IMAGES, 0, 1) == '/') ? DIR_FS_DOCUMENT_ROOT . substr(DIR_WS_CATALOG_IMAGES, 1) . $HTTP_POST_VARS['banners_image_local'] : DIR_FS_DOCUMENT_ROOT . DIR_WS_CATALOG_IMAGES . $HTTP_POST_VARS['banners_image_local'];
+        $image_location = DIR_FS_DOCUMENT_ROOT . DIR_WS_CATALOG_IMAGES . $HTTP_POST_VARS['banners_image_local'];
       }
 
       $db_image_location = ($HTTP_POST_VARS['banners_image_local'] != '') ? 'images/' . $HTTP_POST_VARS['banners_image_local'] : 'images/' . $banners_image_target . $banners_image_name;
@@ -56,7 +56,7 @@ function popupImageWindow(url) {
   <tr>
     <td width="<? echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<? echo BOX_WIDTH; ?>" cellspacing="0" cellpadding="0">
       <tr>
-        <td width="100%"><table border="0" width="100%" cellspacing="0" cellpadding="2">
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
 <!-- left_navigation //-->
 <? $include_file = DIR_WS_INCLUDES . 'column_left.php'; include(DIR_WS_INCLUDES . 'include_once.php'); ?>
 <!-- left_navigation_eof //-->
@@ -66,17 +66,17 @@ function popupImageWindow(url) {
 <!-- body_text //-->
     <td width="100%" valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="0">
       <tr>
-        <td width="100%"><table border="0" width="100%" cellspacing="0" cellpadding="2" class="boxborder">
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="2" class="topBarTitle">
           <tr>
-            <td bgcolor="<? echo TOP_BAR_BACKGROUND_COLOR; ?>" width="100%"><font face="<? echo TOP_BAR_FONT_FACE; ?>" size="<? echo TOP_BAR_FONT_SIZE; ?>" color="<? echo TOP_BAR_FONT_COLOR; ?>">&nbsp;<? echo TOP_BAR_TITLE; ?>&nbsp;</font></td>
+            <td class="topBarTitle">&nbsp;<? echo TOP_BAR_TITLE; ?>&nbsp;</td>
           </tr>
         </table></td>
       </tr>
       <tr>
-        <td width="100%"><table border="0" width="100%" cellspacing="0" cellpadding="0">
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
           <tr>
-            <td><font face="<? echo HEADING_FONT_FACE; ?>" size="<? echo HEADING_FONT_SIZE; ?>" color="<? echo HEADING_FONT_COLOR; ?>">&nbsp;<? echo HEADING_TITLE; ?>&nbsp;</font></td>
-            <td align="right">&nbsp;<? echo tep_image(DIR_WS_CATALOG . 'images/pixel_trans.gif', '', HEADING_IMAGE_WIDTH, HEADING_IMAGE_HEIGHT); ?>&nbsp;</td>
+            <td class="pageHeading">&nbsp;<? echo HEADING_TITLE; ?>&nbsp;</td>
+            <td class="pageHeading" align="right">&nbsp;<? echo tep_image(DIR_WS_CATALOG . 'images/pixel_trans.gif', '', HEADING_IMAGE_WIDTH, HEADING_IMAGE_HEIGHT); ?>&nbsp;</td>
           </tr>
         </table></td>
       </tr>
@@ -89,16 +89,16 @@ function popupImageWindow(url) {
       <tr><form name="new_banner" enctype="multipart/form-data" <? echo 'action="' . tep_href_link(FILENAME_BANNERS_MANAGER, tep_get_all_get_params(array('action', 'pinfo', 'info')) . 'action=insert', 'NONSSL') . '"'; ?> method="post">
         <td><br><table border="0" cellspacing="0" cellpadding="2">
           <tr>
-            <td><font face="<? echo TEXT_FONT_FACE; ?>" size="<? echo TEXT_FONT_SIZE; ?>" color="<? echo TEXT_FONT_COLOR; ?>">&nbsp;<? echo TEXT_BANNERS_TITLE; ?>&nbsp;</font></td>
-            <td><font face="<? echo TEXT_FONT_FACE; ?>" size="<? echo TEXT_FONT_SIZE; ?>" color="<? echo TEXT_FONT_COLOR; ?>">&nbsp;<input type="text" name="banners_title">&nbsp;</font></td>
+            <td class="main">&nbsp;<? echo TEXT_BANNERS_TITLE; ?>&nbsp;</td>
+            <td class="main">&nbsp;<input type="text" name="banners_title">&nbsp;</td>
           </tr>
-	      <tr>
-            <td><font face="<? echo TEXT_FONT_FACE; ?>" size="<? echo TEXT_FONT_SIZE; ?>" color="<? echo TEXT_FONT_COLOR; ?>">&nbsp;<? echo TEXT_BANNERS_URL; ?>&nbsp;</font></td>
-            <td><font face="<? echo TEXT_FONT_FACE; ?>" size="<? echo TEXT_FONT_SIZE; ?>" color="<? echo TEXT_FONT_COLOR; ?>">&nbsp;<input type="text" name="banners_url">&nbsp;</font></td>
+  	      <tr>
+            <td class="main">&nbsp;<? echo TEXT_BANNERS_URL; ?>&nbsp;</td>
+            <td class="main">&nbsp;<input type="text" name="banners_url">&nbsp;</td>
           </tr>
-    	  <tr>
-            <td valign="top"><font face="<? echo TEXT_FONT_FACE; ?>" size="<? echo TEXT_FONT_SIZE; ?>" color="<? echo TEXT_FONT_COLOR; ?>">&nbsp;<? echo TEXT_BANNERS_GROUP; ?>&nbsp;</font></td>
-            <td><font face="<? echo TEXT_FONT_FACE; ?>" size="<? echo TEXT_FONT_SIZE; ?>" color="<? echo TEXT_FONT_COLOR; ?>">
+    	    <tr>
+            <td class="main" valign="top">&nbsp;<? echo TEXT_BANNERS_GROUP; ?>&nbsp;</td>
+            <td class="main">
 <?
     echo '&nbsp;<select name="banners_group">';
     $groups_query = tep_db_query("select distinct banners_group from " . TABLE_BANNERS . " order by banners_group");
@@ -106,15 +106,15 @@ function popupImageWindow(url) {
       echo '<option value="' . $groups['banners_group'] . '">' . $groups['banners_group'] . '</option>';
     }
     echo '</select>' . TEXT_BANNERS_NEW_GROUP;
-?><br>&nbsp;<input type="text" name="new_banners_group">&nbsp;</font></td>
+?><br>&nbsp;<input type="text" name="new_banners_group">&nbsp;</td>
           </tr>
-	      <tr>
-            <td valign="top"><font face="<? echo TEXT_FONT_FACE; ?>" size="<? echo TEXT_FONT_SIZE; ?>" color="<? echo TEXT_FONT_COLOR; ?>">&nbsp;<? echo TEXT_BANNERS_IMAGE; ?>&nbsp;</font></td>
-            <td><font face="<? echo TEXT_FONT_FACE; ?>" size="<? echo TEXT_FONT_SIZE; ?>" color="<? echo TEXT_FONT_COLOR; ?>">&nbsp;<input type="file" name="banners_image"><? echo TEXT_BANNERS_IMAGE_LOCAL; ?><br>&nbsp;<? echo DIR_FS_DOCUMENT_ROOT; if (substr(DIR_WS_CATALOG_IMAGES, 0, 1) == '/') { echo substr(DIR_WS_CATALOG_IMAGES, 1); } else { echo DIR_WS_CATALOG_IMAGES; } ?><input type="text" name="banners_image_local">&nbsp;</font></td>
+	        <tr>
+            <td class="main" valign="top">&nbsp;<? echo TEXT_BANNERS_IMAGE; ?>&nbsp;</td>
+            <td class="main">&nbsp;<input type="file" name="banners_image">&nbsp;<? echo TEXT_BANNERS_IMAGE_LOCAL; ?><br>&nbsp;<? echo DIR_FS_DOCUMENT_ROOT . DIR_WS_CATALOG_IMAGES; ?><input type="text" name="banners_image_local">&nbsp;</td>
           </tr>
-	      <tr>
-            <td><font face="<? echo TEXT_FONT_FACE; ?>" size="<? echo TEXT_FONT_SIZE; ?>" color="<? echo TEXT_FONT_COLOR; ?>">&nbsp;<? echo TEXT_BANNERS_IMAGE_TARGET; ?>&nbsp;</font></td>
-            <td><font face="<? echo TEXT_FONT_FACE; ?>" size="<? echo TEXT_FONT_SIZE; ?>" color="<? echo TEXT_FONT_COLOR; ?>">&nbsp;<? echo DIR_FS_DOCUMENT_ROOT; if (substr(DIR_WS_CATALOG_IMAGES, 0, 1) == '/') { echo substr(DIR_WS_CATALOG_IMAGES, 1); } else { echo DIR_WS_CATALOG_IMAGES; } ?><input type="text" name="banners_image_target">&nbsp;</font></td>
+          <tr>
+            <td class="main">&nbsp;<? echo TEXT_BANNERS_IMAGE_TARGET; ?>&nbsp;</td>
+            <td class="main">&nbsp;<? echo DIR_FS_DOCUMENT_ROOT . DIR_WS_CATALOG_IMAGES; ?><input type="text" name="banners_image_target">&nbsp;</td>
           </tr>
         </table></td>
       </tr>
@@ -124,8 +124,8 @@ function popupImageWindow(url) {
       <tr>
         <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
           <tr>
-            <td><br><font face="<? echo TEXT_FONT_FACE; ?>" size="<? echo TEXT_FONT_SIZE; ?>" color="<? echo TEXT_FONT_COLOR; ?>"><? echo TEXT_BANNERS_INSERT_NOTE; ?></font></td>
-            <td align="right" valign="top"><br><? echo tep_image_submit(DIR_WS_IMAGES . 'button_insert.gif', IMAGE_INSERT) . '&nbsp;&nbsp;&nbsp;<a href="' . tep_href_link(FILENAME_BANNERS_MANAGER, '', 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'button_cancel.gif', IMAGE_CANCEL) . '</a>'; ?></td>
+            <td class="main"><br><? echo TEXT_BANNERS_INSERT_NOTE; ?></td>
+            <td class="main" align="right" valign="top"><br><? echo tep_image_submit(DIR_WS_IMAGES . 'button_insert.gif', IMAGE_INSERT) . '&nbsp;&nbsp;&nbsp;<a href="' . tep_href_link(FILENAME_BANNERS_MANAGER, '', 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'button_cancel.gif', IMAGE_CANCEL) . '</a>'; ?></td>
           </tr>
         </table></td>
       </form></tr>
@@ -138,16 +138,15 @@ function popupImageWindow(url) {
             <td colspan="4"><? echo tep_black_line(); ?></td>
           </tr>
           <tr>
-            <td><font face="<? echo TABLE_HEADING_FONT_FACE; ?>" size="<? echo TABLE_HEADING_FONT_SIZE; ?>" color="<? echo TABLE_HEADING_FONT_COLOR; ?>"><b>&nbsp;<? echo TABLE_HEADING_BANNERS; ?>&nbsp;</b></font></td>
-            <td align="right"><font face="<? echo TABLE_HEADING_FONT_FACE; ?>" size="<? echo TABLE_HEADING_FONT_SIZE; ?>" color="<? echo TABLE_HEADING_FONT_COLOR; ?>"><b>&nbsp;<? echo TABLE_HEADING_GROUPS; ?>&nbsp;</b></font></td>
-            <td align="right"><font face="<? echo TABLE_HEADING_FONT_FACE; ?>" size="<? echo TABLE_HEADING_FONT_SIZE; ?>" color="<? echo TABLE_HEADING_FONT_COLOR; ?>"><b>&nbsp;<? echo TABLE_HEADING_STATISTICS; ?>&nbsp;</b></font></td>
-            <td align="right"><font face="<? echo TABLE_HEADING_FONT_FACE; ?>" size="<? echo TABLE_HEADING_FONT_SIZE; ?>" color="<? echo TABLE_HEADING_FONT_COLOR; ?>"><b>&nbsp;<? echo TABLE_HEADING_STATUS; ?>&nbsp;</b></font></td>
+            <td class="tableHeading">&nbsp;<? echo TABLE_HEADING_BANNERS; ?>&nbsp;</td>
+            <td class="tableHeading" align="right">&nbsp;<? echo TABLE_HEADING_GROUPS; ?>&nbsp;</td>
+            <td class="tableHeading" align="right">&nbsp;<? echo TABLE_HEADING_STATISTICS; ?>&nbsp;</td>
+            <td class="tableHeading" align="right">&nbsp;<? echo TABLE_HEADING_STATUS; ?>&nbsp;</td>
           </tr>
           <tr>
             <td colspan="4"><? echo tep_black_line(); ?></td>
           </tr>
 <?
-    if ($HTTP_GET_VARS['page'] > 1) $rows = $HTTP_GET_VARS['page'] * 20 - 20;
     $banners_query_raw = "select banners_id, banners_title, banners_image, banners_group, status from " . TABLE_BANNERS . " order by banners_title, banners_group";
     $banners_split = new splitPageResults($HTTP_GET_VARS['page'], MAX_DISPLAY_SEARCH_RESULTS, $banners_query_raw, $banners_query_numrows);
     $banners_query = tep_db_query($banners_query_raw);
@@ -155,33 +154,33 @@ function popupImageWindow(url) {
       $info_query = tep_db_query("select sum(banners_shown) as banners_shown, sum(banners_clicked) as banners_clicked from " . TABLE_BANNERS_HISTORY . " where banners_id = '" . $banners['banners_id'] . "'");
       $info = tep_db_fetch_array($info_query);
 
-     $banners_shown = ($info['banners_shown'] != '') ? $info['banners_shown'] : '0';
-     $banners_clicked = ($info['banners_clicked'] != '') ? $info['banners_clicked'] : '0';
+      $banners_shown = ($info['banners_shown'] != '') ? $info['banners_shown'] : '0';
+      $banners_clicked = ($info['banners_clicked'] != '') ? $info['banners_clicked'] : '0';
 ?>
-          <tr bgcolor="#d8e1eb" onmouseover="this.style.background='#cc9999'" onmouseout="this.style.background='#d8e1eb'">
-            <td><font face="<? echo SMALL_TEXT_FONT_FACE; ?>" size="<? echo SMALL_TEXT_FONT_SIZE; ?>" color="<? echo SMALL_TEXT_FONT_COLOR; ?>">&nbsp;<? echo '<a href="javascript:popupImageWindow(\'' . FILENAME_POPUP_IMAGE . '?image=' . DIR_WS_CATALOG . $banners['banners_image'] . '&alt=' . urlencode($banners['banners_title']) . '\')" class="blacklink">' . $banners['banners_title'] . '</a>'; ?>&nbsp;</font></td>
-            <td align="right"><font face="<? echo SMALL_TEXT_FONT_FACE; ?>" size="<? echo SMALL_TEXT_FONT_SIZE; ?>" color="<? echo SMALL_TEXT_FONT_COLOR; ?>">&nbsp;<? echo $banners['banners_group']; ?>&nbsp;</font></td>
-            <td align="right"><font face="<? echo SMALL_TEXT_FONT_FACE; ?>" size="<? echo SMALL_TEXT_FONT_SIZE; ?>" color="<? echo SMALL_TEXT_FONT_COLOR; ?>">&nbsp;<? echo $banners_shown . ' / ' . $banners_clicked; ?>&nbsp;</font></td>
-            <td align="right"><font face="<? echo SMALL_TEXT_FONT_FACE; ?>" size="<? echo SMALL_TEXT_FONT_SIZE; ?>" color="<? echo SMALL_TEXT_FONT_COLOR; ?>">&nbsp;
+          <tr class="tableRow" onmouseover="this.className='tableRowOver'" onmouseout="this.className='tableRow'">
+            <td class="tableData">&nbsp;<? echo '<a href="javascript:popupImageWindow(\'' . FILENAME_POPUP_IMAGE . '?image=' . DIR_WS_CATALOG . $banners['banners_image'] . '&alt=' . urlencode($banners['banners_title']) . '\')" class="blacklink">' . $banners['banners_title'] . '</a>'; ?>&nbsp;</td>
+            <td class="tableData" align="right">&nbsp;<? echo $banners['banners_group']; ?>&nbsp;</td>
+            <td class="tableData" align="right">&nbsp;<? echo $banners_shown . ' / ' . $banners_clicked; ?>&nbsp;</td>
+            <td class="tableData" align="right">
 <?
       if ($banners['status'] == '1') {
         echo tep_image(DIR_WS_IMAGES . 'icon_status_green.gif', 'Active', 10, 10) . '&nbsp;&nbsp;<a href="' . tep_href_link(FILENAME_BANNERS_MANAGER, 'action=setflag&flag=0&id=' . $banners['banners_id'], 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'icon_status_red_light.gif', 'Set Inactive', 10, 10) . '</a>';
       } else {
         echo '<a href="' . tep_href_link(FILENAME_BANNERS_MANAGER, 'action=setflag&flag=1&id=' . $banners['banners_id'], 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'icon_status_green_light.gif', 'Set Active', 10, 10) . '</a>&nbsp;&nbsp;' . tep_image(DIR_WS_IMAGES . 'icon_status_red.gif', 'Inactive', 10, 10);
       }
-?>&nbsp;</font></td>
+?>&nbsp;</td>
           </tr>
 <?
     }
 ?>
           <tr>
-            <td colspan="4"><? echo tep_black_line(); ?></td>
+            <td class="main" colspan="4"><? echo tep_black_line(); ?></td>
           </tr>
           <tr>
             <td colspan="4"><table border="0" width="100%" cellspacing="0" cellpadding="2">
               <tr>
-                <td><font face="<? echo SMALL_TEXT_FONT_FACE; ?>" size="<? echo SMALL_TEXT_FONT_SIZE; ?>" color="<? echo SMALL_TEXT_FONT_COLOR; ?>">&nbsp;<? echo $banners_split->display_count($banners_query_numrows, MAX_DISPLAY_SEARCH_RESULTS, $HTTP_GET_VARS['page'], TEXT_DISPLAY_NUMBER_OF_BANNERS); ?>&nbsp;</font></td>
-                <td align="right"><font face="<? echo SMALL_TEXT_FONT_FACE; ?>" size="<? echo SMALL_TEXT_FONT_SIZE; ?>" color="<? echo SMALL_TEXT_FONT_COLOR; ?>">&nbsp;<? echo TEXT_RESULT_PAGE; ?> <? echo $banners_split->display_links($banners_query_numrows, MAX_DISPLAY_SEARCH_RESULTS, MAX_DISPLAY_PAGE_LINKS, $HTTP_GET_VARS['page']); ?>&nbsp;</font></td>
+                <td class="smallText">&nbsp;<? echo $banners_split->display_count($banners_query_numrows, MAX_DISPLAY_SEARCH_RESULTS, $HTTP_GET_VARS['page'], TEXT_DISPLAY_NUMBER_OF_BANNERS); ?>&nbsp;</td>
+                <td class="smallText" align="right">&nbsp;<? echo TEXT_RESULT_PAGE; ?> <? echo $banners_split->display_links($banners_query_numrows, MAX_DISPLAY_SEARCH_RESULTS, MAX_DISPLAY_PAGE_LINKS, $HTTP_GET_VARS['page']); ?>&nbsp;</td>
               </tr>
               <tr>
                 <td align="right" colspan="2"><? echo '<a href="' . tep_href_link(FILENAME_BANNERS_MANAGER, 'action=new', 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'button_new_banner.gif', IMAGE_NEW_BANNER) . '</a>'; ?></td>
