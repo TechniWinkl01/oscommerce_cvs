4d19b8fdc6aad6d87aa824f48ac320fbd1233b58
mbs <mbs>
2001-07-28 09:02:40 +0000

    some translations to german

--- admin/admin/includes/languages/german/banner_manager.php
+++ admin/admin/includes/languages/german/banner_manager.php
@@ -1,7 +1,7 @@
 <?
 /*
 German Text for The Exchange Project Administration Tool Preview Release 2.2
-Last Update: 20/05/2001
+Last Update: 28/07/2001
 Author(s): Marcel Bossert-Schwab (webmaster@wernich.de)
 */
 
@@ -21,15 +21,15 @@ define('TEXT_BANNERS_NEW_GROUP', ', oder geben Sie unten eine neue Banner-Gruppe
 define('TEXT_BANNERS_IMAGE', 'Bild (Datei):'); 
 define('TEXT_BANNERS_IMAGE_LOCAL', ', oder geben Sie unten die lokale Datei auf Ihrem Server an'); 
 define('TEXT_BANNERS_IMAGE_TARGET', 'Bildziel (Speichern nach):'); 
-define('TEXT_BANNERS_EXPIRES_ON', 'Expires On:');
-define('TEXT_BANNERS_OR_AT', ', or at');
-define('TEXT_BANNERS_IMPRESSIONS', 'impressions/views.');
+define('TEXT_BANNERS_EXPIRES_ON', 'Gültigkeit bis:');
+define('TEXT_BANNERS_OR_AT', ', oder bei');
+define('TEXT_BANNERS_IMPRESSIONS', 'impressionen/anzeigen.');
 define('TEXT_BANNERS_INSERT_NOTE', '<b>Bemerkung:</b><ul><li>Das Bildverzeichnis muss Schreibrecht besitzen!</li><li>F&uuml;llen Sie das Feld \'Bildziel (Speichern nach)\' nicht aus, wenn Sie kein Bild auf Ihren Server kopieren m&ouml;chten (z.B. wenn sich bereits das Bild auf dem Server befindet).</li><li>Das \'Bildziel (Speichern nach)\' Feld muss ein bereits existierendes Verzeichnis mit \'/\' am Ende sein (z.B. banners/).</li></ul>'); 
-define('TEXT_BANNERS_EXPIRCY_NOTE', '<b>Expircy Notes:</b><ul><li>Only one of the two fields should be submitted</li><li>If the banner is not to expire automatically, then leave these fields blank</li></ul>');
+define('TEXT_BANNERS_EXPIRCY_NOTE', '<b>Gültigkeit Bemerkung:</b><ul><li>Nur ein Feld ausfüllen</li><li>Wenn der Banner unbegrenzt angezeigt werden soll, tragen Sie in diesen Feldern nichts ein</li></ul>');
 
-define('TEXT_BANNERS_EXPIRES_AT_DATE', 'Expires At: <b>%s</b>');
-define('TEXT_BANNERS_EXPIRES_AT_IMPRESSIONS', 'Expires At: <b>%s</b> impressions');
-define('TEXT_BANNERS_STATUS_CHANGE', 'Status Change: %s');
+define('TEXT_BANNERS_EXPIRES_AT_DATE', 'Gültigkeit bis zum: <b>%s</b>');
+define('TEXT_BANNERS_EXPIRES_AT_IMPRESSIONS', 'Gültigkeit bis: <b>%s</b> impressionen/anzeigen');
+define('TEXT_BANNERS_STATUS_CHANGE', 'Status geändert: %s');
 
 define('TEXT_INFO_DELETE_INTRO', 'Sind Sie sicher, dass Sie das Banner löschen wollen?');
 ?>
