ce77a336f6f27b82cd7a6d6d29780b58484f63e7
triciab <triciab>
2001-07-30 23:05:28 +0000

    definitions for itransact control panel

---
+++ admin/admin/includes/languages/english/itransact.php
@@ -0,0 +1,9 @@
+<?
+define('TOP_BAR_TITLE', 'iTransact Control Panel');
+define('HEADING_TITLE', 'Open Your Transaction Control Panel');
+define('SUB_BAR_TITLE', 'Click the image below.  Your Gateway ID and Password are required.');
+
+define('CPANEL_URL', 'https://secure.itransact.com/support/login.html');
+define('CPANEL_URL_NAME', 'ctrlpan');
+
+?>
