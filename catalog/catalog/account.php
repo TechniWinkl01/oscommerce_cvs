c5fc58f3b1a59edbff9f1c54473dcade23404be4
dwatkins <dwatkins>
2001-07-31 21:06:11 +0000

    Small bug that wouldnt display the default address if it was different than 0

--- catalog/catalog/account.php
+++ catalog/catalog/account.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: account.php,v 1.36 2001/07/31 17:41:22 dwatkins Exp $
+  $Id: account.php,v 1.37 2001/07/31 21:06:11 dwatkins Exp $
 
   The Exchange Project - Community Made Shopping!
   http://www.theexchangeproject.org
@@ -72,7 +72,7 @@
 <?php
   $is_read_only = true;
 
-  $account_query = tep_db_query("select c.customers_gender, c.customers_firstname, c.customers_lastname, c.customers_dob, c.customers_email_address, a.entry_company, a.entry_street_address, a.entry_suburb, a.entry_postcode, a.entry_city, a.entry_zone_id, a.entry_state, a.entry_country_id, c.customers_telephone, c.customers_fax, c.customers_newsletter from " . TABLE_CUSTOMERS . " c, " . TABLE_ADDRESS_BOOK . " a where c.customers_id = '" . $customer_id . "' and a.customers_id = c.customers_id and a.address_book_id = '" . $customers_default_address_id . "'");
+  $account_query = tep_db_query("select c.customers_gender, c.customers_firstname, c.customers_lastname, c.customers_dob, c.customers_email_address, a.entry_company, a.entry_street_address, a.entry_suburb, a.entry_postcode, a.entry_city, a.entry_zone_id, a.entry_state, a.entry_country_id, c.customers_telephone, c.customers_fax, c.customers_newsletter from " . TABLE_CUSTOMERS . " c, " . TABLE_ADDRESS_BOOK . " a where c.customers_id = '" . $customer_id . "' and a.customers_id = c.customers_id and a.address_book_id = '" . $customer_default_address_id . "'");
   $account = tep_db_fetch_array($account_query);
 
   require(DIR_WS_MODULES . 'account_details.php');
