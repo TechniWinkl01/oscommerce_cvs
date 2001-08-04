c68268db8ddb14efe5590b46a1489d5cf1657cb7
dwatkins <dwatkins>
2001-08-04 19:49:07 +0000

    German transalation

--- admin/admin/includes/languages/german/define_language.php
+++ admin/admin/includes/languages/german/define_language.php
@@ -1,5 +1,5 @@
 <?
-define('TOP_BAR_TITLE', 'Define Language');
-define('HEADING_TITLE', 'Define Language');
-define('SUB_BAR_TITLE', 'Online Language Text Editor');
+define('TOP_BAR_TITLE', 'Sprache definieren');
+define('HEADING_TITLE', 'Sprache definieren');
+define('SUB_BAR_TITLE', 'Online Sprachen Texteditor');
 ?>
