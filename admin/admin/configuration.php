c8137284815758c987c1de0cde90d20d87f1b981
dwatkins <dwatkins>
2001-08-07 16:57:47 +0000

    A couple of finishing touches relating stylesheets

--- admin/admin/configuration.php
+++ admin/admin/configuration.php
@@ -13,7 +13,7 @@
 <link rel="stylesheet" type="text/css" href="includes/stylesheet.css">
 <script language="javascript" src="includes/general.js"></script>
 </head>
-<body onload="SetFocus();" marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0" bgcolor="#FFFFFF">
+<body onload="SetFocus();">
 <!-- header //-->
 <? $include_file = DIR_WS_INCLUDES . 'header.php';  include(DIR_WS_INCLUDES . 'include_once.php'); ?>
 <!-- header_eof //-->
@@ -83,9 +83,9 @@
     }
 
     if ($configuration['cfgID'] == @$cfgInfo->id) {
-      echo '                  <tr bgcolor="#b0c8df">' . "\n";
+      echo '                  <tr class="selectedRow">' . "\n";
     } else {
-      echo '                  <tr bgcolor="#d8e1eb" onmouseover="this.style.background=\'#cc9999\';this.style.cursor=\'hand\'" onmouseout="this.style.background=\'#d8e1eb\'" onclick="document.location.href=\'' . tep_href_link(FILENAME_CONFIGURATION, tep_get_all_get_params(array('info', 'action')) . 'info=' . $configuration['cfgID'], 'NONSSL') . '\'">' . "\n";
+      echo '                  <tr class="tableRow" onmouseover="this.className=\'tableRowOver\';this.style.cursor=\'hand\'" onmouseout="this.className=\'tableRow\'" onclick="document.location.href=\'' . tep_href_link(FILENAME_CONFIGURATION, tep_get_all_get_params(array('info', 'action')) . 'info=' . $configuration['cfgID'], 'NONSSL') . '\'">' . "\n";
     }
 ?>
                 <td class="smallText">&nbsp;<? echo $configuration['cfgTitle']; ?>&nbsp;</td>
@@ -116,12 +116,12 @@
   if ((!$peInfo) && ($HTTP_GET_VARS['action'] == 'new')) $info_box_contents[] = array('align' => 'left', 'text' => '&nbsp;<b>' . TEXT_INFO_HEADING_NEW_PRODUCT . '</b>&nbsp;');
 
 ?>
-              <tr bgcolor="#81a2b6">
+              <tr class="boxHeading">
                 <td>
                   <? new infoBoxHeading($info_box_contents); ?>
                 </td>
               </tr>
-              <tr bgcolor="#81a2b6">
+              <tr class="boxHeading">
                 <td><? echo tep_black_line(); ?></td>
               </tr>
 <?
@@ -146,13 +146,11 @@
     $info_box_contents[] = array('align' => 'left', 'text' => '<br>' . TEXT_INFO_DATE_ADDED . ' ' . tep_date_short($cfgInfo->date_added) . '<br>' . TEXT_INFO_LAST_MODIFIED . ' ' . tep_date_short($cfgInfo->last_modified));
   }
 ?>
-              <tr bgcolor="#b0c8df"><? echo $form; ?>
-                <td>
-                  <? new infoBox($info_box_contents); ?>
-                </td>
+              <tr><? echo $form; ?>
+                <td class="box"><? new infoBox($info_box_contents); ?></td>
               <? if ($form) echo '</form>'; ?></tr>
-              <tr bgcolor="#b0c8df">
-                <td><? echo tep_black_line(); ?></td>
+              <tr>
+                <td class="box"><? echo tep_black_line(); ?></td>
               </tr>
             </table></td>
           </tr>
@@ -167,7 +165,6 @@
 <!-- footer //-->
 <? $include_file = DIR_WS_INCLUDES . 'footer.php';  include(DIR_WS_INCLUDES . 'include_once.php'); ?>
 <!-- footer_eof //-->
-<br>
 </body>
 </html>
 <? $include_file = DIR_WS_INCLUDES . 'application_bottom.php'; include(DIR_WS_INCLUDES . 'include_once.php'); ?>
