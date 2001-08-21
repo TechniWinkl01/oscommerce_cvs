7bde8f04fd999276082fbc4d7c69ab4934b59841
mbs <mbs>
2001-08-21 21:56:43 +0000

    last commit was wrong, here we goes right: the values over page 1 was wrong, fixed now.

--- catalog/catalog/includes/classes/split_page_results.php
+++ catalog/catalog/includes/classes/split_page_results.php
@@ -97,8 +97,8 @@ NOTE: the constructor (also) builds an sql query that counts the total records f
       $to_num = ($max_rows_per_page * $current_page_number);
       if ($to_num > $query_numrows) $to_num = $query_numrows;
       $from_num = ($max_rows_per_page * ($current_page_number - 1));
-      if ($from_num == 0 && $to_num >= 1) {
-        $from_num = 1;
+      if ($to_num == 0) {
+        $from_num = 0;
       } else {
         $from_num++;
       }
