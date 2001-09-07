7953396049edc001397605ea288b5eb4849d66e0
mbs <mbs>
2001-09-07 07:09:24 +0000

    Advanced Search doesn't work correctly in german, this is fixed by the language file now. Thanks to Thomas (harley_vb) for the report.

--- catalog/catalog/includes/languages/german/advanced_search.php
+++ catalog/catalog/includes/languages/german/advanced_search.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: advanced_search.php,v 1.11 2001/05/26 16:49:22 hpdl Exp $
+  $Id: advanced_search.php,v 1.12 2001/09/07 07:09:24 mbs Exp $
 
   The Exchange Project - Community Made Shopping!
   http://www.theexchangeproject.org
@@ -18,7 +18,7 @@ define('ENTRY_CATEGORIES', 'Kategorien:');
 define('ENTRY_INCLUDES_SUBCATEGORIES', 'Unterkategorien mit einbeziehen');
 define('ENTRY_MANUFACTURER', 'Hersteller:');
 define('ENTRY_KEYWORDS', 'Schl&uuml;sselw&ouml;rter:');
-define('ENTRY_PRICE_FROM', 'Preis von:');
+define('ENTRY_PRICE_FROM', 'Preis ab:');
 define('ENTRY_DATE_ADDED_FROM', 'Datum hinzugef&uuml;gt von:');
 define('ENTRY_TO', 'bis:');
 define('ENTRY_SORT_BY' , 'Sortierkriterium f&uuml; die Resultate:');
@@ -33,12 +33,12 @@ define('TEXT_PRICE', 'Preis');
 define('TEXT_PERFORM_ADVANCED_SEARCH', 'Erweiterte Suche starten.');
 define('TEXT_SEARCH_IN_DESCRIPTION', 'Auch in Beschreibungen suchen');
 define('TEXT_ADVANCED_SEARCH_TIPS', '<b>Hilfe zur erweiterten Suche</b><br><br>Die Suchmaschine erm&ouml;glicht die Suche in dem Produktnamen, Produktbeschreibung, Hersteller und Modell.<br><br>Sie haben die M&ouml;glichkeit logische Operatoren wie "AND" (Und) und "OR" (oder) zu verwenden.<br>Als Beispiel also k&ouml;nnten Sie angeben: <u>Microsoft AND Maus</u>.<br>Desweiteren k&ouml;nnen Sie Klammern verwenden um die Suche zu verschachteln, also :<br><u>Microsoft AND (Maus OR Tastatur OR "Visual Basic")</u>.<br>Mit den Anf&uuml;hrungszeichen werden hier mehrere W&ouml;rter zu einem Suchbegriff zusammengefasst.');
-define('JS_AT_LEAST_ONE_INPUT', '* Eines der folgenden Felder muß ausgefüllt werden:\n    Schlüsselw&ouml;rter\n    Datum hinzugefügt von\n    Datum hinzugefügt bis\n    Preis von\n    Preis bis\n');
-define('JS_INVALID_FROM_DATE', '* Unzul&auml;ssiges "Vom Datum"\n');
-define('JS_INVALID_TO_DATE', '* Unzul&auml;ssiges "Bis jetzt"\n');
+define('JS_AT_LEAST_ONE_INPUT', '* Eines der folgenden Felder muß ausgefüllt werden:\n    Schlüsselw&ouml;rter\n    Datum hinzugefügt von\n    Datum hinzugefügt bis\n    Preis ab\n    Preis bis\n');
+define('JS_INVALID_FROM_DATE', '* Unzul&auml;ssiges von Datum\n');
+define('JS_INVALID_TO_DATE', '* Unzul&auml;ssiges bis jetzt\n');
 define('JS_TO_DATE_LESS_THAN_FROM_DATE', '* Mu&szlig; von Datum gr&ouml;sser oder gleich bis jetzt sein\n');
-define('JS_PRICE_FROM_MUST_BE_NUM', '* "Preis von" muß eine Zahl sein\n');
-define('JS_PRICE_TO_MUST_BE_NUM', '* "Preis bis" muß eine Zahl sein\n');
-define('JS_PRICE_TO_LESS_THAN_PRICE_FROM', '* "Preis bis" muß größer oder gleich "Preis von" sein.\n');
+define('JS_PRICE_FROM_MUST_BE_NUM', '* Preis ab, muß eine Zahl sein\n');
+define('JS_PRICE_TO_MUST_BE_NUM', '* Preis bis, muß eine Zahl sein\n');
+define('JS_PRICE_TO_LESS_THAN_PRICE_FROM', '* Preis bis muß größer oder gleich Preis ab sein.\n');
 define('JS_INVALID_KEYWORDS', '* Suchbegriff unzulässig\n');
 ?>
