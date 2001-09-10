96af1e6f0aa18d0a3926a40c9aad37db833cb78a
mbs <mbs>
2001-09-10 07:44:45 +0000

    german translations

--- admin/admin/includes/languages/german/banner_manager.php
+++ admin/admin/includes/languages/german/banner_manager.php
@@ -1,8 +1,13 @@
-<?
+<?php
 /*
-German Text for The Exchange Project Administration Tool Preview Release 2.2
-Last Update: 28/07/2001
-Author(s): Marcel Bossert-Schwab (webmaster@wernich.de)
+  $Id: banner_manager.php,v 1.13 2001/09/10 07:44:45 mbs Exp $
+
+  The Exchange Project - Community Made Shopping!
+  http://www.theexchangeproject.org
+
+  Copyright (c) 2000,2001 The Exchange Project
+
+  Released under the GNU General Public License
 */
 
 define('TOP_BAR_TITLE', 'Banner Manager');
@@ -25,14 +30,14 @@ define('TEXT_BANNERS_HTML_TEXT', 'HTML Text:');
 define('TEXT_BANNERS_EXPIRES_ON', 'Gültigkeit bis:');
 define('TEXT_BANNERS_OR_AT', ', oder bei');
 define('TEXT_BANNERS_IMPRESSIONS', 'impressionen/anzeigen.');
-define('TEXT_BANNERS_SCHEDULED_AT', 'Scheduled At:');
-define('TEXT_BANNERS_BANNER_NOTE', '<b>Banner Notes:</b><ul><li>Use an image or HTML text for the banner - not both.</li><li>HTML Text has priority over an image</li></ul>');
+define('TEXT_BANNERS_SCHEDULED_AT', 'Gültigkeit ab:');
+define('TEXT_BANNERS_BANNER_NOTE', '<b>Banner Bemerkung:</b><ul><li>Sie können Bild- oder HTML-Text-Banner verwenden, beides gleichzeitig ist nicht möglich.</li><li>Wenn Sie beide Bannerarten gleichzeitig verwenden, wird nur der HTML-Text Banner angezeigt.</li></ul>');
 define('TEXT_BANNERS_INSERT_NOTE', '<b>Bemerkung:</b><ul><li>Das Bildverzeichnis muss Schreibrecht besitzen!</li><li>F&uuml;llen Sie das Feld \'Bildziel (Speichern nach)\' nicht aus, wenn Sie kein Bild auf Ihren Server kopieren m&ouml;chten (z.B. wenn sich bereits das Bild auf dem Server befindet).</li><li>Das \'Bildziel (Speichern nach)\' Feld muss ein bereits existierendes Verzeichnis mit \'/\' am Ende sein (z.B. banners/).</li></ul>'); 
 define('TEXT_BANNERS_EXPIRCY_NOTE', '<b>Gültigkeit Bemerkung:</b><ul><li>Nur ein Feld ausfüllen</li><li>Wenn der Banner unbegrenzt angezeigt werden soll, tragen Sie in diesen Feldern nichts ein</li></ul>');
-define('TEXT_BANNERS_SCHEDULE_NOTE', '<b>Schedule Notes:</b><ul><li>If a schedule is set, the banner will be activated on that date.</li><li>All scheduled banners are marked as deactive until their date has arrived, to which they will then be marked active.</li></ul>');
+define('TEXT_BANNERS_SCHEDULE_NOTE', '<b>Gültigkeit ab Bemerkung:</b><ul><li>Bei Verwendung von dieser Funktion, wird der Banner erst ab dem angegeben Datum angezeigt.</li><li>Alle Banner mit dieser Funktion werden bis ihrer Aktivierung, als Deaktiviert angezeigt.</li></ul>');
 
-define('TEXT_BANNERS_DATE_ADDED', 'Date Added:');
-define('TEXT_BANNERS_SCHEDULED_AT_DATE', 'Scheduled At: <b>%s</b>');
+define('TEXT_BANNERS_DATE_ADDED', 'hinzugef&uuml;gt am:');
+define('TEXT_BANNERS_SCHEDULED_AT_DATE', 'Gültigkeit ab: <b>%s</b>');
 define('TEXT_BANNERS_EXPIRES_AT_DATE', 'Gültigkeit bis zum: <b>%s</b>');
 define('TEXT_BANNERS_EXPIRES_AT_IMPRESSIONS', 'Gültigkeit bis: <b>%s</b> impressionen/anzeigen');
 define('TEXT_BANNERS_STATUS_CHANGE', 'Status geändert: %s');
--- admin/admin/includes/languages/german/specials.php
+++ admin/admin/includes/languages/german/specials.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: specials.php,v 1.5 2001/09/09 18:52:45 hpdl Exp $
+  $Id: specials.php,v 1.6 2001/09/10 07:44:45 mbs Exp $
 
   The Exchange Project - Community Made Shopping!
   http://www.theexchangeproject.org
@@ -20,15 +20,15 @@ define('TABLE_HEADING_ACTION', 'Aktion');
 
 define('TEXT_SPECIALS_PRODUCT', 'Produkt:');
 define('TEXT_SPECIALS_SPECIAL_PRICE', 'Angebotpreis:');
-define('TEXT_SPECIALS_EXPIRES_DATE', 'Expiry Date:');
-define('TEXT_SPECIALS_PRICE_TIP', '<b>Specials Notes:</b><ul><li>You can enter a percentage to deduct in the Specials Price field, for example: <b>20%</b></li><li>If you enter a new price, the decimal separator must be a \'.\' (decimal-point), example: <b>49.99</b></li></ul>');
+define('TEXT_SPECIALS_EXPIRES_DATE', 'Gültig bis:');
+define('TEXT_SPECIALS_PRICE_TIP', '<b>Bemerkung:</b><ul><li>Sie können im Feld Angebotpreis auch prozentuale Werte angeben, z.B.: <b>20%</b></li><li>Wenn Sie einen neuen Preis eingeben, muss der Wert mit einem \'.\' getrennt werden, z.B.: <b>49.99</b></li></ul>');
 
 define('TEXT_INFO_DATE_ADDED', 'hinzugef&uuml;gt am:');
 define('TEXT_INFO_LAST_MODIFIED', 'Letzte &Auml;nderung:');
 define('TEXT_INFO_NEW_PRICE', 'neuer Preis:');
 define('TEXT_INFO_ORIGINAL_PRICE', 'alter Preis:');
 define('TEXT_INFO_PERCENTAGE', 'Prozent:');
-define('TEXT_INFO_EXPIRES_AT', 'Expires At: <b>%s</b>');
+define('TEXT_INFO_EXPIRES_AT', 'Gültig bis: <b>%s</b>');
 define('TEXT_INFO_STATUS_CHANGE', 'Status geändert: %s');
 
 define('TEXT_INFO_DELETE_INTRO', 'Sind Sie sicher, dass Sie den Sonderpreis l&ouml;schen m&ouml;chten?');
