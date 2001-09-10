4f3247e7fba15d872dafc0ff8abc5d7ac23f43e3
dwatkins <dwatkins>
2001-09-10 15:28:19 +0000

    spelling mistake

--- catalog/catalog/create_account_success.php
+++ catalog/catalog/create_account_success.php
@@ -45,7 +45,7 @@
         <td><br><? echo tep_black_line(); ?></td>
       </tr>
       <tr>
-        <td align="right"><br><?php echo '<a href="' . tep_href_link(FILENAME_DEFAULT, '', 'NONSSL') . '">' . tep_image_button('button_continue.gif', IMAGE_BUTTON_CONTNUE) . '</a>'; ?>&nbsp;&nbsp;</td>
+        <td align="right"><br><?php echo '<a href="' . tep_href_link(FILENAME_DEFAULT, '', 'NONSSL') . '">' . tep_image_button('button_continue.gif', IMAGE_BUTTON_CONTINUE) . '</a>'; ?>&nbsp;&nbsp;</td>
       </tr>
     </table></td>
 <!-- body_text_eof //-->
