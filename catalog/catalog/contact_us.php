36002284d48637898969fe03a25ad2e47ba0fbc4
dwatkins <dwatkins>
2001-09-12 11:54:58 +0000

    Update to TEP Standards (let me know if I missed anything)
    Added a check to the email address.

--- catalog/catalog/contact_us.php
+++ catalog/catalog/contact_us.php
@@ -1,31 +1,53 @@
-<? include('includes/application_top.php'); ?>
-<? $include_file = DIR_WS_LANGUAGES . $language . '/' . FILENAME_CONTACT_US; include(DIR_WS_INCLUDES . 'include_once.php'); ?>
-<? $location = ' : <a href="' . tep_href_link(FILENAME_CONTACT_US, '', 'NONSSL') . '" class="whitelink">' . NAVBAR_TITLE . '</a>'; ?>
-<?
+<?php
+/*
+  $Id: contact_us.php,v 1.25 2001/09/12 11:54:58 dwatkins Exp $
+
+  The Exchange Project - Community Made Shopping!
+  http://www.theexchangeproject.org
+
+  Copyright (c) 2000,2001 The Exchange Project
+
+  Released under the GNU General Public License
+*/
+
+  require('includes/application_top.php');
+  require(DIR_WS_LANGUAGES . $language . '/' . FILENAME_CONTACT_US);
+
+  $error = false;
   if ($HTTP_GET_VARS['action'] == 'send') {
-    tep_mail(STORE_OWNER, STORE_OWNER_EMAIL_ADDRESS, EMAIL_SUBJECT, $HTTP_POST_VARS['enquiry'], $HTTP_POST_VARS['name'], $HTTP_POST_VARS['email'], '');
-    tep_redirect(tep_href_link(FILENAME_CONTACT_US, 'action=success', 'NONSSL'));
+    if (tep_validate_email(trim($HTTP_POST_VARS['email']))) {
+      tep_mail(STORE_OWNER, STORE_OWNER_EMAIL_ADDRESS, EMAIL_SUBJECT, $HTTP_POST_VARS['enquiry'], $HTTP_POST_VARS['name'], $HTTP_POST_VARS['email'], '');
+      tep_redirect(tep_href_link(FILENAME_CONTACT_US, 'action=success', 'NONSSL'));
+    } else {
+      $error = true;
+    }
   }
+
+  $location = ' : ' . NAVBAR_TITLE; 
 ?>
 <html>
 <head>
-<title><? echo TITLE; ?></title>
-<base href="<? echo (getenv('HTTPS') == 'on' ? HTTPS_SERVER : HTTP_SERVER) . DIR_WS_CATALOG; ?>">
+<title><?php echo TITLE; ?></title>
+<base href="<?php echo (getenv('HTTPS') == 'on' ? HTTPS_SERVER : HTTP_SERVER) . DIR_WS_CATALOG; ?>">
 <link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
 <body marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0" bgcolor="#FFFFFF">
 <!-- header //-->
-<? $include_file = DIR_WS_INCLUDES . 'header.php';  include(DIR_WS_INCLUDES . 'include_once.php'); ?>
+<?php
+  require(DIR_WS_INCLUDES . 'header.php'); 
+?>
 <!-- header_eof //-->
 
 <!-- body //-->
 <table border="0" width="100%" cellspacing="5" cellpadding="5">
   <tr>
-    <td width="<? echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<? echo BOX_WIDTH; ?>" cellspacing="0" cellpadding="0">
+    <td width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="0" cellpadding="0">
       <tr>
-        <td width="100%"><table border="0" width="100%" cellspacing="0" cellpadding="2">
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
 <!-- left_navigation //-->
-<? $include_file = DIR_WS_INCLUDES . 'column_left.php'; include(DIR_WS_INCLUDES . 'include_once.php'); ?>
+<?php
+  require(DIR_WS_INCLUDES . 'column_left.php'); 
+?>
 <!-- left_navigation_eof //-->
         </table></td>
       </tr>
@@ -33,97 +55,90 @@
 <!-- body_text //-->
     <td width="100%" valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="0">
       <tr>
-        <td width="100%"><table border="0" width="100%" cellspacing="0" cellpadding="2" class="topBarTitle">
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="2" class="topBarTitle">
           <tr>
-            <td width="100%" class="topBarTitle">&nbsp;<? echo TOP_BAR_TITLE; ?>&nbsp;</td>
+            <td class="topBarTitle">&nbsp;<?php echo TOP_BAR_TITLE; ?>&nbsp;</td>
           </tr>
         </table></td>
       </tr>
-<? 
+<?php
   if ($HTTP_GET_VARS['action'] == 'success') {
 ?>
       <tr>
         <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
           <tr>
-            <td><? echo tep_image(DIR_WS_IMAGES . 'table_background_man_on_board.gif', HEADING_TITLE); ?></td>
-            <td valign="top" class="main"><div align="center" class="pageHeading"><br><? echo ($HTTP_GET_VARS['action'] == 'success') ? SUB_BAR_TITLE_SENT : SUB_BAR_TITLE; ?>&nbsp;</div><br><? echo TEXT_SUCCESS; ?></td>
+            <td><?php echo tep_image(DIR_WS_IMAGES . 'table_background_man_on_board.gif', HEADING_TITLE); ?></td>
+            <td valign="top" class="main"><div align="center" class="pageHeading"><br><?php echo ($HTTP_GET_VARS['action'] == 'success') ? SUB_BAR_TITLE_SENT : SUB_BAR_TITLE; ?>&nbsp;</div><br><?php echo TEXT_SUCCESS; ?></td>
           </tr>
         </table></td>
       </tr>
       <tr>
-        <td class="fieldKey">&nbsp;</td>
+        <td class="main">&nbsp;</td>
       </tr>
       <tr>
-        <td><? echo tep_black_line(); ?></td>
+        <td><?php echo tep_black_line(); ?></td>
       </tr>
       <tr>
         <td align="right"><br><?php echo '<a href="' . tep_href_link(FILENAME_DEFAULT, '', 'NONSSL') . '">' . tep_image_button('button_continue.gif', IMAGE_BUTTON_CONTINUE) . '</a>'; ?>&nbsp;&nbsp;</td>
       </tr>
-<?
+<?php
   } else {
 ?>
       <tr>
-        <td width="100%"><table border="0" width="100%" cellspacing="0" cellpadding="0">
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
           <tr>
-            <td class="pageHeading">&nbsp;<? echo HEADING_TITLE; ?>&nbsp;</td>
-            <td align="right">&nbsp;<? echo tep_image(DIR_WS_IMAGES . 'table_background_contact_us.gif', HEADING_TITLE, HEADING_IMAGE_WIDTH, HEADING_IMAGE_HEIGHT); ?>&nbsp;</td>
+            <td class="pageHeading">&nbsp;<?php echo HEADING_TITLE; ?>&nbsp;</td>
+            <td align="right">&nbsp;<?php echo tep_image(DIR_WS_IMAGES . 'table_background_contact_us.gif', HEADING_TITLE, HEADING_IMAGE_WIDTH, HEADING_IMAGE_HEIGHT); ?>&nbsp;</td>
           </tr>
         </table></td>
       </tr>
       <tr>
-        <td><? echo tep_black_line(); ?></td>
+        <td><?php echo tep_black_line(); ?></td>
       </tr>
       <tr>
-        <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
-          <tr class="subBar">
-            <td class="subBar">&nbsp;<? echo SUB_BAR_TITLE; ?>&nbsp;</td>
+        <td><form action="<?php echo tep_href_link(FILENAME_CONTACT_US, 'action=send', 'NONSSL'); ?>" method="post"><table border="0" width="100%" cellspacing="0" cellpadding="2">
+          <tr>
+            <td class="main">&nbsp;</td>
           </tr>
           <tr>
-            <td><? echo tep_black_line(); ?></td>
-          </tr>
-        </td></table>
-      </tr>
-      <tr>
-        <td class="fieldKey">&nbsp;</td>
-      </tr>
-      <tr>
-        <td><form action="<? echo tep_href_link(FILENAME_CONTACT_US, 'action=send', 'NONSSL'); ?>" method="post"><table border="0" width="100%" cellspacing="0" cellpadding="2">
-          <tr>
-            <td colspan="2" class="fieldKey">&nbsp;</td>
+            <td class="formAreaTitle"><?php echo SUB_BAR_TITLE; ?></td>
           </tr>
           <tr>
-            <td align="right" class="main"><? echo ENTRY_NAME; ?></td>
-            <td><input name="name" maxlength="32"></td>
+            <td class="main"><table border="0" width="100%" cellspacing="0" cellpadding="2" class="formArea">
+              <tr>
+                <td class="main">&nbsp;<?php echo ENTRY_NAME; ?></td>
+                <td class="main">&nbsp;<?php echo tep_draw_input_field('name', $HTTP_POST_VARS['name']); ?></td>
+              </tr>
+              <tr>
+                <td class="main">&nbsp;<?php echo ENTRY_EMAIL; ?></td>
+                <td class="main">&nbsp;<?php echo tep_draw_input_field('email', $HTTP_POST_VARS['email']); if ($error) echo '&nbsp;' . ENTRY_EMAIL_ADDRESS_CHECK_ERROR; ?></td>
+              </tr>
+              <tr>
+                <td class="main" valign="top">&nbsp;<?php echo ENTRY_ENQUIRY; ?></td>
+                <td class="main">&nbsp;<?php echo tep_draw_textarea_field('enquiry', 'soft', 50, 15, $HTTP_POST_VARS['enquiry']); ?></td>
+              </tr>
+            </table></td>
           </tr>
           <tr>
-            <td align="right" class="main"><? echo ENTRY_EMAIL; ?></td>
-            <td><input name="email" maxlength="96"></td>
+            <td class="main">&nbsp;</td>
           </tr>
           <tr>
-            <td valign="top" align="right" class="main"><? echo ENTRY_ENQUIRY; ?></td>
-            <td><textarea name="enquiry" wrap="soft" cols="40" rows="15"></textarea></td>
-          </tr>
-          <tr>
-            <td colspan="2" class="main">&nbsp;</td>
-          </tr>
-          <tr>
-            <td colspan="2" class="main"><?php echo tep_black_line(); ?></td>
-          </tr>
-          <tr>
-            <td align="right" colspan="2" class="main"><?php echo tep_image_submit('button_continue.gif', IMAGE_BUTTON_CONTINUE); ?>&nbsp;&nbsp;</td>
+            <td align="right" class="main"><?php echo tep_image_submit('button_continue.gif', IMAGE_BUTTON_CONTINUE); ?>&nbsp;&nbsp;</td>
           </tr>
         </table></form></td>
-<?
+<?php
   }
 ?>
       </tr>
     </table></td>
 <!-- body_text_eof //-->
-    <td width="<? echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<? echo BOX_WIDTH; ?>" cellspacing="0" cellpadding="0">
+    <td width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="0" cellpadding="0">
       <tr>
-        <td width="100%"><table border="0" width="100%" cellspacing="0" cellpadding="2">
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
 <!-- right_navigation //-->
-<? $include_file = DIR_WS_INCLUDES . 'column_right.php'; include(DIR_WS_INCLUDES . 'include_once.php'); ?>
+<?php
+  require(DIR_WS_INCLUDES . 'column_right.php');
+?>
 <!-- right_navigation_eof //-->
         </table></td>
       </tr>
@@ -133,9 +148,13 @@
 <!-- body_eof //-->
 
 <!-- footer //-->
-<? $include_file = DIR_WS_INCLUDES . 'footer.php'; include(DIR_WS_INCLUDES . 'include_once.php'); ?>
+<?php
+  require(DIR_WS_INCLUDES . 'footer.php');
+?>
 <!-- footer_eof //-->
 <br>
 </body>
 </html>
-<? $include_file = DIR_WS_INCLUDES . 'application_bottom.php'; include(DIR_WS_INCLUDES . 'include_once.php'); ?>
+<?php 
+  require(DIR_WS_INCLUDES . 'application_bottom.php'); 
+?>
