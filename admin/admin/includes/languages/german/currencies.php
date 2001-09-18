f53ce310e065689ff962c66728e82d05b4676dc1
mbs <mbs>
2001-09-18 10:57:24 +0000

    language translations, add a missing define

--- admin/admin/includes/languages/german/currencies.php
+++ admin/admin/includes/languages/german/currencies.php
@@ -1,8 +1,10 @@
-<?
+<?php
 /*
-German Text for The Exchange Project Administration Tool Preview Release 2.2
-Last Update: 20/05/2001
-Author(s): Marcel Bossert-Schwab (webmaster@wernich.de)
+  The Exchange Project - Community Made Shopping!
+
+  German Text for The Exchange Project Administration Tool
+
+  $Id: currencies.php,v 1.5 2001/09/18 10:57:24 mbs Exp $
 */
 
 define('TOP_BAR_TITLE', 'W&auml;hrungen');
@@ -10,18 +12,19 @@ define('HEADING_TITLE', 'W&auml;hrungen');
 
 define('TABLE_HEADING_CURRENCY_ID', 'ID');
 define('TABLE_HEADING_CURRENCY_NAME', 'W&auml;hrung');
-define('TABLE_HEADING_CURRENCY_CODES', 'Codierung');
+define('TABLE_HEADING_CURRENCY_CODES', 'Kodierung');
+define('TABLE_HEADING_CURRENCY_VALUE', 'Wert');
 define('TABLE_HEADING_ACTION', 'Aktion');
 
 define('TEXT_INFO_EDIT_INTRO', 'Bitte f&uuml;hren Sie alle notwendigen &Auml;nderungen durch');
 define('TEXT_INFO_CURRENCY_TITLE', 'Name:');
-define('TEXT_INFO_CURRENCY_CODE', 'Codierung:');
+define('TEXT_INFO_CURRENCY_CODE', 'Kodierung:');
 define('TEXT_INFO_CURRENCY_SYMBOL_LEFT', 'Symbol Links:');
 define('TEXT_INFO_CURRENCY_SYMBOL_RIGHT', 'Symbol Rechts:');
 define('TEXT_INFO_CURRENCY_DECIMAL_POINT', 'Dezimalkomma:');
 define('TEXT_INFO_CURRENCY_THOUSANDS_POINT', 'Tausendepunkt:');
 define('TEXT_INFO_CURRENCY_DECIMAL_PLACES', 'Dezimalstellen:');
-define('TEXT_INFO_CURRENCY_VALUE', 'Value:');
+define('TEXT_INFO_CURRENCY_VALUE', 'Wert:');
 define('TEXT_INFO_CURRENCY_EXAMPLE', 'Beispiel:');
 define('TEXT_INFO_INSERT_INTRO', 'Bitte f&uuml;llen Sie die folgenden Daten f&uuml;r die neue Wahrung aus:');
 define('TEXT_INFO_DELETE_INTRO', 'Sind Sie sicher, dass Sie diese W&auml;hrung l&ouml;schen m&ouml;chten?');
