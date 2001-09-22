737bd7f0d8f8194b814eb61d1aa4c425e498bb8b
hpdl <hpdl>
2001-09-22 20:26:05 +0000

    update the display of the date

--- catalog/catalog/includes/modules/upcoming_products.php
+++ catalog/catalog/includes/modules/upcoming_products.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: upcoming_products.php,v 1.19 2001/09/20 19:27:10 mbs Exp $
+  $Id: upcoming_products.php,v 1.20 2001/09/22 20:26:05 hpdl Exp $
 
   The Exchange Project - Community Made Shopping!
   http://www.theexchangeproject.org
@@ -10,7 +10,7 @@
   Released under the GNU General Public License
 */
 
-  $expected_query = tep_db_query("select p.products_id, pd.products_name, unix_timestamp(products_date_available) as date_expected from " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd where products_date_available != '' and products_date_available > now() and p.products_id = pd.products_id and pd.language_id = '" . $languages_id . "' order by " . EXPECTED_PRODUCTS_FIELD . " " . EXPECTED_PRODUCTS_SORT . " limit " . MAX_DISPLAY_UPCOMING_PRODUCTS);
+  $expected_query = tep_db_query("select p.products_id, pd.products_name, products_date_available as date_expected from " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd where to_days(products_date_available) >= to_days(now()) and p.products_id = pd.products_id and pd.language_id = '" . $languages_id . "' order by " . EXPECTED_PRODUCTS_FIELD . " " . EXPECTED_PRODUCTS_SORT . " limit " . MAX_DISPLAY_UPCOMING_PRODUCTS);
   if (tep_db_num_rows($expected_query) > 0) {
 ?>
 <!-- upcoming_products //-->
@@ -34,7 +34,7 @@
         echo '              <tr class="upcomingProducts-odd">' . "\n";
       }
       echo '                <td class="smallText">&nbsp;<a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $expected['products_id'], 'NONSSL') . '">' . $expected['products_name'] . '</a>&nbsp;</td>' . "\n";
-      echo '                <td align="right" class="smallText">&nbsp;' . strftime(DATE_FORMAT_SHORT, $expected['date_expected']) . '&nbsp;</td>' . "\n";
+      echo '                <td align="right" class="smallText">&nbsp;' . tep_date_short($expected['date_expected']) . '&nbsp;</td>' . "\n";
       echo '              </tr>' . "\n";
     }
 ?>
