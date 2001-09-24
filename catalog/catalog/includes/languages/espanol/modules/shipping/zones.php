47cb24aad9638c45dd3ee35eeb7fa467f2bdc7fa
dwatkins <dwatkins>
2001-09-24 19:02:04 +0000

    Transalation

--- catalog/catalog/includes/languages/espanol/modules/shipping/zones.php
+++ catalog/catalog/includes/languages/espanol/modules/shipping/zones.php
@@ -1,8 +1,8 @@
 <?php
-define('MODULE_SHIPPING_ZONES_TEXT_TITLE', 'Zone Rates');
-define('MODULE_SHIPPING_ZONES_TEXT_DESCRIPTION', 'Zone Based Rates');
-define('MODULE_SHIPPING_ZONES_TEXT_WAY', 'Shipping to');
-define('MODULE_SHIPPING_ZONES_TEXT_UNITS', 'lb(s)');
-define('MODULE_SHIPPING_ZONES_INVALID_ZONE', 'No shipping available to the selected country');
-define('MODULE_SHIPPING_ZONES_UNDEFINED_RATE', 'The shipping rate cannot be determined at this time');
+define('MODULE_SHIPPING_ZONES_TEXT_TITLE', 'Tarifa por Zona');
+define('MODULE_SHIPPING_ZONES_TEXT_DESCRIPTION', 'Tarifa basada en Zonas');
+define('MODULE_SHIPPING_ZONES_TEXT_WAY', 'Enviar A');
+define('MODULE_SHIPPING_ZONES_TEXT_UNITS', 'kg(s)');
+define('MODULE_SHIPPING_ZONES_INVALID_ZONE', 'No hay envio disponible para ese pais.');
+define('MODULE_SHIPPING_ZONES_UNDEFINED_RATE', 'No se han podido calcular los gastos de envio');
 ?>
