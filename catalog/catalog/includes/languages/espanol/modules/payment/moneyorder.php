68b844d0bc06b1b53e219d0875bf3dcb9e1ed0a3
dgw_ <dgw_>
2001-10-10 21:07:37 +0000

    text translation

--- catalog/catalog/includes/languages/espanol/modules/payment/moneyorder.php
+++ catalog/catalog/includes/languages/espanol/modules/payment/moneyorder.php
@@ -1,6 +1,16 @@
 <?php
+/*
+  $Id: moneyorder.php,v 1.2 2001/10/10 21:07:37 dgw_ Exp $
 
-  define('MODULE_PAYMENT_MONEYORDER_TEXT_TITLE', 'Check/Money Order');
-  define('MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION', 'Make Payable To:<br>' . '&nbsp;&nbsp;&nbsp;Your Company, Inc.<br>' . '&nbsp;&nbsp;&nbsp;555 Something St.<br>' . '&nbsp;&nbsp;&nbsp;Sometown, ST 00000<br><br>' . '&nbsp;Your order will not ship until we receive payment.');
-  define('MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER', 'Make Payable To:' . "\n\n" . 'Your Company, Inc.' . "\n" . '555 Something St.' . "\n" . 'Sometown, ST 00000' . "\n\n" . 'Your order will not ship until we receive payment.');
+  The Exchange Project - Community Made Shopping!
+  http://www.theexchangeproject.org
+
+  Copyright (c) 2000,2001 The Exchange Project
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_PAYMENT_MONEYORDER_TEXT_TITLE', 'Cheque/Transferencia Bancaria');
+  define('MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION', 'Pagadero a:<br>' . '&nbsp;&nbsp;&nbsp;Tu Empresa, S.L.<br>' . '&nbsp;&nbsp;&nbsp;C/Calle<br>' . '&nbsp;&nbsp;&nbsp;000000 Ciudad<br><br>' . '&nbsp;Su pedido se enviará en cuanto se reciba el pago.');
+  define('MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER', 'Pagadero a:<br>' . '&nbsp;&nbsp;&nbsp;Tu Empresa, S.L.<br>' . '&nbsp;&nbsp;&nbsp;C/Calle<br>' . '&nbsp;&nbsp;&nbsp;000000 Ciudad<br><br>' . '&nbsp;Su pedido se enviará en cuanto se reciba el pago.');
 ?>
