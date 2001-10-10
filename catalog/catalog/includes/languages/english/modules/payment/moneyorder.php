b7045f859f4430b3c7a528c5de81a567a97eb6c0
project3000 <project3000>
2001-10-10 19:24:51 +0000

    New payment module to handle check / money orders

---
+++ catalog/catalog/includes/languages/english/modules/payment/moneyorder.php
@@ -0,0 +1,6 @@
+<?php
+
+  define('MODULE_PAYMENT_MONEYORDER_TEXT_TITLE', 'Check/Money Order');
+  define('MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION', 'Make Payable To:<br>' . '&nbsp;&nbsp;&nbsp;Your Company, Inc.<br>' . '&nbsp;&nbsp;&nbsp;555 Something St.<br>' . '&nbsp;&nbsp;&nbsp;Sometown, ST 00000<br><br>' . '&nbsp;Your order will not ship until we receive payment.');
+  define('MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER', 'Make Payable To:' . "\n\n" . 'Your Company, Inc.' . "\n" . '555 Something St.' . "\n" . 'Sometown, ST 00000' . "\n\n" . 'Your order will not ship until we receive payment.');
+?>
---
+++ catalog/catalog/includes/modules/payment/moneyorder.php
@@ -0,0 +1,72 @@
+<?php
+
+  class moneyorder {
+    var $code, $title, $description, $enabled;
+
+// class constructor
+    function moneyorder() {
+      $this->code = 'moneyorder';
+      $this->title = MODULE_PAYMENT_MONEYORDER_TEXT_TITLE;
+      $this->description = MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION;
+      $this->email_footer = MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER;
+      $this->enabled = MODULE_PAYMENT_MONEYORDER_STATUS;
+    }
+
+// class methods
+    function javascript_validation() {
+      return false;
+    }
+
+    function selection() {
+      return false;
+    }
+
+    function pre_confirmation_check() {
+      return false;
+    }
+
+    function confirmation() {
+      $confirmation_string = '          <tr>' . "\n" .
+                             '            <td class="main">&nbsp;' . MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION . '&nbsp;</td>' . "\n" .
+                             '          </tr>' . "\n";
+      return $confirmation_string;
+    }
+
+    function process_button() {
+      return false;
+    }
+
+    function before_process() {
+      return false;
+    }
+
+    function after_process() {
+      return false;
+    }
+
+    function output_error() {
+      return false;
+    }
+
+    function check() {
+      $check = tep_db_query("select configuration_value from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_PAYMENT_MONEYORDER_STATUS'");
+      $check = tep_db_num_rows($check);
+
+      return $check;
+    }
+
+    function install() {
+      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Allow Check/Money Order', 'MODULE_PAYMENT_MONEYORDER_STATUS', '1', 'Do you want to accept Check and Money Order payments?', '6', '1', now());");
+    }
+
+    function remove() {
+      tep_db_query("delete from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_PAYMENT_MONEYORDER_STATUS'");
+    }
+
+    function keys() {
+      $keys = array('MODULE_PAYMENT_MONEYORDER_STATUS');
+
+      return $keys;
+    }
+  }
+?>
