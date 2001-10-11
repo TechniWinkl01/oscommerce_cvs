5154872d742e39e91dac95b239836f173d5af09e
dgw_ <dgw_>
2001-10-11 19:11:45 +0000

    Customer and delivery information is also sent to authorizenet now
    Ive added 3 new settings (remove and install the module to upgrade)
    
    Ive removed the comment in the line concerning SID because there are reports that its necesary for correct operation of the module.
    
    NOTE: Not tested yet

--- catalog/catalog/includes/modules/payment/authorizenet.php
+++ catalog/catalog/includes/modules/payment/authorizenet.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: authorizenet.php,v 1.23 2001/09/15 13:46:32 dwatkins Exp $
+  $Id: authorizenet.php,v 1.24 2001/10/11 19:11:45 dgw_ Exp $
 
   The Exchange Project - Community Made Shopping!
   http://www.theexchangeproject.org
@@ -93,7 +93,12 @@
     }
 
     function process_button() {
-      global $HTTP_POST_VARS, $CardNumber, $total_cost, $total_tax, $shipping_cost, $customer_id;
+      global $HTTP_POST_VARS, $HTTP_SERVER_VARS, $CardNumber, $total_cost, $total_tax, $shipping_cost, $customer_id;
+
+      $customer_query = tep_db_query("select c.customers_firstname, c.customers_lastname, c.customers_telephone, c.customers_email_address, ab.entry_street_address, ab.entry_city, ab.entry_country_id, ab.entry_zone_id, ab.entry_state, ab.entry_postcode from " . TABLE_CUSTOMERS . " c left join " . TABLE_ADDRESS_BOOK . " ab on c.customers_default_address_id = ab.address_book_id and c.customers_id = ab.customers_id where c.customers_id = '" . $customer_id . "'");
+      $customer_values = tep_db_fetch_array($customer_query);
+      $delivery_query = tep_db_query("select ab.entry_firstname, ab.entry_lastname, ab.entry_street_address, ab.entry_city, ab.entry_country_id, ab.entry_zone_id, ab.entry_state, ab.entry_postcode from " . TABLE_ADDRESS_BOOK . " ab where ab.address_book_id = '" . $HTTP_POST_VARS['sendto'] . "' and ab.customers_id = '" . $customer_id . "'");
+      $delivery_values = tep_db_fetch_array($delivery_query);
 
       $process_button_string = tep_draw_hidden_field('x_Login', MODULE_PAYMENT_AUTHORIZENET_LOGIN) .
                                tep_draw_hidden_field('x_Card_Num', $CardNumber) .
@@ -101,12 +106,31 @@
                                tep_draw_hidden_field('x_Amount', number_format($total_cost + $total_tax + $shipping_cost, 2)) .
                                tep_draw_hidden_field('x_ADC_Relay_Response', 'TRUE') .
                                tep_draw_hidden_field('x_ADC_URL', tep_href_link(FILENAME_CHECKOUT_PROCESS, '', 'SSL', true)) .
+                               tep_draw_hidden_field('x_Method', MODULE_PAYMENT_AUTHORIZENET_METHOD) .
                                tep_draw_hidden_field('x_Version', '3.0') .
-                               tep_draw_hidden_field('x_Cust_ID', $customer_id);
-
+                               tep_draw_hidden_field('x_Cust_ID', $customer_id) .
+                               tep_draw_hidden_field('x_Email_Customer', ($email_customers=='1'? 'TRUE': 'FALSE')) .
+                               tep_draw_hidden_field('x_Email_Merchant', ($email_customers=='1'? 'TRUE': 'FALSE')) .
+                               tep_draw_hidden_field('x_first_name', $customer_values['customers_firstname']) .
+                               tep_draw_hidden_field('x_last_name', $customer_values['customers_lastname']) .
+                               tep_draw_hidden_field('x_address', $customer_values['entry_street_address']) .
+                               tep_draw_hidden_field('x_city', $customer_values['entry_city']) .
+                               tep_draw_hidden_field('x_state', tep_get_zone_name($customer_values['entry_country_id'], $customer_values['entry_zone_id'], $customer_values['entry_state'])) .
+                               tep_draw_hidden_field('x_zip', $customer_values['entry_postcode']) .
+                               tep_draw_hidden_field('x_country', tep_get_country_name($customer_values['entry_country_id'])) .
+                               tep_draw_hidden_field('x_phone', $customer_values['customers_telephone']) .
+                               tep_draw_hidden_field('x_email', $customer_values['customers_email_address']) .
+                               tep_draw_hidden_field('x_ship_to_first_name', $delivery_values['entry_firstname']) .
+                               tep_draw_hidden_field('x_ship_to_last_name', $delivery_values['entry_lastname']) .
+                               tep_draw_hidden_field('x_ship_to_address', $delivery_values['entry_street_address']) .
+                               tep_draw_hidden_field('x_ship_to_city', $delivery_values['entry_city']) .
+                               tep_draw_hidden_field('x_ship_to_state', tep_get_zone_name($delivery_values['entry_country_id'], $delivery_values['entry_zone_id'], $delivery_values['entry_state'])) .
+                               tep_draw_hidden_field('x_ship_to_zip', $delivery_values['entry_postcode']) .
+                               tep_draw_hidden_field('x_ship_to_country', tep_get_country_name($delivery_values['entry_country_id'])) .
+                               tep_draw_hidden_field('x_Customer_IP', $HTTP_SERVER_VARS['REMOTE_ADDR']);
       if (MODULE_PAYMENT_AUTHORIZENET_TESTMODE == '1') $process_button_string .= tep_draw_hidden_field('x_Test_Request', 'TRUE');
 
-//      if (!SID) $process_button_string .= tep_draw_hidden_field(tep_session_name(), tep_session_id());
+      if (!SID) $process_button_string .= tep_draw_hidden_field(tep_session_name(), tep_session_id());
 
       return $process_button_string;
     }
@@ -146,16 +170,22 @@
       tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Allow Authorize.net', 'MODULE_PAYMENT_AUTHORIZENET_STATUS', '1', 'Do you want to accept Authorize.net payments?', '6', '0', now())");
       tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Authorize.net Login', 'MODULE_PAYMENT_AUTHORIZENET_LOGIN', 'testing', 'Login used for Authorize.net payments', '6', '0', now())");
       tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Authorize.net Test Mode', 'MODULE_PAYMENT_AUTHORIZENET_TESTMODE', '1', 'Test mode for Authorize.net payments', '6', '0', now())");
+      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Authorize.net E-Mail Client', 'MODULE_PAYMENT_AUTHORIZENET_EMAIL', '0', 'Should Authorize.Net e-mail the customer too? 0=NO, 1=YES', '6', '0', now())");
+      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Authorize.net E-Mail Merchant', 'MODULE_PAYMENT_AUTHORIZENET_EMAIL_MERCHANT', '1', 'Should Authorize.Net e-mail you? 0=NO, 1=YES', '6', '0', now())");
+      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Authorize.net Method', 'MODULE_PAYMENT_AUTHORIZENET_METHOD', 'CC', 'This should be either CC or ECHECK', '6', '0', now())");
     }
 
     function remove() {
       tep_db_query("delete from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_PAYMENT_AUTHORIZENET_STATUS'");
       tep_db_query("delete from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_PAYMENT_AUTHORIZENET_LOGIN'");
       tep_db_query("delete from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_PAYMENT_AUTHORIZENET_TESTMODE'");
+      tep_db_query("delete from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_PAYMENT_AUTHORIZENET_EMAIL'");
+      tep_db_query("delete from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_PAYMENT_AUTHORIZENET_EMAIL_MERCHANT'");
+      tep_db_query("delete from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_PAYMENT_AUTHORIZENET_METHOD'");
     }
 
     function keys() {
-      $keys = array('MODULE_PAYMENT_AUTHORIZENET_STATUS', 'MODULE_PAYMENT_AUTHORIZENET_LOGIN', 'MODULE_PAYMENT_AUTHORIZENET_TESTMODE');
+      $keys = array('MODULE_PAYMENT_AUTHORIZENET_STATUS', 'MODULE_PAYMENT_AUTHORIZENET_LOGIN', 'MODULE_PAYMENT_AUTHORIZENET_TESTMODE', 'MODULE_PAYMENT_AUTHORIZENET_EMAIL', 'MODULE_PAYMENT_AUTHORIZENET_EMAIL_MERCHANT', 'MODULE_PAYMENT_AUTHORIZENET_METHOD');
 
       return $keys;
     }
