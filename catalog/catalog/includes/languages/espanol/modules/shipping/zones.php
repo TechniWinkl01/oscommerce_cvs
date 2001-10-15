6647f61e519ab41d7cdaa0ea1421eb49fc7db36d
mabosch <mabosch>
2001-10-15 07:07:45 +0000

    added required licence informations.

--- catalog/catalog/includes/languages/espanol/modules/shipping/zones.php
+++ catalog/catalog/includes/languages/espanol/modules/shipping/zones.php
@@ -1,4 +1,15 @@
 <?php
+/*
+  $Id: zones.php,v 1.3 2001/10/15 07:07:45 mabosch Exp $
+
+  The Exchange Project - Community Made Shopping!
+  http://www.theexchangeproject.org
+
+  Copyright (c) 2000,2001 The Exchange Project
+
+  Released under the GNU General Public License
+*/
+
 define('MODULE_SHIPPING_ZONES_TEXT_TITLE', 'Tarifa por Zona');
 define('MODULE_SHIPPING_ZONES_TEXT_DESCRIPTION', 'Tarifa basada en Zonas');
 define('MODULE_SHIPPING_ZONES_TEXT_WAY', 'Enviar A');
--- catalog/catalog/includes/languages/german/modules/shipping/zones.php
+++ catalog/catalog/includes/languages/german/modules/shipping/zones.php
@@ -1,4 +1,15 @@
 <?php
+/*
+  $Id: zones.php,v 1.2 2001/10/15 07:07:45 mabosch Exp $
+
+  The Exchange Project - Community Made Shopping!
+  http://www.theexchangeproject.org
+
+  Copyright (c) 2000,2001 The Exchange Project
+
+  Released under the GNU General Public License
+*/
+
 define('MODULE_SHIPPING_ZONES_TEXT_TITLE', 'Zone Rates');
 define('MODULE_SHIPPING_ZONES_TEXT_DESCRIPTION', 'Zone Based Rates');
 define('MODULE_SHIPPING_ZONES_TEXT_WAY', 'Shipping to');
