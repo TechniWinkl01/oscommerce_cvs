1b5d28cadcdfb75ace3b7dc0ad21454711aa4876
mabosch <mabosch>
2001-11-18 12:34:27 +0000

    specials on the box where shown without products descriptions, this is fixed now. thanks to thomas.

--- catalog/catalog/includes/boxes/specials.php
+++ catalog/catalog/includes/boxes/specials.php
@@ -1,10 +1,22 @@
+<?php
+/*
+  $Id: specials.php,v 1.22 2001/11/18 12:34:27 mabosch Exp $
+
+  The Exchange Project - Community Made Shopping!
+  http://www.theexchangeproject.org
+
+  Copyright (c) 2000,2001 The Exchange Project
+
+  Released under the GNU General Public License
+*/
+?>
 <!-- specials //-->
-<?
+<?php
   if ($random_product = tep_random_select("select p.products_id, pd.products_name, p.products_price, p.products_image, s.specials_new_products_price from " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd, " . TABLE_SPECIALS . " s where p.products_status = '1' and p.products_id = s.products_id and pd.products_id = s.products_id and pd.language_id = '" . $languages_id . "' and s.status = '1' order by s.specials_date_added DESC limit " . MAX_RANDOM_SELECT_SPECIALS)) {
 ?>
           <tr>
             <td>
-<?
+<?php
     $info_box_contents = array();
     $info_box_contents[] = array('align' => 'left',
                                  'text'  => '<a href="' . tep_href_link(FILENAME_SPECIALS, '', 'NONSSL') . '" class="infoBoxHeading">' . BOX_HEADING_SPECIALS . '</a>'
@@ -13,14 +25,13 @@
 
     $info_box_contents = array();
     $info_box_contents[] = array('align' => 'center',
-                                 'text'  => '<a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $random_product["products_id"], 'NONSSL') . '">' . tep_image($random_product['products_image'], $random_product['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a><br><s>' . $currencies->format($random_product['products_price']) . '</s><br><span class="productSpecialPrice">' . $currencies->format($random_product['specials_new_products_price']) . '</span><br><a href="' . tep_href_link(FILENAME_SPECIALS, '', 'NONSSL') . '">' . BOX_SPECIALS_MORE . '</a>'
+                                 'text'  => '<a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $random_product["products_id"], 'NONSSL') . '">' . tep_image($random_product['products_image'], $random_product['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a><br><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $random_product['products_id'], 'NONSSL') . '">' . $random_product['products_name'] . '</a><br><s>' . $currencies->format($random_product['products_price']) . '</s><br><span class="productSpecialPrice">' . $currencies->format($random_product['specials_new_products_price']) . '</span><br><a href="' . tep_href_link(FILENAME_SPECIALS, '', 'NONSSL') . '">' . BOX_SPECIALS_MORE . '</a>'
                                 );
     new infoBox($info_box_contents);
 ?>
             </td>
           </tr>
-<?
+<?php
   }
 ?>
 <!-- specials_eof //-->
-
