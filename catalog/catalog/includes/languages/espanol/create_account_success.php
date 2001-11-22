f1cef34a151ffeec12653846660aadbbf05cf06e
dgw_ <dgw_>
2001-11-22 22:32:16 +0000

    added an accent

--- catalog/catalog/includes/languages/espanol/create_account_success.php
+++ catalog/catalog/includes/languages/espanol/create_account_success.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: create_account_success.php,v 1.3 2001/05/26 16:47:29 hpdl Exp $
+  $Id: create_account_success.php,v 1.4 2001/11/22 22:32:16 dgw_ Exp $
 
   The Exchange Project - Community Made Shopping!
   http://www.theexchangeproject.org
@@ -14,5 +14,5 @@ define('NAVBAR_TITLE_1', 'Crear una Cuenta');
 define('NAVBAR_TITLE_2', 'Exito');
 define('TOP_BAR_TITLE', 'Cuenta Creada con Exito!');
 define('HEADING_TITLE', 'Su cuenta ha sido creada!');
-define('TEXT_ACCOUNT_CREATED', 'Enhorabuena! Su cuenta ha sido creada con exito! Ahora puede disfrutar de las ventajas de disponer de una cuenta para mejorar su navegacion en nuestro catalogo. Si tiene <small><b>CUALQUIER</b></small> pregunta sobre el funcionamiento del catalogo, por favor comuniquela a <a href="mailto:' . STORE_OWNER_EMAIL_ADDRESS . '">' . STORE_OWNER_EMAIL_ADDRESS . '</a>.<br><br>Se ha enviado una confirmacion a la direccion de correo que nos ha proporcionado. Si no lo ha recibido en 1 hora pongase en contacto con nosotros.');
+define('TEXT_ACCOUNT_CREATED', 'Enhorabuena! Su cuenta ha sido creada con exito! Ahora puede disfrutar de las ventajas de disponer de una cuenta para mejorar su navegacion en nuestro catalogo. Si tiene <small><b>CUALQUIER</b></small> pregunta sobre el funcionamiento del catalogo, por favor comuniquela a <a href="mailto:' . STORE_OWNER_EMAIL_ADDRESS . '">' . STORE_OWNER_EMAIL_ADDRESS . '</a>.<br><br>Se ha enviado una confirmación a la direccion de correo que nos ha proporcionado. Si no lo ha recibido en 1 hora pongase en contacto con nosotros.');
 ?>
