a660d043ea8bd8e6da9a5a15842092a4d4d66159
jan0815 <jan0815>
2001-11-30 17:47:13 +0000

    Translation

--- admin/admin/includes/languages/german/languages.php
+++ admin/admin/includes/languages/german/languages.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: languages.php,v 1.4 2001/11/30 16:32:01 dgw_ Exp $
+  $Id: languages.php,v 1.5 2001/11/30 17:47:13 jan0815 Exp $
 
   The Exchange Project - Community Made Shopping!
   http://www.theexchangeproject.org
@@ -24,10 +24,10 @@ define('TEXT_INFO_LANGUAGE_CODE', 'Codierung:');
 define('TEXT_INFO_LANGUAGE_IMAGE', 'Bild:');
 define('TEXT_INFO_LANGUAGE_DIRECTORY', 'Verzeichnis:');
 define('TEXT_INFO_LANGUAGE_CHARSET', 'Character Set:');
-define('TEXT_INFO_LANGUAGE_DIRECTION', 'Direction:');
-define('TEXT_INFO_LANGUAGE_DIRECTION_DEFAULT', 'Default');
-define('TEXT_INFO_LANGUAGE_DIRECTION_LEFT_TO_RIGHT', 'Left to right');
-define('TEXT_INFO_LANGUAGE_DIRECTION_RIGHT_TO_LEFT', 'Right to left');
+define('TEXT_INFO_LANGUAGE_DIRECTION', 'Richtung:');
+define('TEXT_INFO_LANGUAGE_DIRECTION_DEFAULT', 'Standard');
+define('TEXT_INFO_LANGUAGE_DIRECTION_LEFT_TO_RIGHT', 'von links nach rechts');
+define('TEXT_INFO_LANGUAGE_DIRECTION_RIGHT_TO_LEFT', 'von rechts nach links');
 define('TEXT_INFO_LANGUAGE_SORT_ORDER', 'Sortierreihenfolge:');
 define('TEXT_INFO_INSERT_INTRO', 'Bitte f&uuml;llen Sie die folgenden Felder f&uuml;r die neue Sprache aus:');
 define('TEXT_INFO_DELETE_INTRO', 'Sind Sie sicher, dass Sie diese Sprache l&ouml;schen m&ouml;chten?');
