6a8840e8f0ec4f5ec5f0a2295f113ea26f3134a7
dgw_ <dgw_>
2001-12-09 23:19:09 +0000

    translation

--- admin/admin/includes/languages/espanol/specials.php
+++ admin/admin/includes/languages/espanol/specials.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: specials.php,v 1.6 2001/12/09 20:51:17 dgw_ Exp $
+  $Id: specials.php,v 1.7 2001/12/09 23:19:09 dgw_ Exp $
 
   The Exchange Project - Community Made Shopping!
   http://www.theexchangeproject.org
@@ -21,15 +21,15 @@ define('TABLE_HEADING_ACTION', 'Accion');
 define('TEXT_SPECIALS_PRODUCT', 'Producto:');
 define('TEXT_SPECIALS_SPECIAL_PRICE', 'Precio de Oferta:');
 define('TEXT_SPECIALS_EXPIRES_DATE', 'Expiry Date:');
-define('TEXT_SPECIALS_PRICE_TIP', '<b>Specials Notes:</b><ul><li>You can enter a percentage to deduct in the Specials Price field, for example: <b>20%</b></li><li>If you enter a new price, the decimal separator must be a \'.\' (decimal-point), example: <b>49.99</b></li><li>Deje la fecha de caducidad vacia si no quiere caducidad</li></ul>');
+define('TEXT_SPECIALS_PRICE_TIP', '<b>Notas:</b><ul><li>Puedes introducir un porcentaje de reduccion del precio del producto, por ejemplo: <b>20%</b></li><li>Si por el contrario, introduces un precio de oferta debes de usar el punto como separador decimal \'.\' (punto decimal), por ejemplo: <b>49.99</b></li><li>Deja la fecha de caducidad vacia si no quieres caducidad</li></ul>');
 
 define('TEXT_INFO_DATE_ADDED', 'Fecha Alta:');
 define('TEXT_INFO_LAST_MODIFIED', 'Ultima Modificacion:');
 define('TEXT_INFO_NEW_PRICE', 'Nuevo Precio:');
 define('TEXT_INFO_ORIGINAL_PRICE', 'Precio Original:');
 define('TEXT_INFO_PERCENTAGE', 'Porcentaje:');
-define('TEXT_INFO_EXPIRES_AT', 'Expires At: <b>%s</b>');
-define('TEXT_INFO_STATUS_CHANGE', 'Status Change: %s');
+define('TEXT_INFO_EXPIRES_AT', 'Fecha de Caducidad: <b>%s</b>');
+define('TEXT_INFO_STATUS_CHANGE', 'Cambio de Estado: %s');
 
 define('TEXT_INFO_DELETE_INTRO', '¿Seguro que desea eliminar este precio de oferta?');
 ?>
--- admin/admin/includes/languages/german/specials.php
+++ admin/admin/includes/languages/german/specials.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: specials.php,v 1.7 2001/12/09 20:51:17 dgw_ Exp $
+  $Id: specials.php,v 1.8 2001/12/09 23:14:57 dgw_ Exp $
 
   The Exchange Project - Community Made Shopping!
   http://www.theexchangeproject.org
@@ -21,7 +21,7 @@ define('TABLE_HEADING_ACTION', 'Aktion');
 define('TEXT_SPECIALS_PRODUCT', 'Produkt:');
 define('TEXT_SPECIALS_SPECIAL_PRICE', 'Angebotpreis:');
 define('TEXT_SPECIALS_EXPIRES_DATE', 'Gültig bis:');
-define('TEXT_SPECIALS_PRICE_TIP', '<b>Bemerkung:</b><ul><li>Sie können im Feld Angebotpreis auch prozentuale Werte angeben, z.B.: <b>20%</b></li><li>Wenn Sie einen neuen Preis eingeben, muss der Wert mit einem \'.\' getrennt werden, z.B.: <b>49.99</b></li><li>Leave the expiry date empty for no expiration</li></ul>');
+define('TEXT_SPECIALS_PRICE_TIP', '<b>Bemerkung:</b><ul><li>Sie können im Feld Angebotpreis auch prozentuale Werte angeben, z.B.: <b>20%</b></li><li>Wenn Sie einen neuen Preis eingeben, muss der Wert mit einem \'.\' getrennt werden, z.B.: <b>49.99</b></li><li>Lassen Sie das Feld <b>\'Gültig bis\'</b> leer, wenn der Angebotspreis zeitlich unbegrenzt gelten soll</li></ul>');
 
 define('TEXT_INFO_DATE_ADDED', 'hinzugef&uuml;gt am:');
 define('TEXT_INFO_LAST_MODIFIED', 'Letzte &Auml;nderung:');
