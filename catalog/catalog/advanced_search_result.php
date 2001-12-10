c0a32ad02da0a417ce5d8d941d691b8059a0cd7e
dgw_ <dgw_>
2001-12-10 10:08:32 +0000

    I forgot to name the column (when the specials status is false)

--- catalog/catalog/advanced_search_result.php
+++ catalog/catalog/advanced_search_result.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: advanced_search_result.php,v 1.40 2001/12/09 20:30:21 dgw_ Exp $
+  $Id: advanced_search_result.php,v 1.41 2001/12/10 10:08:32 dgw_ Exp $
 
   The Exchange Project - Community Made Shopping!
   http://www.theexchangeproject.org
@@ -190,7 +190,7 @@
     $select_column_list .= ', ';
   }
 
-  $select_str = "select distinct " . $select_column_list . " m.manufacturers_id, p.products_id, pd.products_name, p.products_price, IF(s.status, s.specials_new_products_price, NULL), IF(s.status, s.specials_new_products_price, p.products_price) as final_price ";
+  $select_str = "select distinct " . $select_column_list . " m.manufacturers_id, p.products_id, pd.products_name, p.products_price, IF(s.status, s.specials_new_products_price, NULL) as specials_new_products_price, IF(s.status, s.specials_new_products_price, p.products_price) as final_price ";
   $from_str = "from " . TABLE_PRODUCTS . " p left join " . TABLE_MANUFACTURERS . " m using(manufacturers_id), " . TABLE_PRODUCTS_DESCRIPTION . " pd left join " . TABLE_SPECIALS . " s on p.products_id = s.products_id";
   $where_str = " where p.products_status = '1' and p.products_id = pd.products_id and pd.language_id = '" . $languages_id . "' ";
 
