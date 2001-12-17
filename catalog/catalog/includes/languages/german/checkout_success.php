1bdc09ec7068fe34ea1aa851b6ded75eef9a2960
dgw_ <dgw_>
2001-12-17 19:52:24 +0000

    Series of changes to make the "account" area (including the address book, account history and login) work under SSL when it is available.

--- catalog/catalog/includes/languages/german/checkout_success.php
+++ catalog/catalog/includes/languages/german/checkout_success.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: checkout_success.php,v 1.7 2001/05/26 16:49:27 hpdl Exp $
+  $Id: checkout_success.php,v 1.8 2001/12/17 19:52:24 dgw_ Exp $
 
   The Exchange Project - Community Made Shopping!
   http://www.theexchangeproject.org
@@ -14,5 +14,5 @@ define('NAVBAR_TITLE_1', 'Kasse');
 define('NAVBAR_TITLE_2', 'Erfolg');
 define('TOP_BAR_TITLE', 'Bestellassistent fertig');
 define('HEADING_TITLE', 'Ihr Bestellung ist ausgef&uuml;hrt worden.');
-define('TEXT_SUCCESS', 'Ihre Bestellung ist eingegangen und wird bearbeitet! Die Lieferung erfolgt innerhalb von ca. 2-5 Werktagen.<br><br>Sie k&ouml;nnen Ihre Bestellung auf der Seite <a href="' . tep_href_link(FILENAME_ACCOUNT, '', 'NONSSL') . '">\'Mein Konto\'</a> nochmals abfragen.<br><br>Falls Sie Fragen bez&uuml;glich Ihrer Bestellung haben, wenden Sie sich an unseren <a href="mailto:' . STORE_OWNER_EMAIL_ADDRESS . '">Vertrieb</a>.<br><br><font size="3">Vielen Dank f&uuml;r Ihre Bestellung!</font>');
+define('TEXT_SUCCESS', 'Ihre Bestellung ist eingegangen und wird bearbeitet! Die Lieferung erfolgt innerhalb von ca. 2-5 Werktagen.<br><br>Sie k&ouml;nnen Ihre Bestellung auf der Seite <a href="' . tep_href_link(FILENAME_ACCOUNT, '', 'SSL') . '">\'Mein Konto\'</a> nochmals abfragen.<br><br>Falls Sie Fragen bez&uuml;glich Ihrer Bestellung haben, wenden Sie sich an unseren <a href="mailto:' . STORE_OWNER_EMAIL_ADDRESS . '">Vertrieb</a>.<br><br><font size="3">Vielen Dank f&uuml;r Ihre Bestellung!</font>');
 ?>
