c6225e1b2ef6578c7bc6084481701720068bd4aa
dgw_ <dgw_>
2002-01-03 16:31:31 +0000

    The DIV tag in this box is ignored by NS4.x making this box look real bad.
    Now it looks ok in all browsers (I hope!)

--- catalog/catalog/includes/boxes/search.php
+++ catalog/catalog/includes/boxes/search.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: search.php,v 1.18 2002/01/03 00:19:55 dgw_ Exp $
+  $Id: search.php,v 1.19 2002/01/03 16:31:31 dgw_ Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -23,8 +23,8 @@
   $hide = tep_hide_session_id();
   $info_box_contents = array();
   $info_box_contents[] = array('form'  => '<form name="quick_find" method="get" action="' . tep_href_link(FILENAME_ADVANCED_SEARCH_RESULT, '', 'NONSSL', false) . '">',
-                               'align' => 'left',
-                               'text'  => '<div align="center">' . $hide . '<input type="text" name="keywords" size="10" maxlength="30" value="' . htmlspecialchars(StripSlashes(@$HTTP_GET_VARS["keywords"])) . '">&nbsp;' . tep_image_submit('button_quick_find.gif', BOX_HEADING_SEARCH) . '</div>' . BOX_SEARCH_TEXT . '<br><div align="center"><a href="' . tep_href_link(FILENAME_ADVANCED_SEARCH, '', 'NONSSL') . '">&nbsp;<b>' . BOX_SEARCH_ADVANCED_SEARCH . '</b>&nbsp;</a></div>'
+                               'align' => 'center',
+                               'text'  => $hide . '<input type="text" name="keywords" size="10" maxlength="30" value="' . htmlspecialchars(StripSlashes(@$HTTP_GET_VARS["keywords"])) . '">&nbsp;' . tep_image_submit('button_quick_find.gif', BOX_HEADING_SEARCH) . '<br>' . BOX_SEARCH_TEXT . '<br><a href="' . tep_href_link(FILENAME_ADVANCED_SEARCH, '', 'NONSSL') . '"><b>' . BOX_SEARCH_ADVANCED_SEARCH . '</b></a>'
                               );
   new infoBox($info_box_contents);
 ?>
