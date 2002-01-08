d560bf3253610c737f13261efeb77a7519ee306d
project3000 <project3000>
2002-01-08 22:58:01 +0000

    tep standard updates

--- catalog/catalog/includes/boxes/tell_a_friend.php
+++ catalog/catalog/includes/boxes/tell_a_friend.php
@@ -1,11 +1,11 @@
 <?php
 /*
-  $Id: tell_a_friend.php,v 1.11 2002/01/03 20:55:11 dgw_ Exp $
+  $Id: tell_a_friend.php,v 1.12 2002/01/08 22:58:01 project3000 Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2001 osCommerce
+  Copyright (c) 2002 osCommerce
 
   Released under the GNU General Public License
 */
@@ -20,13 +20,13 @@
                               );
   new infoBoxHeading($info_box_contents, false, false);
 
-  $hide = '<input type="hidden" name="products_id" value="' . $HTTP_GET_VARS['products_id'] . '">';
+  $hide = tep_draw_hidden_field('products_id', $HTTP_GET_VARS['products_id']);
   $hide .= tep_hide_session_id();
 
   $info_box_contents = array();
   $info_box_contents[] = array('form'  => '<form name="tell_a_friend" method="get" action="' . tep_href_link(FILENAME_TELL_A_FRIEND, '', 'NONSSL', false) . '">',
                                'align' => 'left',
-                               'text'  => '<input type="text" name="send_to" size="10">' . tep_image_submit('button_tell_a_friend.gif', BOX_HEADING_TELL_A_FRIEND) . $hide . '<br>' . BOX_TELL_A_FRIEND_TEXT
+                               'text'  => '<div align="center">' . tep_draw_input_field('send_to', '', 'size="10"') . '&nbsp;' . tep_image_submit('button_tell_a_friend.gif', BOX_HEADING_TELL_A_FRIEND) . $hide . '</div>' . BOX_TELL_A_FRIEND_TEXT
                               );
   new infoBox($info_box_contents);
 ?>
