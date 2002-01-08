5329b6682d774e76600c6fe1d867942387618a36
dgw_ <dgw_>
2002-01-08 12:59:46 +0000

    update

--- admin/admin/includes/languages/espanol/file_manager.php
+++ admin/admin/includes/languages/espanol/file_manager.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: file_manager.php,v 1.8 2002/01/08 02:41:09 hpdl Exp $
+  $Id: file_manager.php,v 1.9 2002/01/08 12:59:46 dgw_ Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -14,7 +14,7 @@ define('HEADING_TITLE', 'Administrador de Archivos');
 
 define('TABLE_HEADING_FILENAME', 'Nombre');
 define('TABLE_HEADING_SIZE', 'Tamaño');
-define('TABLE_HEADING_PERMISSIONS', 'Permissions');
+define('TABLE_HEADING_PERMISSIONS', 'Permisos');
 define('TABLE_HEADING_USER', 'User');
 define('TABLE_HEADING_GROUP', 'Group');
 define('TABLE_HEADING_LAST_MODIFIED', 'Last Modified');
