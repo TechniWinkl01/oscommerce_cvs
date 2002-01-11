e7103dcdc838cc62983cfee57312660e60db16e2
dgw_ <dgw_>
2002-01-11 20:02:25 +0000

    Fixed a bug I introduced earlier on, the array should be initialized outside of the loop.
    Ive tried simplifying the query made here, any feedback on performance is appreciated.
    
    Instead of retreiving random rows, it now picks up the last products that have been bought. The list will change dynamicly as people buy, unless you have cache, of course.
    
    Adding the limit clause to this query can make a great difference, maybe eliminating the need for cache altogether.

--- catalog/catalog/includes/modules/also_purchased_products.php
+++ catalog/catalog/includes/modules/also_purchased_products.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: also_purchased_products.php,v 1.14 2002/01/11 19:20:11 dgw_ Exp $
+  $Id: also_purchased_products.php,v 1.15 2002/01/11 20:02:25 dgw_ Exp $
 
   The Exchange Project - Community Made Shopping!
   http://www.theexchangeproject.org
@@ -13,7 +13,7 @@
 <!-- also_purchased_products //-->
 <?php
   if ($HTTP_GET_VARS['products_id']) {
-    $orders_query = tep_db_query("select distinct p.products_id, p.products_image, pd.products_name from " . TABLE_ORDERS_PRODUCTS . " op1, " . TABLE_ORDERS_PRODUCTS . " op2, " . TABLE_ORDERS . " o, " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd where op1.products_id = '" . $HTTP_GET_VARS['products_id'] . "' and op1.orders_id = op2.orders_id and op2.products_id <> '" . $HTTP_GET_VARS['products_id'] . "' and op2.products_id = p.products_id and op2.products_id = pd.products_id and pd.language_id = '" . $languages_id . "' and op2.orders_id = o.orders_id order by pd.products_name");
+    $orders_query = tep_db_query("select distinct p.products_id, p.products_image, pd.products_name from " . TABLE_ORDERS . " o, " . TABLE_ORDERS_PRODUCTS . " op, " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd where o.orders_id = op.orders_id and op.products_id = p.products_id and p.products_id = pd.products_id and pd.language_id = '" . $languages_id . "' and p.products_id <> '" . $HTTP_GET_VARS['products_id'] . "' order by o.date_purchased desc limit " . MAX_DISPLAY_ALSO_PURCHASED);
     $num_products_ordered = tep_db_num_rows($orders_query);
     if ($num_products_ordered >= MIN_DISPLAY_ALSO_PURCHASED) {
 
@@ -21,20 +21,10 @@
       $info_box_contents[] = array('align' => 'left', 'text' => TEXT_ALSO_PURCHASED_PRODUCTS);
       new contentBoxHeading($info_box_contents);
 
-      // randomly select products from products ordered
-      $rows_to_display = array();
-      srand((double)microtime()*1000000); // seed the random number generator
-      while (sizeof($rows_to_display) < MAX_DISPLAY_ALSO_PURCHASED && sizeof($rows_to_display) < $num_products_ordered) {
-        $random_row = @rand(0, ($num_products_ordered - 1));
-        if (!tep_in_array($random_row, $rows_to_display)) $rows_to_display[sizeof($rows_to_display)] = $random_row;
-      }
-      sort($rows_to_display);
       $row = 0;
       $col = 0;
-      for ($i = 0; $i < sizeof($rows_to_display); $i++) {
-        tep_db_data_seek($orders_query, $rows_to_display[$i]);
-        $orders_values = tep_db_fetch_array($orders_query);
-        $info_box_contents = array();
+      $info_box_contents = array();
+      while ($orders_values = tep_db_fetch_array($orders_query)) {
         $info_box_contents[$row][$col] = array('align' => 'center',
                                                'params' => 'class="smallText" width="33%" valign="top"',
                                                'text' => '<a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $orders_values['products_id']) . '">' . tep_image(DIR_WS_IMAGES . $orders_values['products_image'], $orders_values['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a><br><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $orders_values['products_id']) . '">' . $orders_values['products_name'] . '</a>');
