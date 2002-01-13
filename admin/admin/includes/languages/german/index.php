62206076d8d6c54730b5cf1876ad3a06e4ffdd77
jan0815 <jan0815>
2002-01-13 17:36:31 +0000

    99% multilngual :-) German translations

--- admin/admin/includes/languages/german/index.php
+++ admin/admin/includes/languages/german/index.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: index.php,v 1.1 2002/01/13 17:26:47 hpdl Exp $
+  $Id: index.php,v 1.2 2002/01/13 17:36:31 jan0815 Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -10,10 +10,10 @@
   Released under the GNU General Public License
 */
 
-define('HEADING_TITLE', 'Choose an action..');
+define('HEADING_TITLE', 'Option w&auml;hlen ...');
 
-define('BOX_TITLE_ORDERS', 'Orders');
-define('BOX_TITLE_STATISTICS', 'Statistics');
+define('BOX_TITLE_ORDERS', 'Bestellungen');
+define('BOX_TITLE_STATISTICS', 'Statistik');
 
 define('BOX_ENTRY_SUPPORT_SITE', 'Support Site');
 define('BOX_ENTRY_SUPPORT_FORUMS', 'Support Forums');
@@ -24,16 +24,16 @@ define('BOX_ENTRY_LIVE_DISCUSSIONS', 'Live Discussions');
 define('BOX_ENTRY_CVS_REPOSITORY', 'CVS Repository');
 define('BOX_ENTRY_INFORMATION_PORTAL', 'Information Portal');
 
-define('BOX_ENTRY_CUSTOMERS', 'Customers:');
-define('BOX_ENTRY_PRODUCTS', 'Products:');
-define('BOX_ENTRY_REVIEWS', 'Reviews:');
+define('BOX_ENTRY_CUSTOMERS', 'Kunden:');
+define('BOX_ENTRY_PRODUCTS', 'Produkte:');
+define('BOX_ENTRY_REVIEWS', 'Bewertungen:');
 
-define('CATALOG_CONTENTS', 'Contents');
+define('CATALOG_CONTENTS', 'Inhalt');
 
-define('REPORTS_PRODUCTS', 'Products');
-define('REPORTS_ORDERS', 'Orders');
+define('REPORTS_PRODUCTS', 'Produkte');
+define('REPORTS_ORDERS', 'Bestellungen');
 
-define('TOOLS_BACKUP', 'Backup');
-define('TOOLS_BANNERS', 'Banners');
-define('TOOLS_FILES', 'Files');
+define('TOOLS_BACKUP', 'Datensicherung');
+define('TOOLS_BANNERS', 'Bannerverwaltung');
+define('TOOLS_FILES', 'Dateimanager');
 ?>
