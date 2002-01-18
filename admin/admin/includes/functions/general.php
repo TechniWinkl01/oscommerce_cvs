df61158fafec75a1b5485e2c1f4ae35fe2ef9a2e
hpdl <hpdl>
2002-01-18 12:35:46 +0000

    new function to parse tax values
    this brings in new meaning to the TAX_DECIMAL_PLACES parameter
    
    if that parameter is 0, all leading '0's from the tax value will be removed
    
    for example:
    
    17.5000 becomes 17.5, 17.0000 becomes 17
    
    if the parameter is more than 0, for example 2, the tax value will be
    padded this amount of decimals, for example
    
    17.5000 becomes 17.50, 17.0000 becomes 17.00
    
    no rounding is done here as in the current approach with number_format()

--- admin/admin/includes/functions/general.php
+++ admin/admin/includes/functions/general.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: general.php,v 1.104 2002/01/16 19:54:30 hpdl Exp $
+  $Id: general.php,v 1.105 2002/01/18 12:35:46 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -1042,5 +1042,39 @@ function tep_address_format($format_id, $delivery_values, $html, $boln, $eoln) {
       eval("\$temp=$constant;");
     }
     return $temp;
+  }
+
+////
+// Output the tax percentage with optional padded decimals
+  function tep_display_tax_value($value, $padding = TAX_DECIMAL_PLACES) {
+    if (strpos($value, '.')) {
+      $loop = true;
+      while ($loop) {
+        if (substr($value, -1) == '0') {
+          $value = substr($value, 0, -1);
+        } else {
+          $loop = false;
+          if (substr($value, -1) == '.') {
+            $value = substr($value, 0, -1);
+          }
+        }
+      }
+    }
+
+    if ($padding > 0) {
+      if ($decimal_pos = strpos($value, '.')) {
+        $decimals = strlen(substr($value, ($decimal_pos+1)));
+        for ($i=$decimals; $i<$padding; $i++) {
+          $value .= '0';
+        }
+      } else {
+        $value .= '.';
+        for ($i=0; $i<$padding; $i++) {
+          $value .= '0';
+        }
+      }
+    }
+
+    return $value;
   }
 ?>
