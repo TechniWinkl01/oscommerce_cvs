5ca629e8ae7e9728d00f81a359e21f498e5f4280
harley_vb <harley_vb>
2002-01-19 22:44:34 +0000

    German translations corrected/reviewed

--- admin/admin/includes/languages/german/mail.php
+++ admin/admin/includes/languages/german/mail.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: mail.php,v 1.8 2002/01/18 17:28:53 hpdl Exp $
+  $Id: mail.php,v 1.9 2002/01/19 22:44:34 harley_vb Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -10,16 +10,16 @@
   Released under the GNU General Public License
 */
 
-define('HEADING_TITLE', 'Email an Kunden versenden');
+define('HEADING_TITLE', 'eMail an Kunden versenden');
 
 define('TEXT_CUSTOMER', 'Kunde:');
 define('TEXT_SUBJECT', 'Betreff:');
 define('TEXT_FROM', 'Absender:');
 define('TEXT_MESSAGE', 'Nachricht:');
-define('TEXT_SELECT_CUSTOMER', 'Benutzer Auswählen');
+define('TEXT_SELECT_CUSTOMER', 'Kunden ausw&auml;hlen');
 define('TEXT_ALL_CUSTOMERS', 'Alle Kunden');
 define('TEXT_NEWSLETTER_CUSTOMERS', 'An alle Newsletter-Abonnenten');
 
-define('NOTICE_EMAIL_SENT_TO', 'Notice: Email sent to: %s');
-define('ERROR_NO_CUSTOMER_SELECTED', 'Error: No customer has been selected.');
+define('NOTICE_EMAIL_SENT_TO', 'Hinweis: eMail wurde versendet an: %s');
+define('ERROR_NO_CUSTOMER_SELECTED', 'Fehler: Es wurde kein Kunde ausgew&auml;hlt.');
 ?>
