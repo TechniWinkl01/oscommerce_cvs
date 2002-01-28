985efb766163bdca15d031c066bc04a38a313606
harley_vb <harley_vb>
2002-01-28 09:46:12 +0000

    German translations corrected/reviewed

--- admin/admin/includes/languages/german/geo_zones.php
+++ admin/admin/includes/languages/german/geo_zones.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: geo_zones.php,v 1.6 2002/01/16 15:28:09 hpdl Exp $
+  $Id: geo_zones.php,v 1.7 2002/01/28 09:46:12 harley_vb Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -10,37 +10,37 @@
   Released under the GNU General Public License
 */
 
-define('HEADING_TITLE', 'Tax Zones');
+define('HEADING_TITLE', 'Steuerzonen');
 
-define('TABLE_HEADING_COUNTRY', 'Country');
-define('TABLE_HEADING_COUNTRY_ZONE', 'Zone');
-define('TABLE_HEADING_TAX_ZONES', 'Tax Zones');
-define('TABLE_HEADING_ACTION', 'Action');
+define('TABLE_HEADING_COUNTRY', 'Land');
+define('TABLE_HEADING_COUNTRY_ZONE', 'Bundesland');
+define('TABLE_HEADING_TAX_ZONES', 'Steuerzonen');
+define('TABLE_HEADING_ACTION', 'Aktion');
 
-define('TEXT_INFO_HEADING_NEW_ZONE', 'New Zone');
-define('TEXT_INFO_NEW_ZONE_INTRO', 'Please enter the new zone information');
+define('TEXT_INFO_HEADING_NEW_ZONE', 'Neue Steuerzone');
+define('TEXT_INFO_NEW_ZONE_INTRO', 'Bitte geben Sie die neue Steuerzone mit allen relevanten Daten ein');
 
-define('TEXT_INFO_HEADING_EDIT_ZONE', 'Edit Zone');
-define('TEXT_INFO_EDIT_ZONE_INTRO', 'Please make any necessary changes');
+define('TEXT_INFO_HEADING_EDIT_ZONE', 'Steuerzone bearbeiten');
+define('TEXT_INFO_EDIT_ZONE_INTRO', 'Bitte f&uuml;hren Sie alle notwendigen &Auml;nderungen durch.');
 
-define('TEXT_INFO_HEADING_DELETE_ZONE', 'Delete Zone');
-define('TEXT_INFO_DELETE_ZONE_INTRO', 'Are you sure you want to delete this zone?');
+define('TEXT_INFO_HEADING_DELETE_ZONE', 'Steuerzone l&ouml;schen');
+define('TEXT_INFO_DELETE_ZONE_INTRO', 'Sind Sie sicher, dass Sie diese Steuerzone l&ouml;schen wollen?');
 
-define('TEXT_INFO_HEADING_NEW_SUB_ZONE', 'New Sub Zone');
-define('TEXT_INFO_NEW_SUB_ZONE_INTRO', 'Please enter the new sub zone information');
+define('TEXT_INFO_HEADING_NEW_SUB_ZONE', 'Neue Unterzone');
+define('TEXT_INFO_NEW_SUB_ZONE_INTRO', 'Bitte geben Sie die neue Unterzone mit allen relevanten Daten ein');
 
-define('TEXT_INFO_HEADING_EDIT_SUB_ZONE', 'Edit Sub Zone');
-define('TEXT_INFO_EDIT_SUB_ZONE_INTRO', 'Please make any necessary changes');
+define('TEXT_INFO_HEADING_EDIT_SUB_ZONE', 'Unterzone bearbeiten');
+define('TEXT_INFO_EDIT_SUB_ZONE_INTRO', 'Bitte f&uuml;hren Sie alle notwendigen &Auml;nderungen durch.');
 
-define('TEXT_INFO_HEADING_DELETE_SUB_ZONE', 'Delete Sub Zone');
-define('TEXT_INFO_DELETE_SUB_ZONE_INTRO', 'Are you sure you want to delete this sub zone?');
+define('TEXT_INFO_HEADING_DELETE_SUB_ZONE', 'Unterzone l&ouml;schen');
+define('TEXT_INFO_DELETE_SUB_ZONE_INTRO', 'Sind Sie sicher, dass Sie diese Unterzone l&ouml;schen wollen?');
 
-define('TEXT_INFO_DATE_ADDED', 'Date Added:');
-define('TEXT_INFO_LAST_MODIFIED', 'Last Modified:');
-define('TEXT_INFO_ZONE_NAME', 'Zone Name:');
-define('TEXT_INFO_ZONE_DESCRIPTION', 'Description:');
-define('TEXT_INFO_COUNTRY', 'Country:');
-define('TEXT_INFO_COUNTRY_ZONE', 'Zone:');
+define('TEXT_INFO_DATE_ADDED', 'hinzugef&uuml;gt am:');
+define('TEXT_INFO_LAST_MODIFIED', 'letzte &Auml;nderung:');
+define('TEXT_INFO_ZONE_NAME', 'Name der Steuerzone:');
+define('TEXT_INFO_ZONE_DESCRIPTION', 'Beschreibung:');
+define('TEXT_INFO_COUNTRY', 'Land:');
+define('TEXT_INFO_COUNTRY_ZONE', 'Bundesland:');
 define('TYPE_BELOW', '*');
 define('PLEASE_SELECT', '*');
 ?>
