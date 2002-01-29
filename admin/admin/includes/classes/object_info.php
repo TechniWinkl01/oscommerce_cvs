49d406a26a289dddad6ce411ef843c66536be262
hpdl <hpdl>
2002-01-29 18:00:28 +0000

    leave integers as integers, and arrays as arrays - the rest become strings (until needed in original format)

--- admin/admin/includes/classes/object_info.php
+++ admin/admin/includes/classes/object_info.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: object_info.php,v 1.2 2001/12/30 08:18:46 hpdl Exp $
+  $Id: object_info.php,v 1.3 2002/01/29 18:00:28 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -15,7 +15,13 @@
 // class constructor
     function objectInfo($object_array) {
       while (list($key, $value) = each($object_array)) {
-        $this->$key = tep_db_prepare_input($value);
+        if (is_array($value)) {
+          $this->$key = $value;
+        } elseif (is_int($value)) {
+          $this->$key = $value;
+        } else {
+          $this->$key = tep_db_prepare_input($value);
+        }
       }
     }
   }
