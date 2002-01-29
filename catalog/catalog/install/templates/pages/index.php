50d8920b7c00126ba10ff077fd2eea8fa799bcb5
hpdl <hpdl>
2002-01-29 12:26:21 +0000

    text updates

--- catalog/catalog/install/templates/pages/index.php
+++ catalog/catalog/install/templates/pages/index.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: index.php,v 1.1 2002/01/02 13:02:39 hpdl Exp $
+  $Id: index.php,v 1.2 2002/01/29 12:26:21 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -14,9 +14,9 @@
 
 <p class="pageTitle">Welcome to osCommerce!</p>
 
-<p>osCommerce is an open source e-commerce solution under on going development by the open source community. It's feature packed out-of-the-box installation allows store owners to setup, run, and maintain their online stores with minimum effort and with no costs involved.</p>
+<p>osCommerce is an open source e-commerce solution under on going development by the open source community. Its feature packed out-of-the-box installation allows store owners to setup, run, and maintain their online stores with minimum effort and with no costs involved.</p>
 
-<p>osCommerce combines open source solutions to provide a free and open application development platform, which includes the powerful PHP web scripting language, the stable Apache web server, and the fast MySQL database server.</p>
+<p>osCommerce combines open source solutions to provide a free and open development platform, which includes the <i>powerful</i> PHP web scripting language, the <i>stable</i> Apache web server, and the <i>fast</i> MySQL database server.</p>
 
 <p>With no restrictions or special requirements, osCommerce can be installed on any PHP3 or PHP4 enabled web server, on any environment that PHP and MySQL supports, which includes Linux, Solaris, BSD, and Microsoft Windows environments.</p>
 
