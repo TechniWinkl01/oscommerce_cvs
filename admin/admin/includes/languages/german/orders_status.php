70862d2b0e355eb17697614547b7569fbb0bb1ef
harley_vb <harley_vb>
2002-01-30 11:10:08 +0000

    German translations corrected/reviewed.

--- admin/admin/includes/languages/german/orders_status.php
+++ admin/admin/includes/languages/german/orders_status.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: orders_status.php,v 1.6 2002/01/29 14:43:00 hpdl Exp $
+  $Id: orders_status.php,v 1.7 2002/01/30 11:10:08 harley_vb Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -23,7 +23,7 @@ define('TEXT_INFO_HEADING_NEW_ORDERS_STATUS', 'Neuer Bestellstatus');
 define('TEXT_INFO_HEADING_EDIT_ORDERS_STATUS', 'Bestellstatus bearbeiten');
 define('TEXT_INFO_HEADING_DELETE_ORDERS_STATUS', 'Bestellstatus l&ouml;schen');
 
-define('ERROR_REMOVE_DEFAULT_ORDER_STATUS', 'Error: The default order status can not be removed. Please set another order status as default, and try again.');
+define('ERROR_REMOVE_DEFAULT_ORDER_STATUS', 'Fehler: Der Standard-Bestellstatus kann nicht gel&ouml;scht werden. Bitte definieren Sie einen neuen Standard-Bestellstatus und wiederholen Sie den Vorgang.');
 define('ERROR_STATUS_USED_IN_ORDERS', 'Fehler: Dieser Bestellstatus wird zur Zeit noch bei den Bestellungen verwendet.');
 define('ERROR_STATUS_USED_IN_HISTORY', 'Fehler: Dieser Bestellstatus wird zur Zeit noch in der Bestellhistorie verwendet.');
 ?>
