c083e19e29733ead51993f0ada949667151d183b
harley_vb <harley_vb>
2002-02-19 16:31:15 +0000

    correct the link from 'theexchangeproject.org' to 'oscommerce.com'

--- admin/admin/includes/header.php
+++ admin/admin/includes/header.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: header.php,v 1.15 2002/01/26 17:15:43 hpdl Exp $
+  $Id: header.php,v 1.16 2002/02/19 16:31:15 harley_vb Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -17,12 +17,12 @@
 <table border="0" width="100%" cellspacing="0" cellpadding="0">
   <tr class="header">
     <td valign="middle"><?php echo tep_image(DIR_WS_IMAGES . 'header_exchange_logo.gif', STORE_NAME, '57', '50') . tep_image(DIR_WS_IMAGES . 'pixel_trans.gif', '', '6', '1') . tep_image(DIR_WS_IMAGES . 'header_exchange.gif', STORE_NAME, '351', '50'); ?></td>
-    <td align="right"><?php echo '<a href="http://theexchangeproject.org">' . tep_image(DIR_WS_IMAGES . 'header_support.gif', HEADER_TITLE_SUPPORT_SITE, '50', '50') . '</a>'; ?>&nbsp;&nbsp;<?php echo '<a href="' . DIR_WS_CATALOG . FILENAME_DEFAULT . '">' . tep_image(DIR_WS_IMAGES . 'header_checkout.gif', HEADER_TITLE_ONLINE_CATALOG, '53', '50') . '</a>'; ?>&nbsp;&nbsp;<?php echo '<a href="' . tep_href_link(FILENAME_DEFAULT, '', 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'header_administration.gif', HEADER_TITLE_ADMINISTRATION, '50', '50') . '</a>'; ?>&nbsp;&nbsp;</td>
+    <td align="right"><?php echo '<a href="http://www.oscommerce.com">' . tep_image(DIR_WS_IMAGES . 'header_support.gif', HEADER_TITLE_SUPPORT_SITE, '50', '50') . '</a>'; ?>&nbsp;&nbsp;<?php echo '<a href="' . DIR_WS_CATALOG . FILENAME_DEFAULT . '">' . tep_image(DIR_WS_IMAGES . 'header_checkout.gif', HEADER_TITLE_ONLINE_CATALOG, '53', '50') . '</a>'; ?>&nbsp;&nbsp;<?php echo '<a href="' . tep_href_link(FILENAME_DEFAULT, '', 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . 'header_administration.gif', HEADER_TITLE_ADMINISTRATION, '50', '50') . '</a>'; ?>&nbsp;&nbsp;</td>
   </tr>
 </table>
 <table border="0" width="100%" cellspacing="0" cellpadding="2">
   <tr class="headerNavigation">
     <td class="headerNavigation"><b>&nbsp;&nbsp;<?php echo '<a href="' . tep_href_link(FILENAME_DEFAULT, '', 'NONSSL') . '" class="whitelink">' . HEADER_TITLE_TOP . '</a>'; ?></b></td>
-    <td class="headerNavigation" align="right"><b><?php echo '<a href="http://theexchangeproject.org" class="whitelink">' . HEADER_TITLE_SUPPORT_SITE . '</a>'; ?> &nbsp;|&nbsp; <?php echo '<a href="' . DIR_WS_CATALOG . '" class="whitelink">' . HEADER_TITLE_ONLINE_CATALOG . '</a>'; ?> &nbsp;|&nbsp; <?php echo '<a href="' . tep_href_link(FILENAME_DEFAULT, '', 'NONSSL') . '" class="whitelink">' . HEADER_TITLE_ADMINISTRATION . '</a>'; ?>&nbsp;&nbsp;</b></td>
+    <td class="headerNavigation" align="right"><b><?php echo '<a href="http://www.oscommerce.com" class="whitelink">' . HEADER_TITLE_SUPPORT_SITE . '</a>'; ?> &nbsp;|&nbsp; <?php echo '<a href="' . DIR_WS_CATALOG . '" class="whitelink">' . HEADER_TITLE_ONLINE_CATALOG . '</a>'; ?> &nbsp;|&nbsp; <?php echo '<a href="' . tep_href_link(FILENAME_DEFAULT, '', 'NONSSL') . '" class="whitelink">' . HEADER_TITLE_ADMINISTRATION . '</a>'; ?>&nbsp;&nbsp;</b></td>
   </tr>
 </table>
