0bb346e087d286c90a6b890453a468c51098d4d7
dgw_ <dgw_>
2002-03-06 16:10:52 +0000

    fixed bug 213.

--- admin/admin/customers.php
+++ admin/admin/customers.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: customers.php,v 1.63 2002/02/08 17:39:14 hpdl Exp $
+  $Id: customers.php,v 1.64 2002/03/06 16:10:52 dgw_ Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -446,9 +446,9 @@ function check_form() {
     $search = '';
     if ( ($HTTP_GET_VARS['search']) && (tep_not_null($HTTP_GET_VARS['search'])) ) {
       $keywords = tep_db_input(tep_db_prepare_input($HTTP_GET_VARS['search']));
-      $search = "and (c.customers_lastname like '%" . $keywords . "%' or c.customers_firstname like '%" . $keywords . "%' or c.customers_email_address like '%" . $keywords . "')";
+      $search = "where c.customers_lastname like '%" . $keywords . "%' or c.customers_firstname like '%" . $keywords . "%' or c.customers_email_address like '%" . $keywords . "'";
     }
-    $customers_query_raw = "select c.customers_id, c.customers_lastname, c.customers_firstname, c.customers_email_address, a.entry_country_id from " . TABLE_CUSTOMERS . " c left join " . TABLE_ADDRESS_BOOK . " a on c.customers_default_address_id = a.address_book_id where c.customers_id = a.customers_id " . $search . " order by c.customers_lastname";
+    $customers_query_raw = "select c.customers_id, c.customers_lastname, c.customers_firstname, c.customers_email_address, a.entry_country_id from " . TABLE_CUSTOMERS . " c left join " . TABLE_ADDRESS_BOOK . " a on c.customers_id = a.customers_id and c.customers_default_address_id = a.address_book_id " . $search . " order by c.customers_lastname";
     $customers_split = new splitPageResults($HTTP_GET_VARS['page'], MAX_DISPLAY_SEARCH_RESULTS, $customers_query_raw, $customers_query_numrows);
     $customers_query = tep_db_query($customers_query_raw);
     while ($customers = tep_db_fetch_array($customers_query)) {
