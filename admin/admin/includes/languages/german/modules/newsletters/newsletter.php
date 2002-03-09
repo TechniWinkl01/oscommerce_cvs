b2f62feff5b0033dd1b46e385ec2395fcc86a551
harley_vb <harley_vb>
2002-03-09 20:09:06 +0000

    add a new modular backend to the newsletter feature of osC
    the content of the newsletter is stored in the database, and the sending
    of the content is done via the newsletter modules, which allows each
    module to use their own logic

---
+++ admin/admin/includes/languages/german/modules/newsletters/newsletter.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  $Id: newsletter.php,v 1.1 2002/03/09 20:09:06 harley_vb Exp $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('TEXT_COUNT_CUSTOMERS', 'Kunden, welche das Rundschreiben erhalten: %s');
+?>
