082da9a0c319c400adcea640578575b28293b284
harley_vb <harley_vb>
2002-03-09 17:53:45 +0000

    German translation updates/corrections

--- admin/admin/includes/languages/german/newsletters.php
+++ admin/admin/includes/languages/german/newsletters.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: newsletters.php,v 1.5 2002/03/08 22:10:08 hpdl Exp $
+  $Id: newsletters.php,v 1.6 2002/03/09 17:53:45 harley_vb Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -10,30 +10,30 @@
   Released under the GNU General Public License
 */
 
-define('HEADING_TITLE', 'Newsletter Manager');
+define('HEADING_TITLE', 'Rundschreiben Manager');
 
-define('TABLE_HEADING_NEWSLETTERS', 'Newsletters');
-define('TABLE_HEADING_SIZE', 'Size');
+define('TABLE_HEADING_NEWSLETTERS', 'Rundschreiben');
+define('TABLE_HEADING_SIZE', 'Gr&ouml;sse');
 define('TABLE_HEADING_MODULE', 'Module');
-define('TABLE_HEADING_SENT', 'Sent');
+define('TABLE_HEADING_SENT', 'Gesendet');
 define('TABLE_HEADING_STATUS', 'Status');
-define('TABLE_HEADING_ACTION', 'Action');
+define('TABLE_HEADING_ACTION', 'Aktion');
 
 define('TEXT_NEWSLETTER_MODULE', 'Module:');
-define('TEXT_NEWSLETTER_TITLE', 'Newsletter Title:');
-define('TEXT_NEWSLETTER_CONTENT', 'Content:');
+define('TEXT_NEWSLETTER_TITLE', 'Titel des Rundschreibens:');
+define('TEXT_NEWSLETTER_CONTENT', 'Inhalt:');
 
-define('TEXT_NEWSLETTER_DATE_ADDED', 'Date Added:');
-define('TEXT_NEWSLETTER_DATE_SENT', 'Date Sent:');
+define('TEXT_NEWSLETTER_DATE_ADDED', 'hinzugef&uuml;gt am:');
+define('TEXT_NEWSLETTER_DATE_SENT', 'Datum gesendet:');
 
-define('TEXT_INFO_DELETE_INTRO', 'Are you sure you want to delete this newsletter?');
+define('TEXT_INFO_DELETE_INTRO', 'Sind Sie sicher, dass Sie dieses Rundschreiben l&ouml;schen m&ouml;chten?');
 
-define('TEXT_PLEASE_WAIT', 'Please wait .. sending emails ..<br><br>Please do not interrupt this process!');
-define('TEXT_FINISHED_SENDING_EMAILS', 'Finished sending e-mails!');
+define('TEXT_PLEASE_WAIT', 'Bitte warten Sie .. eMails werden gesendet ..<br><br>Bitte unterbrechen Sie diesen Prozess nicht!');
+define('TEXT_FINISHED_SENDING_EMAILS', 'eMails wurden versendet!');
 
-define('ERROR_NEWSLETTER_TITLE', 'Error: Newsletter title required');
-define('ERROR_NEWSLETTER_MODULE', 'Error: Newsletter module required');
-define('ERROR_REMOVE_UNLOCKED_NEWSLETTER', 'Error: Please lock the newsletter before deleting it.');
-define('ERROR_EDIT_UNLOCKED_NEWSLETTER', 'Error: Please lock the newsletter before editing it.');
-define('ERROR_SEND_UNLOCKED_NEWSLETTER', 'Error: Please lock the newsletter before sending it.');
+define('ERROR_NEWSLETTER_TITLE', 'Fehler: Ein Titel f&uuml;r das Rundschreiben ist erforderlich.');
+define('ERROR_NEWSLETTER_MODULE', 'Fehler: Das Newsletter Modul wird ben&ouml;tigt.');
+define('ERROR_REMOVE_UNLOCKED_NEWSLETTER', 'Fehler: Bitte sperren Sie das Rundschreiben bevor Sie es l&ouml;schen.');
+define('ERROR_EDIT_UNLOCKED_NEWSLETTER', 'Fehler: Bitte sperren Sie das Rundschreiben bevor Sie es bearbeiten.');
+define('ERROR_SEND_UNLOCKED_NEWSLETTER', 'Fehler: Bitte sperren Sie das Rundschreiben bevor Sie es versenden.');
 ?>
