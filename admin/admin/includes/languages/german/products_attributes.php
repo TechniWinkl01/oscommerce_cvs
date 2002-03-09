8dbb0f29b84a030ec5cd820bb6f9eecd71a4a810
harley_vb <harley_vb>
2002-03-09 18:04:51 +0000

    German translations corrected/reviewed

--- admin/admin/includes/languages/german/products_attributes.php
+++ admin/admin/includes/languages/german/products_attributes.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: products_attributes.php,v 1.7 2002/03/09 16:54:04 dgw_ Exp $
+  $Id: products_attributes.php,v 1.8 2002/03/09 18:04:51 harley_vb Exp $
 
   The Exchange Project - Community Made Shopping!
   http://www.theexchangeproject.org
@@ -20,17 +20,17 @@ define('TABLE_HEADING_PRODUCT', 'Artikelname');
 define('TABLE_HEADING_OPT_NAME', 'Optionsname');
 define('TABLE_HEADING_OPT_VALUE', 'Optionswert');
 define('TABLE_HEADING_OPT_PRICE', 'Preis');
-define('TABLE_HEADING_OPT_PRICE_PREFIX', 'Pr&auml;fix (+/-)');
+define('TABLE_HEADING_OPT_PRICE_PREFIX', 'Vorzeichen (+/-)');
 define('TABLE_HEADING_ACTION', 'Aktion');
-define('TABLE_HEADING_DOWNLOAD', 'Downloadable products:');
-define('TABLE_TEXT_FILENAME', 'Filename:');
-define('TABLE_TEXT_MAX_DAYS', 'Expiry days:');
-define('TABLE_TEXT_MAX_COUNT', 'Maximum download count:');
+define('TABLE_HEADING_DOWNLOAD', 'Downloadbare Produkte:');
+define('TABLE_TEXT_FILENAME', 'Dateiname:');
+define('TABLE_TEXT_MAX_DAYS', 'Zeitspanne:');
+define('TABLE_TEXT_MAX_COUNT', 'Maximale Anzahl zum Herunterladen:');
 
 define('MAX_ROW_LISTS_OPTIONS', 10);
 
 define('TEXT_WARNING_OF_DELETE', 'Mit dieser Option sind Artikel, sowie Optionsmerkmale verbunden - L&ouml;schen wird nicht empfohlen.');
 define('TEXT_OK_TO_DELETE', 'Mit dieser Option sind keine Artikel, sowie Optionsmerkmale verbunden - Sie kann gel&ouml;scht werden.');
-define('TEXT_OPTION_ID', 'Option ID');
-define('TEXT_OPTION_NAME', 'Option Name');
+define('TEXT_OPTION_ID', 'Options ID');
+define('TEXT_OPTION_NAME', 'Optionsname');
 ?>
