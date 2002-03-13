e9caa8bd2bbbd8ae07b232d1f137b34e78346cea
harley_vb <harley_vb>
2002-03-13 15:39:25 +0000

    German translations corrected/reviewed

--- catalog/catalog/includes/languages/german/checkout_success.php
+++ catalog/catalog/includes/languages/german/checkout_success.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: checkout_success.php,v 1.13 2002/03/13 13:07:06 hpdl Exp $
+  $Id: checkout_success.php,v 1.14 2002/03/13 15:39:25 harley_vb Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -14,10 +14,10 @@ define('NAVBAR_TITLE_1', 'Kasse');
 define('NAVBAR_TITLE_2', 'Erfolg');
 define('HEADING_TITLE', 'Ihr Bestellung ist ausgef&uuml;hrt worden.');
 define('TEXT_SUCCESS', 'Ihre Bestellung ist eingegangen und wird bearbeitet! Die Lieferung erfolgt innerhalb von ca. 2-5 Werktagen.');
-define('TEXT_NOTIFY_PRODUCTS', 'Please notify me of updates to the products I have selected below:');
-define('TEXT_SEE_ORDERS', 'You can view your order history by going to the <a href="' . tep_href_link(FILENAME_ACCOUNT, '', 'SSL') . '">\'My Account\'</a> page and by clicking on <a href="' . tep_href_link(FILENAME_ACCOUNT_HISTORY, '', 'SSL') . '">\'History\'</a>.');
-define('TEXT_CONTACT_STORE_OWNER', 'Please direct any questions you have to the <a href="' . tep_href_link(FILENAME_CONTACT_US) . '">store owner</a>.');
-define('TEXT_THANKS_FOR_SHOPPING', 'Thanks for shopping with us online!');
+define('TEXT_NOTIFY_PRODUCTS', 'Bitte benachrichtigen Sie mich &uuml;ber Aktuelles zu folgenden Produkten:');
+define('TEXT_SEE_ORDERS', 'Sie k&ouml;nnen Ihre Bestellung(en) auf der Seite <a href="' . tep_href_link(FILENAME_ACCOUNT, '', 'SSL') . '"><u>\'Mein Konto\'</a></u> jederzeit einsehen und sich dort auch Ihre <a href="' . tep_href_link(FILENAME_ACCOUNT_HISTORY, '', 'SSL') . '"><u>\'Bestell&uuml;bersicht\'</u></a> anzeigen lassen.');
+define('TEXT_CONTACT_STORE_OWNER', 'Falls Sie Fragen bez&uuml;glich Ihrer Bestellung haben, wenden Sie sich an unseren <a href="' . tep_href_link(FILENAME_CONTACT_US) . '"><u>Vertrieb</u></a>.');
+define('TEXT_THANKS_FOR_SHOPPING', 'Wir danken Ihnen f&uuml;r Ihren Online-Einkauf!');
 define('TABLE_HEADING_DOWNLOAD_DATE', 'herunterladen m&ouml;glich bis:');
 define('TABLE_HEADING_DOWNLOAD_COUNT', 'max. Anz. Downloads');
 define('HEADING_DOWNLOAD', 'Artikel herunterladen:');
