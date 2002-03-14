8671fbc1d30a8d1b3510d3a580c5d2fbb721038c
hpdl <hpdl>
2002-03-14 21:52:43 +0000

    standard updates
    the whos online feature should now be able to read the customers shopping
    cart on servers that have (native) file based sessions

--- admin/admin/includes/functions/sessions.php
+++ admin/admin/includes/functions/sessions.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: sessions.php,v 1.4 2001/09/30 16:22:15 mbs Exp $
+  $Id: sessions.php,v 1.5 2002/03/14 21:52:43 hpdl Exp $
 
   The Exchange Project - Community Made Shopping!
   http://www.theexchangeproject.org
@@ -11,58 +11,52 @@
 */
 
   function tep_session_start() {
-
     return session_start();
-
   }
 
   function tep_session_register($variable) {
-
     return session_register($variable);
-
   }
 
   function tep_session_is_registered($variable) {
-
     return session_is_registered($variable);
-
   }
 
   function tep_session_unregister($variable) {
-
     return session_unregister($variable);
-
   }
 
-  function tep_session_id($sessid='') {
-
-    if ($sessid) 
-       return session_id($sessid);
-    else
-       return session_id();
-      
+  function tep_session_id($sessid = '') {
+    if ($sessid != '') {
+      return session_id($sessid);
+    } else {
+      return session_id();
+    }
   }
 
-  function tep_session_name($name='') {
-
-    if ($name)
+  function tep_session_name($name = '') {
+    if ($name != '') {
       return session_name($name);
-    else
+    } else {
       return session_name();
-
+    }
   }
 
   function tep_session_close() {
-
     if (function_exists('session_close')) {
       return session_close();
     }
-
   }
 
   function tep_session_destroy() {
-
     return session_destroy();
+  }
 
+  function tep_session_save_path($path = '') {
+    if ($path != '') {
+      return session_save_path($path);
+    } else {
+      return session_save_path();
+    }
   }
 ?>
--- admin/admin/whos_online.php
+++ admin/admin/whos_online.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: whos_online.php,v 1.20 2002/01/28 12:29:55 harley_vb Exp $
+  $Id: whos_online.php,v 1.21 2002/03/14 21:52:43 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -38,7 +38,7 @@
 <!-- left_navigation_eof //-->
     </table></td>
 <!-- body_text //-->
-    <td width="100%" valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="0">
+    <td width="100%" valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="2">
       <tr>
         <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
           <tr>
@@ -73,7 +73,7 @@
     if ( ((!$HTTP_GET_VARS['info']) || (@$HTTP_GET_VARS['info'] == $whos_online['session_id'])) && (!$info) ) {
       $info = $whos_online['session_id'];
     }
-    if ($whos_online['session_id'] == @$info) {
+    if ($whos_online['session_id'] == $info) {
       echo '              <tr class="selectedRow">' . "\n";
     } else {
       echo '              <tr class="tableRow" onmouseover="this.className=\'tableRowOver\';this.style.cursor=\'hand\'" onmouseout="this.className=\'tableRow\'" onclick="document.location.href=\'' . tep_href_link(FILENAME_WHOS_ONLINE, tep_get_all_get_params(array('info', 'action')) . 'info=' . $whos_online['session_id'], 'NONSSL') . '\'">' . "\n";
@@ -85,7 +85,7 @@
                 <td class="tableData" align="center"><?php echo $whos_online['ip_address']; ?></td>
                 <td class="tableData"><?php echo date('H:i:s', $whos_online['time_entry']); ?></td>
                 <td class="tableData" align="center"><?php echo date('H:i:s', $whos_online['time_last_click']); ?></td>
-                <td class="tableData"><?php if (eregi('^(.*)' . tep_session_name() . '=[a-f,0-9]+[&]*(.*)', $whos_online['last_page_url'], $array)) echo $array[1] . $array[2]; else echo $whos_online['last_page_url']; ?>&nbsp</td>
+                <td class="tableData"><?php if (eregi('^(.*)' . tep_session_name() . '=[a-f,0-9]+[&]*(.*)', $whos_online['last_page_url'], $array)) { echo $array[1] . $array[2]; } else { echo $whos_online['last_page_url']; } ?>&nbsp;</td>
               </tr>
 <?php
   }
@@ -97,42 +97,43 @@
                 <td class="smallText" colspan="7"><?php echo sprintf(TEXT_NUMBER_OF_CUSTOMERS, tep_db_num_rows($whos_online_query)); ?></td>
               </tr>
             </table></td>
-            <td width="25%" valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="0">
 <?php
-  $info_box_contents = array();
-  $info_box_contents[] = array('align' => 'left', 'text' => '&nbsp;<b>' . TABLE_HEADING_SHOPPING_CART . '</b>');
-?>
-              <tr class="boxHeading">
-                <td><?php new infoBoxHeading($info_box_contents); ?></td>
-              </tr>
-              <tr class="boxHeading">
-                <td><?php echo tep_draw_separator(); ?></td>
-              </tr>
-<?php
-  $info_box_contents = array();
-  $session_data = tep_db_query("select value from " . TABLE_SESSIONS . " WHERE sesskey = '$info'");
-  if (tep_db_num_rows($session_data)) {
-    $session_data = tep_db_fetch_array($session_data);
-    $session_data = trim($session_data['value']);
+  $heading = array();
+  $contents = array();
+  if ($info) {
+    $heading[] = array('text' => '<b>' . TABLE_HEADING_SHOPPING_CART . '</b>');
+
+    $session_data = tep_db_query("select value from " . TABLE_SESSIONS . " WHERE sesskey = '" . $info . "'");
+    if (tep_db_num_rows($session_data)) {
+      $session_data = trim(tep_db_fetch_array($session_data));
+    } else {
+      $session_data = @file(tep_session_save_path() . '/sess_' . $info);
+      $session_data = trim($session_data[0]);
+    }
     session_decode($session_data);
 
     $products = $cart->get_products();
     for ($i=0; $i<sizeof($products); $i++) {
-      $info_box_contents[] = array('text' => $products[$i]['quantity'] . 'x' . $products[$i]['name']);
+      $contents[] = array('text' => $products[$i]['quantity'] . ' x ' . $products[$i]['name']);
     }
-    $info_box_contents[] = array('text' => tep_black_line());
-    $info_box_contents[] = array('align' => 'right',
-                                 'text'  => TEXT_SHOPPING_CART_SUBTOTAL . ' ' . tep_currency_format($cart->show_total())
-                                );
+
+    if (sizeof($contents) > 0) {
+      $contents[] = array('text' => tep_draw_separator('pixel_black.gif', '100%', '1'));
+      $contents[] = array('align' => 'right', 'text'  => TEXT_SHOPPING_CART_SUBTOTAL . ' ' . tep_currency_format($cart->show_total()));
+    } else {
+      $contents[] = array('text' => '');
+    }
+  }
+
+  if ( (tep_not_null($heading)) && (tep_not_null($contents)) ) {
+    echo '            <td width="25%" valign="top">' . "\n";
+
+    $box = new box;
+    echo $box->infoBox($heading, $contents);
+
+    echo '            </td>' . "\n";
   }
 ?>
-              <tr>
-                <td class="box"><?php new infoBox($info_box_contents); ?></td>
-              </tr>
-              <tr>
-                <td class="box"><?php echo tep_draw_separator(); ?></td>
-              </tr>
-            </table></td>
           </tr>
         </table></td>
       </tr>
@@ -145,6 +146,7 @@
 <!-- footer //-->
 <?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
 <!-- footer_eof //-->
+<br>
 </body>
 </html>
 <?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
