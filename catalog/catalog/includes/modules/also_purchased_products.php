2710917b849788afb2529507c6270241572d03ed
hpdl <hpdl>
2002-03-15 01:00:43 +0000

    fixed the 'customer also purchased' module to only display matching products (from same orders)

--- catalog/catalog/includes/modules/also_purchased_products.php
+++ catalog/catalog/includes/modules/also_purchased_products.php
@@ -1,22 +1,22 @@
 <?php
 /*
-  $Id: also_purchased_products.php,v 1.16 2002/01/11 22:17:57 dgw_ Exp $
+  $Id: also_purchased_products.php,v 1.17 2002/03/15 01:00:43 hpdl Exp $
 
-  The Exchange Project - Community Made Shopping!
-  http://www.theexchangeproject.org
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
 
-  Copyright (c) 2000,2001 The Exchange Project
+  Copyright (c) 2002 osCommerce
 
   Released under the GNU General Public License
 */
+
+  if ($HTTP_GET_VARS['products_id']) {
+    $orders_query = tep_db_query("select distinct p.products_id, p.products_image, pd.products_name from orders_products opa, orders_products opb, orders o, products p, products_description pd where opa.products_id = '" . $HTTP_GET_VARS['products_id'] . "' and opa.orders_id = opb.orders_id and opb.products_id != '" . $HTTP_GET_VARS['products_id'] . "' and opb.products_id = p.products_id and p.products_id = pd.products_id and pd.language_id = '" . $languages_id . "' and opb.orders_id = o.orders_id order by o.date_purchased desc limit " . MAX_DISPLAY_ALSO_PURCHASED);
+    $num_products_ordered = tep_db_num_rows($orders_query);
+    if ($num_products_ordered >= MIN_DISPLAY_ALSO_PURCHASED) {
 ?>
 <!-- also_purchased_products //-->
 <?php
-  if ($HTTP_GET_VARS['products_id']) {
-    $orders_query = tep_db_query("select p.products_id, p.products_image, pd.products_name from " . TABLE_ORDERS . " o, " . TABLE_ORDERS_PRODUCTS . " op, " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd where o.orders_id = op.orders_id and op.products_id = p.products_id and p.products_id = pd.products_id and pd.language_id = '" . $languages_id . "' and p.products_id <> '" . $HTTP_GET_VARS['products_id'] . "' group by p.products_id order by o.date_purchased desc limit " . MAX_DISPLAY_ALSO_PURCHASED);
-    $num_products_ordered = tep_db_num_rows($orders_query);
-    if ($num_products_ordered >= MIN_DISPLAY_ALSO_PURCHASED) {
-
       $info_box_contents = array();
       $info_box_contents[] = array('align' => 'left', 'text' => TEXT_ALSO_PURCHASED_PRODUCTS);
       new contentBoxHeading($info_box_contents);
@@ -24,10 +24,10 @@
       $row = 0;
       $col = 0;
       $info_box_contents = array();
-      while ($orders_values = tep_db_fetch_array($orders_query)) {
+      while ($orders = tep_db_fetch_array($orders_query)) {
         $info_box_contents[$row][$col] = array('align' => 'center',
                                                'params' => 'class="smallText" width="33%" valign="top"',
-                                               'text' => '<a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $orders_values['products_id']) . '">' . tep_image(DIR_WS_IMAGES . $orders_values['products_image'], $orders_values['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a><br><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $orders_values['products_id']) . '">' . $orders_values['products_name'] . '</a>');
+                                               'text' => '<a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $orders['products_id']) . '">' . tep_image(DIR_WS_IMAGES . $orders['products_image'], $orders['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a><br><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $orders['products_id']) . '">' . $orders['products_name'] . '</a>');
         $col ++;
         if ($col > 2) {
           $col = 0;
