3f71ee6ba4d38620d93bd3d289e1479edbcf7e52
hpdl <hpdl>
2002-03-17 00:46:10 +0000

    new buttons for the new design

--- admin/admin/customers.php
+++ admin/admin/customers.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: customers.php,v 1.65 2002/03/16 00:50:37 hpdl Exp $
+  $Id: customers.php,v 1.66 2002/03/17 00:46:10 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -104,10 +104,32 @@
   if ($HTTP_GET_VARS['action'] == 'edit') {
 ?>
 <script language="javascript"><!--
+document.write('<style type="text/css"><!--');
+document.write('.collapse { position: absolute; visibility: hidden; }');
+document.write('.expand { position: relative; visibility: visible; }');
+document.write('//--></style>');
+
+function show(ement) {
+  ement.style.position = 'relative';
+  ement.style.visibility = 'visible';
+}
+
+function hide(ement) {
+  ement.style.position = 'absolute';
+  ement.style.visibility = 'hidden';
+}
+
 function resetStateText(theForm) {
   theForm.entry_state.value = '';
+
   if (theForm.entry_zone_id.options.length > 1) {
     theForm.entry_state.value = '<?php echo JS_STATE_SELECT; ?>';
+
+    show(theForm.entry_zone_id);
+    hide(theForm.entry_state);
+  } else {
+    hide(theForm.entry_zone_id);
+    show(theForm.entry_state);
   }
 }
 
@@ -140,64 +162,49 @@ function check_form() {
   var error = 0;
   var error_message = "<?php echo JS_ERROR; ?>";
 
-  var customers_firstname = document.customers.customers_firstname.value;
-  var customers_lastname = document.customers.customers_lastname.value;
-<?php if (ACCOUNT_COMPANY == 'true') echo 'var entry_company = document.customers.entry_company.value;' . "\n"; ?>
-<?php if (ACCOUNT_DOB == 'true') echo 'var customers_dob = document.customers.customers_dob.value;' . "\n"; ?>
-  var customers_email_address = document.customers.customers_email_address.value;  
-  var entry_street_address = document.customers.entry_street_address.value;
-  var entry_postcode = document.customers.entry_postcode.value;
-  var entry_city = document.customers.entry_city.value;
-  var customers_telephone = document.customers.customers_telephone.value;
+  var frm = document.customers;
+  var erfrm = document.error_messages;
+  var radioGroup = false;
 
-<?php if (ACCOUNT_GENDER == 'true') { ?>
-  if (document.customers.customers_gender[0].checked || document.customers.customers_gender[1].checked) {
-  } else {
-    error_message = error_message + "<?php echo JS_GENDER; ?>";
-    error = 1;
-  }
-<?php } ?>
-
-  if (customers_firstname = "" || customers_firstname.length < <?php echo ENTRY_FIRST_NAME_MIN_LENGTH; ?>) {
-    error_message = error_message + "<?php echo JS_FIRST_NAME; ?>";
-    error = 1;
-  }
-
-  if (customers_lastname = "" || customers_lastname.length < <?php echo ENTRY_LAST_NAME_MIN_LENGTH; ?>) {
-    error_message = error_message + "<?php echo JS_LAST_NAME; ?>";
-    error = 1;
-  }
-
-<?php if (ACCOUNT_DOB == 'true') { ?>
-  if (customers_dob = "" || customers_dob.length < <?php echo ENTRY_DOB_MIN_LENGTH; ?>) {
-    error_message = error_message + "<?php echo JS_DOB; ?>";
-    error = 1;
-  }
-<?php } ?>
-
-  if (customers_email_address = "" || customers_email_address.length < <?php echo ENTRY_EMAIL_ADDRESS_MIN_LENGTH; ?>) {
-    error_message = error_message + "<?php echo JS_EMAIL_ADDRESS; ?>";
-    error = 1;
-  }
-
-  if (entry_street_address = "" || entry_street_address.length < <?php echo ENTRY_STREET_ADDRESS_MIN_LENGTH; ?>) {
-    error_message = error_message + "<?php echo JS_ADDRESS; ?>";
-    error = 1;
-  }
-
-  if (entry_postcode = "" || entry_postcode.length < <?php echo ENTRY_POSTCODE_MIN_LENGTH; ?>) {
-    error_message = error_message + "<?php echo JS_POST_CODE; ?>";
-    error = 1;
-  }
-
-  if (entry_city = "" || entry_city.length < <?php echo ENTRY_CITY_MIN_LENGTH; ?>) {
-    error_message = error_message + "<?php echo JS_CITY; ?>";
-    error = 1;
+  for (var i=0; i<frm.length; i++) {
+    if (frm.elements[i].type == 'text') {
+      if (eval(erfrm[frm.elements[i].name+'_error'])) {
+        if (frm.elements[i].value == '' || frm.elements[i].length < erfrm[frm.elements[i].name+'_min'].value) {
+          error_message = error_message + erfrm[frm.elements[i].name+'_error'].value + "\n";
+          error = 1;
+        }
+      }
+    } else if (frm.elements[i].type == 'radio') {
+      if ( (eval(erfrm[frm.elements[i].name+'_error'])) && (!radioGroup) ) {
+        if (!radioGroup) radioGroup = frm.elements[i].name;
+        var radio = 'frm.'+frm.elements[i].name;
+        if (!eval(radio+'.length')) {
+        } else {
+          var control = false;
+          for (var j=0; j<eval(radio+'.length'); j++) {
+            if (eval(radio+'['+j+'].checked')) {
+              control = true;
+            }
+          }
+          if (!control) {
+            error_message = error_message + erfrm[frm.elements[i].name+'_error'].value + "\n";
+            error = 1;
+          }
+        }
+      }
+    } else if (frm.elements[i].type == 'select-one') {
+      if (eval(erfrm[frm.elements[i].name+'_error'])) {
+        if (frm.elements[i].value == 0) {
+          error_message = error_message + erfrm[frm.elements[i].name+'_error'].value + "\n";
+          error = 1;
+        }
+      }
+    }
   }
 
 <?php if (ACCOUNT_STATE == 'true') { ?>
   if (document.customers.entry_zone_id.options.length <= 1) {
-    if (document.customers.entry_state.value == "" || document.customers.entry_state.length < 4 ) {
+    if (document.customers.entry_state.value == "" || document.customers.entry_state.length < <?php echo ENTRY_STATE_MIN_LENGTH; ?> ) {
        error_message = error_message + "<?php echo JS_STATE; ?>";
        error = 1;
     }
@@ -209,16 +216,6 @@ function check_form() {
     }
   }
 <?php } ?>
-
-  if (document.customers.entry_country_id.value == 0) {
-    error_message = error_message + "<?php echo JS_COUNTRY; ?>";
-    error = 1;
-  }
-
-  if (customers_telephone = "" || customers_telephone.length < <?php echo ENTRY_TELEPHONE_MIN_LENGTH; ?>) {
-    error_message = error_message + "<?php echo JS_TELEPHONE; ?>";
-    error = 1;
-  }
 
   if (error == 1) {
     alert(error_message);
@@ -256,6 +253,26 @@ function check_form() {
     $newsletter_array = array(array('id' => '1', 'text' => ENTRY_NEWSLETTER_YES),
                               array('id' => '0', 'text' => ENTRY_NEWSLETTER_NO));
 ?>
+<form name="error_messages">
+<?php if (ACCOUNT_GENDER == 'true') echo '<input type="hidden" name="customers_gender_error" value="' . JS_GENDER . '">'; ?>
+<input type="hidden" name="customers_firstname_min" value="<?php echo ENTRY_FIRST_NAME_MIN_LENGTH; ?>">
+<input type="hidden" name="customers_firstname_error" value="<?php echo JS_FIRST_NAME; ?>">
+<input type="hidden" name="customers_lastname_min" value="<?php echo ENTRY_LAST_NAME_MIN_LENGTH; ?>">
+<input type="hidden" name="customers_lastname_error" value="<?php echo JS_LAST_NAME; ?>">
+<input type="hidden" name="customers_dob_min" value="<?php echo ENTRY_DOB_MIN_LENGTH; ?>">
+<input type="hidden" name="customers_dob_error" value="<?php echo JS_DOB; ?>">
+<input type="hidden" name="customers_email_address_min" value="<?php echo ENTRY_EMAIL_ADDRESS_MIN_LENGTH; ?>">
+<input type="hidden" name="customers_email_address_error" value="<?php echo JS_EMAIL_ADDRESS; ?>">
+<input type="hidden" name="entry_street_address_min" value="<?php echo ENTRY_STREET_ADDRESS_MIN_LENGTH; ?>">
+<input type="hidden" name="entry_street_address_error" value="<?php echo JS_ADDRESS; ?>">
+<input type="hidden" name="entry_postcode_min" value="<?php echo ENTRY_POSTCODE_MIN_LENGTH; ?>">
+<input type="hidden" name="entry_postcode_error" value="<?php echo JS_POST_CODE; ?>">
+<input type="hidden" name="entry_city_min" value="<?php echo ENTRY_CITY_MIN_LENGTH; ?>">
+<input type="hidden" name="entry_city_error" value="<?php echo JS_CITY; ?>">
+<input type="hidden" name="entry_country_id_error" value="<?php echo JS_COUNTRY; ?>">
+<input type="hidden" name="customers_telephone_min" value="<?php echo ENTRY_TELEPHONE_MIN_LENGTH; ?>">
+<input type="hidden" name="customers_telephone_error" value="<?php echo JS_TELEPHONE; ?>">
+</form>
       <tr>
         <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
           <tr>
@@ -365,11 +382,7 @@ function check_form() {
 ?>
           <tr>
             <td class="main"><?php echo ENTRY_STATE; ?></td>
-            <td class="main"><?php echo tep_draw_pull_down_menu('entry_zone_id', tep_prepare_country_zones_pull_down($cInfo->entry_country_id), $cInfo->entry_zone_id, 'onChange="resetStateText(this.form)";'); ?></td>
-          </tr>
-          <tr>
-            <td class="main">&nbsp;</td>
-            <td class="main"><?php echo tep_draw_input_field('entry_state', $cInfo->entry_state, 'maxlength="32" onChange="resetZoneSelected(this.form);"'); ?></td>
+            <td class="main"><div><?php echo tep_draw_pull_down_menu('entry_zone_id', tep_prepare_country_zones_pull_down($cInfo->entry_country_id), $cInfo->entry_zone_id, 'onChange="resetStateText(this.form);" class="expand"') . '</div><div>' . tep_draw_input_field('entry_state', $cInfo->entry_state, 'maxlength="32" onChange="resetZoneSelected(this.form);" class="collapse"'); ?></div></td>
           </tr>
 <?php
     }
