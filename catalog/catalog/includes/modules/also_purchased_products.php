dceccd49f36e05951781088853ded7004448e8be
project3000 <project3000>
2002-03-23 09:26:16 +0000

    replaced hard coded table names with defines from application_top
    thanks for the report victor!

--- catalog/catalog/includes/modules/also_purchased_products.php
+++ catalog/catalog/includes/modules/also_purchased_products.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: also_purchased_products.php,v 1.17 2002/03/15 01:00:43 hpdl Exp $
+  $Id: also_purchased_products.php,v 1.18 2002/03/23 09:26:16 project3000 Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -11,7 +11,7 @@
 */
 
   if ($HTTP_GET_VARS['products_id']) {
-    $orders_query = tep_db_query("select distinct p.products_id, p.products_image, pd.products_name from orders_products opa, orders_products opb, orders o, products p, products_description pd where opa.products_id = '" . $HTTP_GET_VARS['products_id'] . "' and opa.orders_id = opb.orders_id and opb.products_id != '" . $HTTP_GET_VARS['products_id'] . "' and opb.products_id = p.products_id and p.products_id = pd.products_id and pd.language_id = '" . $languages_id . "' and opb.orders_id = o.orders_id order by o.date_purchased desc limit " . MAX_DISPLAY_ALSO_PURCHASED);
+    $orders_query = tep_db_query("select distinct p.products_id, p.products_image, pd.products_name from " . TABLE_ORDERS_PRODUCTS . " opa, " . TABLE_ORDERS_PRODUCTS . " opb, " . TABLE_ORDERS . " o, " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd where opa.products_id = '" . $HTTP_GET_VARS['products_id'] . "' and opa.orders_id = opb.orders_id and opb.products_id != '" . $HTTP_GET_VARS['products_id'] . "' and opb.products_id = p.products_id and p.products_id = pd.products_id and pd.language_id = '" . $languages_id . "' and opb.orders_id = o.orders_id order by o.date_purchased desc limit " . MAX_DISPLAY_ALSO_PURCHASED);
     $num_products_ordered = tep_db_num_rows($orders_query);
     if ($num_products_ordered >= MIN_DISPLAY_ALSO_PURCHASED) {
 ?>
