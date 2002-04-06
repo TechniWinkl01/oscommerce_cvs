3f0a39f7751227718f2b8320cc3f05cc86ecb0cb
hpdl <hpdl>
2002-04-06 15:46:43 +0000

    updates to use the new order class
    * note: until the order storage table is implemented (which will be done
    soon) the account history page may display the wrong order total
    information as it is using the order_total modules (which it should not do)

--- catalog/catalog/account_history_info.php
+++ catalog/catalog/account_history_info.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: account_history_info.php,v 1.73 2002/03/31 20:10:42 clescuyer Exp $
+  $Id: account_history_info.php,v 1.74 2002/04/06 15:46:43 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -27,9 +27,10 @@
 
   $location = ' &raquo; <a href="' . tep_href_link(FILENAME_ACCOUNT, '', 'SSL') . '" class="headerNavigation">' . NAVBAR_TITLE_1 . '</a> &raquo; <a href="' . tep_href_link(FILENAME_ACCOUNT_HISTORY, '', 'SSL') . '" class="headerNavigation">' . NAVBAR_TITLE_2 . '</a> &raquo; <a href="' . tep_href_link(FILENAME_ACCOUNT_HISTORY_INFO, 'order_id=' . $HTTP_GET_VARS['order_id'], 'SSL') . '" class="headerNavigation">' . NAVBAR_TITLE_3 . '</a>';
 
-// load payment modules as objects
-  require(DIR_WS_CLASSES . 'payment.php');
-  $payment_modules = new payment;
+  require(DIR_WS_CLASSES . 'order_total.php');
+  require(DIR_WS_CLASSES . 'order.php');
+  $order = new order($HTTP_GET_VARS['order_id']);
+  $order_total_modules = new order_total;
 ?>
 <!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html <?php echo HTML_PARAMS; ?>>
@@ -67,102 +68,56 @@
       </tr>
       <tr>
         <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
-
+          <tr>
+            <td class="tableHeading" colspan="2"><?php echo TABLE_HEADING_PRODUCTS; ?></td>
+            <td class="tableHeading" align="right"><?php echo TABLE_HEADING_TAX; ?></td>
+            <td class="tableHeading" align="right"><?php echo TABLE_HEADING_TOTAL; ?></td>
+          </tr>
+          <tr>
+            <td colspan="4"><?php echo tep_draw_separator(); ?></td>
+          </tr>
 <?php
-  $order_currency_query = tep_db_query("select currency, currency_value from " . TABLE_ORDERS . " where orders_id = '" . $HTTP_GET_VARS['order_id'] . "'");
-  $order_currency = tep_db_fetch_array($order_currency_query);
+  for ($i=0; $i<sizeof($order->products); $i++) {
+      echo '          <tr>' . "\n" .
+           '            <td class="main" valign="top" align="right" width="30">' . $order->products[$i]['qty'] . '&nbsp;x</td>' . "\n" .
+           '            <td class="main" valign="top">' . $order->products[$i]['name'];
 
-  $orders_products_query = tep_db_query("select products_id, products_name, products_price, final_price, products_tax, products_quantity, orders_products_id 
-                                         from " . TABLE_ORDERS_PRODUCTS . " 
-                                         where orders_id = '" . $HTTP_GET_VARS['order_id'] . "'");
-  $total_cost = 0;
-  $total_tax = 0;
-  $i = 0;
-  while ($orders_products = tep_db_fetch_array($orders_products_query)) {
-    $products[$i]['name'] = $orders_products['products_name'];
-    $products[$i]['quantity'] = $orders_products['products_quantity'];
-    $products[$i]['tax'] = $orders_products['products_tax'];
-    $products[$i]['price'] = $orders_products['products_price'];
-    $final_price = $orders_products['final_price'];
-    $attributes_query = tep_db_query("select products_options, products_options_values, price_prefix, options_values_price 
-                        from " . TABLE_ORDERS_PRODUCTS_ATTRIBUTES . " 
-                        where orders_id = '" . $HTTP_GET_VARS['order_id'] . "'
-                         and orders_products_id = '" . $orders_products['orders_products_id'] . "'");
-// Build data structure for order_details module
-    if (@tep_db_num_rows($attributes_query)) {
-      $j = 0;
-      while ($attributes = tep_db_fetch_array($attributes_query)) {
-        $products[$i][$j]['products_options_name'] = $attributes['products_options'];
-        $products[$i][$j]['products_options_values_name'] = $attributes['products_options_values'];
-        $products[$i][$j]['price_prefix'] = $attributes['price_prefix'];
-        $products[$i][$j]['options_values_price'] = $attributes['options_values_price'];
-        $products[$i]['attributes'][$j] = 1;
-        $j++;
+      if (sizeof($order->products[$i]['attributes']) > 0) {
+        for ($j=0; $j<sizeof($order->products[$i]['attributes']); $j++) {
+          echo '<br><nobr><small>&nbsp;<i> - ' . $order->products[$i]['attributes'][$j]['option'] . ': ' . $order->products[$i]['attributes'][$j]['value'];
+          if ($order->products[$i]['attributes'][$j]['price'] != '0') echo ' (' . $order->products[$i]['attributes'][$j]['prefix'] . $currencies->format($order->products[$i]['attributes'][$j]['price']) . ')';
+          echo '</i></small></nobr>';
+        }
       }
-    }
-    $i++;
 
-    $cost = ($orders_products['products_quantity'] * $final_price);
-    if (DISPLAY_PRICE_WITH_TAX == true) {
-      $total_tax += (($orders_products['products_quantity'] * $final_price) * (($orders_products['products_tax']/100)+1) - ($orders_products['products_quantity'] * $final_price));
-    } else {
-      $total_tax += ($cost * $orders_products['products_tax']/100);
-    }
-    $total_cost += $cost;
+      echo '</td>' . "\n" .
+           '            <td class="main" align="right" valign="top">' . tep_display_tax_value($order->products[$i]['tax']) . '%</td>' . "\n";
+
+      if (DISPLAY_PRICE_WITH_TAX == true) {
+        echo '            <td class="main" align="right" valign="top">' . $currencies->format((($order->products[$i]['final_price'] * $order->products[$i]['qty']) * $order->products[$i]['tax']/100) + ($order->products[$i]['final_price'] * $order->products[$i]['qty'])) . '</td>' . "\n";
+      } else {
+        echo '            <td class="main" align="right" valign="top">' . $currencies->format($order->products[$i]['final_price'] * $order->products[$i]['qty']) . '</td>' . "\n";
+      }
+      echo '          </tr>' . "\n";
   }
-  require(DIR_WS_MODULES. 'order_details.php');
-
 ?>
           <tr>
             <td colspan="4"><?php echo tep_draw_separator(); ?></td>
           </tr>
           <tr>
-            <td colspan="4" align="right"><table border="0" width="100%" cellspacing="0" cellpadding="0" align="right">
-              <tr>
+            <td colspan="4" align="right"><table border="0" cellspacing="0" cellpadding="1">
 <?php
-    if (DISPLAY_PRICE_WITH_TAX == true) {
-?>
-                <td align="right" class="main"><?php echo TABLE_SUBHEADING_SUBTOTAL; ?></td>
-                <td align="right" class="main"><?php echo $currencies->format($total_cost + $total_tax, true, $order_currency['currency'], $order_currency['currency_value']); ?></td>
-<?php
-    } else {
-?>
-                <td align="right" class="main"><?php echo TABLE_SUBHEADING_SUBTOTAL; ?></td>
-                <td align="right" class="main"><?php echo $currencies->format($total_cost, true, $order_currency['currency'], $order_currency['currency_value']); ?></td>
-<?php
-    }
-?>
-              </tr>
-<?php
-  $order_query = tep_db_query("select delivery_name as name, delivery_street_address as street_address, delivery_suburb as suburb, delivery_city as city, delivery_postcode as postcode, delivery_state as state, delivery_country as country, delivery_address_format_id as format_id, payment_method, shipping_cost, shipping_method, comments from " . TABLE_ORDERS . " where orders_id = '" . $HTTP_GET_VARS['order_id'] . "'");
-  $order = tep_db_fetch_array($order_query);
-
-  if ($total_tax > 0) {
-?>
-              <tr>
-                <td align="right" class="main"><?php echo TABLE_SUBHEADING_TAX; ?></td>
-                <td align="right" class="main"><?php echo $currencies->format($total_tax, true, $order_currency['currency'], $order_currency['currency_value']); ?></td>
-              </tr>
-<?php
-  }
-
-  $shipping = $order['shipping_cost'];
-  if ( (MODULE_SHIPPING_INSTALLED) || ($shipping > 0) ) {
-?>
-              <tr>
-                <td align="right" class="main"><?php echo $order['shipping_method'] . " " . TABLE_SUBHEADING_SHIPPING; ?></td>
-                <td align="right" class="main"><?php echo $currencies->format($shipping, true, $order_currency['currency'], $order_currency['currency_value']); ?></td>
-              </tr>
-<?php
+  if (MODULE_ORDER_TOTAL_INSTALLED) {
+    $order_total_modules->process();
+    echo $order_total_modules->output();
   }
 ?>
-              <tr>
-                <td align="right" class="main"><b><?php echo TABLE_SUBHEADING_TOTAL; ?></td>
-                <td align="right" class="main"><?php echo $currencies->format($total_cost + $total_tax + $shipping, true, $order_currency['currency'], $order_currency['currency_value']); ?></b></td>
-              </tr>
             </table></td>
           </tr>
         </table></td>
+      </tr>
+      <tr>
+        <td><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
       </tr>
       <tr>
         <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
@@ -172,55 +127,56 @@
           <tr>
             <td><?php echo tep_draw_separator(); ?></td>
           </tr>
-<?php
-     $boln = '<tr>' . "\n" . '            <td class="main">';
-     $eoln = "</td>\n              </tr>\n";
-     echo tep_address_format($order['format_id'], $order, true, $boln, $eoln);
-?>
+          <tr>
+            <td class="main"><?php echo tep_address_format($order->delivery['format_id'], $order->delivery, 1, ' ', '<br>'); ?></td>
+          </tr>
         </table></td>
       </tr>
-<?php
-   if (MODULE_PAYMENT_INSTALLED) {
-?>
       <tr>
-        <td class="main"><br><table border="0" width="100%" cellspacing="0" cellpadding="2">
+        <td><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+      <tr>
+        <td class="main"><table border="0" width="100%" cellspacing="0" cellpadding="2">
           <tr>
-            <td class="tableHeading"><?php echo TABLE_HEADING_PAYMENT_METHOD; ?></td>
+            <td class="main"><b><?php echo TABLE_HEADING_PAYMENT_METHOD; ?></b></td>
           </tr>
           <tr>
             <td><?php echo tep_draw_separator(); ?></td>
           </tr>
-<?php
-// load the show_info function from the payment modules
-    echo $payment_modules->show_info();
-?>
+          <tr>
+            <td class="main"><?php echo $order->info['payment_method']; ?></td>
+          </tr>
         </table></td>
       </tr>
 <?php
-  }
+  if (tep_not_null($order->info['comments'])) {
 ?>
+      <tr>
+        <td><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
       <tr>
         <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
-<?php
-  if ($order['comments']) {
-?>
           <tr>
-            <td class="main"><br><b><?php echo TABLE_HEADING_COMMENTS; ?></b></td>
+            <td class="main"><b><?php echo TABLE_HEADING_COMMENTS; ?></b></td>
           </tr>
           <tr>
             <td><?php echo tep_draw_separator(); ?></td>
           </tr>
           <tr>
-            <td class="main"><?php echo nl2br($order['comments']); ?></td>
+            <td class="main"><?php echo nl2br($order->info['comments']); ?></td>
           </tr>
-<?php
-  }
-?>
         </table></td>
       </tr>
-<?php if (DOWNLOAD_ENABLED == 'true') include(DIR_WS_BOXES . 'downloads.php'); ?>
+<?php
+  }
+
+  if (DOWNLOAD_ENABLED == 'true') include(DIR_WS_BOXES . 'downloads.php');
+?>
       <tr>
-        <td align="right" class="main"><br><?php echo '<a href="' . tep_href_link(FILENAME_ACCOUNT_HISTORY, tep_get_all_get_params(array('order_id')), 'SSL') . '">' . tep_image_button('button_back.gif', IMAGE_BUTTON_BACK) . '</a>'; ?></td>
+        <td><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+      <tr>
+        <td align="right" class="main"><?php echo '<a href="' . tep_href_link(FILENAME_ACCOUNT_HISTORY, tep_get_all_get_params(array('order_id')), 'SSL') . '">' . tep_image_button('button_back.gif', IMAGE_BUTTON_BACK) . '</a>'; ?></td>
       </tr>
     </table></td>
 <!-- body_text_eof //-->
