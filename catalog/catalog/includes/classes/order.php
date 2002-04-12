39c98998aa52e4f0201906b73a89c3330e21bccc
hpdl <hpdl>
2002-04-12 22:33:26 +0000

    remove the database references to the shipping_cost and shipping_method fields

--- catalog/catalog/includes/classes/order.php
+++ catalog/catalog/includes/classes/order.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: order.php,v 1.6 2002/04/08 01:13:43 hpdl Exp $
+  $Id: order.php,v 1.7 2002/04/12 22:33:26 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -28,10 +28,10 @@
     }
 
     function query($order_id) {
-      $order_query = tep_db_query("select customers_name, customers_street_address, customers_suburb, customers_city, customers_postcode, customers_state, customers_country, customers_telephone, customers_email_address, customers_address_format_id, delivery_name, delivery_street_address, delivery_suburb, delivery_city, delivery_postcode, delivery_state, delivery_country, delivery_address_format_id, payment_method, cc_type, cc_owner, cc_number, cc_expires, shipping_method, shipping_cost, comments, currency, currency_value, date_purchased, orders_status, last_modified from " . TABLE_ORDERS . " where orders_id = '" . tep_db_input($order_id) . "'");
+      $order_query = tep_db_query("select customers_name, customers_street_address, customers_suburb, customers_city, customers_postcode, customers_state, customers_country, customers_telephone, customers_email_address, customers_address_format_id, delivery_name, delivery_street_address, delivery_suburb, delivery_city, delivery_postcode, delivery_state, delivery_country, delivery_address_format_id, payment_method, cc_type, cc_owner, cc_number, cc_expires, comments, currency, currency_value, date_purchased, orders_status, last_modified from " . TABLE_ORDERS . " where orders_id = '" . tep_db_input($order_id) . "'");
       $order = tep_db_fetch_array($order_query);
 
-      $totals_query = tep_db_query("select title, text from " . TABLE_ORDERS_TOTAL . " where orders_id = '" . $order_id . "' order by sort_order");
+      $totals_query = tep_db_query("select title, text from " . TABLE_ORDERS_TOTAL . " where orders_id = '" . tep_db_input($order_id) . "' order by sort_order");
       while ($totals = tep_db_fetch_array($totals_query)) {
         $this->totals[] = array('title' => $totals['title'],
                                 'text' => $totals['text']);
@@ -44,8 +44,6 @@
                           'cc_owner' => $order['cc_owner'],
                           'cc_number' => $order['cc_number'],
                           'cc_expires' => $order['cc_expires'],
-                          'shipping_method' => $order['shipping_method'],
-                          'shipping_cost' => $order['shipping_cost'],
                           'comments' => $order['comments'],
                           'date_purchased' => $order['date_purchased'],
                           'orders_status' => $order['orders_status'],
@@ -81,8 +79,6 @@
                                         'price' => $orders_products['products_price'],
                                         'final_price' => $orders_products['final_price']);
 
-//        $products_price = $this->products[$index]['qty'] * $this->products[$index]['price'];
-
         $subindex = 0;
         $attributes_query = tep_db_query("select products_options, products_options_values, options_values_price, price_prefix from " . TABLE_ORDERS_PRODUCTS_ATTRIBUTES . " where orders_id = '" . tep_db_input($order_id) . "' and orders_products_id = '" . $orders_products['orders_products_id'] . "'");
         if (tep_db_num_rows($attributes_query)) {
@@ -92,27 +88,11 @@
                                                                      'prefix' => $attributes['price_prefix'],
                                                                      'price' => $attributes['options_values_price']);
 
-/*
-            if ($this->products[$index]['attributes'][$subindex]['prefix'] == '+') {
-              $products_price += $this->products[$index]['attributes'][$subindex]['price'] * $this->products[$index]['qty'];
-            } else {
-              $products_price -= $this->products[$index]['attributes'][$subindex]['price'] * $this->products[$index]['qty'];
-            }
-*/
             $subindex++;
           }
         }
-
-//        $this->info['subtotal'] += $products_price;
-//        $this->info['tax'] += $this->products[$index]['tax']/100 * $products_price;
-        $this->info['subtotal'] += $this->products[$index]['final_price'] * $this->products[$index]['qty'];
-        $this->info['tax'] += $this->products[$index]['tax']/100 * ($this->products[$index]['final_price'] * $this->products[$index]['qty']);
-        $this->info['tax_groups']["{$this->products[$index]['tax']}"] += $this->products[$index]['tax']/100 * ($this->products[$index]['final_price'] * $this->products[$index]['qty']);
-
         $index++;
       }
-
-      $this->info['total'] = $this->info['subtotal'] + $this->info['tax'] + $this->info['shipping_cost'];
     }
 
     function cart() {
