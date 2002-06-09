95298ad35ba12c2719cc3d4d3190ad7d69cb3fa3
dgw_ <dgw_>
2002-06-09 16:20:14 +0000

    Closed bug #443, under certaing cirscumtances, products in the also purchase list would be duplicated.
    For example, buy any 2 products twice.

--- catalog/catalog/includes/modules/also_purchased_products.php
+++ catalog/catalog/includes/modules/also_purchased_products.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: also_purchased_products.php,v 1.19 2002/04/05 11:44:28 dgw_ Exp $
+  $Id: also_purchased_products.php,v 1.20 2002/06/09 16:20:14 dgw_ Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -11,7 +11,7 @@
 */
 
   if ($HTTP_GET_VARS['products_id']) {
-    $orders_query = tep_db_query("select distinct p.products_id, p.products_image, pd.products_name from " . TABLE_ORDERS_PRODUCTS . " opa, " . TABLE_ORDERS_PRODUCTS . " opb, " . TABLE_ORDERS . " o, " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd where opa.products_id = '" . $HTTP_GET_VARS['products_id'] . "' and opa.orders_id = opb.orders_id and opb.products_id != '" . $HTTP_GET_VARS['products_id'] . "' and opb.products_id = p.products_id and p.products_id = pd.products_id and pd.language_id = '" . $languages_id . "' and opb.orders_id = o.orders_id and p.products_status = '1' order by o.date_purchased desc limit " . MAX_DISPLAY_ALSO_PURCHASED);
+    $orders_query = tep_db_query("select p.products_id, p.products_image from " . TABLE_ORDERS_PRODUCTS . " opa, " . TABLE_ORDERS_PRODUCTS . " opb, " . TABLE_ORDERS . " o, " . TABLE_PRODUCTS . " p where opa.products_id = '" . $HTTP_GET_VARS['products_id'] . "' and opa.orders_id = opb.orders_id and opb.products_id != '" . $HTTP_GET_VARS['products_id'] . "' and opb.products_id = p.products_id and opb.orders_id = o.orders_id and p.products_status = '1' group by p.products_id order by o.date_purchased desc limit " . MAX_DISPLAY_ALSO_PURCHASED);
     $num_products_ordered = tep_db_num_rows($orders_query);
     if ($num_products_ordered >= MIN_DISPLAY_ALSO_PURCHASED) {
 ?>
@@ -25,6 +25,7 @@
       $col = 0;
       $info_box_contents = array();
       while ($orders = tep_db_fetch_array($orders_query)) {
+        $orders['products_name'] = tep_get_products_name($orders['products_id']);
         $info_box_contents[$row][$col] = array('align' => 'center',
                                                'params' => 'class="smallText" width="33%" valign="top"',
                                                'text' => '<a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $orders['products_id']) . '">' . tep_image(DIR_WS_IMAGES . $orders['products_image'], $orders['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a><br><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $orders['products_id']) . '">' . $orders['products_name'] . '</a>');
