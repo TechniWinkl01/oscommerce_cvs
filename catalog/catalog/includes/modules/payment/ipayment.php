fdce1f68102ea9b811a1c43579f5edd0eb6319d3
jan0815 <jan0815>
2002-06-11 21:40:43 +0000

    This is the update for the iPayment.de gateway. It uses the new protocol version. Though I have tested it I ask the german community to assist in extensive testing. The current module will only allow credit-card payments.
    Maybe an option for ELV will be added.
    
    Have fun! And remember - best things in life are for free ;-)
    
    Jan Wildeboer

--- catalog/catalog/includes/modules/payment/ipayment.php
+++ catalog/catalog/includes/modules/payment/ipayment.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: ipayment.php,v 1.22 2002/05/30 15:36:36 dgw_ Exp $
+  $Id: ipayment.php,v 1.23 2002/06/11 21:40:43 jan0815 Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -47,7 +47,7 @@
                           '  </tr>' . "\n" .
                           '  <tr>' . "\n" .
                           '    <td class="main">&nbsp;' . MODULE_PAYMENT_IPAYMENT_TEXT_CREDIT_CHECKNUMBER . '&nbsp;</td>' . "\n" .
-                          '    <td class="main">&nbsp;' . tep_draw_input_field('ipayment_cc_checknumber') . '&nbsp;</td>' . "\n" .
+                          '    <td class="main">&nbsp;' . tep_draw_input_field('ipayment_cc_checkcode') . '&nbsp;</td>' . "\n" .
                           '  </tr>' . "\n" .
                           '  <tr>' . "\n" .
                           '    <td class="main">&nbsp;' . MODULE_PAYMENT_IPAYMENT_TEXT_CREDIT_CARD_EXPIRES . '&nbsp;</td>' . "\n" .
@@ -68,7 +68,7 @@
       if ($cc_val == '1') $cc_val = ValidateExpiry($HTTP_POST_VARS['ipayment_cc_expires_month'], $HTTP_POST_VARS['ipayment_cc_expires_year']);
 
       if ($cc_val != '1') {
-        $payment_error_return = 'payment_error=' . $payment . '&cc_owner=' . urlencode($HTTP_POST_VARS['ipayment_cc_owner']) . '&cc_checknumber=' . $HTTP_POST_VARS['ipayment_cc_checknumber'] . '&cc_expires_month=' . $HTTP_POST_VARS['ipayment_cc_expires_month'] . '&cc_expires_year=' . $HTTP_POST_VARS['ipayment_cc_expires_year'] . '&cc_val=' . urlencode($cc_val);
+        $payment_error_return = 'payment_error=' . $payment . '&cc_owner=' . urlencode($HTTP_POST_VARS['ipayment_cc_owner']) . '&cc_checknumber=' . $HTTP_POST_VARS['ipayment_cc_checkcode'] . '&cc_expires_month=' . $HTTP_POST_VARS['ipayment_cc_expires_month'] . '&cc_expires_year=' . $HTTP_POST_VARS['ipayment_cc_expires_year'] . '&cc_val=' . urlencode($cc_val);
         tep_redirect(tep_href_link(FILENAME_CHECKOUT_PAYMENT, $payment_error_return, 'SSL', true, false));
       }
     }
@@ -76,24 +76,25 @@
     function confirmation() {
       global $checkout_form_action;
 
-      $checkout_form_action = 'https://ipayment.de/merchant/' . MODULE_PAYMENT_IPAYMENT_ID . '/processor.php3';
+      $checkout_form_action = 'https://ipayment.de/merchant/' . MODULE_PAYMENT_IPAYMENT_ID . '/processor.php';
     }
 
     function process_button() {
       global $payment, $HTTP_POST_VARS, $order, $currencies;
 
-      $process_button_string = tep_draw_hidden_field('silent', 'true') .
-                               tep_draw_hidden_field('cc_userid', MODULE_PAYMENT_IPAYMENT_USER_ID) .
+      $process_button_string = tep_draw_hidden_field('silent', '1') .
+	                           tep_draw_hidden_field('trx_paymenttyp', 'cc') .
+							   tep_draw_hidden_field('trx_currency', MODULE_PAYMENT_IPAYMENT_CURRENCY) .
+                               tep_draw_hidden_field('trxuser_id', MODULE_PAYMENT_IPAYMENT_USER_ID) .
+                               tep_draw_hidden_field('trxpassword', MODULE_PAYMENT_IPAYMENT_PASSWORD) .
                                tep_draw_hidden_field('item_name', STORE_NAME) .
-                               tep_draw_hidden_field('cc_amount', number_format($order->info['total'] * 100 * $currencies->get_value(MODULE_PAYMENT_IPAYMENT_CURRENCY), 0, '','')) .
+                               tep_draw_hidden_field('trx_amount', number_format($order->info['total'] * 100 * $currencies->get_value(MODULE_PAYMENT_IPAYMENT_CURRENCY), 0, '','')) .
                                tep_draw_hidden_field('cc_expdate_month', $HTTP_POST_VARS['ipayment_cc_expires_month']) .
                                tep_draw_hidden_field('cc_expdate_year', $HTTP_POST_VARS['ipayment_cc_expires_year']) .
                                tep_draw_hidden_field('cc_number', $HTTP_POST_VARS['ipayment_cc_number']) .
-                               tep_draw_hidden_field('cc_checknumber', $HTTP_POST_VARS['ipayment_cc_checknumber']) .
-                               tep_draw_hidden_field('cc_name', $HTTP_POST_VARS['ipayment_cc_owner']) .
-                               tep_draw_hidden_field('cc_email', $order->customer['email_address']) .
-                               tep_draw_hidden_field('redirect_action', 'GET') .
-                               tep_draw_hidden_field('cc_currency', MODULE_PAYMENT_IPAYMENT_CURRENCY) .
+                               tep_draw_hidden_field('cc_checkcode', $HTTP_POST_VARS['ipayment_cc_checkcode']) .
+                               tep_draw_hidden_field('addr_name', $HTTP_POST_VARS['ipayment_cc_owner']) .
+                               tep_draw_hidden_field('addr_email', $order->customer['email_address']) .
                                tep_draw_hidden_field('redirect_url', tep_href_link(FILENAME_CHECKOUT_PROCESS, '', 'SSL', true)) .
                                tep_draw_hidden_field('silent_error_url', tep_href_link(FILENAME_CHECKOUT_PAYMENT, 'payment_error=' . $payment . '&cc_owner=' . urlencode($HTTP_POST_VARS['ipayment_cc_owner']), 'SSL', true));
 
@@ -142,18 +143,20 @@
       tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Allow iPayment', 'MODULE_PAYMENT_IPAYMENT_STATUS', '1', 'Do you want to accept iPayment payments?', '6', '1', now())");
       tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('iPayment Account No.', 'MODULE_PAYMENT_IPAYMENT_ID', '99999', 'Your Account No. at iPayment.', '6', '2', now())");
       tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('iPayment User ID', 'MODULE_PAYMENT_IPAYMENT_USER_ID', '99999', 'Your User ID at iPayment.', '6', '3', now())");
-      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('iPayment Currency', 'MODULE_PAYMENT_IPAYMENT_CURRENCY', 'EUR', 'iPayment can charge in EUR, DEM, or USD.', '6', '4', now())");
+      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('iPayment password', 'MODULE_PAYMENT_IPAYMENT_PASSWORD', '0', 'Your iPayment password. For users of the old gateway version this should be \'0\'', '6', '4', now())");
+      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('iPayment Currency', 'MODULE_PAYMENT_IPAYMENT_CURRENCY', 'EUR', 'iPayment can charge in EUR, DEM, or USD.', '6', '5', now())");
     }
 
     function remove() {
       tep_db_query("delete from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_PAYMENT_IPAYMENT_STATUS'");
       tep_db_query("delete from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_PAYMENT_IPAYMENT_ID'");
       tep_db_query("delete from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_PAYMENT_IPAYMENT_USER_ID'");
+      tep_db_query("delete from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_PAYMENT_IPAYMENT_PASSWORD'");
       tep_db_query("delete from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_PAYMENT_IPAYMENT_CURRENCY'");
     }
 
     function keys() {
-      $keys = array('MODULE_PAYMENT_IPAYMENT_STATUS', 'MODULE_PAYMENT_IPAYMENT_ID', 'MODULE_PAYMENT_IPAYMENT_USER_ID', 'MODULE_PAYMENT_IPAYMENT_CURRENCY');
+      $keys = array('MODULE_PAYMENT_IPAYMENT_STATUS', 'MODULE_PAYMENT_IPAYMENT_ID', 'MODULE_PAYMENT_IPAYMENT_USER_ID', 'MODULE_PAYMENT_IPAYMENT_PASSWORD', 'MODULE_PAYMENT_IPAYMENT_CURRENCY');
 
       return $keys;
     }
