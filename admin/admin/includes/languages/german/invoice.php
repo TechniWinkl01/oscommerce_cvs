27e96d77c8a11729290eca4379bc8637a9e12b03
harley_vb <harley_vb>
2002-06-15 12:45:05 +0000

    German translations corrected/reviewed

--- admin/admin/includes/languages/german/invoice.php
+++ admin/admin/includes/languages/german/invoice.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: invoice.php,v 1.1 2002/06/11 18:17:59 dgw_ Exp $
+  $Id: invoice.php,v 1.2 2002/06/15 12:45:05 harley_vb Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -10,21 +10,21 @@
   Released under the GNU General Public License
 */
 
-define('TABLE_HEADING_COMMENTS', 'Comments');
-define('TABLE_HEADING_PRODUCTS_MODEL', 'Model');
-define('TABLE_HEADING_PRODUCTS', 'Products');
-define('TABLE_HEADING_TAX', 'Tax');
-define('TABLE_HEADING_TOTAL', 'Total');
-define('TABLE_HEADING_PRICE_EXCLUDING_TAX', 'Price (ex)');
-define('TABLE_HEADING_PRICE_INCLUDING_TAX', 'Price (inc)');
-define('TABLE_HEADING_TOTAL_EXCLUDING_TAX', 'Total (ex)');
-define('TABLE_HEADING_TOTAL_INCLUDING_TAX', 'Total (inc)');
+define('TABLE_HEADING_COMMENTS', 'Kommentar');
+define('TABLE_HEADING_PRODUCTS_MODEL', 'Artikel-Nr.');
+define('TABLE_HEADING_PRODUCTS', 'Artikel');
+define('TABLE_HEADING_TAX', 'MwSt.');
+define('TABLE_HEADING_TOTAL', 'Summe');
+define('TABLE_HEADING_PRICE_EXCLUDING_TAX', 'Preis (exkl.)');
+define('TABLE_HEADING_PRICE_INCLUDING_TAX', 'Preis (inkl.)');
+define('TABLE_HEADING_TOTAL_EXCLUDING_TAX', 'Summe (exkl.)');
+define('TABLE_HEADING_TOTAL_INCLUDING_TAX', 'Summe (inkl.)');
 
-define('ENTRY_SOLD_TO', 'SOLD TO:');
-define('ENTRY_SHIP_TO', 'SHIP TO:');
-define('ENTRY_PAYMENT_METHOD', 'Payment Method:');
-define('ENTRY_SUB_TOTAL', 'Sub-Total:');
-define('ENTRY_TAX', 'Tax:');
-define('ENTRY_SHIPPING', 'Shipping:');
-define('ENTRY_TOTAL', 'Total:');
+define('ENTRY_SOLD_TO', 'Rechnungsanschrift:');
+define('ENTRY_SHIP_TO', 'Lieferanschrift:');
+define('ENTRY_PAYMENT_METHOD', 'Zahlungsweise:');
+define('ENTRY_SUB_TOTAL', 'Zwischensumme:');
+define('ENTRY_TAX', 'MwSt.:');
+define('ENTRY_SHIPPING', 'Versandkosten:');
+define('ENTRY_TOTAL', 'Gesamtsumme:');
 ?>
