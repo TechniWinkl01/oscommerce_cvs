51881e46a890714ce451eaaf9dcce12453803960
harley_vb <harley_vb>
2002-06-15 12:00:29 +0000

    German translations corrected/reviewed

--- admin/admin/includes/languages/german/banner_statistics.php
+++ admin/admin/includes/languages/german/banner_statistics.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: banner_statistics.php,v 1.1 2002/05/07 23:07:11 hpdl Exp $
+  $Id: banner_statistics.php,v 1.2 2002/06/15 12:00:29 harley_vb Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -10,25 +10,25 @@
   Released under the GNU General Public License
 */
 
-define('HEADING_TITLE', 'Banner Statistics');
+define('HEADING_TITLE', 'Bannerstatistik');
 
 define('TABLE_HEADING_SOURCE', 'Source');
-define('TABLE_HEADING_VIEWS', 'Views');
-define('TABLE_HEADING_CLICKS', 'Clicks');
+define('TABLE_HEADING_VIEWS', 'Anzeigen');
+define('TABLE_HEADING_CLICKS', 'Klicks');
 
 define('TEXT_BANNERS_DATA', 'D<br>a<br>t<br>a');
-define('TEXT_BANNERS_DAILY_STATISTICS', '%s Daily Statistics For %s %s');
-define('TEXT_BANNERS_MONTHLY_STATISTICS', '%s Monthly Statistics For %s');
-define('TEXT_BANNERS_YEARLY_STATISTICS', '%s Yearly Statistics');
+define('TEXT_BANNERS_DAILY_STATISTICS', '%s Tagesstatistik f&uuml;r %s %s');
+define('TEXT_BANNERS_MONTHLY_STATISTICS', '%s Monatsstatistik f&uuml;r %s');
+define('TEXT_BANNERS_YEARLY_STATISTICS', '%s Jahresstatistik');
 
-define('STATISTICS_TYPE_DAILY', 'Daily');
-define('STATISTICS_TYPE_MONTHLY', 'Monthly');
-define('STATISTICS_TYPE_YEARLY', 'Yearly');
+define('STATISTICS_TYPE_DAILY', 't&auml;glich');
+define('STATISTICS_TYPE_MONTHLY', 'monatlich');
+define('STATISTICS_TYPE_YEARLY', 'j&auml;hrlich');
 
-define('TITLE_TYPE', 'Type:');
-define('TITLE_YEAR', 'Year:');
-define('TITLE_MONTH', 'Month:');
+define('TITLE_TYPE', 'Typ:');
+define('TITLE_YEAR', 'Jahr:');
+define('TITLE_MONTH', 'Monat:');
 
-define('ERROR_GRAPHS_DIRECTORY_DOES_NOT_EXIST', 'Error: Graphs directory does not exist. Please create a \'graphs\' directory inside \'images\'.');
-define('ERROR_GRAPHS_DIRECTORY_NOT_WRITEABLE', 'Error: Graphs directory is not writeable.');
+define('ERROR_GRAPHS_DIRECTORY_DOES_NOT_EXIST', 'Fehler: Das Verzeichnis \'graphs\' ist nicht vorhanden! Bitte erstellen Sie ein Verzeichnis \'graphs\' im Verzeichnis \'images\'.');
+define('ERROR_GRAPHS_DIRECTORY_NOT_WRITEABLE', 'Fehler: Das Verzeichnis \'graphs\' ist schreibgesch&uuml;tzt!');
 ?>
