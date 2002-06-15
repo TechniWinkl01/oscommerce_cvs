50c0bea6bdcb6b383aa59b15d4c71f257f97a802
harley_vb <harley_vb>
2002-06-15 13:01:56 +0000

    German translations corrected/reviewed

--- admin/admin/includes/languages/german/packingslip.php
+++ admin/admin/includes/languages/german/packingslip.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: packingslip.php,v 1.1 2002/06/11 17:41:56 dgw_ Exp $
+  $Id: packingslip.php,v 1.2 2002/06/15 13:01:56 harley_vb Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -10,11 +10,11 @@
   Released under the GNU General Public License
 */
 
-define('TABLE_HEADING_COMMENTS', 'Comments');
-define('TABLE_HEADING_PRODUCTS_MODEL', 'Model');
-define('TABLE_HEADING_PRODUCTS', 'Products');
+define('TABLE_HEADING_COMMENTS', 'Kommentar');
+define('TABLE_HEADING_PRODUCTS_MODEL', 'Artikel-Nr.');
+define('TABLE_HEADING_PRODUCTS', 'Artikel');
 
-define('ENTRY_SOLD_TO', 'SOLD TO:');
-define('ENTRY_SHIP_TO', 'SHIP TO:');
-define('ENTRY_PAYMENT_METHOD', 'Payment Method:');
+define('ENTRY_SOLD_TO', 'Rechnungsanschrift:');
+define('ENTRY_SHIP_TO', 'Lieferanschrift:');
+define('ENTRY_PAYMENT_METHOD', 'Zahlungsweise:');
 ?>
