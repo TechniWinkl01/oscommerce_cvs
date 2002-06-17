c56b42673abb3a5366be48b9885172c13afdbe29
harley_vb <harley_vb>
2002-06-17 18:46:12 +0000

    German translations corrected/reviewed

--- catalog/catalog/includes/languages/german/account_history_info.php
+++ catalog/catalog/includes/languages/german/account_history_info.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: account_history_info.php,v 1.15 2002/06/11 19:49:53 hpdl Exp $
+  $Id: account_history_info.php,v 1.16 2002/06/17 18:46:12 harley_vb Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -16,24 +16,24 @@ define('NAVBAR_TITLE_3', 'Bestellinformationen');
 
 define('HEADING_TITLE', 'Bestellinformationen');
 
-define('HEADING_ORDER_NUMBER', 'Bestellungsnummer #%s');
-define('HEADING_ORDER_DATE', 'Bestellungsdatum:');
+define('HEADING_ORDER_NUMBER', 'Bestellungnummer #%s');
+define('HEADING_ORDER_DATE', 'Bestellungdatum:');
 define('HEADING_ORDER_TOTAL', 'Summe:');
 
 define('HEADING_DELIVERY_ADDRESS', 'Lieferanschrift');
-define('HEADING_SHIPPING_METHOD', 'Shipping Method');
+define('HEADING_SHIPPING_METHOD', 'Versandart');
 
 define('HEADING_PRODUCTS', 'Produkte');
 define('HEADING_TAX', 'MwSt.');
 define('HEADING_TOTAL', 'Summe');
 
-define('HEADING_BILLING_INFORMATION', 'Billing Information');
-define('HEADING_BILLING_ADDRESS', 'Billing Address');
+define('HEADING_BILLING_INFORMATION', 'Rechnungsinformationen');
+define('HEADING_BILLING_ADDRESS', 'Rechnungsadresse');
 define('HEADING_PAYMENT_METHOD', 'Zahlungsweise');
 
-define('HEADING_ORDER_STATUS', 'Order Status');
-define('HEADING_COMMENT', 'Comment');
-define('TEXT_NO_COMMENTS_AVAILABLE', 'No comments available.');
+define('HEADING_ORDER_STATUS', 'Bestellstatus');
+define('HEADING_COMMENT', 'Kommentar');
+define('TEXT_NO_COMMENTS_AVAILABLE', 'Kein Kommentar vorhanden.');
 
 define('TABLE_HEADING_DOWNLOAD_DATE', 'herunterladen m&ouml;glich bis:');
 define('TABLE_HEADING_DOWNLOAD_COUNT', 'max. Anz. Downloads');
