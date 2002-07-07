b7708e39b36c564e3119bf16fe534caeb6a27f36
project3000 <project3000>
2002-07-07 05:15:54 +0000

    Fix bug 572.
    When the Gender field was disabled it was sending an email with Dear Evans
    I have changed this to use firstname if the gender field is disabled.

--- catalog/catalog/includes/languages/espanol/create_account_process.php
+++ catalog/catalog/includes/languages/espanol/create_account_process.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: create_account_process.php,v 1.9 2001/12/20 14:14:15 dgw_ Exp $
+  $Id: create_account_process.php,v 1.10 2002/07/07 05:15:54 project3000 Exp $
 
   The Exchange Project - Community Made Shopping!
   http://www.theexchangeproject.org
@@ -17,7 +17,7 @@ define('HEADING_TITLE', 'Informacion de Mi Cuenta');
 define('EMAIL_SUBJECT', 'Bienvenido a ' . STORE_NAME);
 define('EMAIL_GREET_MR', 'Estimado ' . stripslashes($HTTP_POST_VARS['lastname']) . ',' . "\n\n");
 define('EMAIL_GREET_MS', 'Estimado ' . stripslashes($HTTP_POST_VARS['lastname']) . ',' . "\n\n");
-define('EMAIL_GREET_NONE', 'Estimado ' . stripslashes($HTTP_POST_VARS['lastname']) . ',' . "\n\n");
+define('EMAIL_GREET_NONE', 'Estimado ' . stripslashes($HTTP_POST_VARS['firstname']) . ',' . "\n\n");
 define('EMAIL_WELCOME', 'le damos la bienvenida a <b>' . STORE_NAME . '</b>.' . "\n\n");
 define('EMAIL_TEXT', 'Ahora puede disfrutar de los <b>servicios</b> que le ofrecemos. Algunos de estos servicios son:' . "\n\n" . '<li><b>Carrito Permanente</b> - Cualquier producto añadido a su carrito permanecera en el hasta que lo elimine, o hasta que realice la compra.' . "\n" . '<li><b>Libro de Direcciones</b> - Podemos enviar sus productos a otras direcciones aparte de la suya! Esto es perfecto para enviar regalos de cumpleaños directamente a la persona que cumple años.' . "\n" . '<li><b>Historia de Pedidos</b> - Vea la relacion de compras que ha realizado con nosotros.' . "\n" . '<li><b>Comentarios</b> - Comparta su opinion sobre los productos con otros clientes.' . "\n\n");
 define('EMAIL_CONTACT', 'Para cualquier consulta sobre nuestros servicios, por favor escriba a: ' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n\n");
--- catalog/catalog/includes/languages/german/create_account_process.php
+++ catalog/catalog/includes/languages/german/create_account_process.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: create_account_process.php,v 1.13 2002/04/17 15:57:07 harley_vb Exp $
+  $Id: create_account_process.php,v 1.14 2002/07/07 05:15:54 project3000 Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -17,7 +17,7 @@ define('HEADING_TITLE', 'Meine Zugangsinformation');
 define('EMAIL_SUBJECT', 'Willkommen zu ' . STORE_NAME);
 define('EMAIL_GREET_MR', 'Sehr geehrter Herr ' . stripslashes($HTTP_POST_VARS['lastname']) . ',' . "\n\n");
 define('EMAIL_GREET_MS', 'Sehr geehrte Frau ' . stripslashes($HTTP_POST_VARS['lastname']) . ',' . "\n\n");
-define('EMAIL_GREET_NONE', 'Sehr geehrte ' . stripslashes($HTTP_POST_VARS['lastname']) . ',' . "\n\n");
+define('EMAIL_GREET_NONE', 'Sehr geehrte ' . stripslashes($HTTP_POST_VARS['firstname']) . ',' . "\n\n");
 define('EMAIL_WELCOME', 'willkommen zu <b>' . STORE_NAME . '</b>.' . "\n\n");
 define('EMAIL_TEXT', 'Sie können jetzt unser <b>Mitglieder-Service</b> nutzen. Der Service bietet unter anderem:' . "\n\n" . '<li><b>Kundeneinkaufswagen</b> - Jeder Artikel bleibt registriert bis Sie zur Kasse gehen, oder die Produkte aus dem Warenkorb entfernen.' . "\n" . '<li><b>Adressbuch</b> - Wir können jetzt die Produkte zu der von Ihnen ausgesuchten Adresse senden. Der perfekte Weg ein Geburtstagsgeschenk zu versenden.' . "\n" . '<li><b>Vorherige Bestellung</b> - Sie können Ihre vorherigen Bestellungen überprüfen.' . "\n" . '<li><b>Meinungen über Produkte</b> - Teilen Sie Ihre Meinung mit anderen Kunden.' . "\n\n");
 define('EMAIL_CONTACT', 'Falls Sie Fragen über unserem Mitglieder-Service haben, wenden Sie sich bitte an den Vertrieb: ' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n\n");
