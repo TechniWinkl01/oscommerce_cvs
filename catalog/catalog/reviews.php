d1e130b9fe1f2480cd2443b3c77f425319c9e073
project3000 <project3000>
2002-09-09 15:33:48 +0000

    Fix bug 699.

--- catalog/catalog/reviews.php
+++ catalog/catalog/reviews.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: reviews.php,v 1.44 2002/07/21 23:38:57 hpdl Exp $
+  $Id: reviews.php,v 1.45 2002/09/09 15:33:48 project3000 Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -50,8 +50,7 @@
       <tr>
         <td><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
       </tr>
-      <tr>
-        <td>
+
 <?php
   $reviews_query_raw = "select r.reviews_id, rd.reviews_text, r.reviews_rating, r.date_added, p.products_id, pd.products_name, p.products_image, r.customers_name from " . TABLE_REVIEWS . " r, " . TABLE_REVIEWS_DESCRIPTION . " rd, " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd where p.products_status = '1' and p.products_id = r.products_id and r.reviews_id = rd.reviews_id and p.products_id = pd.products_id and pd.language_id = '" . $languages_id . "' and rd.languages_id = '" . $languages_id . "' order by r.reviews_id DESC";
   $reviews_split = new splitPageResults($HTTP_GET_VARS['page'], MAX_DISPLAY_NEW_REVIEWS, $reviews_query_raw, $reviews_numrows);
@@ -69,6 +68,23 @@
                              'date_added' => tep_date_long($reviews['date_added']));
   }
 
+  if ($reviews_numrows > 0 && (PREV_NEXT_BAR_LOCATION == '1' || PREV_NEXT_BAR_LOCATION == '3')) {
+?>
+      <tr>
+        <td><br><table border="0" width="100%" cellspacing="0" cellpadding="2">
+          <tr>
+            <td class="smallText"><?php echo $reviews_split->display_count($reviews_numrows, MAX_DISPLAY_NEW_REVIEWS, $HTTP_GET_VARS['page'], TEXT_DISPLAY_NUMBER_OF_REVIEWS); ?></td>
+            <td align="right" class="smallText"><?php echo TEXT_RESULT_PAGE; ?> <?php echo $reviews_split->display_links($reviews_numrows, MAX_DISPLAY_NEW_REVIEWS, MAX_DISPLAY_PAGE_LINKS, $HTTP_GET_VARS['page'], tep_get_all_get_params(array('page', 'info', 'x', 'y'))); ?></td>
+          </tr>
+        </table></td>
+      </tr>
+
+<?php
+  }
+?>
+      <tr>
+        <td>
+<?php
   require(DIR_WS_MODULES  . 'reviews.php');
 ?>
         </td>
