7b8f3868f7f99a7ab841f3fb291cefd244d9616e
dgw_ <dgw_>
2002-10-27 20:36:21 +0000

    We dont want decimals here.

--- catalog/catalog/default.php
+++ catalog/catalog/default.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: default.php,v 1.77 2002/07/21 23:38:57 hpdl Exp $
+  $Id: default.php,v 1.78 2002/10/27 20:36:21 dgw_ Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -96,7 +96,7 @@
     while ($categories = tep_db_fetch_array($categories_query)) {
       $rows++;
       $cPath_new = tep_get_path($categories['categories_id']);
-      $width = (100 / MAX_DISPLAY_CATEGORIES_PER_ROW) . '%';
+      $width = (int)(100 / MAX_DISPLAY_CATEGORIES_PER_ROW) . '%';
       echo '                <td align="center" class="smallText" style="width: ' . $width . '" valign="top"><a href="' . tep_href_link(FILENAME_DEFAULT, $cPath_new, 'NONSSL') . '">' . tep_image(DIR_WS_IMAGES . $categories['categories_image'], $categories['categories_name'], SUBCATEGORY_IMAGE_WIDTH, SUBCATEGORY_IMAGE_HEIGHT) . '<br>' . $categories['categories_name'] . '</a></td>' . "\n";
       if ((($rows / MAX_DISPLAY_CATEGORIES_PER_ROW) == floor($rows / MAX_DISPLAY_CATEGORIES_PER_ROW)) && ($rows != tep_db_num_rows($categories_query))) {
         echo '              </tr>' . "\n";
