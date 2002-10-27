b46825db18d91d911f94b68a7c94c8c6ae18886f
dgw_ <dgw_>
2002-10-27 18:43:35 +0000

    Fixed bug #767 ... instead of dropping all tables, we only drop osCommerce's tables.

--- admin/admin/backup.php
+++ admin/admin/backup.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: backup.php,v 1.53 2002/08/19 01:30:25 hpdl Exp $
+  $Id: backup.php,v 1.54 2002/10/27 18:43:35 dgw_ Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -242,13 +242,10 @@
             }
           }
 
-          $tables_query = tep_db_query('show tables');
-          while ($tables = tep_db_fetch_array($tables_query)) {
-            list(,$table) = each($tables);
-            tep_db_query("drop table " . $table);
-          }
+          osc_db_query("drop table if exists address_book, address_format, banners, banners_history, categories, categories_description, configuration, configuration_group, counter, counter_history, countries, currencies, customers, customers_basket, customers_basket_attributes, customers_info, languages, manufacturers, manufacturers_info, orders, orders_products, orders_status, orders_status_history, orders_products_attributes, orders_products_download, products, products_attributes, products_attributes_download, prodcts_description, products_options, products_options_values, products_options_values_to_products_options, products_to_categories, reviews, reviews_description, sessions, specials, tax_class, tax_rates, geo_zones, whos_online, zones, zones_to_geo_zones");
 
-          for ($i=0; $i<sizeof($sql_array); $i++) {
+          $sql_array_size = sizeof($sql_array);
+          for ($i=0; $i<$sql_array_size; $i++) {
             tep_db_query($sql_array[$i]);
           }
 
@@ -350,7 +347,8 @@
     }
     sort($contents);
 
-    for ($files=0; $files<sizeof($contents); $files++) {
+    $contents_size = sizeof($contents);
+    for ($files=0; $files<$contents_size; $files++) {
       $entry = $contents[$files];
 
       $check = 0;
