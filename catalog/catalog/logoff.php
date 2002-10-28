f6a1315904cd31f35fb4a6bc984a984555277141
hpdl <hpdl>
2002-10-28 23:40:30 +0000

    Thanks to David for the tip - there is no reason to reset the shopping cart contents as it is being destroyed anyway with the tep_session_destroy() call

--- catalog/catalog/logoff.php
+++ catalog/catalog/logoff.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: logoff.php,v 1.7 2002/10/28 01:21:13 hpdl Exp $
+  $Id: logoff.php,v 1.8 2002/10/28 23:40:30 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -12,11 +12,10 @@
 
   require('includes/application_top.php');
 
-  $cart->reset(FALSE);
-
   tep_session_destroy();
 
   require('includes/counter.php');
+
   tep_redirect(tep_href_link(FILENAME_DEFAULT, '', 'NONSSL'));
 
   require(DIR_WS_INCLUDES . 'application_bottom.php');
