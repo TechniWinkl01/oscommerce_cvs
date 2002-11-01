c2b2721923d4ac5828a1a8b0a759d3c7e1182125
hpdl <hpdl>
2002-11-01 05:00:46 +0000

    update the payment module API to use for the new checkout procedure

--- catalog/catalog/includes/classes/payment.php
+++ catalog/catalog/includes/classes/payment.php
@@ -1,207 +1,168 @@
 <?php
 /*
-  $Id: payment.php,v 1.29 2002/04/03 22:03:43 hpdl Exp $
+  $Id: payment.php,v 1.30 2002/11/01 05:00:46 hpdl Exp $
 
-  The Exchange Project - Community Made Shopping!
-  http://www.theexchangeproject.org
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
 
-  Copyright (c) 2000,2001 The Exchange Project
+  Copyright (c) 2002 osCommerce
 
   Released under the GNU General Public License
 */
 
   class payment {
-    var $modules;
+    var $modules, $selected_module;
 
 // class constructor
-    function payment() {
-      global $language;
+    function payment($module = '') {
+      global $payment, $language, $PHP_SELF;
 
-      if (MODULE_PAYMENT_INSTALLED) {
-        $this->modules = explode(';', MODULE_PAYMENT_INSTALLED); // get array of installed/active modules
+      if (tep_not_null(MODULE_PAYMENT_INSTALLED)) {
+        $this->modules = explode(';', MODULE_PAYMENT_INSTALLED);
 
-        reset($this->modules);
-        while (list(, $value) = each($this->modules)) { // get module defines
-          include(DIR_WS_LANGUAGES . $language . '/modules/payment/' . $value);
-          include(DIR_WS_MODULES . 'payment/' . $value);
+        $include_modules = array();
 
-          $class = substr($value, 0, strrpos($value, '.'));
-          $GLOBALS[$class] = new $class;
+        if ( (tep_not_null($module)) && (in_array($module . '.' . substr($PHP_SELF, (strrpos($PHP_SELF, '.')+1)), $this->modules)) ) {
+          $this->selected_module = $module;
+
+          $include_modules[] = array('class' => $module, 'file' => $module . '.php');
+        } else {
+          reset($this->modules);
+          while (list(, $value) = each($this->modules)) {
+            $class = substr($value, 0, strrpos($value, '.'));
+            $include_modules[] = array('class' => $class, 'file' => $value);
+          }
+        }
+
+        for ($i=0, $n=sizeof($include_modules); $i<$n; $i++) {
+          include(DIR_WS_LANGUAGES . $language . '/modules/payment/' . $include_modules[$i]['file']);
+          include(DIR_WS_MODULES . 'payment/' . $include_modules[$i]['file']);
+
+          $GLOBALS[$include_modules[$i]['class']] = new $include_modules[$i]['class'];
+        }
+
+// if there is only one payment method, select it as default because in
+// checkout_confirmation.php the $payment variable is being assigned the
+// $HTTP_POST_VARS['payment'] value which will be empty (no radio button selection possible)
+        if ( (sizeof($this->modules) == 1) && (!is_object($GLOBALS[$payment])) ) {
+          $payment = $include_modules[0]['class'];
+        }
+
+        if ( (tep_not_null($module)) && (in_array($module, $this->modules)) && (isset($GLOBALS[$module]->form_action_url)) ) {
+          $this->form_action_url = $GLOBALS[$module]->form_action_url;
         }
       }
     }
 
 // class methods
     function javascript_validation() {
-      $javascript_validation_string = '';
-      if (MODULE_PAYMENT_INSTALLED) {
+      $js = '';
+      if (is_array($this->modules)) {
+        $js = '<script language="javascript"><!-- ' . "\n" .
+              'function check_form() {' . "\n" .
+              '  var error = 0;' . "\n" .
+              '  var error_message = "' . JS_ERROR . '";' . "\n" .
+              '  var payment_value = null;' . "\n" .
+              '  if (document.checkout_payment.payment.length) {' . "\n" .
+              '    for (var i=0; i<document.checkout_payment.payment.length; i++) {' . "\n" .
+              '      if (document.checkout_payment.payment[i].checked) {' . "\n" .
+              '        payment_value = document.checkout_payment.payment[i].value;' . "\n" .
+              '      }' . "\n" .
+              '    }' . "\n" .
+              '  } else if (document.checkout_payment.payment.checked) {' . "\n" .
+              '    payment_value = document.checkout_payment.payment.value;' . "\n" .
+              '  } else if (document.payment.checkout_payment.value) {' . "\n" .
+              '    payment_value = document.checkout_payment.payment.value;' . "\n" .
+              '  }' . "\n\n";
+
         reset($this->modules);
         while (list(, $value) = each($this->modules)) {
           $class = substr($value, 0, strrpos($value, '.'));
           if ($GLOBALS[$class]->enabled) {
-            $javascript_validation_string .= $GLOBALS[$class]->javascript_validation();
+            $js .= $GLOBALS[$class]->javascript_validation();
           }
         }
+
+        $js .= "\n" . '  if (payment_value == null) {' . "\n" .
+               '    error_message = error_message + "' . JS_ERROR_NO_PAYMENT_MODULE_SELECTED . '";' . "\n" .
+               '    error = 1;' . "\n" .
+               '  }' . "\n\n" .
+               '  if (error == 1) {' . "\n" .
+               '    alert(error_message);' . "\n" .
+               '    return false;' . "\n" .
+               '  } else {' . "\n" .
+               '    return true;' . "\n" .
+               '  }' . "\n" .
+               '}' . "\n" .
+               '//--></script>' . "\n";
       }
 
-      return $javascript_validation_string;
+      return $js;
     }
 
     function selection() {
-      $selection_string = '';
-      if (MODULE_PAYMENT_INSTALLED) {
+      $selection_array = array();
+
+      if (is_array($this->modules)) {
         reset($this->modules);
         while (list(, $value) = each($this->modules)) {
           $class = substr($value, 0, strrpos($value, '.'));
           if ($GLOBALS[$class]->enabled) {
-            $selection_string .= '<table border="0" cellspacing="0" cellpadding="0" width="100%">' . "\n" .
-                                 '  <tr class="payment-odd">' . "\n" .
-                                 '    <td class="main">' . $GLOBALS[$class]->title . '</td>' . "\n" .
-                                 '    <td align="right" class="main">';
-// display radio button option if more than 1 payment module is installed
-            if (tep_count_payment_modules() > 1) {
-              $selection_string .= tep_draw_radio_field('payment', $GLOBALS[$class]->code);
-            } else {
-              $selection_string .= tep_draw_hidden_field('payment', $GLOBALS[$class]->code);
-            }
-            $selection_string .= '&nbsp;</td>' . "\n" .
-                                 '  </tr>' . "\n" .
-                                 '  <tr class="payment-even">' . "\n" .
-                                 '    <td colspan="2">';
-            $selection_string .= $GLOBALS[$class]->selection();
-            $selection_string .= '&nbsp;</td>' . "\n" .
-                                 '  </tr>' . "\n" .
-                                 '</table>' . "\n";
+            $selection = $GLOBALS[$class]->selection();
+            if (is_array($selection)) $selection_array[] = $selection;
           }
         }
       }
 
-      return $selection_string;
+      return $selection_array;
     }
 
     function pre_confirmation_check() {
-      global $payment;
-
-      if (MODULE_PAYMENT_INSTALLED) {
-        $payment_module_selected = false;
-        reset($this->modules);
-        while (list(, $value) = each($this->modules)) {
-          $class = substr($value, 0, strrpos($value, '.'));
-          if ( ($GLOBALS[$class]->code == $payment) && ($GLOBALS[$class]->enabled) ) {
-            $payment_module_selected = true;
-            $GLOBALS[$class]->pre_confirmation_check();
-          }
-        }
-
-        if (!$payment_module_selected) {
-          tep_redirect(tep_href_link(FILENAME_CHECKOUT_PAYMENT, 'error_message=' . urlencode(ERROR_NO_PAYMENT_MODULE_SELECTED), 'SSL'));
+      if (is_array($this->modules)) {
+        if (is_object($GLOBALS[$this->selected_module]) && ($GLOBALS[$this->selected_module]->enabled) ) {
+          $GLOBALS[$this->selected_module]->pre_confirmation_check();
         }
       }
     }
 
     function confirmation() {
-      global $payment;
-
-      $confirmation_string = '';
-      if (MODULE_PAYMENT_INSTALLED) {
-        reset($this->modules);
-        while (list(, $value) = each($this->modules)) {
-          $class = substr($value, 0, strrpos($value, '.'));
-          if ( ($GLOBALS[$class]->code == $payment) && ($GLOBALS[$class]->enabled) ) {
-            $confirmation_string .= '<table border="0" cellspacing="0" cellpadding="0" width="100%">' . "\n" .
-                                    '  <tr>' . "\n" .
-                                    '    <td class="main">' . $GLOBALS[$class]->title . '</td>' . "\n" .
-                                    '  </tr>' . "\n" .
-                                    '</table>' . "\n";
-            $confirmation_string .= $GLOBALS[$class]->confirmation();
-          }
+      if (is_array($this->modules)) {
+        if (is_object($GLOBALS[$this->selected_module]) && ($GLOBALS[$this->selected_module]->enabled) ) {
+          return $GLOBALS[$this->selected_module]->confirmation();
         }
       }
-
-      return $confirmation_string;
     }
 
     function process_button() {
-      global $payment;
-
-      $process_button_string = '';
-      if (MODULE_PAYMENT_INSTALLED) {
-        reset($this->modules);
-        while (list(, $value) = each($this->modules)) {
-          $class = substr($value, 0, strrpos($value, '.'));
-          if ( ($GLOBALS[$class]->code == $payment) && ($GLOBALS[$class]->enabled) ) {
-            $process_button_string .= $GLOBALS[$class]->process_button();
-          }
+      if (is_array($this->modules)) {
+        if (is_object($GLOBALS[$this->selected_module]) && ($GLOBALS[$this->selected_module]->enabled) ) {
+          return $GLOBALS[$this->selected_module]->process_button();
         }
       }
-
-      return $process_button_string;
     }
 
     function before_process() {
-      global $payment;
-
-      if (MODULE_PAYMENT_INSTALLED) {
-        reset($this->modules);
-        while (list(, $value) = each($this->modules)) {
-          $class = substr($value, 0, strrpos($value, '.'));
-          if ( ($GLOBALS[$class]->code == $payment) && ($GLOBALS[$class]->enabled) ) {
-            $GLOBALS[$class]->before_process();
-          }
+      if (is_array($this->modules)) {
+        if (is_object($GLOBALS[$this->selected_module]) && ($GLOBALS[$this->selected_module]->enabled) ) {
+          return $GLOBALS[$this->selected_module]->before_process();
         }
       }
     }
 
     function after_process() {
-      global $payment;
-
-      if (MODULE_PAYMENT_INSTALLED) {
-        reset($this->modules);
-        while (list(, $value) = each($this->modules)) {
-          $class = substr($value, 0, strrpos($value, '.'));
-          if ( ($GLOBALS[$class]->code == $payment) && ($GLOBALS[$class]->enabled) ) {
-            $GLOBALS[$class]->after_process();
-          }
+      if (is_array($this->modules)) {
+        if (is_object($GLOBALS[$this->selected_module]) && ($GLOBALS[$this->selected_module]->enabled) ) {
+          return $GLOBALS[$this->selected_module]->after_process();
         }
       }
     }
 
-    function show_info() {
-      global $order;
-
-      $show_info_string = '';
-      if (MODULE_PAYMENT_INSTALLED) {
-        reset($this->modules);
-        while (list(, $value) = each($this->modules)) {
-          $class = substr($value, 0, strrpos($value, '.'));
-          if ($GLOBALS[$class]->code == $order['payment_method']) {
-            $payment_text = $GLOBALS[$class]->title;
-          }
-        }
-        $show_info_string .= '          <tr>' . "\n" .
-                             '            <td class="main">' . $payment_text. '</td>' . "\n" .
-                             '          </tr>' . "\n";
-      }
-
-      return $show_info_string;
-    }
-
-    function output_error() {
-      global $HTTP_GET_VARS;
-
-      $output_error_string = '';
-      if (MODULE_PAYMENT_INSTALLED) {
-        reset($this->modules);
-        while (list(, $value) = each($this->modules)) {
-          $class = substr($value, 0, strrpos($value, '.'));
-          if ($GLOBALS[$class]->code == $HTTP_GET_VARS['payment_error']) {
-            $output_error_string .= $GLOBALS[$class]->output_error();
-          }
+    function get_error() {
+      if (is_array($this->modules)) {
+        if (is_object($GLOBALS[$this->selected_module]) && ($GLOBALS[$this->selected_module]->enabled) ) {
+          return $GLOBALS[$this->selected_module]->get_error();
         }
       }
-
-      return $output_error_string;
     }
-
   }
 ?>
