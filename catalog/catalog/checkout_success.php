bd4592e7bceaa22fd0420f41e5babc5c4ad08fb6
hpdl <hpdl>
2002-11-01 04:27:01 +0000

    updated the checkout success page for the new checkout procedure
    the order comment field has been moved from the checkout payment page to this page

--- catalog/catalog/checkout_success.php
+++ catalog/catalog/checkout_success.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: checkout_success.php,v 1.36 2002/10/16 21:35:13 project3000 Exp $
+  $Id: checkout_success.php,v 1.37 2002/11/01 04:27:01 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -12,23 +12,52 @@
 
   require('includes/application_top.php');
 
+  if ($HTTP_GET_VARS['action'] == 'update') {
+    if (tep_not_null($HTTP_POST_VARS['comment']) && tep_session_is_registered('last_order')) {
+      $comment = tep_db_prepare_input($HTTP_POST_VARS['comment']);
+
+      if (tep_session_is_registered('customer_id')) {
+        tep_db_query("update " . TABLE_ORDERS . " set comments = '" . tep_db_input($comment) . "' where customers_id = '" . $customer_id . "' and orders_id = '" . $last_order . "'");
+      } else {
+        tep_db_query("update " . TABLE_ORDERS . " set comments = '" . tep_db_input($comment) . "' where customers_id = '0' and orders_id = '" . $last_order . "'");
+      }
+
+      tep_session_unregister('last_order');
+    }
+
+    $notify_string = '';
+    if (tep_session_is_registered('customer_id')) {
+      $notify_string .= 'action=notify&';
+      $notify = $HTTP_POST_VARS['notify'];
+      if (!is_array($notify)) $notify = array($notify);
+      for ($i=0, $n=sizeof($notify); $i<$n; $i++) {
+        $notify_string .= 'notify[]=' . $notify[$i] . '&';
+      }
+      if (strlen($notify_string) > 0) $notify_string = substr($notify_string, 0, -1);
+    }
+
+    tep_redirect(tep_href_link(FILENAME_DEFAULT, $notify_string, 'SSL'));
+  }
+
   require(DIR_WS_LANGUAGES . $language . '/' . FILENAME_CHECKOUT_SUCCESS);
 
   $breadcrumb->add(NAVBAR_TITLE_1);
   $breadcrumb->add(NAVBAR_TITLE_2);
 
-  $global_query = tep_db_query("select global_product_notifications from " . TABLE_CUSTOMERS_INFO . " where customers_info_id = '" . $customer_id . "'");
-  $global = tep_db_fetch_array($global_query);
+  if (tep_session_is_registered('customer_id')) {
+    $global_query = tep_db_query("select global_product_notifications from " . TABLE_CUSTOMERS_INFO . " where customers_info_id = '" . $customer_id . "'");
+    $global = tep_db_fetch_array($global_query);
 
-  if ($global['global_product_notifications'] != '1') {
-    $orders_query = tep_db_query("select orders_id from " . TABLE_ORDERS . " where customers_id = '" . $customer_id . "' order by date_purchased desc limit 1");
-    $orders = tep_db_fetch_array($orders_query);
+    if ($global['global_product_notifications'] != '1') {
+      $orders_query = tep_db_query("select orders_id from " . TABLE_ORDERS . " where customers_id = '" . $customer_id . "' order by date_purchased desc limit 1");
+      $orders = tep_db_fetch_array($orders_query);
 
-    $products_array = array();
-    $products_query = tep_db_query("select products_id, products_name from " . TABLE_ORDERS_PRODUCTS . " where orders_id = '" . $orders['orders_id'] . "' order by products_name");
-    while ($products = tep_db_fetch_array($products_query)) {
-      $products_array[] = array('id' => $products['products_id'],
-                                'text' => $products['products_name']);
+      $products_array = array();
+      $products_query = tep_db_query("select products_id, products_name from " . TABLE_ORDERS_PRODUCTS . " where orders_id = '" . $orders['orders_id'] . "' order by products_name");
+      while ($products = tep_db_fetch_array($products_query)) {
+        $products_array[] = array('id' => $products['products_id'],
+                                  'text' => $products['products_name']);
+      }
     }
   }
 ?>
@@ -54,24 +83,42 @@
 <!-- left_navigation_eof //-->
     </table></td>
 <!-- body_text //-->
-    <td width="100%" valign="top"><form name="order" <?php echo 'action="' . tep_href_link(FILENAME_DEFAULT, 'action=notify') . '"'; ?> method="post"><table border="0" width="100%" cellspacing="0" cellpadding="0">
+    <td width="100%" valign="top"><form name="order" <?php echo 'action="' . tep_href_link(FILENAME_CHECKOUT_SUCCESS, 'action=update', 'SSL') . '"'; ?> method="post"><table border="0" width="100%" cellspacing="0" cellpadding="0">
       <tr>
         <td><table border="0" width="100%" cellspacing="4" cellpadding="2">
           <tr>
             <td valign="top"><?php echo tep_image(DIR_WS_IMAGES . 'table_background_man_on_board.gif', HEADING_TITLE); ?></td>
             <td valign="top" class="main"><?php echo tep_draw_separator('pixel_trans.gif', '1', '10'); ?><div align="center" class="pageHeading"><?php echo HEADING_TITLE; ?></div><br><?php echo TEXT_SUCCESS; ?><br><br>
 <?php
-  if ($global['global_product_notifications'] != '1') {
-    echo TEXT_NOTIFY_PRODUCTS . '<br><p class="productsNotifications">';
-    for ($i=0; $i<sizeof($products_array); $i++) {
-      echo tep_draw_checkbox_field('notify[]', $products_array[$i]['id']) . ' ' . $products_array[$i]['text'] . '<br>';
+  if (tep_session_is_registered('customer_id')) {
+    if ($global['global_product_notifications'] != '1') {
+      echo TEXT_NOTIFY_PRODUCTS . '<br><p class="productsNotifications">';
+      for ($i=0; $i<sizeof($products_array); $i++) {
+        echo tep_draw_checkbox_field('notify[]', $products_array[$i]['id']) . ' ' . $products_array[$i]['text'] . '<br>';
+      }
+      echo '</p>';
+    } else {
+      echo TEXT_SEE_ORDERS . '<br><br>' . TEXT_CONTACT_STORE_OWNER;
     }
-    echo '</p>';
-  } else {
-    echo TEXT_SEE_ORDERS . '<br><br>' . TEXT_CONTACT_STORE_OWNER;
   }
 ?>
             <h3><?php echo TEXT_THANKS_FOR_SHOPPING; ?></h3></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+      <tr>
+        <td class="main"><b><?php echo TABLE_HEADING_COMMENTS; ?></b></td>
+      </tr>
+      <tr>
+        <td><?php echo tep_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+      <tr>
+        <td><table border="0" width="100%" cellspacing="1" cellpadding="6" class="infoBox">
+          <tr class="infoBoxContents">
+            <td class="main"><?php echo tep_draw_textarea_field('comment', 'virtual', '60', '5'); ?></td>
           </tr>
         </table></td>
       </tr>
--- catalog/catalog/includes/languages/espanol/checkout_success.php
+++ catalog/catalog/includes/languages/espanol/checkout_success.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: checkout_success.php,v 1.8 2002/03/13 13:07:06 hpdl Exp $
+  $Id: checkout_success.php,v 1.9 2002/11/01 04:27:01 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -12,12 +12,17 @@
 
 define('NAVBAR_TITLE_1', 'Pedido');
 define('NAVBAR_TITLE_2', 'Realizado con Exito');
+
 define('HEADING_TITLE', 'Su Pedido ha sido Procesado!');
+
 define('TEXT_SUCCESS', 'Su pedido ha sido realizado con exito! Sus productos llegaran a su destino de 2 a 5 dias laborales.');
 define('TEXT_NOTIFY_PRODUCTS', 'Please notify me of updates to the products I have selected below:');
 define('TEXT_SEE_ORDERS', 'You can view your order history by going to the <a href="' . tep_href_link(FILENAME_ACCOUNT, '', 'SSL') . '">\'My Account\'</a> page and by clicking on <a href="' . tep_href_link(FILENAME_ACCOUNT_HISTORY, '', 'SSL') . '">\'History\'</a>.');
 define('TEXT_CONTACT_STORE_OWNER', 'Please direct any questions you have to the <a href="' . tep_href_link(FILENAME_CONTACT_US) . '">store owner</a>.');
 define('TEXT_THANKS_FOR_SHOPPING', 'Thanks for shopping with us online!');
+
+define('TABLE_HEADING_COMMENTS', 'Enter a comment for the order processed');
+
 define('TABLE_HEADING_DOWNLOAD_DATE', 'Expiry Date');
 define('TABLE_HEADING_DOWNLOAD_COUNT', 'Max # downloads');
 define('HEADING_DOWNLOAD', 'Download your products here:');
