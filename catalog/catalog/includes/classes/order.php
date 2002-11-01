4a3be0a5513f980aa7161ca5b6fe46a19017957b
hpdl <hpdl>
2002-11-01 03:02:56 +0000

    add the billing address information to the order class
    please note the shipping information in the cart() method is now being retrieved from an array, which is stored in the session in the new checkout procedure

--- catalog/catalog/includes/classes/order.php
+++ catalog/catalog/includes/classes/order.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: order.php,v 1.17 2002/08/08 22:58:28 hpdl Exp $
+  $Id: order.php,v 1.18 2002/11/01 03:02:56 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -32,7 +32,7 @@
 
       $order_id = tep_db_prepare_input($order_id);
 
-      $order_query = tep_db_query("select customers_name, customers_street_address, customers_suburb, customers_city, customers_postcode, customers_state, customers_country, customers_telephone, customers_email_address, customers_address_format_id, delivery_name, delivery_street_address, delivery_suburb, delivery_city, delivery_postcode, delivery_state, delivery_country, delivery_address_format_id, payment_method, cc_type, cc_owner, cc_number, cc_expires, comments, currency, currency_value, date_purchased, orders_status, last_modified from " . TABLE_ORDERS . " where orders_id = '" . tep_db_input($order_id) . "'");
+      $order_query = tep_db_query("select customers_name, customers_street_address, customers_suburb, customers_city, customers_postcode, customers_state, customers_country, customers_telephone, customers_email_address, customers_address_format_id, delivery_name, delivery_street_address, delivery_suburb, delivery_city, delivery_postcode, delivery_state, delivery_country, delivery_address_format_id, billing_name, billing_street_address, billing_suburb, billing_city, billing_postcode, billing_state, billing_country, billing_address_format_id, payment_method, cc_type, cc_owner, cc_number, cc_expires, comments, currency, currency_value, date_purchased, orders_status, last_modified from " . TABLE_ORDERS . " where orders_id = '" . tep_db_input($order_id) . "'");
       $order = tep_db_fetch_array($order_query);
 
       $totals_query = tep_db_query("select title, text from " . TABLE_ORDERS_TOTAL . " where orders_id = '" . tep_db_input($order_id) . "' order by sort_order");
@@ -84,6 +84,15 @@
                               'country' => $order['delivery_country'],
                               'format_id' => $order['delivery_address_format_id']);
 
+      $this->billing = array('name' => $order['billing_name'],
+                             'street_address' => $order['billing_street_address'],
+                             'suburb' => $order['billing_suburb'],
+                             'city' => $order['billing_city'],
+                             'postcode' => $order['billing_postcode'],
+                             'state' => $order['billing_state'],
+                             'country' => $order['billing_country'],
+                             'format_id' => $order['billing_address_format_id']);
+
       $index = 0;
       $orders_products_query = tep_db_query("select orders_products_id, products_name, products_model, products_price, products_tax, products_quantity, final_price from " . TABLE_ORDERS_PRODUCTS . " where orders_id = '" . tep_db_input($order_id) . "'");
       while ($orders_products = tep_db_fetch_array($orders_products_query)) {
@@ -114,13 +123,16 @@
     }
 
     function cart() {
-      global $customer_id, $sendto, $cart, $products, $languages_id, $currency, $currencies, $payment;
+      global $customer_id, $sendto, $billto, $cart, $languages_id, $currency, $currencies, $shipping, $payment;
 
       $customer_address_query = tep_db_query("select c.customers_firstname, c.customers_lastname, c.customers_telephone, c.customers_email_address, ab.entry_street_address, ab.entry_suburb, ab.entry_postcode, ab.entry_city, ab.entry_zone_id, z.zone_name, co.countries_id, co.countries_name, co.countries_iso_code_2, co.countries_iso_code_3, co.address_format_id, ab.entry_state from " . TABLE_CUSTOMERS . " c, " . TABLE_ADDRESS_BOOK . " ab left join " . TABLE_ZONES . " z on (ab.entry_zone_id = z.zone_id) left join " . TABLE_COUNTRIES . " co on (ab.entry_country_id = co.countries_id) where c.customers_id = '" . $customer_id . "' and ab.customers_id = '" . $customer_id . "' and c.customers_default_address_id = ab.address_book_id");
       $customer_address = tep_db_fetch_array($customer_address_query);
 
       $shipping_address_query = tep_db_query("select ab.entry_firstname, ab.entry_lastname, ab.entry_company, ab.entry_street_address, ab.entry_suburb, ab.entry_postcode, ab.entry_city, ab.entry_zone_id, z.zone_name, ab.entry_country_id, c.countries_id, c.countries_name, c.countries_iso_code_2, c.countries_iso_code_3, c.address_format_id, ab.entry_state from " . TABLE_ADDRESS_BOOK . " ab left join " . TABLE_ZONES . " z on (ab.entry_zone_id = z.zone_id) left join " . TABLE_COUNTRIES . " c on (ab.entry_country_id = c.countries_id) where ab.customers_id = '" . $customer_id . "' and ab.address_book_id = '" . $sendto . "'");
       $shipping_address = tep_db_fetch_array($shipping_address_query);
+      
+      $billing_address_query = tep_db_query("select ab.entry_firstname, ab.entry_lastname, ab.entry_company, ab.entry_street_address, ab.entry_suburb, ab.entry_postcode, ab.entry_city, ab.entry_zone_id, z.zone_name, ab.entry_country_id, c.countries_id, c.countries_name, c.countries_iso_code_2, c.countries_iso_code_3, c.address_format_id, ab.entry_state from " . TABLE_ADDRESS_BOOK . " ab left join " . TABLE_ZONES . " z on (ab.entry_zone_id = z.zone_id) left join " . TABLE_COUNTRIES . " c on (ab.entry_country_id = c.countries_id) where ab.customers_id = '" . $customer_id . "' and ab.address_book_id = '" . $billto . "'");
+      $billing_address = tep_db_fetch_array($billing_address_query);
 
       $this->info = array('currency' => $currency,
                           'currency_value' => $currencies->currencies[$currency]['value'],
@@ -129,8 +141,8 @@
                           'cc_owner' => $GLOBALS['cc_owner'],
                           'cc_number' => $GLOBALS['cc_number'],
                           'cc_expires' => $GLOBALS['cc_expires'],
-                          'shipping_method' => $GLOBALS['shipping_method'],
-                          'shipping_cost' => $GLOBALS['shipping_cost'],
+                          'shipping_method' => $shipping['title'],
+                          'shipping_cost' => $shipping['cost'],
                           'comments' => $GLOBALS['comments']);
 
       if (is_object($GLOBALS[$payment])) {
@@ -160,8 +172,19 @@
                               'country_id' => $shipping_address['entry_country_id'],
                               'format_id' => $shipping_address['address_format_id']);
 
+      $this->billing = array('firstname' => $billing_address['entry_firstname'],
+                             'lastname' => $billing_address['entry_lastname'],
+                             'street_address' => $billing_address['entry_street_address'],
+                             'suburb' => $billing_address['entry_suburb'],
+                             'city' => $billing_address['entry_city'],
+                             'postcode' => $billing_address['entry_postcode'],
+                             'state' => ((tep_not_null($billing_address['entry_state'])) ? $billing_address['entry_state'] : $billing_address['zone_name']),
+                             'country' => array('id' => $billing_address['countries_id'], 'title' => $billing_address['countries_name'], 'iso_code_2' => $billing_address['countries_iso_code_2'], 'iso_code_3' => $billing_address['countries_iso_code_3']),
+                             'country_id' => $billing_address['entry_country_id'],
+                             'format_id' => $billing_address['address_format_id']);
+
       $index = 0;
-      if (!is_array($products)) $products = $cart->get_products();
+      $products = $cart->get_products();
       for ($i=0; $i<sizeof($products); $i++) {
         $this->products[$index] = array('qty' => $products[$i]['quantity'],
                                         'name' => $products[$i]['name'],
