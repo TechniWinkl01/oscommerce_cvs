6cbe3b822aed68915ca22c90503ab0dd20d1a964
hpdl <hpdl>
2002-11-01 05:59:00 +0000

    update the admin order class to use the billing address information

--- admin/admin/includes/classes/order.php
+++ admin/admin/includes/classes/order.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: order.php,v 1.3 2002/04/12 22:16:17 hpdl Exp $
+  $Id: order.php,v 1.4 2002/11/01 05:59:00 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -24,7 +24,7 @@
     }
 
     function query($order_id) {
-      $order_query = tep_db_query("select customers_name, customers_street_address, customers_suburb, customers_city, customers_postcode, customers_state, customers_country, customers_telephone, customers_email_address, customers_address_format_id, delivery_name, delivery_street_address, delivery_suburb, delivery_city, delivery_postcode, delivery_state, delivery_country, delivery_address_format_id, payment_method, cc_type, cc_owner, cc_number, cc_expires, comments, currency, currency_value, date_purchased, orders_status, last_modified from " . TABLE_ORDERS . " where orders_id = '" . tep_db_input($order_id) . "'");
+      $order_query = tep_db_query("select customers_name, customers_street_address, customers_suburb, customers_city, customers_postcode, customers_state, customers_country, customers_telephone, customers_email_address, customers_address_format_id, delivery_name, delivery_street_address, delivery_suburb, delivery_city, delivery_postcode, delivery_state, delivery_country, delivery_address_format_id, billing_name, billing_street_address, billing_suburb, billing_city, billing_postcode, billing_state, billing_country, billing_address_format_id, payment_method, cc_type, cc_owner, cc_number, cc_expires, comments, currency, currency_value, date_purchased, orders_status, last_modified from " . TABLE_ORDERS . " where orders_id = '" . tep_db_input($order_id) . "'");
       $order = tep_db_fetch_array($order_query);
 
       $totals_query = tep_db_query("select title, text from " . TABLE_ORDERS_TOTAL . " where orders_id = '" . tep_db_input($order_id) . "' order by sort_order");
@@ -64,6 +64,15 @@
                               'state' => $order['delivery_state'],
                               'country' => $order['delivery_country'],
                               'format_id' => $order['delivery_address_format_id']);
+
+      $this->billing = array('name' => $order['billing_name'],
+                             'street_address' => $order['billing_street_address'],
+                             'suburb' => $order['billing_suburb'],
+                             'city' => $order['billing_city'],
+                             'postcode' => $order['billing_postcode'],
+                             'state' => $order['billing_state'],
+                             'country' => $order['billing_country'],
+                             'format_id' => $order['billing_address_format_id']);
 
       $index = 0;
       $orders_products_query = tep_db_query("select orders_products_id, products_name, products_model, products_price, products_tax, products_quantity, final_price from " . TABLE_ORDERS_PRODUCTS . " where orders_id = '" . tep_db_input($order_id) . "'");
