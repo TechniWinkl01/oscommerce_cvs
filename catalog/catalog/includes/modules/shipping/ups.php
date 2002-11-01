fdfa9190c9e9df86e470512ffe26d97dd4a4982a
hpdl <hpdl>
2002-11-01 04:50:10 +0000

    updated the UPS shipping module to use the new shipping API for the new checkout procedure
    now all relevant rates are returned in the one query - worldwide shipping is also now possible (from America)

--- catalog/catalog/includes/modules/shipping/ups.php
+++ catalog/catalog/includes/modules/shipping/ups.php
@@ -1,17 +1,17 @@
 <?php
 /*
-  $Id: ups.php,v 1.43 2002/08/13 16:00:42 dgw_ Exp $
+  $Id: ups.php,v 1.44 2002/11/01 04:50:10 hpdl Exp $
 
-  The Exchange Project - Community Made Shopping!
-  http://www.theexchangeproject.org
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
 
-  Copyright (c) 2000,2001 The Exchange Project
+  Copyright (c) 2002 osCommerce
 
   Released under the GNU General Public License
 */
 
   class ups {
-    var $code, $title, $descrption, $icon, $enabled, $quote;
+    var $code, $title, $descrption, $icon, $enabled, $types;
 
 // class constructor
     function ups() {
@@ -20,115 +20,74 @@
       $this->description = MODULE_SHIPPING_UPS_TEXT_DESCRIPTION;
       $this->icon = DIR_WS_ICONS . 'shipping_ups.gif';
       $this->enabled = MODULE_SHIPPING_UPS_STATUS;
+
+      $this->types = array('1DM' => 'Next Day Air Early AM',
+                           '1DML' => 'Next Day Air Early AM Letter',
+                           '1DA' => 'Next Day Air',
+                           '1DAL' => 'Next Day Air Letter',
+                           '1DAPI' => 'Next Day Air Intra (Puerto Rico)',
+                           '1DP' => 'Next Day Air Saver',
+                           '1DPL' => 'Next Day Air Saver Letter',
+                           '2DM' => '2nd Day Air AM',
+                           '2DML' => '2nd Day Air AM Letter',
+                           '2DA' => '2nd Day Air',
+                           '2DAL' => '2nd Day Air Letter',
+                           '3DS' => '3 Day Select',
+                           'GNDCOM' => 'Ground Commercial',
+                           'GNDRES' => 'Ground Residential',
+                           'STD' => 'Canada Standard',
+                           'XPR' => 'Worldwide Express',
+                           'XPRL' => 'worldwide Express Letter',
+                           'XDM' => 'Worldwide Express Plus',
+                           'XDML' => 'Worldwide Express Plus Letter',
+                           'XPD' => 'Worldwide Expedited');
     }
 
 // class methods
-    function selection() {
-      $selection_string = '<table border="0" cellspacing="0" cellpadding="0" width="100%">' . "\n" .
-                          '  <tr>' . "\n" .
-                          '    <td class="main">' . (($this->icon) ? tep_image($this->icon, $this->title) : '') . ' ' . MODULE_SHIPPING_UPS_TEXT_TITLE . '</td>' . "\n" .
-                          '    <td align="right" class="main"><select name="shipping_ups_prod">' .
-                                                             '<option value="GND">' . MODULE_SHIPPING_UPS_TEXT_OPT_GND . '</option>' .
-                                                             '<option value="1DM">' . MODULE_SHIPPING_UPS_TEXT_OPT_1DM . '</option>' .
-                                                             '<option value="1DA">' . MODULE_SHIPPING_UPS_TEXT_OPT_1DA . '</option>' .
-                                                             '<option value="1DP">' . MODULE_SHIPPING_UPS_TEXT_OPT_1DP . '</option>' .
-                                                             '<option value="2DM">' . MODULE_SHIPPING_UPS_TEXT_OPT_2DM . '</option>' .
-                                                             '<option value="3DS">' . MODULE_SHIPPING_UPS_TEXT_OPT_3DS . '</option>' .
-                                                             '<option value="STD">' . MODULE_SHIPPING_UPS_TEXT_OPT_STD . '</option>' .
-                                                             '<option value="XPR">' . MODULE_SHIPPING_UPS_TEXT_OPT_XPR . '</option>' .
-                                                             '<option value="XDM">' . MODULE_SHIPPING_UPS_TEXT_OPT_XDM . '</option>' .
-                                                             '<option value="XPD">' . MODULE_SHIPPING_UPS_TEXT_OPT_XPD . '</option>' .
-                                                             '</select>' . tep_draw_checkbox_field('shipping_quote_ups', '1', true) . '</td>' . "\n" .
-                          '  </tr>' . "\n" .
-                          '</table>' . "\n";
+    function quote($method = '') {
+      global $HTTP_POST_VARS, $order, $shipping_weight, $shipping_num_boxes;
 
-      return $selection_string;
-    }
-
-    function quote() {
-      global $HTTP_POST_VARS, $address_values, $shipping_weight, $shipping_ups_cost, $shipping_ups_method, $shipping_num_boxes, $shipping_quoted;
-
-      if ( ($GLOBALS['shipping_quote_all'] == '1') || ($GLOBALS['shipping_quote_ups'] == '1') ) {
-        $shipping_quoted = 'ups';
-        $prod = ($HTTP_POST_VARS['shipping_ups_prod']) ? $HTTP_POST_VARS['shipping_ups_prod'] : 'GND';
-        include(DIR_WS_CLASSES . '_ups.php');
-        $rate = new _Ups;
-        $rate->upsProduct($prod); // See upsProduct() function for codes
-        $country_name = tep_get_countries(STORE_COUNTRY, '1');
-        $rate->origin(STORE_ORIGIN_ZIP, $country_name['countries_iso_code_2']); // Use ISO country codes!
-        $country_name = tep_get_countries($address_values['country_id'], '1');
-        $country_post = str_replace(' ', '', $address_values['postcode']);
-        $rate->dest($country_post, $country_name['countries_iso_code_2']);      // Use ISO country codes!
-        $rate->rate(MODULE_SHIPPING_UPS_PICKUP);        // See the rate() function for codes
-        $rate->container(MODULE_SHIPPING_UPS_PACKAGE);    // See the container() function for codes
-        $rate->weight($shipping_weight);
-        $rate->rescom(MODULE_SHIPPING_UPS_RES);    // See the rescom() function for codes
-        $this->quote = $rate->getQuote();
-        $shipping_ups_cost = (SHIPPING_HANDLING + $this->quote) * $shipping_num_boxes;
-        $shipping_ups_method = 'UPS ' . $prod . ' ' . $shipping_num_boxes . ' X ' . $shipping_weight;
+      if ( (tep_not_null($method)) && (isset($this->types[$method])) ) {
+        $prod = $method;
+      } else {
+        $prod = 'GNDRES';
       }
-    }
 
-    function cheapest() {
-      global $shipping_count, $shipping_cheapest, $shipping_cheapest_cost, $shipping_ups_cost;
+      if ($method) $this->_upsAction('3'); // return a single quote
 
-      if ( ($GLOBALS['shipping_quote_all'] == '1') || ($GLOBALS['shipping_quote_ups'] == '1') ) {
-        if ($shipping_count == 0) {
-          $shipping_cheapest = 'ups';
-          $shipping_cheapest_cost = $shipping_ups_cost;
-        } else {
-          if ($shipping_ups_cost < $shipping_cheapest_cost) {
-            $shipping_cheapest = 'ups';
-            $shipping_cheapest_cost = $shipping_ups_cost;
-          }
+      $this->_upsProduct($prod);
+
+      $country_name = tep_get_countries(STORE_COUNTRY, true);
+      $this->_upsOrigin(STORE_ORIGIN_ZIP, $country_name['countries_iso_code_2']);
+      $this->_upsDest($order->delivery['postcode'], $order->delivery['country']['iso_code_2']);
+      $this->_upsRate(MODULE_SHIPPING_UPS_PICKUP);
+      $this->_upsContainer(MODULE_SHIPPING_UPS_PACKAGE);
+      $this->_upsWeight($shipping_weight);
+      $this->_upsRescom(MODULE_SHIPPING_UPS_RES);
+      $upsQuote = $this->_upsGetQuote();
+
+      if ( (is_array($upsQuote)) && (sizeof($upsQuote) > 0) ) {
+        $this->quotes = array('id' => $this->code,
+                              'module' => $this->title . ' (' . $shipping_num_boxes . ' x ' . $shipping_weight . 'lbs)');
+
+        $methods = array();
+        for ($i=0; $i<sizeof($upsQuote); $i++) {
+          list($type, $cost) = each($upsQuote[$i]);
+
+          $methods[] = array('id' => $type,
+                             'title' => $this->types[$type],
+                             'cost' => (SHIPPING_HANDLING + $cost) * $shipping_num_boxes);
         }
-        $shipping_count++;
-      }
-    }
 
-    function display() {
-      global $HTTP_GET_VARS, $currencies, $shipping_ups_cost, $shipping_ups_method, $shipping_cheapest, $shipping_selected;
-
-// set a global for the radio field (auto select cheapest shipping method)
-      if (!$shipping_selected) $shipping_selected = $shipping_cheapest;
-
-      if ( ($GLOBALS['shipping_quote_all'] == '1') || ($GLOBALS['shipping_quote_ups'] == '1') ) {
-        if ($this->quote > 0) {
-          $display_string = '<table border="0" width="100%" cellspacing="0" cellpadding="0">' . "\n" .
-                            '  <tr>' . "\n" .
-                            '    <td class="main">' . (($this->icon) ? tep_image($this->icon, $this->title) : '') . ' ' . MODULE_SHIPPING_UPS_TEXT_TITLE . ' <small><i>(' . $shipping_ups_method . ')</i></small></td>' . "\n" .
-                            '    <td align="right" class="main">' . $currencies->format($shipping_ups_cost);
-          if (tep_count_shipping_modules() > 1) {
-            $display_string .= tep_draw_radio_field('shipping_selected', 'ups') .
-                               tep_draw_hidden_field('shipping_ups_cost', $shipping_ups_cost) .
-                               tep_draw_hidden_field('shipping_ups_method', $shipping_ups_method) . '</td>' . "\n";
-          } else {
-            $display_string .= tep_draw_hidden_field('shipping_selected', 'ups') .
-                               tep_draw_hidden_field('shipping_ups_cost', $shipping_ups_cost) .
-                               tep_draw_hidden_field('shipping_ups_method', $shipping_ups_method) . '</td>' . "\n";
-          }
-          $display_string .= '  </tr>' . "\n" .
-                             '</table>' . "\n";
-        } else {
-          $display_string .= '<table border="0" width="100%" cellspacing="0" cellpadding="0">' . "\n" .
-                             '  <tr>' . "\n" .
-                             '    <td class="main">' . (($this->icon) ? tep_image($this->icon, $this->title) : '') . ' ' . MODULE_SHIPPING_UPS_TEXT_TITLE . '</td>' . "\n" .
-                             '    <td class="main"><font color="#ff0000">Error:</font> ' . $this->quote . '</td>' . "\n" .
-                             '  </tr>' . "\n" .
-                             '</table>' . "\n";
-        }
+        $this->quotes['methods'] = $methods;
+      } else {
+        $this->quotes = array('module' => $this->title,
+                              'error' => 'An error occured with the UPS shipping calculations.<br>If you prefer to use UPS as your shipping method, please contact the store owner.');
       }
 
-      return $display_string;
-    }
+      if (tep_not_null($this->icon)) $this->quotes['icon'] = tep_image($this->icon, $this->title);
 
-    function confirm() {
-      global $HTTP_POST_VARS, $shipping_cost, $shipping_method, $shipping_selected;
-
-      if ($shipping_selected == 'ups') {
-        $shipping_cost = $HTTP_POST_VARS['shipping_ups_cost'];
-        $shipping_method = $HTTP_POST_VARS['shipping_ups_method'];
-      }
+      return $this->quotes;
     }
 
     function check() {
@@ -141,22 +100,166 @@
 
     function install() {
       tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Enable UPS Shipping', 'MODULE_SHIPPING_UPS_STATUS', '1', 'Do you want to offer UPS shipping?', '6', '9', now())");
-  	  tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('UPS Pickup Method', 'MODULE_SHIPPING_UPS_PICKUP', 'CC', 'How do you give packages to UPS? CC - Customer Counter, RDP - Daily Pickup, OTP - One Time Pickup, LC - Letter Center, OCA - On Call Air', '6', '10', now())");
-	    tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('UPS Packaging?', 'MODULE_SHIPPING_UPS_PACKAGE', 'CP', 'CP - Your Packaging, ULE - UPS Letter, UT - UPS Tube, UBE - UPS Express Box', '6', '11', now())");
-	    tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Residential Delivery?', 'MODULE_SHIPPING_UPS_RES', 'RES', 'Quote for Residential (RES) or Commercial Delivery (COM)', '6', '12', now())");
+      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('UPS Pickup Method', 'MODULE_SHIPPING_UPS_PICKUP', 'CC', 'How do you give packages to UPS? CC - Customer Counter, RDP - Daily Pickup, OTP - One Time Pickup, LC - Letter Center, OCA - On Call Air', '6', '10', now())");
+      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('UPS Packaging?', 'MODULE_SHIPPING_UPS_PACKAGE', 'CP', 'CP - Your Packaging, ULE - UPS Letter, UT - UPS Tube, UBE - UPS Express Box', '6', '11', now())");
+      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Residential Delivery?', 'MODULE_SHIPPING_UPS_RES', 'RES', 'Quote for Residential (RES) or Commercial Delivery (COM)', '6', '12', now())");
     }
 
     function remove() {
-      tep_db_query("delete from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_SHIPPING_UPS_STATUS'");
-      tep_db_query("delete from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_SHIPPING_UPS_PICKUP'");
-      tep_db_query("delete from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_SHIPPING_UPS_PACKAGE'");
-      tep_db_query("delete from " . TABLE_CONFIGURATION . " where configuration_key = 'MODULE_SHIPPING_UPS_RES'");
+      $keys = '';
+      $keys_array = $this->keys();
+      for ($i=0; $i<sizeof($keys_array); $i++) {
+        $keys .= "'" . $keys_array[$i] . "',";
+      }
+      $keys = substr($keys, 0, -1);
+
+      tep_db_query("delete from " . TABLE_CONFIGURATION . " where configuration_key in (" . $keys . ")");
     }
 
     function keys() {
-      $keys = array('MODULE_SHIPPING_UPS_STATUS', 'MODULE_SHIPPING_UPS_PICKUP', 'MODULE_SHIPPING_UPS_PACKAGE', 'MODULE_SHIPPING_UPS_RES');
+      return array('MODULE_SHIPPING_UPS_STATUS', 'MODULE_SHIPPING_UPS_PICKUP', 'MODULE_SHIPPING_UPS_PACKAGE', 'MODULE_SHIPPING_UPS_RES');
+    }
 
-      return $keys;
+    function _upsProduct($prod){
+      $this->_upsProductCode = $prod;
+    }
+
+    function _upsOrigin($postal, $country){
+      $this->_upsOriginPostalCode = $postal;
+      $this->_upsOriginCountryCode = $country;
+    }
+
+    function _upsDest($postal, $country){
+      $postal = str_replace(' ', '', $postal);
+
+      if ($country == 'US') {
+        $this->_upsDestPostalCode = substr($postal, 0, 5);
+      } else {
+        $this->_upsDestPostalCode = $postal;
+      }
+
+      $this->_upsDestCountryCode = $country;
+    }
+
+    function _upsRate($foo) {
+      switch ($foo) {
+        case 'RDP':
+          $this->_upsRateCode = 'Regular+Daily+Pickup';
+          break;
+        case 'OCA':
+          $this->_upsRateCode = 'On+Call+Air';
+          break;
+        case 'OTP':
+          $this->_upsRateCode = 'One+Time+Pickup';
+          break;
+        case 'LC':
+          $this->_upsRateCode = 'Letter+Center';
+          break;
+        case 'CC':
+          $this->_upsRateCode = 'Customer+Counter';
+          break;
+      }
+    }
+
+    function _upsContainer($foo) {
+      switch ($foo) {
+        case 'CP': // Customer Packaging
+          $this->_upsContainerCode = '00';
+          break;
+        case 'ULE': // UPS Letter Envelope
+          $this->_upsContainerCode = '01';
+          break;
+        case 'UT': // UPS Tube
+          $this->_upsContainerCode = '03';
+          break;
+        case 'UEB': // UPS Express Box
+          $this->_upsContainerCode = '21';
+          break;
+        case 'UW25': // UPS Worldwide 25 kilo
+          $this->_upsContainerCode = '24';
+          break;
+        case 'UW10': // UPS Worldwide 10 kilo
+          $this->_upsContainerCode = '25';
+          break;
+      }
+    }
+
+    function _upsWeight($foo) {
+      $this->_upsPackageWeight = $foo;
+    }
+
+    function _upsRescom($foo) {
+      switch ($foo) {
+        case 'RES': // Residential Address
+          $this->_upsResComCode = '1';
+          break;
+        case 'COM': // Commercial Address
+          $this->_upsResComCode = '2';
+          break;
+      }
+    }
+
+    function _upsAction($action) {
+      /* 3 - Single Quote
+         4 - All Available Quotes */
+
+      $this->_upsActionCode = $action;
+    }
+
+    function _upsGetQuote() {
+      if (!isset($this->_upsActionCode)) $this->_upsActionCode = '4';
+
+      $request = join('&', array('accept_UPS_license_agreement=yes',
+                                 '10_action=' . $this->_upsActionCode,
+                                 '13_product=' . $this->_upsProductCode,
+                                 '14_origCountry=' . $this->_upsOriginCountryCode,
+                                 '15_origPostal=' . $this->_upsOriginPostalCode,
+                                 '19_destPostal=' . $this->_upsDestPostalCode,
+                                 '22_destCountry=' . $this->_upsDestCountryCode,
+                                 '23_weight=' . $this->_upsPackageWeight,
+                                 '47_rate_chart=' . $this->_upsRateCode,
+                                 '48_container=' . $this->_upsContainerCode,
+                                 '49_residential=' . $this->_upsResComCode));
+
+      $http = new httpClient();
+      if ($http->Connect('www.ups.com', 80)) {
+        $http->addHeader('Host', 'www.ups.com');
+        $http->addHeader('User-Agent', 'osCommerce');
+        $http->addHeader('Connection', 'Close');
+
+        if ($http->Get('/using/services/rave/qcostcgi.cgi?' . $request)) $body = $http->getBody();
+
+        $http->Disconnect();
+      } else {
+        return 'error';
+      }
+
+      $body_array = explode("\n", $body);
+
+      $returnval = array();
+
+      for ($i=0, $n=sizeof($body_array); $i<$n; $i++) {
+        $result = explode('%', $body_array[$i]);
+        $errcode = substr($result[0], -1);
+        switch ($errcode) {
+          case 3:
+            if (is_array($returnval)) $returnval[] = array($result[1] => $result[8]);
+            break;
+          case 4:
+            if (is_array($returnval)) $returnval[] = array($result[1] => $result[8]);
+            break;
+          case 5:
+            $returnval = $result[1];
+            break;
+          case 6:
+            $returnval = $result[1];
+            break;
+        }
+      }
+
+      if (empty($returnval)) $returnval = 'error';
+
+      return $returnval;
     }
   }
 ?>
