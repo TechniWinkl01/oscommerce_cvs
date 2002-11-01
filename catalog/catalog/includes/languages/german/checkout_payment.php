888a54cdc7977d812637a2f74d82bfa2edfa27b1
harley_vb <harley_vb>
2002-11-01 18:30:26 +0000

    German translation

--- catalog/catalog/includes/languages/german/checkout_payment.php
+++ catalog/catalog/includes/languages/german/checkout_payment.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: checkout_payment.php,v 1.16 2002/11/01 03:33:53 hpdl Exp $
+  $Id: checkout_payment.php,v 1.17 2002/11/01 18:30:26 harley_vb Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -15,14 +15,14 @@ define('NAVBAR_TITLE_2', 'Zahlungsweise');
 
 define('HEADING_TITLE', 'Zahlungsweise:');
 
-define('TABLE_HEADING_BILLING_ADDRESS', 'Billing Address');
-define('TEXT_SELECTED_BILLING_DESTINATION', 'Please choose from your address book where you would like the invoice to be sent to.');
-define('TITLE_BILLING_ADDRESS', 'Billing Address:');
+define('TABLE_HEADING_BILLING_ADDRESS', 'Rechnungsadresse');
+define('TEXT_SELECTED_BILLING_DESTINATION', 'Bitte w&auml;hlen Sie aus Ihrem Adressbuch die gew&uuml;nschte Rechnungsadresse f&uuml;r Ihre Bestellung aus.');
+define('TITLE_BILLING_ADDRESS', 'Rechnungsadresse:');
 
-define('TABLE_HEADING_PAYMENT_METHOD', 'Payment Method');
-define('TEXT_SELECT_PAYMENT_METHOD', 'Please select the preferred payment method to use on this order.');
-define('TITLE_PLEASE_SELECT', 'Please Select');
+define('TABLE_HEADING_PAYMENT_METHOD', 'Zahlungsweise');
+define('TEXT_SELECT_PAYMENT_METHOD', 'Bitte w&auml;hlen Sie die gew&uuml;nschte Zahlungsweise f&uuml;r Ihre Bestellung aus.');
+define('TITLE_PLEASE_SELECT', 'Bitte w&auml;hlen Sie');
 
-define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', 'Continue Checkout Procedure');
-define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', 'to confirm this order.');
+define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', 'Fortsetzung des Bestellvorganges');
+define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', 'zur Best&auml;tigung Ihrer Bestellung.');
 ?>
