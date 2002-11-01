1154419121342f10374b2307f85ec903623c04b1
hpdl <hpdl>
2002-11-01 04:42:35 +0000

    updated the shipping module API for the new checkout procedure
    optimizations were made - now when a shipping method has been selected, only its class is being included and no other shipping module class
    
    the updated API is incompatible to shipping modules that have been made for 2.2-CVS prior the new checkout procedure commit - these modules need to be updated in order for them to be used with the new checkout procedure

--- catalog/catalog/includes/classes/shipping.php
+++ catalog/catalog/includes/classes/shipping.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: shipping.php,v 1.14 2002/09/27 16:17:57 thomasamoulton Exp $
+  $Id: shipping.php,v 1.15 2002/11/01 04:42:35 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -14,44 +14,39 @@
     var $modules;
 
 // class constructor
-    function shipping() {
-      global $shipping_count, $language;
+    function shipping($module = '') {
+      global $language, $PHP_SELF;
 
-      $shipping_count = 0;
-      if (MODULE_SHIPPING_INSTALLED) {
-        $this->modules = explode(';', MODULE_SHIPPING_INSTALLED); // get array of accepted modules
-        reset($this->modules);
-        while (list(, $value) = each($this->modules)) {
-          include(DIR_WS_LANGUAGES . $language . '/modules/shipping/' . $value);
-          include(DIR_WS_MODULES . 'shipping/' . $value);
+      if (tep_not_null(MODULE_SHIPPING_INSTALLED)) {
+        $this->modules = explode(';', MODULE_SHIPPING_INSTALLED);
 
-          $class = substr($value, 0, strrpos($value, '.'));
-          $GLOBALS[$class] = new $class;
-        }
-      }
-    }
+        $include_modules = array();
 
-// class methods
-    function selection() {
-      $selection_string = '';
-      if (MODULE_SHIPPING_INSTALLED) {
-        reset($this->modules);
-        while (list(, $value) = each($this->modules)) {
-          $class = substr($value, 0, strrpos($value, '.'));
-          if ($GLOBALS[$class]->enabled) {
-            $selection_string .= $GLOBALS[$class]->selection();
+        if ( (tep_not_null($module)) && (in_array(substr($module['id'], 0, strpos($module['id'], '_')) . '.' . substr($PHP_SELF, (strrpos($PHP_SELF, '.')+1)), $this->modules)) ) {
+          $include_modules[] = array('class' => substr($module['id'], 0, strpos($module['id'], '_')), 'file' => substr($module['id'], 0, strpos($module['id'], '_')) . '.' . substr($PHP_SELF, (strrpos($PHP_SELF, '.')+1)));
+        } else {
+          reset($this->modules);
+          while (list(, $value) = each($this->modules)) {
+            $class = substr($value, 0, strrpos($value, '.'));
+            $include_modules[] = array('class' => $class, 'file' => $value);
           }
         }
-        $selection_string .= tep_draw_hidden_field('shipping_quote_all', '0');
-      }
 
-      return $selection_string;
+        for ($i=0, $n=sizeof($include_modules); $i<$n; $i++) {
+          include(DIR_WS_LANGUAGES . $language . '/modules/shipping/' . $include_modules[$i]['file']);
+          include(DIR_WS_MODULES . 'shipping/' . $include_modules[$i]['file']);
+
+          $GLOBALS[$include_modules[$i]['class']] = new $include_modules[$i]['class'];
+        }
+      }
     }
 
-    function quote() {
+    function quote($method = '', $module = '') {
       global $total_weight, $shipping_weight, $shipping_quoted, $shipping_num_boxes;
 
-      if (MODULE_SHIPPING_INSTALLED) {
+      $quotes_array = array();
+
+      if (is_array($this->modules)) {
         $shipping_quoted = '';
         $shipping_num_boxes = 1;
         $shipping_weight = $total_weight;
@@ -68,52 +63,60 @@
           $shipping_weight = $shipping_weight + ($shipping_weight*SHIPPING_BOX_PADDING/100);
         }
 
+        $include_quotes = array();
+
         reset($this->modules);
         while (list(, $value) = each($this->modules)) {
           $class = substr($value, 0, strrpos($value, '.'));
-          if ($GLOBALS[$class]->enabled) {
-            $GLOBALS[$class]->quote();
+          if (tep_not_null($module)) {
+            if ( ($module == $class) && ($GLOBALS[$class]->enabled) ) {
+              $include_quotes[] = $class;
+            }
+          } elseif ($GLOBALS[$class]->enabled) {
+            $include_quotes[] = $class;
           }
         }
+
+        for ($i=0; $i<sizeof($include_quotes); $i++) {
+          $quotes = $GLOBALS[$include_quotes[$i]]->quote($method);
+          if (is_array($quotes)) $quotes_array[] = $quotes;
+        }
       }
+
+      return $quotes_array;
     }
 
     function cheapest() {
-      if (MODULE_SHIPPING_INSTALLED) {
+      if (is_array($this->modules)) {
+        $rates = array();
+
         reset($this->modules);
         while (list(, $value) = each($this->modules)) {
           $class = substr($value, 0, strrpos($value, '.'));
           if ($GLOBALS[$class]->enabled) {
-            $GLOBALS[$class]->cheapest();
+            $quotes = $GLOBALS[$class]->quotes;
+            for ($i=0; $i<sizeof($quotes['methods']); $i++) {
+              if ($quotes['methods'][$i]['cost']) {
+                $rates[] = array('id' => $quotes['id'] . '_' . $quotes['methods'][$i]['id'],
+                                 'title' => $quotes['module'] . ' (' . $quotes['methods'][$i]['title'] . ')',
+                                 'cost' => $quotes['methods'][$i]['cost']);
+              }
+            }
           }
         }
-      }
-    }
 
-    function display() {
-      $display_string = '';
-      if (MODULE_SHIPPING_INSTALLED) {
-        reset($this->modules);
-        while (list(, $value) = each($this->modules)) {
-          $class = substr($value, 0, strrpos($value, '.'));
-          if ($GLOBALS[$class]->enabled) {
-            $display_string .= $GLOBALS[$class]->display();
+        $cheapest = false;
+        for ($i=0; $i<sizeof($rates); $i++) {
+          if (is_array($cheapest)) {
+            if ($rates[$i]['cost'] < $cheapest['cost']) {
+              $cheapest = $rates[$i];
+            }
+          } else {
+            $cheapest = $rates[$i];
           }
         }
-      }
 
-      return $display_string;
-    }
-
-    function confirm() {
-      if (MODULE_SHIPPING_INSTALLED) {
-        reset($this->modules);
-        while (list(, $value) = each($this->modules)) {
-          $class = substr($value, 0, strrpos($value, '.'));
-          if ($GLOBALS[$class]->enabled) {
-            $GLOBALS[$class]->confirm();
-          }
-        }
+        return $cheapest;
       }
     }
   }
