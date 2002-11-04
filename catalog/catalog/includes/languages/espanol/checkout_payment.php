e2d922a0800d65b48280854715ecc7f9960c9a2d
hpdl <hpdl>
2002-11-04 00:52:18 +0000

    added new text to display to the customer when there is only one payment method available to use (as no selections can occur with just 1 method)

--- catalog/catalog/includes/languages/espanol/checkout_payment.php
+++ catalog/catalog/includes/languages/espanol/checkout_payment.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: checkout_payment.php,v 1.12 2002/11/01 03:33:53 hpdl Exp $
+  $Id: checkout_payment.php,v 1.13 2002/11/04 00:52:18 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -22,6 +22,7 @@ define('TITLE_BILLING_ADDRESS', 'Billing Address:');
 define('TABLE_HEADING_PAYMENT_METHOD', 'Payment Method');
 define('TEXT_SELECT_PAYMENT_METHOD', 'Please select the preferred payment method to use on this order.');
 define('TITLE_PLEASE_SELECT', 'Please Select');
+define('TEXT_ENTER_PAYMENT_INFORMATION', 'This is currently the only payment method available to use on this order.');
 
 define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', 'Continue Checkout Procedure');
 define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', 'to confirm this order.');
--- catalog/catalog/includes/languages/german/checkout_payment.php
+++ catalog/catalog/includes/languages/german/checkout_payment.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: checkout_payment.php,v 1.17 2002/11/01 18:30:26 harley_vb Exp $
+  $Id: checkout_payment.php,v 1.18 2002/11/04 00:52:18 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -22,6 +22,7 @@ define('TITLE_BILLING_ADDRESS', 'Rechnungsadresse:');
 define('TABLE_HEADING_PAYMENT_METHOD', 'Zahlungsweise');
 define('TEXT_SELECT_PAYMENT_METHOD', 'Bitte w&auml;hlen Sie die gew&uuml;nschte Zahlungsweise f&uuml;r Ihre Bestellung aus.');
 define('TITLE_PLEASE_SELECT', 'Bitte w&auml;hlen Sie');
+define('TEXT_ENTER_PAYMENT_INFORMATION', 'This is currently the only payment method available to use on this order.');
 
 define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', 'Fortsetzung des Bestellvorganges');
 define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', 'zur Best&auml;tigung Ihrer Bestellung.');
