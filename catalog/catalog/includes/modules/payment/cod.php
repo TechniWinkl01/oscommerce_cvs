fc69db225fa6644c72a6dfb4a6d588bd37a53e08
hpdl <hpdl>
2002-11-04 02:13:51 +0000

    introduce a new zone concept - when a zone is attached to this module, this module will only be enabled if the destination zone applies with the attached zone
    this concept is useful for this module - the cash on delivery payment module - to limit which zones this module applies to (ie, to avoid having cash on delivery available on a world wide basis)
    
    the concept is very basic, and uses the already implemented zone logic for the tax zones [zones can be setup at the Administration Tool -> Locations -> Tax Zones section which will be shown as a pull down menu in the Administration Tool -> Modules -> Payment -> Cash on Delivery configuration section]
    
    this is a commit to be reviewed by the core team - if accepted, the current Tax Zones title should be changed as it would no longer be used just for taxes, and the other modules would more than likely be updated as well

--- catalog/catalog/includes/modules/payment/cod.php
+++ catalog/catalog/includes/modules/payment/cod.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: cod.php,v 1.17 2002/11/01 05:02:43 hpdl Exp $
+  $Id: cod.php,v 1.18 2002/11/04 02:13:51 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -15,10 +15,27 @@
 
 // class constructor
     function cod() {
+      global $customer_id, $order;
+
       $this->code = 'cod';
       $this->title = MODULE_PAYMENT_COD_TEXT_TITLE;
       $this->description = MODULE_PAYMENT_COD_TEXT_DESCRIPTION;
       $this->enabled = ((MODULE_PAYMENT_COD_STATUS == 'True') ? true : false);
+
+      if ( ($this->enabled == true) && (MODULE_PAYMENT_COD_ZONE != '0') ) {
+        $check_flag = false;
+        $check_query = tep_db_query("select zone_country_id from " . TABLE_ZONES_TO_GEO_ZONES . " where geo_zone_id = '" . MODULE_PAYMENT_COD_ZONE . "'");
+        while ($check = tep_db_fetch_array($check_query)) {
+          if ($check['zone_country_id'] == $order->billing['country']['id']) {
+            $check_flag = true;
+            break;
+          }
+        }
+
+        if ($check_flag == false) {
+          $this->enabled = false;
+        }
+      }
     }
 
 // class methods
@@ -65,6 +82,7 @@
 
     function install() {
       tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Cash On Delivery Module', 'MODULE_PAYMENT_COD_STATUS', 'True', 'Do you want to accept Cash On Delevery payments?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now());");
+      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function, date_added) values ('Payment Zone', 'MODULE_PAYMENT_COD_ZONE', '0', 'If a zone is selected, only enable this payment method for that zone.', '6', '2', 'tep_get_zone_class_title', 'tep_cfg_pull_down_zone_classes(', now())");
     }
 
     function remove() {
@@ -79,7 +97,7 @@
     }
 
     function keys() {
-      return array('MODULE_PAYMENT_COD_STATUS');
+      return array('MODULE_PAYMENT_COD_STATUS', 'MODULE_PAYMENT_COD_ZONE');
     }
   }
 ?>
