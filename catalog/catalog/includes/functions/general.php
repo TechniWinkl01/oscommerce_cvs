5ff0b6b49157dcff9511795bffd6c36d5b43464f
dgw_ <dgw_>
2002-11-19 19:04:41 +0000

    The line: 	mt_rand(0, 0) fails on some setups.
    It really doesnt make any sense to get a random number between 0 and 0 so I've added an extra check that skips mt_rand() when $min and
    $max are equal.
    
    This is an attempt to fix bug #800

--- catalog/catalog/includes/functions/general.php
+++ catalog/catalog/includes/functions/general.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: general.php,v 1.198 2002/11/19 00:28:24 dgw_ Exp $
+  $Id: general.php,v 1.199 2002/11/19 19:04:41 dgw_ Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -1133,6 +1133,8 @@
     }
     if (!isset($min) && !isset($max)) {
       return mt_rand();
+    } elseif ($min == $max) {
+      return $min;
     } else {
       return mt_rand($min, $max);
     }
