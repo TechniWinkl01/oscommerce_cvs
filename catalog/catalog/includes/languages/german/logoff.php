2ce19ea33a25d8e6441cdf2c9a75f7ed83558286
hpdl <hpdl>
2003-01-29 20:35:34 +0000

    update the german logoff text
    thanks to Norbert Bernhardt for providing them

--- catalog/catalog/includes/languages/german/logoff.php
+++ catalog/catalog/includes/languages/german/logoff.php
@@ -1,16 +1,16 @@
 <?php
 /*
-  $Id $
+  $Id: logoff.php,v 1.2 2003/01/29 20:35:34 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2002 osCommerce
+  Copyright (c) 2003 osCommerce
 
   Released under the GNU General Public License
 */
 
-define('HEADING_TITLE', 'Log Off');
-define('NAVBAR_TITLE', 'Log Off');
-define('TEXT_MAIN', 'You have been logged off your account. It is now safe to leave the computer.<br><br>Your shopping cart has been saved, the items inside it will be restored whenever you log back into your account.');
+define('HEADING_TITLE', 'Abmeldung');
+define('NAVBAR_TITLE', 'Abmeldung');
+define('TEXT_MAIN', 'Sie wurden soeben sicher abgemeldet, so da&szlig; niemand mehr Ihre Daten einsehen kann.<br><br>Ihr Warenkorb wurde zwischengespeichert und steht Ihnen nach der n&auml;chsten Anmeldung wieder zur Verf&uuml;gung.');
 ?>
