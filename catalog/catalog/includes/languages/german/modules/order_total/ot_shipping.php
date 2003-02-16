c5031e5a8aff2b4d93e09b4269c1c0f51c9d46bf
harley_vb <harley_vb>
2003-02-16 01:18:31 +0000

    German translation corrected/reviewed

--- catalog/catalog/includes/languages/german/modules/order_total/ot_shipping.php
+++ catalog/catalog/includes/languages/german/modules/order_total/ot_shipping.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: ot_shipping.php,v 1.3 2003/02/05 22:34:47 hpdl Exp $
+  $Id: ot_shipping.php,v 1.4 2003/02/16 01:18:31 harley_vb Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -13,6 +13,6 @@
   define('MODULE_ORDER_TOTAL_SHIPPING_TITLE', 'Versandkosten');
   define('MODULE_ORDER_TOTAL_SHIPPING_DESCRIPTION', 'Versandkosten einer Bestellung');
 
-  define('FREE_SHIPPING_TITLE', 'Free Shipping');
-  define('FREE_SHIPPING_DESCRIPTION', 'Free shipping for orders over %s');
+  define('FREE_SHIPPING_TITLE', 'Versandkostenfrei');
+  define('FREE_SHIPPING_DESCRIPTION', 'Versandkostenfrei bei einem Bestellwert &uuml;ber %s');
 ?>
