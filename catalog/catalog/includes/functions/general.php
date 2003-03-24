dc6ceb39648a6ef647a12407aa6f11f061799526
hpdl <hpdl>
2003-03-24 20:34:08 +0000

    fix a rounding issue
    currencies with no decimal places are updated by 1.0 when the value
    includes cents equal to or greater than 50
    
    an extra step is added to increment prices by 0.1 when only one decimal
    place is used; str_repeat() would return an error when the repeated string
    is to be done 0 times ($precision-1)

--- catalog/catalog/includes/functions/general.php
+++ catalog/catalog/includes/functions/general.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: general.php,v 1.215 2003/03/17 22:15:50 hpdl Exp $
+  $Id: general.php,v 1.216 2003/03/24 20:34:08 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -265,13 +265,19 @@
   }
 
 ////
-// Wrapper function for round() for php3 compatibility
+// Wrapper function for round()
   function tep_round($number, $precision) {
     if (strpos($number, '.') && (strlen(substr($number, strpos($number, '.')+1)) > $precision)) {
       $number = substr($number, 0, strpos($number, '.') + 1 + $precision + 1);
 
       if (substr($number, -1) >= 5) {
-        $number = substr($number, 0, -1) + ('0.' . str_repeat(0, $precision-1) . '1');
+        if ($precision > 1) {
+          $number = substr($number, 0, -1) + ('0.' . str_repeat(0, $precision-1) . '1');
+        } elseif ($precision == 1) {
+          $number = substr($number, 0, -1) + 0.1;
+        } else {
+          $number = substr($number, 0, -1) + 1;
+        }
       } else {
         $number = substr($number, 0, -1);
       }
