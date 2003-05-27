4471a97d894dbc3a3fe968f5c01da821ece18457
hpdl <hpdl>
2003-05-27 17:13:10 +0000

    use the updated splitPageResults class

--- catalog/catalog/account_history.php
+++ catalog/catalog/account_history.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: account_history.php,v 1.59 2003/05/19 20:17:49 hpdl Exp $
+  $Id: account_history.php,v 1.60 2003/05/27 17:13:10 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -61,8 +61,8 @@
 <?php
   if (($orders_total = tep_count_customer_orders()) > 0) {
     $history_query_raw = "select o.orders_id, o.date_purchased, o.delivery_name, o.billing_name, ot.text as order_total, s.orders_status_name from " . TABLE_ORDERS . " o, " . TABLE_ORDERS_TOTAL . " ot, " . TABLE_ORDERS_STATUS . " s where o.customers_id = '" . (int)$customer_id . "' and o.orders_id = ot.orders_id and ot.class = 'ot_total' and o.orders_status = s.orders_status_id and s.language_id = '" . (int)$languages_id . "' order by orders_id DESC";
-    $history_split = new splitPageResults($HTTP_GET_VARS['page'], MAX_DISPLAY_ORDER_HISTORY, $history_query_raw, $history_numrows);
-    $history_query = tep_db_query($history_query_raw);
+    $history_split = new splitPageResults($history_query_raw, $HTTP_GET_VARS['page'], MAX_DISPLAY_ORDER_HISTORY);
+    $history_query = tep_db_query($history_split->sql_query);
 
     while ($history = tep_db_fetch_array($history_query)) {
       $products_query = tep_db_query("select count(*) as count from " . TABLE_ORDERS_PRODUCTS . " where orders_id = '" . $history['orders_id'] . "'");
@@ -108,8 +108,8 @@
   if ($orders_total > 0) {
 ?>
           <tr>
-            <td class="smallText" valign="top"><?php echo $history_split->display_count($history_numrows, MAX_DISPLAY_ORDER_HISTORY, $HTTP_GET_VARS['page'], TEXT_DISPLAY_NUMBER_OF_ORDERS); ?></td>
-            <td class="smallText" align="right"><?php echo TEXT_RESULT_PAGE; echo $history_split->display_links($history_numrows, MAX_DISPLAY_ORDER_HISTORY, MAX_DISPLAY_PAGE_LINKS, $HTTP_GET_VARS['page'], tep_get_all_get_params(array('page', 'info', 'x', 'y'))); ?></td>
+            <td class="smallText" valign="top"><?php echo $history_split->display_count(TEXT_DISPLAY_NUMBER_OF_ORDERS); ?></td>
+            <td class="smallText" align="right"><?php echo TEXT_RESULT_PAGE . ' ' . $history_split->display_links(MAX_DISPLAY_PAGE_LINKS, tep_get_all_get_params(array('page', 'info', 'x', 'y'))); ?></td>
           </tr>
 <?php
   }
