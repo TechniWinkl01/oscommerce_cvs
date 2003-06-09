6af3fba5a164eac9c8534e59ff2f7104ddf25a07
hpdl <hpdl>
2003-06-09 21:25:32 +0000

    standards updates

--- catalog/catalog/includes/functions/whos_online.php
+++ catalog/catalog/includes/functions/whos_online.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: whos_online.php,v 1.8 2003/02/21 16:38:36 dgw_ Exp $
+  $Id: whos_online.php,v 1.9 2003/06/09 21:25:32 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -16,7 +16,7 @@
     if (tep_session_is_registered('customer_id')) {
       $wo_customer_id = $customer_id;
 
-      $customer_query = tep_db_query("select customers_firstname, customers_lastname from " . TABLE_CUSTOMERS . " where customers_id = '" . $customer_id . "'");
+      $customer_query = tep_db_query("select customers_firstname, customers_lastname from " . TABLE_CUSTOMERS . " where customers_id = '" . (int)$customer_id . "'");
       $customer = tep_db_fetch_array($customer_query);
 
       $wo_full_name = addslashes($customer['customers_firstname'] . ' ' . $customer['customers_lastname']);
@@ -35,13 +35,13 @@
 // remove entries that have expired
     tep_db_query("delete from " . TABLE_WHOS_ONLINE . " where time_last_click < '" . $xx_mins_ago . "'");
 
-    $stored_customer_query = tep_db_query("select count(*) as count from " . TABLE_WHOS_ONLINE . " where session_id = '" . $wo_session_id . "'");
+    $stored_customer_query = tep_db_query("select count(*) as count from " . TABLE_WHOS_ONLINE . " where session_id = '" . (int)$wo_session_id . "'");
     $stored_customer = tep_db_fetch_array($stored_customer_query);
 
     if ($stored_customer['count'] > 0) {
-      tep_db_query("update " . TABLE_WHOS_ONLINE . " set customer_id = '" . $wo_customer_id . "', full_name = '" . $wo_full_name . "', ip_address = '" . $wo_ip_address . "', time_last_click = '" . $current_time . "', last_page_url = '" . $wo_last_page_url . "' where session_id = '" . $wo_session_id . "'");
+      tep_db_query("update " . TABLE_WHOS_ONLINE . " set customer_id = '" . (int)$wo_customer_id . "', full_name = '" . $wo_full_name . "', ip_address = '" . $wo_ip_address . "', time_last_click = '" . $current_time . "', last_page_url = '" . $wo_last_page_url . "' where session_id = '" . $wo_session_id . "'");
     } else {
-      tep_db_query("insert into " . TABLE_WHOS_ONLINE . " (customer_id, full_name, session_id, ip_address, time_entry, time_last_click, last_page_url) values ('" . $wo_customer_id . "', '" . $wo_full_name . "', '" . $wo_session_id . "', '" . $wo_ip_address . "', '" . $current_time . "', '" . $current_time . "', '" . $wo_last_page_url . "')");
+      tep_db_query("insert into " . TABLE_WHOS_ONLINE . " (customer_id, full_name, session_id, ip_address, time_entry, time_last_click, last_page_url) values ('" . (int)$wo_customer_id . "', '" . $wo_full_name . "', '" . $wo_session_id . "', '" . $wo_ip_address . "', '" . $current_time . "', '" . $current_time . "', '" . $wo_last_page_url . "')");
     }
   }
 ?>
