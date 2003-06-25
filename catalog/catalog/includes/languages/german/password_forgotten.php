b6364c063aca9747534efa674bb757d753108ac3
hpdl <hpdl>
2003-06-25 21:17:02 +0000

    fix bug 1196 - translation update

--- catalog/catalog/includes/languages/german/password_forgotten.php
+++ catalog/catalog/includes/languages/german/password_forgotten.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: password_forgotten.php,v 1.10 2003/06/09 22:46:46 hpdl Exp $
+  $Id: password_forgotten.php,v 1.11 2003/06/25 21:17:02 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -15,7 +15,7 @@ define('NAVBAR_TITLE_2', 'Passwort vergessen');
 
 define('HEADING_TITLE', 'Wie war noch mal mein Passwort?');
 
-define('TEXT_MAIN', 'If you\'ve forgotten your password, enter your e-mail address below and we\'ll send you an e-mail message containing your new password.');
+define('TEXT_MAIN', 'Sollten Sie Ihr Passwort nicht mehr wissen, geben Sie bitte unten Ihre eMail-Adresse ein um umgehend ein neues Passwort per eMail zu erhalten.');
 
 define('TEXT_NO_EMAIL_ADDRESS_FOUND', 'Fehler: Die eingegebene eMail-Adresse ist nicht registriert. Bitte versuchen Sie es noch einmal.');
 
