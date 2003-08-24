c0aeb0931c9c5302dc9e6844ba892db0f0300e4d
project3000 <project3000>
2003-08-24 08:21:55 +0000

    Fix a couple of typo's. This closes bug 1353. Thanks for the report.

--- catalog/catalog/includes/languages/english/shopping_cart.php
+++ catalog/catalog/includes/languages/english/shopping_cart.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: shopping_cart.php,v 1.13 2002/04/05 20:24:02 project3000 Exp $
+  $Id: shopping_cart.php,v 1.14 2003/08/24 08:21:55 project3000 Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -21,6 +21,6 @@ define('TEXT_CART_EMPTY', 'Your Shopping Cart is empty!');
 define('SUB_TITLE_SUB_TOTAL', 'Sub-Total:');
 define('SUB_TITLE_TOTAL', 'Total:');
 
-define('OUT_OF_STOCK_CANT_CHECKOUT', 'Products marked with ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ' dont exist in desired quantity in our stock.<br>Please alter the quantity of products marked with (' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . '), Thank you');
-define('OUT_OF_STOCK_CAN_CHECKOUT', 'Products marked with ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ' dont exist in desired quantity in our stock.<br>You can buy them anyway and check the quantity we have in stock for immediate deliver in the checkout process.');
+define('OUT_OF_STOCK_CANT_CHECKOUT', 'Products marked with ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ' don\'t exist in desired quantity in our stock.<br>Please alter the quantity of products marked with (' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . '), Thank you');
+define('OUT_OF_STOCK_CAN_CHECKOUT', 'Products marked with ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ' don\'t exist in desired quantity in our stock.<br>You can buy them anyway and check the quantity we have in stock for immediate delivery in the checkout process.');
 ?>
