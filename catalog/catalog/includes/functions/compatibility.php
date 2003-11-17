5f3a8459ad61a9b89cf6af92039e78355f52faca
hpdl <hpdl>
2003-11-17 17:39:00 +0000

    remove php 3 compatibility logic
    reference the super global variables ($_GET, $_POST, ..) on PHP 4.0.x
    servers (the $_SESSION variable is not referenced as it cannot be
    referenced to (the session will not work))

--- catalog/catalog/includes/functions/compatibility.php
+++ catalog/catalog/includes/functions/compatibility.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: compatibility.php,v 1.19 2003/04/09 16:12:54 project3000 Exp $
+  $Id: compatibility.php,v 1.20 2003/11/17 17:39:00 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -8,16 +8,18 @@
   Copyright (c) 2003 osCommerce
 
   Released under the GNU General Public License
-
-  Modified by Marco Canini, <m.canini@libero.it>
-  - Fixed a bug with arrays in $HTTP_xxx_VARS
 */
 
-////
+  if (PHP_VERSION < 4.1) {
+    if (isset($HTTP_SERVER_VARS)) $_SERVER =& $HTTP_SERVER_VARS;
+    if (isset($HTTP_GET_VARS)) $_GET =& $HTTP_GET_VARS;
+    if (isset($HTTP_POST_VARS)) $_POST =& $HTTP_POST_VARS;
+    if (isset($HTTP_COOKIE_VARS)) $_COOKIE =& $HTTP_COOKIE_VARS;
+    if (isset($HTTP_POST_FILES)) $_FILES =& $HTTP_POST_FILES;
+    if (isset($HTTP_ENV_VARS)) $_ENV =& $HTTP_ENV_VARS;
+  }
+
 // Recursively handle magic_quotes_gpc turned off.
-// This is due to the possibility of have an array in
-// $HTTP_xxx_VARS
-// Ie, products attributes
   function do_magic_quotes_gpc(&$ar) {
     if (!is_array($ar)) return false;
 
@@ -30,46 +32,10 @@
     }
   }
 
-// $HTTP_xxx_VARS are always set on php4
-  if (!is_array($HTTP_GET_VARS)) $HTTP_GET_VARS = array();
-  if (!is_array($HTTP_POST_VARS)) $HTTP_POST_VARS = array();
-  if (!is_array($HTTP_COOKIE_VARS)) $HTTP_COOKIE_VARS = array();
-
 // handle magic_quotes_gpc turned off.
   if (!get_magic_quotes_gpc()) {
-    do_magic_quotes_gpc($HTTP_GET_VARS);
-    do_magic_quotes_gpc($HTTP_POST_VARS);
-    do_magic_quotes_gpc($HTTP_COOKIE_VARS);
-  }
-
-  if (!function_exists('array_splice')) {
-    function array_splice(&$array, $maximum) {
-      if (sizeof($array) >= $maximum) {
-        for ($i=0; $i<$maximum; $i++) {
-          $new_array[$i] = $array[$i];
-        }
-        $array = $new_array;
-      }
-    }
-  }
-
-  if (!function_exists('in_array')) {
-    function in_array($lookup_value, $lookup_array) {
-      reset($lookup_array);
-      while (list($key, $value) = each($lookup_array)) {
-        if ($value == $lookup_value) return true;
-      }
-
-      return false;
-    }
-  }
-
-  if (!function_exists('array_reverse')) {
-    function array_reverse($array) {
-      for ($i=0, $n=sizeof($array); $i<$n; $i++) $array_reversed[$i] = $array[($n-$i-1)];
-
-      return $array_reversed;
-    }
+    do_magic_quotes_gpc($_GET);
+    do_magic_quotes_gpc($_POST);
   }
 
   if (!function_exists('constant')) {
@@ -77,82 +43,6 @@
       eval("\$temp=$constant;");
 
       return $temp;
-    }
-  }
-
-  if (!function_exists('is_null')) {
-    function is_null($value) {
-      if (is_array($value)) {
-        if (sizeof($value) > 0) {
-          return false;
-        } else {
-          return true;
-        }
-      } else {
-        if (($value != '') && ($value != 'NULL') && (strlen(trim($value)) > 0)) {
-          return false;
-        } else {
-          return true;
-        }
-      }
-    }
-  }
-
-  if (!function_exists('array_merge')) {
-    function array_merge($array1, $array2, $array3 = '') {
-      if (empty($array3) && !is_array($array3)) $array3 = array();
-      while (list($key, $val) = each($array1)) $array_merged[$key] = $val;
-      while (list($key, $val) = each($array2)) $array_merged[$key] = $val;
-      if (sizeof($array3) > 0) while (list($key, $val) = each($array3)) $array_merged[$key] = $val;
-
-      return (array) $array_merged;
-    }
-  }
-
-  if (!function_exists('is_numeric')) {
-    function is_numeric($param) {
-      return ereg('^[0-9]{1,50}.?[0-9]{0,50}$', $param);
-    }
-  }
-
-  if (!function_exists('array_slice')) {
-    function array_slice($array, $offset, $length = 0) {
-      if ($offset < 0 ) {
-        $offset = sizeof($array) + $offset;
-      }
-      $length = ((!$length) ? sizeof($array) : (($length < 0) ? sizeof($array) - $length : $length + $offset));
-      for ($i = $offset; $i<$length; $i++) {
-        $tmp[] = $array[$i];
-      }
-
-      return $tmp;
-    }
-  }
-
-  if (!function_exists('array_map')) {
-    function array_map($callback, $array) {
-      if (is_array($array)) {
-        $_new_array = array();
-        reset($array);
-        while (list($key, $value) = each($array)) {
-          $_new_array[$key] = array_map($callback, $array[$key]);
-        }
-        return $_new_array;
-      } else {
-        return $callback($array);
-      }
-    }
-  }
-
-  if (!function_exists('str_repeat')) {
-    function str_repeat($string, $number) {
-      $repeat = '';
-
-      for ($i=0; $i<$number; $i++) {
-        $repeat .= $string;
-      }
-
-      return $repeat;
     }
   }
 
@@ -167,6 +57,20 @@
         }
       }
       return false;
+    }
+  }
+
+  if (!function_exists('array_unique')) {
+    function array_unique($array) {
+      $tmp_array = array();
+
+      for ($i=0, $n=sizeof($array); $i<$n; $i++) {
+        if (!in_array($array[$i], $tmp_array)) {
+          $tmp_array[] = $array[$i];
+        }
+      }
+
+      return $tmp_array;
     }
   }
 ?>
