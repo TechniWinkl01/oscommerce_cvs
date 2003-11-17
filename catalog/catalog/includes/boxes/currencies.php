b63b180f3e8f7920561a7dd2d3fe898818187c22
hpdl <hpdl>
2003-11-17 19:47:34 +0000

    use the new session class use the super global variables

--- catalog/catalog/includes/boxes/currencies.php
+++ catalog/catalog/includes/boxes/currencies.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: currencies.php,v 1.16 2003/02/12 20:27:31 hpdl Exp $
+  $Id: currencies.php,v 1.17 2003/11/17 19:47:34 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -28,9 +28,9 @@
     }
 
     $hidden_get_variables = '';
-    reset($HTTP_GET_VARS);
-    while (list($key, $value) = each($HTTP_GET_VARS)) {
-      if ( ($key != 'currency') && ($key != tep_session_name()) && ($key != 'x') && ($key != 'y') ) {
+    reset($_GET);
+    while (list($key, $value) = each($_GET)) {
+      if ( ($key != 'currency') && ($key != $osC_Session->name) && ($key != 'x') && ($key != 'y') ) {
         $hidden_get_variables .= tep_draw_hidden_field($key, $value);
       }
     }
@@ -38,7 +38,7 @@
     $info_box_contents = array();
     $info_box_contents[] = array('form' => tep_draw_form('currencies', tep_href_link(basename($PHP_SELF), '', $request_type, false), 'get'),
                                  'align' => 'center',
-                                 'text' => tep_draw_pull_down_menu('currency', $currencies_array, $currency, 'onChange="this.form.submit();" style="width: 100%"') . $hidden_get_variables . tep_hide_session_id());
+                                 'text' => tep_draw_pull_down_menu('currency', $currencies_array, $osC_Session->value('currency'), 'onChange="this.form.submit();" style="width: 100%"') . $hidden_get_variables . tep_hide_session_id());
 
     new infoBox($info_box_contents);
 ?>
