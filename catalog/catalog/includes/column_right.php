4c008d6a588fd0458ac763ca15287a856a2add52
hpdl <hpdl>
2003-11-17 17:35:04 +0000

    use the new session class use the super global variables

--- catalog/catalog/includes/column_right.php
+++ catalog/catalog/includes/column_right.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: column_right.php,v 1.17 2003/06/09 22:06:41 hpdl Exp $
+  $Id: column_right.php,v 1.18 2003/11/17 17:34:05 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -12,13 +12,13 @@
 
   require(DIR_WS_BOXES . 'shopping_cart.php');
 
-  if (isset($HTTP_GET_VARS['products_id'])) include(DIR_WS_BOXES . 'manufacturer_info.php');
+  if (isset($_GET['products_id'])) include(DIR_WS_BOXES . 'manufacturer_info.php');
 
-  if (tep_session_is_registered('customer_id')) include(DIR_WS_BOXES . 'order_history.php');
+  if ($osC_Customer->isLoggedOn()) include(DIR_WS_BOXES . 'order_history.php');
 
-  if (isset($HTTP_GET_VARS['products_id'])) {
-    if (tep_session_is_registered('customer_id')) {
-      $check_query = tep_db_query("select count(*) as count from " . TABLE_CUSTOMERS_INFO . " where customers_info_id = '" . (int)$customer_id . "' and global_product_notifications = '1'");
+  if (isset($_GET['products_id'])) {
+    if ($osC_Customer->isLoggedOn()) {
+      $check_query = tep_db_query("select count(*) as count from " . TABLE_CUSTOMERS_INFO . " where customers_info_id = '" . (int)$osC_Customer->id . "' and global_product_notifications = '1'");
       $check = tep_db_fetch_array($check_query);
       if ($check['count'] > 0) {
         include(DIR_WS_BOXES . 'best_sellers.php');
@@ -32,7 +32,7 @@
     include(DIR_WS_BOXES . 'best_sellers.php');
   }
 
-  if (isset($HTTP_GET_VARS['products_id'])) {
+  if (isset($_GET['products_id'])) {
     if (basename($PHP_SELF) != FILENAME_TELL_A_FRIEND) include(DIR_WS_BOXES . 'tell_a_friend.php');
   } else {
     include(DIR_WS_BOXES . 'specials.php');
--- catalog/catalog/includes/header.php
+++ catalog/catalog/includes/header.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: header.php,v 1.42 2003/06/10 18:20:38 hpdl Exp $
+  $Id: header.php,v 1.43 2003/11/17 17:35:04 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -12,14 +12,14 @@
 
 // check if the 'install' directory exists, and warn of its existence
   if (WARN_INSTALL_EXISTENCE == 'true') {
-    if (file_exists(dirname($HTTP_SERVER_VARS['SCRIPT_FILENAME']) . '/install')) {
+    if (file_exists(dirname($_SERVER['SCRIPT_FILENAME']) . '/install')) {
       $messageStack->add('header', WARNING_INSTALL_DIRECTORY_EXISTS, 'warning');
     }
   }
 
 // check if the configure.php file is writeable
   if (WARN_CONFIG_WRITEABLE == 'true') {
-    if ( (file_exists(dirname($HTTP_SERVER_VARS['SCRIPT_FILENAME']) . '/includes/configure.php')) && (is_writeable(dirname($HTTP_SERVER_VARS['SCRIPT_FILENAME']) . '/includes/configure.php')) ) {
+    if ( (file_exists(dirname($_SERVER['SCRIPT_FILENAME']) . '/includes/configure.php')) && (is_writeable(dirname($_SERVER['SCRIPT_FILENAME']) . '/includes/configure.php')) ) {
       $messageStack->add('header', WARNING_CONFIG_FILE_WRITEABLE, 'warning');
     }
   }
@@ -27,9 +27,9 @@
 // check if the session folder is writeable
   if (WARN_SESSION_DIRECTORY_NOT_WRITEABLE == 'true') {
     if (STORE_SESSIONS == '') {
-      if (!is_dir(tep_session_save_path())) {
+      if (!is_dir($osC_Session->save_path)) {
         $messageStack->add('header', WARNING_SESSION_DIRECTORY_NON_EXISTENT, 'warning');
-      } elseif (!is_writeable(tep_session_save_path())) {
+      } elseif (!is_writeable($osC_Session->save_path)) {
         $messageStack->add('header', WARNING_SESSION_DIRECTORY_NOT_WRITEABLE, 'warning');
       }
     }
@@ -61,25 +61,25 @@
 <table border="0" width="100%" cellspacing="0" cellpadding="1">
   <tr class="headerNavigation">
     <td class="headerNavigation">&nbsp;&nbsp;<?php echo $breadcrumb->trail(' &raquo; '); ?></td>
-    <td align="right" class="headerNavigation"><?php if (tep_session_is_registered('customer_id')) { ?><a href="<?php echo tep_href_link(FILENAME_LOGOFF, '', 'SSL'); ?>" class="headerNavigation"><?php echo HEADER_TITLE_LOGOFF; ?></a> &nbsp;|&nbsp; <?php } ?><a href="<?php echo tep_href_link(FILENAME_ACCOUNT, '', 'SSL'); ?>" class="headerNavigation"><?php echo HEADER_TITLE_MY_ACCOUNT; ?></a> &nbsp;|&nbsp; <a href="<?php echo tep_href_link(FILENAME_SHOPPING_CART); ?>" class="headerNavigation"><?php echo HEADER_TITLE_CART_CONTENTS; ?></a> &nbsp;|&nbsp; <a href="<?php echo tep_href_link(FILENAME_CHECKOUT_SHIPPING, '', 'SSL'); ?>" class="headerNavigation"><?php echo HEADER_TITLE_CHECKOUT; ?></a> &nbsp;&nbsp;</td>
+    <td align="right" class="headerNavigation"><?php if ($osC_Customer->isLoggedOn()) { echo '<a href="' . tep_href_link(FILENAME_LOGOFF, '', 'SSL') . '" class="headerNavigation">' . HEADER_TITLE_LOGOFF . '</a> &nbsp;|&nbsp; '; } echo '<a href="' . tep_href_link(FILENAME_ACCOUNT, '', 'SSL') . '" class="headerNavigation">' . HEADER_TITLE_MY_ACCOUNT . '</a> &nbsp;|&nbsp; <a href="' . tep_href_link(FILENAME_SHOPPING_CART) . '" class="headerNavigation">' . HEADER_TITLE_CART_CONTENTS . '</a> &nbsp;|&nbsp; <a href="' . tep_href_link(FILENAME_CHECKOUT_SHIPPING, '', 'SSL') . '" class="headerNavigation">' . HEADER_TITLE_CHECKOUT . '</a>'; ?>&nbsp;&nbsp;</td>
   </tr>
 </table>
 <?php
-  if (isset($HTTP_GET_VARS['error_message']) && tep_not_null($HTTP_GET_VARS['error_message'])) {
+  if (isset($_GET['error_message']) && tep_not_null($_GET['error_message'])) {
 ?>
 <table border="0" width="100%" cellspacing="0" cellpadding="2">
   <tr class="headerError">
-    <td class="headerError"><?php echo htmlspecialchars(urldecode($HTTP_GET_VARS['error_message'])); ?></td>
+    <td class="headerError"><?php echo htmlspecialchars(urldecode($_GET['error_message'])); ?></td>
   </tr>
 </table>
 <?php
   }
 
-  if (isset($HTTP_GET_VARS['info_message']) && tep_not_null($HTTP_GET_VARS['info_message'])) {
+  if (isset($_GET['info_message']) && tep_not_null($_GET['info_message'])) {
 ?>
 <table border="0" width="100%" cellspacing="0" cellpadding="2">
   <tr class="headerInfo">
-    <td class="headerInfo"><?php echo htmlspecialchars($HTTP_GET_VARS['info_message']); ?></td>
+    <td class="headerInfo"><?php echo htmlspecialchars($_GET['info_message']); ?></td>
   </tr>
 </table>
 <?php
