b87e976ab21bd959039d0a4b680c51d6d26bec50
hpdl <hpdl>
2004-04-08 04:40:32 +0000

    use the database table prefix set in the configuration

--- admin/admin/includes/database_tables.php
+++ admin/admin/includes/database_tables.php
@@ -1,58 +1,59 @@
 <?php
 /*
-  $Id: database_tables.php,v 1.1 2003/06/20 00:18:30 hpdl Exp $
+  $Id: database_tables.php,v 1.2 2004/04/08 04:40:32 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2003 osCommerce
+  Copyright (c) 2004 osCommerce
 
   Released under the GNU General Public License
 */
 
 // define the database table names used in the project
-  define('TABLE_ADDRESS_BOOK', 'address_book');
-  define('TABLE_ADDRESS_FORMAT', 'address_format');
-  define('TABLE_BANNERS', 'banners');
-  define('TABLE_BANNERS_HISTORY', 'banners_history');
-  define('TABLE_CATEGORIES', 'categories');
-  define('TABLE_CATEGORIES_DESCRIPTION', 'categories_description');
-  define('TABLE_CONFIGURATION', 'configuration');
-  define('TABLE_CONFIGURATION_GROUP', 'configuration_group');
-  define('TABLE_COUNTRIES', 'countries');
-  define('TABLE_CURRENCIES', 'currencies');
-  define('TABLE_CUSTOMERS', 'customers');
-  define('TABLE_CUSTOMERS_BASKET', 'customers_basket');
-  define('TABLE_CUSTOMERS_BASKET_ATTRIBUTES', 'customers_basket_attributes');
-  define('TABLE_CUSTOMERS_INFO', 'customers_info');
-  define('TABLE_LANGUAGES', 'languages');
-  define('TABLE_MANUFACTURERS', 'manufacturers');
-  define('TABLE_MANUFACTURERS_INFO', 'manufacturers_info');
-  define('TABLE_NEWSLETTERS', 'newsletters');
-  define('TABLE_ORDERS', 'orders');
-  define('TABLE_ORDERS_PRODUCTS', 'orders_products');
-  define('TABLE_ORDERS_PRODUCTS_ATTRIBUTES', 'orders_products_attributes');
-  define('TABLE_ORDERS_PRODUCTS_DOWNLOAD', 'orders_products_download');
-  define('TABLE_ORDERS_STATUS', 'orders_status');
-  define('TABLE_ORDERS_STATUS_HISTORY', 'orders_status_history');
-  define('TABLE_ORDERS_TOTAL', 'orders_total');
-  define('TABLE_PRODUCTS', 'products');
-  define('TABLE_PRODUCTS_ATTRIBUTES', 'products_attributes');
-  define('TABLE_PRODUCTS_ATTRIBUTES_DOWNLOAD', 'products_attributes_download');
-  define('TABLE_PRODUCTS_DESCRIPTION', 'products_description');
-  define('TABLE_PRODUCTS_NOTIFICATIONS', 'products_notifications');
-  define('TABLE_PRODUCTS_OPTIONS', 'products_options');
-  define('TABLE_PRODUCTS_OPTIONS_VALUES', 'products_options_values');
-  define('TABLE_PRODUCTS_OPTIONS_VALUES_TO_PRODUCTS_OPTIONS', 'products_options_values_to_products_options');
-  define('TABLE_PRODUCTS_TO_CATEGORIES', 'products_to_categories');
-  define('TABLE_REVIEWS', 'reviews');
-  define('TABLE_REVIEWS_DESCRIPTION', 'reviews_description');
-  define('TABLE_SESSIONS', 'sessions');
-  define('TABLE_SPECIALS', 'specials');
-  define('TABLE_TAX_CLASS', 'tax_class');
-  define('TABLE_TAX_RATES', 'tax_rates');
-  define('TABLE_GEO_ZONES', 'geo_zones');
-  define('TABLE_ZONES_TO_GEO_ZONES', 'zones_to_geo_zones');
-  define('TABLE_WHOS_ONLINE', 'whos_online');
-  define('TABLE_ZONES', 'zones');
+  define('TABLE_ADDRESS_BOOK', DB_TABLE_PREFIX . 'address_book');
+  define('TABLE_ADDRESS_FORMAT', DB_TABLE_PREFIX . 'address_format');
+  define('TABLE_BANNERS', DB_TABLE_PREFIX . 'banners');
+  define('TABLE_BANNERS_HISTORY', DB_TABLE_PREFIX . 'banners_history');
+  define('TABLE_CATEGORIES', DB_TABLE_PREFIX . 'categories');
+  define('TABLE_CATEGORIES_DESCRIPTION', DB_TABLE_PREFIX . 'categories_description');
+  define('TABLE_CONFIGURATION', DB_TABLE_PREFIX . 'configuration');
+  define('TABLE_CONFIGURATION_GROUP', DB_TABLE_PREFIX . 'configuration_group');
+  define('TABLE_COUNTER', DB_TABLE_PREFIX . 'counter');
+  define('TABLE_COUNTER_HISTORY', DB_TABLE_PREFIX . 'counter_history');
+  define('TABLE_COUNTRIES', DB_TABLE_PREFIX . 'countries');
+  define('TABLE_CURRENCIES', DB_TABLE_PREFIX . 'currencies');
+  define('TABLE_CUSTOMERS', DB_TABLE_PREFIX . 'customers');
+  define('TABLE_CUSTOMERS_BASKET', DB_TABLE_PREFIX . 'customers_basket');
+  define('TABLE_CUSTOMERS_BASKET_ATTRIBUTES', DB_TABLE_PREFIX . 'customers_basket_attributes');
+  define('TABLE_CUSTOMERS_INFO', DB_TABLE_PREFIX . 'customers_info');
+  define('TABLE_LANGUAGES', DB_TABLE_PREFIX . 'languages');
+  define('TABLE_MANUFACTURERS', DB_TABLE_PREFIX . 'manufacturers');
+  define('TABLE_MANUFACTURERS_INFO', DB_TABLE_PREFIX . 'manufacturers_info');
+  define('TABLE_ORDERS', DB_TABLE_PREFIX . 'orders');
+  define('TABLE_ORDERS_PRODUCTS', DB_TABLE_PREFIX . 'orders_products');
+  define('TABLE_ORDERS_PRODUCTS_ATTRIBUTES', DB_TABLE_PREFIX . 'orders_products_attributes');
+  define('TABLE_ORDERS_PRODUCTS_DOWNLOAD', DB_TABLE_PREFIX . 'orders_products_download');
+  define('TABLE_ORDERS_STATUS', DB_TABLE_PREFIX . 'orders_status');
+  define('TABLE_ORDERS_STATUS_HISTORY', DB_TABLE_PREFIX . 'orders_status_history');
+  define('TABLE_ORDERS_TOTAL', DB_TABLE_PREFIX . 'orders_total');
+  define('TABLE_PRODUCTS', DB_TABLE_PREFIX . 'products');
+  define('TABLE_PRODUCTS_ATTRIBUTES', DB_TABLE_PREFIX . 'products_attributes');
+  define('TABLE_PRODUCTS_ATTRIBUTES_DOWNLOAD', DB_TABLE_PREFIX . 'products_attributes_download');
+  define('TABLE_PRODUCTS_DESCRIPTION', DB_TABLE_PREFIX . 'products_description');
+  define('TABLE_PRODUCTS_NOTIFICATIONS', DB_TABLE_PREFIX . 'products_notifications');
+  define('TABLE_PRODUCTS_OPTIONS', DB_TABLE_PREFIX . 'products_options');
+  define('TABLE_PRODUCTS_OPTIONS_VALUES', DB_TABLE_PREFIX . 'products_options_values');
+  define('TABLE_PRODUCTS_OPTIONS_VALUES_TO_PRODUCTS_OPTIONS', DB_TABLE_PREFIX . 'products_options_values_to_products_options');
+  define('TABLE_PRODUCTS_TO_CATEGORIES', DB_TABLE_PREFIX . 'products_to_categories');
+  define('TABLE_REVIEWS', DB_TABLE_PREFIX . 'reviews');
+  define('TABLE_REVIEWS_DESCRIPTION', DB_TABLE_PREFIX . 'reviews_description');
+  define('TABLE_SESSIONS', DB_TABLE_PREFIX . 'sessions');
+  define('TABLE_SPECIALS', DB_TABLE_PREFIX . 'specials');
+  define('TABLE_TAX_CLASS', DB_TABLE_PREFIX . 'tax_class');
+  define('TABLE_TAX_RATES', DB_TABLE_PREFIX . 'tax_rates');
+  define('TABLE_GEO_ZONES', DB_TABLE_PREFIX . 'geo_zones');
+  define('TABLE_ZONES_TO_GEO_ZONES', DB_TABLE_PREFIX . 'zones_to_geo_zones');
+  define('TABLE_WHOS_ONLINE', DB_TABLE_PREFIX . 'whos_online');
+  define('TABLE_ZONES', DB_TABLE_PREFIX . 'zones');
 ?>
