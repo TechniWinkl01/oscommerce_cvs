e4984f2a27e5abac90f38f45da597143d4819b70
hpdl <hpdl>
2004-07-22 22:00:23 +0000

    language updates

--- catalog/catalog/includes/languages/german/modules/payment/moneyorder.php
+++ catalog/catalog/includes/languages/german/modules/payment/moneyorder.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: moneyorder.php,v 1.9 2003/07/11 09:04:23 jan0815 Exp $
+  $Id: moneyorder.php,v 1.10 2004/07/22 22:00:23 hpdl Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -11,6 +11,6 @@
 */
 
   define('MODULE_PAYMENT_MONEYORDER_TEXT_TITLE', 'Scheck/Vorkasse');
-  define('MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION', 'Zahlbar an:&nbsp;' . MODULE_PAYMENT_MONEYORDER_PAYTO . '<br>Adressat:<br><br>' . nl2br(STORE_NAME_ADDRESS) . '<br><br>' . 'Ihre Bestellung wird nicht versandt, bis wir das Geld erhalten haben!');
-  define('MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER', "Zahlbar an: ". MODULE_PAYMENT_MONEYORDER_PAYTO . "\n\nAdressat:\n" . STORE_NAME_ADDRESS . "\n\n" . 'Ihre Bestellung wir nicht versandt, bis wird das Geld erhalten haben!');
+  define('MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION', 'Zahlbar an:&nbsp;%s<br>Adressat:<br><br>' . nl2br(STORE_NAME_ADDRESS) . '<br><br>' . 'Ihre Bestellung wird nicht versandt, bis wir das Geld erhalten haben!');
+  define('MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER', 'Zahlbar an: %s' . "\n\n" . 'Adressat:' . "\n" . STORE_NAME_ADDRESS . "\n\n" . 'Ihre Bestellung wir nicht versandt, bis wird das Geld erhalten haben!');
 ?>
