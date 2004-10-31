3b8d2a5d75e4f16d656a60e4ff37cd7c7e76dfb6
mevans <mevans>
2004-10-31 10:01:35 +0000

    Add new low stock report

---
+++ admin/admin/includes/languages/german/modules/statistics/low_stock.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id: low_stock.php,v 1.1 2004/10/31 10:01:34 mevans Exp $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2004 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('MODULE_STATISTICS_LOW_STOCK_TITLE', 'Low Stock');
+
+define('MODULE_STATISTICS_LOW_STOCK_TABLE_HEADING_PRODUCTS', 'Products');
+define('MODULE_STATISTICS_LOW_STOCK_TABLE_HEADING_LEVEL', 'Stock Level');
+?>
