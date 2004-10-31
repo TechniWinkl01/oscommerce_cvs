4568cfb9d7f2bccbebd339bcd7796431b26fe5c7
mevans <mevans>
2004-10-31 09:58:43 +0000

    Make search in description default for quick search

--- catalog/catalog/includes/boxes/search.php
+++ catalog/catalog/includes/boxes/search.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  $Id: search.php,v 1.23 2004/07/22 16:56:08 hpdl Exp $
+  $Id: search.php,v 1.24 2004/10/31 09:58:43 mevans Exp $
 
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
@@ -22,7 +22,7 @@
   $info_box_contents = array();
   $info_box_contents[] = array('form' => tep_draw_form('quick_find', tep_href_link(FILENAME_ADVANCED_SEARCH_RESULT, '', 'NONSSL', false), 'get'),
                                'align' => 'center',
-                               'text' => osc_draw_input_field('keywords', '', 'size="10" maxlength="30" style="width: ' . (BOX_WIDTH-30) . 'px"') . '&nbsp;' . tep_hide_session_id() . tep_image_submit('button_quick_find.gif', BOX_HEADING_SEARCH) . '<br>' . BOX_SEARCH_TEXT . '<br><a href="' . tep_href_link(FILENAME_ADVANCED_SEARCH) . '"><b>' . BOX_SEARCH_ADVANCED_SEARCH . '</b></a>');
+                               'text' => osc_draw_input_field('keywords', '', 'size="10" maxlength="30" style="width: ' . (BOX_WIDTH-30) . 'px"') . '&nbsp;' . tep_hide_session_id() . osc_draw_hidden_field('search_in_description' , '1') . tep_image_submit('button_quick_find.gif', BOX_HEADING_SEARCH) . '<br>' . BOX_SEARCH_TEXT . '<br><a href="' . tep_href_link(FILENAME_ADVANCED_SEARCH) . '"><b>' . BOX_SEARCH_ADVANCED_SEARCH . '</b></a>');
 
   new infoBox($info_box_contents);
 ?>
